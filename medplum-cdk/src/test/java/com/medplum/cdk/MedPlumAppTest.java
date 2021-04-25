aeee8dd558f2000b37fda11a2ea73b83f279b47a
Cody Ebberson <cody@ebberson.com>
2021-04-24 18:34:24 -0700

    Re-organized non-shared maven dependencies

--- medplum-cdk/src/test/java/com/medplum/cdk/MedPlumAppTest.java
+++ medplum-cdk/src/test/java/com/medplum/cdk/MedPlumAppTest.java
@@ -6,28 +6,23 @@ import java.io.IOException;
 
 import org.junit.jupiter.api.Test;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-
 import software.amazon.awscdk.core.App;
 import software.amazon.awscdk.core.Environment;
 import software.amazon.awscdk.core.StackProps;
 
 public class MedPlumAppTest {
-    private final static ObjectMapper JSON = new ObjectMapper().configure(SerializationFeature.INDENT_OUTPUT, true);
 
     @Test
     public void testStack() throws IOException {
-        final App app = new App();
-        final MedPlumStack stack = new MedPlumStack(app, "test", StackProps.builder()
+        final var app = new App();
+        final var stack = new MedPlumStack(app, "test", StackProps.builder()
                 .env(Environment.builder()
                         .region("us-east-1")
                         .account("647991932601")
                         .build())
                 .build());
 
-        final JsonNode actual = JSON.valueToTree(app.synth().getStackArtifact(stack.getArtifactId()).getTemplate());
-        assertNotNull(actual);
+        final var template = app.synth().getStackArtifact(stack.getArtifactId()).getTemplate();
+        assertNotNull(template);
     }
 }
