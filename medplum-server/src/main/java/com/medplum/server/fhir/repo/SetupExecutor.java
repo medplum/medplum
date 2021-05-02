f5fa6efcd7f3c5470146f5b45b2e08291dbd9b80
Cody Ebberson <cody@ebberson.com>
2021-05-01 17:16:02 -0700

    Added more entity types to setup script

--- medplum-server/src/main/java/com/medplum/server/fhir/repo/SetupExecutor.java
+++ medplum-server/src/main/java/com/medplum/server/fhir/repo/SetupExecutor.java
@@ -1,11 +1,21 @@
 package com.medplum.server.fhir.repo;
 
+import static java.util.Collections.*;
+
+import java.util.Arrays;
+
 import org.apache.commons.lang3.RandomStringUtils;
+import org.mindrot.jbcrypt.BCrypt;
 
 import com.medplum.fhir.StandardOperations;
 import com.medplum.fhir.types.Bundle;
+import com.medplum.fhir.types.Bundle.BundleEntry;
 import com.medplum.fhir.types.ClientApplication;
+import com.medplum.fhir.types.HumanName;
 import com.medplum.fhir.types.OperationOutcome;
+import com.medplum.fhir.types.Organization;
+import com.medplum.fhir.types.Practitioner;
+import com.medplum.fhir.types.User;
 import com.medplum.server.search.SearchRequest;
 import com.medplum.server.security.SecurityUser;
 
@@ -28,10 +38,37 @@ public class SetupExecutor {
             return StandardOperations.invalid("Already setup");
         }
 
-        // Create a client application
-        return repo.create(SecurityUser.SYSTEM_USER, ClientApplication.create()
+        final Organization organization = repo.create(SecurityUser.SYSTEM_USER, Organization.create()
+                .name("MedPlum")
+                .build())
+                .resource(Organization.class);
+
+        final Practitioner practitioner = repo.create(SecurityUser.SYSTEM_USER, Practitioner.create()
+                .name(singletonList(HumanName.create()
+                        .given(singletonList("Admin"))
+                        .family("User")
+                        .build()))
+                .build())
+                .resource(Practitioner.class);
+
+        final User user = repo.create(SecurityUser.SYSTEM_USER, User.create()
+                .email("admin@medplum.com")
+                .passwordHash(BCrypt.hashpw("admin", BCrypt.gensalt()))
+                .practitioner(practitioner.createReference())
+                .build())
+                .resource(User.class);
+
+        final ClientApplication clientApplication = repo.create(SecurityUser.SYSTEM_USER, ClientApplication.create()
                 .secret(RandomStringUtils.randomAlphanumeric(64))
                 .redirectUri("https://example.com/redirect")
+                .build())
+                .resource(ClientApplication.class);
+
+        return StandardOperations.ok(Bundle.create().entry(Arrays.asList(
+                BundleEntry.create().resource(organization).build(),
+                BundleEntry.create().resource(practitioner).build(),
+                BundleEntry.create().resource(user).build(),
+                BundleEntry.create().resource(clientApplication).build()))
                 .build());
     }
 }
