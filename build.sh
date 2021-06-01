48599a8427d23c976b92fd7f5590b8a27a5f6063
Cody Ebberson <cody@ebberson.com>
2021-06-01 15:14:17 -0700

    Added missing resource files, fixed build script

--- build.sh
+++ build.sh
@@ -8,7 +8,20 @@ set -e
 # Echo commands
 set -x
 
+# Diagnostics
 node --version
 npm --version
+
+# Install
 npm i
+
+# Build
+npm run build --workspace=packages/definitions
+npm run build --workspace=packages/core
+npm run build --workspace=packages/ui
+npm run build --workspace=packages/console
+npm run build --workspace=packages/graphiql
+npm run build --workspace=packages/server
+
+# Test
 npx jest
---
+++ packages/definitions/lib/fhir/r4/README.md
@@ -0,0 +1,5 @@
+# FHIR Definitions
+
+Version 4.0.1
+
+http://www.hl7.org/fhir/downloads.html
