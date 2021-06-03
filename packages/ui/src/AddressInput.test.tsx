18998dbf4ada1637da54456a35bbb876ab4123a8
Cody Ebberson <cody@ebberson.com>
2021-06-03 11:19:27 -0700

    UI code coverage

---
+++ packages/ui/src/AddressInput.test.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { AddressInput } from './AddressInput';
+
+test('AddressInput renders', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<AddressInput name="a" value={{ line: ['123 main st'], city: 'Happy' }} />, div);
+});
+
+test('AddressInput renders undefined value', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<AddressInput name="a" />, div);
+});
