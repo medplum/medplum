04a0e51546c8b8c813225f99f35a956c3176794b
Cody Ebberson <cody@ebberson.com>
2021-06-05 15:24:02 -0700

    Consistent tsconfig for Node 16 and es2020

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -5,20 +5,7 @@ import { initDatabase } from './database';
 
 async function main() {
   const configName = process.argv.length === 3 ? process.argv[2] : 'file:medplum.config.json';
-  console.log('configName', configName);
-
   const config = await loadConfig(configName);
-  console.log('config', config);
-
-  // const envName = process.argv.length === 3 ? process.argv[2] : 'localhost';
-  // console.log('envName', envName);
-
-  // const config = await loadConfig(envName);
-  // console.log('config', config);
-
-  // const secrets = await getSecrets('arn:aws:secretsmanager:us-east-1:647991932601:secret:MedplumStackBackEndDatabase-EGCWqzSdj8J9-MsYQit');
-  // console.log('secrets', secrets);
-
   await initDatabase(config.database);
 
   const app = await initApp(express());
