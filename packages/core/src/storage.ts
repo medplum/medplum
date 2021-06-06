c507a59a2fc40a990d0cf1c5f0bef05b61cab959
Cody Ebberson <cody@ebberson.com>
2021-06-06 09:01:05 -0700

    Make MedplumClient work in both browser and server

--- packages/core/src/storage.ts
+++ packages/core/src/storage.ts
@@ -1,7 +1,48 @@
 
-export class Storage {
+export interface Storage {
+  clear(): void;
+  getString(key: string): string | undefined;
+  setString(key: string, value: string | undefined): void;
+  getObject<T>(key: string): T | undefined;
+  setObject<T>(key: string, value: T): void;
+}
 
-  clear() {
+export class MemoryStorage implements Storage {
+  private data: Record<string, string>;
+
+  constructor() {
+    this.data = {};
+  }
+
+  clear(): void {
+    this.data = {};
+  }
+
+  getString(key: string): string | undefined {
+    return this.data[key];
+  }
+
+  setString(key: string, value: string | undefined): void {
+    if (value) {
+      this.data[key] = value;
+    } else {
+      delete this.data[key];
+    }
+  }
+
+  getObject<T>(key: string): T | undefined {
+    const str = this.getString(key);
+    return str ? JSON.parse(str) as T : undefined;
+  }
+
+  setObject<T>(key: string, value: T) {
+    this.setString(key, value ? JSON.stringify(value) : undefined);
+  }
+}
+
+export class LocalStorage implements Storage {
+
+  clear(): void {
     localStorage.clear();
   }
 
@@ -9,7 +50,7 @@ export class Storage {
     return localStorage.getItem(key) || undefined;
   }
 
-  setString(key: string, value: string | undefined) {
+  setString(key: string, value: string | undefined): void {
     if (value) {
       localStorage.setItem(key, value);
     } else {
