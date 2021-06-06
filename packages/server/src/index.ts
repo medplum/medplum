3eae4bf4b7d240b6df71c732a77271f996b6082a
Cody Ebberson <cody@ebberson.com>
2021-06-06 09:49:17 -0700

    Moved seeder to separate project

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -5,7 +5,11 @@ import { initDatabase } from './database';
 
 async function main() {
   const configName = process.argv.length === 3 ? process.argv[2] : 'file:medplum.config.json';
+  console.log('configName', configName);
+
   const config = await loadConfig(configName);
+  console.log('config', config);
+
   await initDatabase(config.database);
 
   const app = await initApp(express());
--- packages/server/src/setup.test.ts
+++
@@ -1,9 +0,0 @@
-import { closeDatabase, initDatabase } from './database';
-import { setup } from './setup';
-
-test('Setup completes', async (done) => {
-  await initDatabase({ client: 'sqlite3' });
-  await setup();
-  await closeDatabase();
-  done();
-});
