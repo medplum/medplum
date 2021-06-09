1cf11f9d111e18e20a7c948c05d145c21efaf433
Cody Ebberson <cody@ebberson.com>
2021-06-08 17:45:26 -0700

    Version 0.0.22

--- scripts/deploy-console.sh
+++ scripts/deploy-console.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
 pushd packages/console
-aws s3 cp dist/ s3://medplum-console/ --profile medplum --region us-east-1 --recursive
+aws s3 cp dist/ s3://medplum-console/ \
+  --profile medplum \
+  --region us-east-1 \
+  --recursive \
+  --cache-control "public, max-age=31536000" \
+  --exclude "*.html"
+aws s3 cp dist/ s3://medplum-console/ \
+  --profile medplum \
+  --region us-east-1 \
+  --recursive \
+  --cache-control "no-cache" \
+  --exclude "*" \
+  --include "*.html"
 popd
--- scripts/deploy-storybook.sh
+++ scripts/deploy-storybook.sh
@@ -2,5 +2,19 @@
 
 pushd packages/ui
 npm run storybook
-aws s3 cp storybook-static/ s3://medplum-storybook/ --profile medplum --region us-east-1 --recursive --acl public-read
+aws s3 cp storybook-static/ s3://medplum-storybook/ \
+  --profile medplum \
+  --region us-east-1 \
+  --recursive \
+  --acl public-read \
+  --cache-control "public, max-age=31536000" \
+  --exclude "*.html"
+aws s3 cp storybook-static/ s3://medplum-storybook/ \
+  --profile medplum \
+  --region us-east-1 \
+  --recursive \
+  --acl public-read \
+  --cache-control "no-cache" \
+  --exclude "*" \
+  --include "*.html"
 popd
