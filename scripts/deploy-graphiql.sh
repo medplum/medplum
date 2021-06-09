10134aed2cf296c636dd57cda6a3ec8321cc8f6c
Cody Ebberson <cody@ebberson.com>
2021-06-08 21:51:12 -0700

    Generate GraphQL schema

--- scripts/deploy-graphiql.sh
+++ scripts/deploy-graphiql.sh
@@ -5,12 +5,14 @@ aws s3 cp dist/ s3://medplum-graphiql/ \
   --profile medplum \
   --region us-east-1 \
   --recursive \
+  --acl public-read \
   --cache-control "public, max-age=31536000" \
   --exclude "*.html"
 aws s3 cp dist/ s3://medplum-graphiql/ \
   --profile medplum \
   --region us-east-1 \
   --recursive \
+  --acl public-read \
   --cache-control "no-cache" \
   --exclude "*" \
   --include "*.html"
