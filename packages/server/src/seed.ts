6023750ef40d342bcd031081468ade40afe1fae3
Cody Ebberson <cody@ebberson.com>
2021-08-18 11:07:02 -0700

    Import code system and value set values

--- packages/server/src/seed.ts
+++ packages/server/src/seed.ts
@@ -1,6 +1,7 @@
-import { Bundle, BundleEntry, ClientApplication, createReference, isOk, OperationOutcomeError, Project, Resource, SearchParameter, StructureDefinition, User } from '@medplum/core';
+import { Bundle, BundleEntry, ClientApplication, CodeSystem, createReference, isOk, OperationOutcomeError, Project, Resource, SearchParameter, StructureDefinition, User, ValueSet } from '@medplum/core';
 import { readJson } from '@medplum/definitions';
 import { randomUUID } from 'crypto';
+import { Pool } from 'pg';
 import { registerNew } from './auth/register';
 import { MEDPLUM_CLIENT_APPLICATION_ID, MEDPLUM_PROJECT_ID, PUBLIC_PROJECT_ID } from './constants';
 import { getClient } from './database';
@@ -101,6 +102,58 @@ async function createClientApplication(): Promise<void> {
  */
 async function createValueSetElements(): Promise<void> {
   const client = getClient();
+  const bundle = readJson('fhir/r4/valuesets.json') as Bundle<CodeSystem | ValueSet>;
+
+  for (const entry of bundle.entry as BundleEntry<CodeSystem | ValueSet>[]) {
+    const resource = entry.resource;
+    if (resource?.resourceType === 'CodeSystem') {
+      await importCodeSystem(client, resource);
+    } else if (resource?.resourceType === 'ValueSet') {
+      await importValueSet(client, resource);
+    }
+  }
+
+  await importSnomed(client);
+}
+
+/**
+ * Imports all value set elements from a CodeSystem.
+ * See: https://www.hl7.org/fhir/codesystem.html
+ * @param client The database client.
+ * @param codeSystem The FHIR CodeSystem resource.
+ */
+async function importCodeSystem(client: Pool, codeSystem: CodeSystem): Promise<void> {
+  if (codeSystem.valueSet && codeSystem.concept) {
+    for (const concept of codeSystem.concept) {
+      if (concept.code && concept.display) {
+        await insertValueSetElement(client, codeSystem.valueSet, concept.code, concept.display);
+      }
+    }
+  }
+}
+
+/**
+ * Imports all value set elements from a ValueSet.
+ * See: https://www.hl7.org/fhir/valueset.html
+ * @param client The database client.
+ * @param valueSet The FHIR ValueSet resource.
+ */
+async function importValueSet(client: Pool, valueSet: ValueSet): Promise<void> {
+  const includes = valueSet.compose?.include;
+  if (includes) {
+    for (const include of includes) {
+      if (include.system && include.concept) {
+        for (const concept of include.concept) {
+          if (concept.code && concept.display) {
+            await insertValueSetElement(client, include.system, concept.code, concept.display);
+          }
+        }
+      }
+    }
+  }
+}
+
+async function importSnomed(client: Pool): Promise<void> {
   const system = 'https://snomed.info/sct';
 
   const values = [
@@ -111,13 +164,17 @@ async function createValueSetElements(): Promise<void> {
   ];
 
   for (const value of values) {
-    await new InsertQuery('ValueSetElement', {
-      id: randomUUID(),
-      system,
-      code: value.id,
-      display: value.name
-    }).execute(client);
+    await insertValueSetElement(client, system, value.id, value.name);
   }
+}
+
+async function insertValueSetElement(client: Pool, system: string, code: string, display: string): Promise<void> {
+  new InsertQuery('ValueSetElement', {
+    id: randomUUID(),
+    system,
+    code,
+    display
+  }).execute(client);
 }
 
 /**
