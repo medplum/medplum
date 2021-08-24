d5dd116f1bc9458e64e5851f6a5d3be70bfce818
Cody Ebberson <cody@ebberson.com>
2021-08-24 13:53:04 -0700

    Use CDK for front end

--- packages/infra/src/index.ts
+++ packages/infra/src/index.ts
@@ -1,14 +1,17 @@
 import * as cdk from '@aws-cdk/core';
 import { BackEnd } from './backend';
 import { ACCOUNT_NUMBER, REGION } from './constants';
+import { FrontEnd } from './frontend';
 
 class MedplumStack extends cdk.Stack {
   backEnd: BackEnd;
+  frontEnd: FrontEnd;
 
   constructor(scope: cdk.App, id: string, props: cdk.StackProps) {
     super(scope, id, props);
 
     this.backEnd = new BackEnd(this, 'BackEnd');
+    this.frontEnd = new FrontEnd(this, 'FrontEnd');
   }
 }
 
@@ -24,6 +27,7 @@ export function main() {
 
   console.log('Stack', stack.stackId);
   console.log('BackEnd', stack.backEnd.id);
+  console.log('FrontEnd', stack.frontEnd.id);
 
   return app.synth();
 }
--- scripts/deploy-app.sh
+++ scripts/deploy-app.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 pushd packages/app
-aws s3 cp dist/ s3://medplum-app/ \
+aws s3 cp dist/ s3://app.medplum.com/ \
   --profile medplum \
   --region us-east-1 \
   --recursive \
   --cache-control "public, max-age=31536000" \
   --exclude "*.html"
-aws s3 cp dist/ s3://medplum-app/ \
+aws s3 cp dist/ s3://app.medplum.com/ \
   --profile medplum \
   --region us-east-1 \
   --recursive \
