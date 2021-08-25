a67be59f2a291337b85ad03fa9b150dd57f4376d
Cody Ebberson <cody@ebberson.com>
2021-08-24 21:45:18 -0700

    Added reinstall.sh script for borked node_modules

---
+++ scripts/reinstall.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+rm -rf node_modules
+rm -rf packages/app/node_modules
+rm -rf packages/core/node_modules
+rm -rf packages/definitions/node_modules
+rm -rf packages/generator/node_modules
+rm -rf packages/graphiql/node_modules
+rm -rf packages/infra/node_modules
+rm -rf packages/server/node_modules
+rm -rf packages/ui/node_modules
+npm ci
