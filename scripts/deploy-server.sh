8c6a4d83736cb56df178cc4c6c7e860fff5b2a7d
Cody Ebberson <cody@ebberson.com>
2021-09-07 09:35:47 -0700

    Remove aws profile from deploy-app script

--- scripts/deploy-server.sh
+++ scripts/deploy-server.sh
@@ -26,9 +26,6 @@ set -e
 # Echo commands
 set -x
 
-# Login to AWS ECR
-#aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $DOCKER_SERVER
-
 # Build the Docker image
 docker build . -t $DOCKER_REPOSITORY:latest -t $DOCKER_REPOSITORY:$GITHUB_SHA
 
