420b7454a25492fa2038f1e012fd0a9dff1b497c
Cody Ebberson <cody@ebberson.com>
2021-09-07 08:29:44 -0700

    Updated ci/cd config

--- scripts/deploy-server.sh
+++ scripts/deploy-server.sh
@@ -1,19 +1,45 @@
 #!/usr/bin/env bash
 
+if [[ -z "${DOCKER_SERVER}" ]]; then
+  echo "DOCKER_SERVER is missing"
+  exit 1
+fi
+
+if [[ -z "${DOCKER_REPOSITORY}" ]]; then
+  echo "DOCKER_REPOSITORY is missing"
+  exit 1
+fi
+
+if [[ -z "${ECS_CLUSTER}" ]]; then
+  echo "ECS_CLUSTER is missing"
+  exit 1
+fi
+
+if [[ -z "${ECS_SERVICE}" ]]; then
+  echo "ECS_SERVICE is missing"
+  exit 1
+fi
+
+# Fail on error
+set -e
+
+# Echo commands
+set -x
+
 # Login to AWS ECR
-aws ecr get-login-password --profile medplum --region us-east-1 | docker login --username AWS --password-stdin 647991932601.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --profile medplum --region us-east-1 | docker login --username AWS --password-stdin $DOCKER_SERVER
 
 # Build the Docker image
-docker build . -t 647991932601.dkr.ecr.us-east-1.amazonaws.com/medplum-server:latest -t 647991932601.dkr.ecr.us-east-1.amazonaws.com/medplum-server:0.1.6
+docker build . -t $DOCKER_REPOSITORY:latest -t $DOCKER_REPOSITORY:0.1.6
 
 # Push the Docker image
-docker push 647991932601.dkr.ecr.us-east-1.amazonaws.com/medplum-server:0.1.6
-docker push 647991932601.dkr.ecr.us-east-1.amazonaws.com/medplum-server:latest
+docker push $DOCKER_REPOSITORY:0.1.6
+docker push $DOCKER_REPOSITORY:latest
 
 # Update the medplum fargate service
 aws ecs update-service \
   --profile medplum \
   --region us-east-1 \
-  --cluster MedplumStack-BackEndCluster6B6DC4A8-b7rLxsX1zQdL \
-  --service MedplumStack-BackEndFargateServiceD3B260C0-S35OIZcOSp1P \
+  --cluster $ECS_CLUSTER \
+  --service $ECS_SERVICE \
   --force-new-deployment
