85d6475b65273157ba0c24131993281335f6f823
Cody Ebberson <cody@ebberson.com>
2021-09-28 20:12:06 -0700

    Update client credentials compartment logic

--- packages/server/src/auth/register.test.ts
+++ packages/server/src/auth/register.test.ts
@@ -1,4 +1,4 @@
-import { ClientApplication } from '@medplum/core';
+import { ClientApplication, Patient } from '@medplum/core';
 import { randomUUID } from 'crypto';
 import express from 'express';
 import request from 'supertest';
@@ -143,6 +143,109 @@ describe('Register', () => {
       .send(client);
 
     expect(res2.status).toBe(201);
+  });
+
+  test('ClientApplication is restricted to project', async () => {
+    // User1 registers
+    // User1 creates a patient
+    // User2 registers
+    // User2 creates a client
+    // Client should not be able to see User1 patients
+    // Client should not see User1 patients in search
+    const res = await request(app)
+      .post('/auth/register')
+      .type('json')
+      .send({
+        firstName: 'User1',
+        lastName: 'User1',
+        projectName: 'User1 Project',
+        email: `user1-${randomUUID()}@example.com`,
+        password: 'password!@#'
+      });
+
+    expect(res.status).toBe(200);
+    expect(res.body.profile).not.toBeUndefined();
+
+    const patient: Patient = {
+      resourceType: 'Patient',
+      name: [{
+        given: ['Patient1'],
+        family: 'Patient1'
+      }]
+    };
+
+    const res2 = await request(app)
+      .post(`/fhir/R4/Patient`)
+      .set('Authorization', 'Bearer ' + res.body.accessToken)
+      .type('json')
+      .send(patient);
+
+    expect(res2.status).toBe(201);
+
+    const res3 = await request(app)
+      .post('/auth/register')
+      .type('json')
+      .send({
+        firstName: 'User2',
+        lastName: 'User2',
+        projectName: 'User2 Project',
+        email: `user2-${randomUUID()}@example.com`,
+        password: 'password!@#'
+      });
+
+    expect(res3.status).toBe(200);
+    expect(res3.body.profile).not.toBeUndefined();
+
+    // Try to access User1 patient using User2 directly
+    // This should fail
+    const res4 = await request(app)
+      .get(`/fhir/R4/Patient/${res2.body.id}`)
+      .set('Authorization', 'Bearer ' + res3.body.accessToken);
+    expect(res4.status).toBe(404);
+
+    // Create a client application
+    const client: ClientApplication = {
+      resourceType: 'ClientApplication',
+      name: 'User2 Client',
+      secret: generateSecret(48),
+      redirectUri: 'https://example.com'
+    };
+
+    const res5 = await request(app)
+      .post(`/fhir/R4/ClientApplication`)
+      .set('Authorization', 'Bearer ' + res3.body.accessToken)
+      .type('json')
+      .send(client);
+
+    expect(res5.status).toBe(201);
+
+    // Get a token using the client
+    const res6 = await request(app)
+      .post('/oauth2/token')
+      .type('form')
+      .send({
+        grant_type: 'client_credentials',
+        client_id: res5.body.id,
+        client_secret: res5.body.secret
+      });
+    expect(res6.status).toBe(200);
+    expect(res6.body.error).toBeUndefined();
+    expect(res6.body.access_token).not.toBeUndefined();
+
+    // Try to access User1 patient using User2 directly
+    // This should fail
+    const res7 = await request(app)
+      .get(`/fhir/R4/Patient/${res2.body.id}`)
+      .set('Authorization', 'Bearer ' + res6.body.access_token);
+    expect(res7.status).toBe(404);
+
+    // Try to search for patients using User2 client
+    // This should return empty set
+    const res8 = await request(app)
+      .get(`/fhir/R4/Patient`)
+      .set('Authorization', 'Bearer ' + res6.body.access_token);
+    expect(res8.status).toBe(200);
+    expect(res8.body.entry.length).toEqual(0);
   });
 
 });
