524a5d4d5504b341b158018d8b32898eb2fdc3fb
Cody Ebberson <cody@ebberson.com>
2021-09-30 08:25:48 -0700

    Fixed build

--- packages/ui/src/utils/format.test.ts
+++ packages/ui/src/utils/format.test.ts
@@ -4,6 +4,6 @@ test('formatDateTime', () => {
   jest.spyOn(Date.prototype, 'toLocaleString').mockReturnValue('foo');
 
   expect(formatDateTime(undefined)).toEqual('');
-  expect(formatDateTime(new Date('2020-01-01T12:00:00Z'))).toEqual('foo');
+  expect(formatDateTime('')).toEqual('');
   expect(formatDateTime('2020-01-01T12:00:00Z')).toEqual('foo');
 });
