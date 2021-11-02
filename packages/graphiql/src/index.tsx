a5ca439acc7c891aea86f4199262fbc5d1ebf7e3
Cody Ebberson <cody@ebberson.com>
2021-11-02 13:51:06 -0700

    Updated GraphiQL help text

--- packages/graphiql/src/index.tsx
+++ packages/graphiql/src/index.tsx
@@ -6,6 +6,32 @@ import { render } from 'react-dom';
 import 'regenerator-runtime/runtime.js';
 import 'graphiql/graphiql.css';
 
+const HELP_TEXT = `# Welcome to Medplum GraphiQL
+#
+# Medplum is a healthcare platform that helps you quickly develop
+# high-quality compliant applications. Medplum includes a FHIR server,
+# React component library, and developer console.
+#
+# Type queries into this side of the screen, and you will see intelligent
+# typeaheads aware of the current GraphQL type schema and live syntax and
+# validation errors highlighted within the text.
+#
+# GraphQL queries typically start with a "{" character. Lines that start
+# with a # are ignored.
+#
+# Here is an example query to search for patients named Alice:
+#
+#   {
+#     PatientList(name: "Alice") {
+#       name {
+#         given,
+#         family
+#       }
+#     }
+#   }
+#
+`;
+
 const medplum = new MedplumClient({
   baseUrl: process.env.MEDPLUM_BASE_URL,
   clientId: process.env.MEDPLUM_CLIENT_ID,
@@ -23,10 +49,11 @@ function App() {
   return profile ? (
     <GraphiQL
       fetcher={async graphQLParams => medplum.graphql(graphQLParams)}
+      defaultQuery={HELP_TEXT}
     />
   ) : (
     <Document width={450}>
-      <SignInForm />
+      <SignInForm onSuccess={() => undefined} />
     </Document>
   );
 }
