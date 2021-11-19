b47bcf393385ba688ae6ec1af1a89c83f0849ab8
Cody Ebberson <cody@ebberson.com>
2021-11-19 12:32:57 -0800

    Replace ts-jest with babel-jest

---
+++ packages/app/babel.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-react',
+    '@babel/preset-typescript'
+  ],
+  plugins: [
+    [
+      '@babel/plugin-proposal-class-properties'
+    ],
+    [
+      '@babel/plugin-transform-react-jsx'
+    ],
+    [
+      '@babel/plugin-transform-runtime'
+    ]
+  ]
+};
