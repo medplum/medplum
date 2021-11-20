605819df80bd99dcd83d003ebb0c7ef812f573b7
Cody Ebberson <cody@ebberson.com>
2021-11-19 17:12:46 -0800

    Removed dup healthcheck test

--- packages/server/src/app.test.ts
+++ packages/server/src/app.test.ts
@@ -26,9 +26,4 @@ describe('App', () => {
     expect(res.status).toBe(200);
   });
 
-  test('Get healthcheck', async () => {
-    const res = await request(app).get('/healthcheck');
-    expect(res.status).toBe(200);
-  });
-
 });
