2442b90421a39a6f7a2ede5e1a54e7683200a14b
Cody Ebberson <cody@ebberson.com>
2021-12-05 10:02:21 -0800

    Remove use of aws --profile medplum (#187)

--- scripts/deploy-storybook.sh
+++ scripts/deploy-storybook.sh
@@ -3,13 +3,11 @@
 pushd packages/ui
 npm run storybook
 aws s3 cp storybook-static/ s3://medplum-docs/storybook/ \
-  --profile medplum \
   --region us-east-1 \
   --recursive \
   --cache-control "public, max-age=31536000" \
   --exclude "*.html"
 aws s3 cp storybook-static/ s3://medplum-docs/storybook/ \
-  --profile medplum \
   --region us-east-1 \
   --recursive \
   --cache-control "no-cache" \
