fd4fa196ba0c31c16bbef5a9b05a9bbc4bdf121a
Cody Ebberson <cody@ebberson.com>
2021-12-05 09:12:29 -0800

    Remove use of aws --profile medplum

--- scripts/deploy-graphiql.sh
+++ scripts/deploy-graphiql.sh
@@ -2,13 +2,11 @@
 
 pushd packages/graphiql
 aws s3 cp dist/ s3://medplum-docs/graphiql/ \
-  --profile medplum \
   --region us-east-1 \
   --recursive \
   --cache-control "public, max-age=31536000" \
   --exclude "*.html"
 aws s3 cp dist/ s3://medplum-docs/graphiql/ \
-  --profile medplum \
   --region us-east-1 \
   --recursive \
   --cache-control "no-cache" \
