b86d925fc02c6778a5e558a7f5cb0d7810801cf4
Cody Ebberson <cody@ebberson.com>
2021-12-07 14:31:39 -0800

    More tests for oauth2/token endpoint (#197)

--- packages/server/src/oauth/token.test.ts
+++ packages/server/src/oauth/token.test.ts
@@ -1,10 +1,11 @@
-import { ClientApplication } from '@medplum/core';
+import { assertOk, ClientApplication } from '@medplum/core';
 import { randomUUID } from 'crypto';
 import express from 'express';
 import request from 'supertest';
 import { initApp } from '../app';
 import { loadTestConfig, MedplumServerConfig } from '../config';
 import { closeDatabase, initDatabase } from '../database';
+import { repo } from '../fhir';
 import { getDefaultClientApplication, seedDatabase } from '../seed';
 import { initKeys } from './keys';
 import { hashCode } from './token';
@@ -135,6 +136,30 @@ describe('OAuth2 Token', () => {
     expect(res.body.error_description).toBe('Invalid secret');
   });
 
+  test('Token for client empty secret', async () => {
+    // Create a client without an secret
+    const [outcome, badClient] = await repo.createResource<ClientApplication>({
+      resourceType: 'ClientApplication',
+      name: 'Bad Client',
+      description: 'Bad Client',
+      secret: '',
+      redirectUri: 'https://example.com'
+    });
+    assertOk(outcome);
+
+    const res = await request(app)
+      .post('/oauth2/token')
+      .type('form')
+      .send({
+        grant_type: 'client_credentials',
+        client_id: badClient.id,
+        client_secret: 'wrong-client-secret'
+      });
+    expect(res.status).toBe(400);
+    expect(res.body.error).toBe('invalid_request');
+    expect(res.body.error_description).toBe('Invalid client');
+  });
+
   test('Token for authorization_code with missing code', async () => {
     const res = await request(app)
       .post('/oauth2/token')
@@ -261,6 +286,97 @@ describe('OAuth2 Token', () => {
     expect(res2.body.refresh_token).toBeDefined();
   });
 
+  test('Authorization code token failure with client ID', async () => {
+    const params = new URLSearchParams({
+      response_type: 'code',
+      client_id: client.id as string,
+      redirect_uri: config.appBaseUrl,
+      scope: 'openid',
+      code_challenge: 'xyz',
+      code_challenge_method: 'plain'
+    });
+    const res = await request(app)
+      .post('/oauth2/authorize?' + params.toString())
+      .type('form')
+      .send({
+        email: 'admin@medplum.com',
+        password: 'admin',
+        nonce: 'asdf',
+        state: 'xyz'
+      });
+    expect(res.status).toBe(302);
+    expect(res.headers.location).toBeDefined();
+
+    const location = new URL(res.headers.location);
+    expect(location.searchParams.get('error')).toBeNull();
+
+    const res2 = await request(app)
+      .post('/oauth2/token')
+      .type('form')
+      .send({
+        grant_type: 'authorization_code',
+        client_id: 'wrong-client-id',
+        code: location.searchParams.get('code'),
+        code_verifier: 'xyz'
+      });
+    expect(res2.status).toBe(400);
+    expect(res2.body.error).toBe('invalid_request');
+    expect(res2.body.error_description).toBe('Invalid client');
+  });
+
+  test('Authorization code token failure already granted', async () => {
+    const params = new URLSearchParams({
+      response_type: 'code',
+      client_id: client.id as string,
+      redirect_uri: config.appBaseUrl,
+      scope: 'openid',
+      code_challenge: 'xyz',
+      code_challenge_method: 'plain'
+    });
+    const res = await request(app)
+      .post('/oauth2/authorize?' + params.toString())
+      .type('form')
+      .send({
+        email: 'admin@medplum.com',
+        password: 'admin',
+        nonce: 'asdf',
+        state: 'xyz'
+      });
+    expect(res.status).toBe(302);
+    expect(res.headers.location).toBeDefined();
+
+    const location = new URL(res.headers.location);
+    expect(location.searchParams.get('error')).toBeNull();
+
+    const res2 = await request(app)
+      .post('/oauth2/token')
+      .type('form')
+      .send({
+        grant_type: 'authorization_code',
+        code: location.searchParams.get('code'),
+        code_verifier: 'xyz'
+      });
+    expect(res2.status).toBe(200);
+    expect(res2.body.token_type).toBe('Bearer');
+    expect(res2.body.scope).toBe('openid');
+    expect(res2.body.expires_in).toBe(3600);
+    expect(res2.body.id_token).toBeDefined();
+    expect(res2.body.access_token).toBeDefined();
+    expect(res2.body.refresh_token).toBeDefined();
+
+    const res3 = await request(app)
+      .post('/oauth2/token')
+      .type('form')
+      .send({
+        grant_type: 'authorization_code',
+        code: location.searchParams.get('code'),
+        code_verifier: 'xyz'
+      });
+    expect(res3.status).toBe(400);
+    expect(res3.body.error).toBe('invalid_grant');
+    expect(res3.body.error_description).toBe('Token already granted');
+  });
+
   test('Refresh token without token', async () => {
     const res = await request(app)
       .post('/oauth2/token')
@@ -296,6 +412,7 @@ describe('OAuth2 Token', () => {
       code_challenge: 'xyz',
       code_challenge_method: 'plain'
     });
+
     const res = await request(app)
       .post('/oauth2/authorize?' + params.toString())
       .type('form')
@@ -327,6 +444,7 @@ describe('OAuth2 Token', () => {
     expect(res2.body.id_token).toBeDefined();
     expect(res2.body.access_token).toBeDefined();
     expect(res2.body.refresh_token).toBeDefined();
+
     const res3 = await request(app)
       .post('/oauth2/token')
       .type('form')
@@ -393,6 +511,7 @@ describe('OAuth2 Token', () => {
       code_challenge: codeHash,
       code_challenge_method: 'S256'
     });
+
     const res = await request(app)
       .post('/oauth2/authorize?' + params.toString())
       .type('form')
@@ -424,6 +543,7 @@ describe('OAuth2 Token', () => {
     expect(res2.body.id_token).toBeDefined();
     expect(res2.body.access_token).toBeDefined();
     expect(res2.body.refresh_token).toBeDefined();
+
     const res3 = await request(app)
       .post('/oauth2/token')
       .type('form')
@@ -438,6 +558,230 @@ describe('OAuth2 Token', () => {
     expect(res3.body.id_token).toBeDefined();
     expect(res3.body.access_token).toBeDefined();
     expect(res3.body.refresh_token).toBeDefined();
+  });
+
+  test('Refresh token Basic auth success', async () => {
+    const params = new URLSearchParams({
+      response_type: 'code',
+      client_id: client.id as string,
+      redirect_uri: config.appBaseUrl,
+      scope: 'openid',
+      code_challenge: 'xyz',
+      code_challenge_method: 'plain'
+    });
+
+    const res = await request(app)
+      .post('/oauth2/authorize?' + params.toString())
+      .type('form')
+      .send({
+        email: 'admin@medplum.com',
+        password: 'admin',
+        nonce: 'asdf',
+        state: 'xyz'
+      })
+      .expect(302);
+    expect(res.status).toBe(302);
+    expect(res.headers.location).toBeDefined();
+
+    const location = new URL(res.headers.location);
+    expect(location.searchParams.get('error')).toBeNull();
+
+    const res2 = await request(app)
+      .post('/oauth2/token')
+      .type('form')
+      .send({
+        grant_type: 'authorization_code',
+        code: location.searchParams.get('code'),
+        code_verifier: 'xyz'
+      });
+    expect(res2.status).toBe(200);
+    expect(res2.body.token_type).toBe('Bearer');
+    expect(res2.body.scope).toBe('openid');
+    expect(res2.body.expires_in).toBe(3600);
+    expect(res2.body.id_token).toBeDefined();
+    expect(res2.body.access_token).toBeDefined();
+    expect(res2.body.refresh_token).toBeDefined();
+
+    const res3 = await request(app)
+      .post('/oauth2/token')
+      .set('Authorization', 'Basic ' + Buffer.from(client.id + ':' + client.secret).toString('base64'))
+      .type('form')
+      .send({
+        grant_type: 'refresh_token',
+        refresh_token: res2.body.refresh_token
+      });
+    expect(res3.status).toBe(200);
+    expect(res3.body.token_type).toBe('Bearer');
+    expect(res3.body.scope).toBe('openid');
+    expect(res3.body.expires_in).toBe(3600);
+    expect(res3.body.id_token).toBeDefined();
+    expect(res3.body.access_token).toBeDefined();
+    expect(res3.body.refresh_token).toBeDefined();
+  });
+
+  test('Refresh token Basic auth failure wrong auth type', async () => {
+    const params = new URLSearchParams({
+      response_type: 'code',
+      client_id: client.id as string,
+      redirect_uri: config.appBaseUrl,
+      scope: 'openid',
+      code_challenge: 'xyz',
+      code_challenge_method: 'plain'
+    });
+
+    const res = await request(app)
+      .post('/oauth2/authorize?' + params.toString())
+      .type('form')
+      .send({
+        email: 'admin@medplum.com',
+        password: 'admin',
+        nonce: 'asdf',
+        state: 'xyz'
+      })
+      .expect(302);
+    expect(res.status).toBe(302);
+    expect(res.headers.location).toBeDefined();
+
+    const location = new URL(res.headers.location);
+    expect(location.searchParams.get('error')).toBeNull();
+
+    const res2 = await request(app)
+      .post('/oauth2/token')
+      .type('form')
+      .send({
+        grant_type: 'authorization_code',
+        code: location.searchParams.get('code'),
+        code_verifier: 'xyz'
+      });
+    expect(res2.status).toBe(200);
+    expect(res2.body.token_type).toBe('Bearer');
+    expect(res2.body.scope).toBe('openid');
+    expect(res2.body.expires_in).toBe(3600);
+    expect(res2.body.id_token).toBeDefined();
+    expect(res2.body.access_token).toBeDefined();
+    expect(res2.body.refresh_token).toBeDefined();
+
+    const res3 = await request(app)
+      .post('/oauth2/token')
+      .set('Authorization', 'Bearer ' + Buffer.from(client.id + ':' + client.secret).toString('base64'))
+      .type('form')
+      .send({
+        grant_type: 'refresh_token',
+        refresh_token: res2.body.refresh_token
+      });
+    expect(res3.status).toBe(400);
+    expect(res3.body.error).toBe('invalid_request');
+    expect(res3.body.error_description).toBe('Invalid authorization header');
+  });
+
+  test('Refresh token Basic auth failure wrong client ID', async () => {
+    const params = new URLSearchParams({
+      response_type: 'code',
+      client_id: client.id as string,
+      redirect_uri: config.appBaseUrl,
+      scope: 'openid',
+      code_challenge: 'xyz',
+      code_challenge_method: 'plain'
+    });
+
+    const res = await request(app)
+      .post('/oauth2/authorize?' + params.toString())
+      .type('form')
+      .send({
+        email: 'admin@medplum.com',
+        password: 'admin',
+        nonce: 'asdf',
+        state: 'xyz'
+      })
+      .expect(302);
+    expect(res.status).toBe(302);
+    expect(res.headers.location).toBeDefined();
+
+    const location = new URL(res.headers.location);
+    expect(location.searchParams.get('error')).toBeNull();
+
+    const res2 = await request(app)
+      .post('/oauth2/token')
+      .type('form')
+      .send({
+        grant_type: 'authorization_code',
+        code: location.searchParams.get('code'),
+        code_verifier: 'xyz'
+      });
+    expect(res2.status).toBe(200);
+    expect(res2.body.token_type).toBe('Bearer');
+    expect(res2.body.scope).toBe('openid');
+    expect(res2.body.expires_in).toBe(3600);
+    expect(res2.body.id_token).toBeDefined();
+    expect(res2.body.access_token).toBeDefined();
+    expect(res2.body.refresh_token).toBeDefined();
+
+    const res3 = await request(app)
+      .post('/oauth2/token')
+      .set('Authorization', 'Basic ' + Buffer.from('wrong-id' + ':' + client.secret).toString('base64'))
+      .type('form')
+      .send({
+        grant_type: 'refresh_token',
+        refresh_token: res2.body.refresh_token
+      });
+    expect(res3.status).toBe(400);
+    expect(res3.body.error).toBe('invalid_grant');
+    expect(res3.body.error_description).toBe('Incorrect client');
+  });
+
+  test('Refresh token Basic auth failure wrong secret', async () => {
+    const params = new URLSearchParams({
+      response_type: 'code',
+      client_id: client.id as string,
+      redirect_uri: config.appBaseUrl,
+      scope: 'openid',
+      code_challenge: 'xyz',
+      code_challenge_method: 'plain'
+    });
+
+    const res = await request(app)
+      .post('/oauth2/authorize?' + params.toString())
+      .type('form')
+      .send({
+        email: 'admin@medplum.com',
+        password: 'admin',
+        nonce: 'asdf',
+        state: 'xyz'
+      })
+      .expect(302);
+    expect(res.status).toBe(302);
+    expect(res.headers.location).toBeDefined();
+
+    const location = new URL(res.headers.location);
+    expect(location.searchParams.get('error')).toBeNull();
+
+    const res2 = await request(app)
+      .post('/oauth2/token')
+      .type('form')
+      .send({
+        grant_type: 'authorization_code',
+        code: location.searchParams.get('code'),
+        code_verifier: 'xyz'
+      });
+    expect(res2.status).toBe(200);
+    expect(res2.body.token_type).toBe('Bearer');
+    expect(res2.body.scope).toBe('openid');
+    expect(res2.body.expires_in).toBe(3600);
+    expect(res2.body.id_token).toBeDefined();
+    expect(res2.body.access_token).toBeDefined();
+    expect(res2.body.refresh_token).toBeDefined();
+
+    const res3 = await request(app)
+      .post('/oauth2/token')
+      .set('Authorization', 'Basic ' + Buffer.from(client.id + ':').toString('base64'))
+      .type('form')
+      .send({
+        grant_type: 'refresh_token',
+        refresh_token: res2.body.refresh_token
+      });
+    expect(res3.status).toBe(400);
+    expect(res3.body.error).toBe('invalid_grant');
+    expect(res3.body.error_description).toBe('Incorrect client secret');
   });
 
 });
