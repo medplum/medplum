4ec986106a3b34653839c05a061c33b1d8ddb029
Cody Ebberson <cody@ebberson.com>
2022-01-02 12:21:42 -0800

    Test

--- packages/app/src/admin/InvitePage.test.tsx
+++ packages/app/src/admin/InvitePage.test.tsx
@@ -57,4 +57,35 @@ describe('InvitePage', () => {
 
     expect(screen.getByTestId('success')).toBeInTheDocument();
   });
+
+  test('Submit with access policy', async () => {
+    setup('/admin/projects/123/invite');
+
+    await act(async () => {
+      await waitFor(() => screen.getByText('Invite'));
+    });
+
+    expect(screen.getByText('Invite')).toBeInTheDocument();
+
+    await act(async () => {
+      fireEvent.change(screen.getByTestId('firstName'), {
+        target: { value: 'George' },
+      });
+      fireEvent.change(screen.getByTestId('lastName'), {
+        target: { value: 'Washington' },
+      });
+      fireEvent.change(screen.getByTestId('email'), {
+        target: { value: 'george@example.com' },
+      });
+      fireEvent.change(screen.getByTestId('input-element'), {
+        target: { value: '{"reference":"AccessPolicy/123"' },
+      });
+    });
+
+    await act(async () => {
+      fireEvent.click(screen.getByText('Invite'));
+    });
+
+    expect(screen.getByTestId('success')).toBeInTheDocument();
+  });
 });
