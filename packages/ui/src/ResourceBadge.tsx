668cb2adee99749a1ae4ff273abf6c6b3c16bf6e
Cody Ebberson <cody@ebberson.com>
2022-01-07 15:04:00 -0800

    Cleaned blame table styles

--- packages/ui/src/ResourceBadge.tsx
+++ packages/ui/src/ResourceBadge.tsx
@@ -6,13 +6,14 @@ import './ResourceBadge.css';
 
 export interface ResourceBadgeProps {
   value?: Reference | Resource;
+  size?: 'xsmall' | 'small' | 'medium' | 'large';
   link?: boolean;
 }
 
 export function ResourceBadge(props: ResourceBadgeProps): JSX.Element {
   return (
     <div className="medplum-resource-badge">
-      <Avatar size="small" value={props.value} link={props.link} />
+      <Avatar size={props.size ?? 'small'} value={props.value} link={props.link} />
       <ResourceName value={props.value} link={props.link} />
     </div>
   );
--- packages/ui/src/ResourceBlame.tsx
+++ packages/ui/src/ResourceBlame.tsx
@@ -1,11 +1,11 @@
-import { Bundle } from '@medplum/fhirtypes';
+import { Bundle, Resource } from '@medplum/fhirtypes';
 import React, { useEffect, useState } from 'react';
-import { Avatar } from './Avatar';
-import { DateTimeDisplay } from './DateTimeDisplay';
+import { InputRow } from './InputRow';
+import { MedplumLink } from './MedplumLink';
 import { useMedplum } from './MedplumProvider';
-import { ResourceName } from './ResourceName';
-import { blame } from './utils/blame';
+import { ResourceBadge } from './ResourceBadge';
 import './ResourceBlame.css';
+import { blame } from './utils/blame';
 
 export interface ResourceBlameProps {
   history?: Bundle;
@@ -27,6 +27,7 @@ export function ResourceBlame(props: ResourceBlameProps): JSX.Element {
     return <div>Loading...</div>;
   }
 
+  const resource = value.entry?.[0]?.resource as Resource;
   const table = blame(value);
   return (
     <table className="medplum-blame">
@@ -35,12 +36,12 @@ export function ResourceBlame(props: ResourceBlameProps): JSX.Element {
           <tr key={'row-' + index} className={row.span > 0 ? 'start-row' : 'normal-row'}>
             {row.span > 0 && (
               <td className="details" rowSpan={row.span}>
-                <Avatar size="xsmall" value={row.meta.author} />
-                <ResourceName value={row.meta.author} link={true} />
-                <br />
-                <DateTimeDisplay value={row.meta.lastUpdated} />
-                <br />
-                <span>{row.meta.versionId}</span>
+                <InputRow justifyContent="space-between">
+                  <ResourceBadge value={row.meta.author} size="xsmall" link={true} />
+                  <MedplumLink to={getVersionUrl(resource, row.meta.versionId as string)}>
+                    {getTimeString(row.meta.lastUpdated as string)}
+                  </MedplumLink>
+                </InputRow>
               </td>
             )}
             <td className="line-number">{index + 1}</td>
@@ -52,4 +53,39 @@ export function ResourceBlame(props: ResourceBlameProps): JSX.Element {
       </tbody>
     </table>
   );
+}
+
+function getVersionUrl(resource: Resource, versionId: string): string {
+  return `/${resource.resourceType}/${resource.id}/_history/${versionId}`;
+}
+
+function getTimeString(lastUpdated: string): string {
+  const seconds = (Date.now() - Date.parse(lastUpdated)) / 1000;
+
+  const years = seconds / 31536000;
+  if (years > 1) {
+    return Math.floor(years) + ' years ago';
+  }
+
+  const months = seconds / 2592000;
+  if (months > 1) {
+    return Math.floor(months) + ' months ago';
+  }
+
+  const days = seconds / 86400;
+  if (days > 1) {
+    return Math.floor(days) + ' days ago';
+  }
+
+  const hours = seconds / 3600;
+  if (hours > 1) {
+    return Math.floor(hours) + ' hours ago';
+  }
+
+  const minutes = seconds / 60;
+  if (minutes > 1) {
+    return Math.floor(minutes) + ' minutes ago';
+  }
+
+  return Math.floor(seconds) + ' seconds ago';
 }
