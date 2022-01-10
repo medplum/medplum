b5cada6e6f85134e3c305d8fca0d3b5f0611abac
Cody Ebberson <cody@ebberson.com>
2022-01-10 08:27:56 -0800

    Fixed ResourceType autocomplete (#278)

--- packages/server/src/seeds/valuesets.ts
+++ packages/server/src/seeds/valuesets.ts
@@ -1,5 +1,13 @@
 import { readJson } from '@medplum/definitions';
-import { Bundle, BundleEntry, CodeSystem, CodeSystemConcept, ValueSet } from '@medplum/fhirtypes';
+import {
+  Bundle,
+  BundleEntry,
+  CodeSystem,
+  CodeSystemConcept,
+  ValueSet,
+  ValueSetComposeInclude,
+  ValueSetComposeIncludeConcept,
+} from '@medplum/fhirtypes';
 import { randomUUID } from 'crypto';
 import { Pool } from 'pg';
 import { getClient } from '../database';
@@ -14,93 +22,146 @@ export async function createValueSetElements(): Promise<void> {
 
   const bundle = readJson('fhir/r4/valuesets.json') as Bundle<CodeSystem | ValueSet>;
 
-  for (const entry of bundle.entry as BundleEntry<CodeSystem | ValueSet>[]) {
-    const resource = entry.resource;
+  new ValueSystemImporter(client, bundle).importValueSetElements();
+}
+
+class ValueSystemImporter {
+  constructor(private client: Pool, private bundle: Bundle<CodeSystem | ValueSet>) {}
+
+  async importValueSetElements(): Promise<void> {
+    for (const entry of this.bundle.entry as BundleEntry<CodeSystem | ValueSet>[]) {
+      const resource = entry.resource;
+      if (resource) {
+        await this.importResource(resource);
+      }
+    }
+
+    await this.importSnomed();
+  }
+
+  private async importResource(resource: CodeSystem | ValueSet): Promise<void> {
     if (resource?.resourceType === 'CodeSystem') {
-      await importCodeSystem(client, resource);
+      await this.importCodeSystem(resource);
     } else if (resource?.resourceType === 'ValueSet') {
-      await importValueSet(client, resource);
+      await this.importValueSet(resource);
     }
   }
 
-  await importSnomed(client);
-}
-
-/**
- * Imports all value set elements from a CodeSystem.
- * See: https://www.hl7.org/fhir/codesystem.html
- * @param client The database client.
- * @param codeSystem The FHIR CodeSystem resource.
- */
-async function importCodeSystem(client: Pool, codeSystem: CodeSystem): Promise<void> {
-  if (!codeSystem.valueSet || !codeSystem.concept) {
-    return;
-  }
-  for (const concept of codeSystem.concept) {
-    importCodeSystemConcept(client, codeSystem.valueSet, concept);
-  }
-}
-
-/**
- * Recursively imports CodeSystem concepts.
- * See: https://www.hl7.org/fhir/codesystem-definitions.html#CodeSystem.concept
- * @param client The database client.
- * @param system The ValueSet system.
- * @param concept The CodeSystem concept.
- */
-async function importCodeSystemConcept(client: Pool, system: string, concept: CodeSystemConcept): Promise<void> {
-  if (concept.code && concept.display) {
-    await insertValueSetElement(client, system, concept.code, concept.display);
-  }
-  if (concept.concept) {
-    for (const child of concept.concept) {
-      await importCodeSystemConcept(client, system, child);
+  /**
+   * Imports all value set elements from a CodeSystem.
+   * See: https://www.hl7.org/fhir/codesystem.html
+   * @param codeSystem The FHIR CodeSystem resource.
+   */
+  private async importCodeSystem(codeSystem: CodeSystem): Promise<void> {
+    if (codeSystem.valueSet) {
+      await this.importCodeSystemAs(codeSystem, codeSystem.valueSet);
     }
   }
-}
 
-/**
- * Imports all value set elements from a ValueSet.
- * See: https://www.hl7.org/fhir/valueset.html
- * @param client The database client.
- * @param valueSet The FHIR ValueSet resource.
- */
-async function importValueSet(client: Pool, valueSet: ValueSet): Promise<void> {
-  if (!valueSet.url || !valueSet.compose?.include) {
-    return;
-  }
-  for (const include of valueSet.compose.include) {
-    if (!include.system || !include.concept) {
-      continue;
-    }
-    for (const concept of include.concept) {
-      if (concept.code && concept.display) {
-        await insertValueSetElement(client, include.system, concept.code, concept.display);
+  /**
+   * Imports all value set elements from a CodeSystem.
+   * See: https://www.hl7.org/fhir/codesystem.html
+   * @param codeSystem The FHIR CodeSystem resource.
+   * @param system The ValueSet system.
+   */
+  private async importCodeSystemAs(codeSystem: CodeSystem, system: string): Promise<void> {
+    if (codeSystem.concept) {
+      for (const concept of codeSystem.concept) {
+        await this.importCodeSystemConcept(system, concept);
       }
     }
   }
-}
 
-async function importSnomed(client: Pool): Promise<void> {
-  const system = 'https://snomed.info/sct';
-
-  const values = [
-    { id: '316791000119102', name: 'Pain in left knee' },
-    { id: '316931000119104', name: 'Pain in right knee' },
-    { id: '287045000', name: 'Pain in left arm' },
-    { id: '287046004', name: 'Pain in right arm' },
-  ];
-
-  for (const value of values) {
-    await insertValueSetElement(client, system, value.id, value.name);
+  /**
+   * Recursively imports CodeSystem concepts.
+   * See: https://www.hl7.org/fhir/codesystem-definitions.html#CodeSystem.concept
+   * @param system The ValueSet system.
+   * @param concept The CodeSystem concept.
+   */
+  private async importCodeSystemConcept(system: string, concept: CodeSystemConcept): Promise<void> {
+    if (concept.code && concept.display) {
+      await this.insertValueSetElement(system, concept.code, concept.display);
+    }
+    if (concept.concept) {
+      for (const child of concept.concept) {
+        await this.importCodeSystemConcept(system, child);
+      }
+    }
   }
-}
 
-async function insertValueSetElement(client: Pool, system: string, code: string, display: string): Promise<void> {
-  new InsertQuery('ValueSetElement', {
-    id: randomUUID(),
-    system,
-    code,
-    display,
-  }).execute(client);
+  /**
+   * Imports all value set elements from a ValueSet.
+   * See: https://www.hl7.org/fhir/valueset.html
+   * @param valueSet The FHIR ValueSet resource.
+   */
+  private async importValueSet(valueSet: ValueSet): Promise<void> {
+    if (valueSet.url && valueSet.compose?.include) {
+      for (const include of valueSet.compose.include) {
+        await this.importValueSetInclude(valueSet.url, include);
+      }
+    }
+  }
+
+  /**
+   * Imports one set of ValueSet included elements.
+   * @param system The ValueSet system.
+   * @param include The included codes or system references.
+   */
+  private async importValueSetInclude(system: string, include: ValueSetComposeInclude): Promise<void> {
+    if (include.concept) {
+      await this.importValueSetConcepts(system, include.concept);
+    } else if (include.system) {
+      const includedResource = this.getByUrl(include.system);
+      if (includedResource) {
+        await this.importCodeSystemAs(includedResource, system);
+      }
+    }
+  }
+
+  /**
+   * Imports a collection of ValueSet concepts into the system.
+   * @param system The ValueSet system.
+   * @param concepts The included concepts.
+   */
+  private async importValueSetConcepts(system: string, concepts: ValueSetComposeIncludeConcept[]): Promise<void> {
+    for (const concept of concepts) {
+      if (concept.code && concept.display) {
+        await this.insertValueSetElement(system, concept.code, concept.display);
+      }
+    }
+  }
+
+  private getByUrl(url: string): CodeSystem | undefined {
+    for (const entry of this.bundle.entry as BundleEntry<CodeSystem | ValueSet>[]) {
+      const resource = entry.resource;
+      if (resource?.resourceType === 'CodeSystem' && resource?.url === url) {
+        return resource;
+      }
+    }
+    return undefined;
+  }
+
+  private async importSnomed(): Promise<void> {
+    const system = 'https://snomed.info/sct';
+
+    const values = [
+      { id: '316791000119102', name: 'Pain in left knee' },
+      { id: '316931000119104', name: 'Pain in right knee' },
+      { id: '287045000', name: 'Pain in left arm' },
+      { id: '287046004', name: 'Pain in right arm' },
+    ];
+
+    for (const value of values) {
+      await this.insertValueSetElement(system, value.id, value.name);
+    }
+  }
+
+  private async insertValueSetElement(system: string, code: string, display: string): Promise<void> {
+    new InsertQuery('ValueSetElement', {
+      id: randomUUID(),
+      system,
+      code,
+      display,
+    }).execute(this.client);
+  }
 }
