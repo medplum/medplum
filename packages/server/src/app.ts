aa787222619872acc2813f101a3dd4d6a2b7dcd5
Cody Ebberson <cody@ebberson.com>
2022-01-14 21:35:35 -0800

    Add security headers

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -25,15 +25,36 @@ const corsOptions: cors.CorsOptions = {
 };
 
 /**
- * Disables all caching.
+ * Sets standard headers for all requests.
  * @param req The request.
  * @param res The response.
  * @param next The next handler.
  */
-function cacheHandler(req: Request, res: Response, next: NextFunction): void {
+function standardHeaders(req: Request, res: Response, next: NextFunction): void {
+  // Disables all caching
   res.set('Cache-Control', 'no-store, no-cache, must-revalidate');
   res.set('Expires', 'Wed, 21 Oct 2015 07:28:00 GMT');
   res.set('Pragma', 'no-cache');
+
+  if (getConfig().baseUrl.startsWith('https://')) {
+    // Only connect to this site and subdomains via HTTPS for the next two years
+    // and also include in the preload list
+    res.set('Strict-Transport-Security', 'max-age=63072000; includeSubDomains; preload');
+  }
+
+  // Set Content Security Policy
+  // As an API server, block everything
+  // See: https://stackoverflow.com/a/45631261/2051724
+  res.set('Content-Security-Policy', "default-src 'none'");
+
+  // Prevent browsers from incorrectly detecting non-scripts as scripts
+  res.set('X-Content-Type-Options', 'nosniff');
+
+  // Disallow attempts to iframe site
+  res.set('X-Frame-Options', 'DENY');
+
+  // Block pages from loading when they detect reflected XSS attacks
+  res.set('X-XSS-Protection', '1; mode=block');
   next();
 }
 
@@ -69,7 +90,7 @@ export async function initApp(app: Express): Promise<Express> {
   const config = getConfig();
   app.set('trust proxy', true);
   app.set('x-powered-by', false);
-  app.use(cacheHandler);
+  app.use(standardHeaders);
   app.use(cors(corsOptions));
   app.use('/fhir/R4/Binary', [authenticateToken], binaryRouter);
   app.use(
