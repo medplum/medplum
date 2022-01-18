907be0d578c193c35b57ab93871756ceb0d8e929
Cody Ebberson <cody@ebberson.com>
2022-01-17 18:57:18 -0800

    Release version 0.4.0 (#313)

--- scripts/version.sh
+++ scripts/version.sh
@@ -13,12 +13,13 @@ if [[ -z "$NEW_VERSION" ]]; then
 fi
 
 set_version () {
-  sed -i "s_\"version\": \"$OLD_VERSION\"_\"version\": \"$NEW_VERSION\"_g" $1
-  sed -i "s_\"@medplum/definitions\": \"$OLD_VERSION\"_\"@medplum/definitions\": \"$NEW_VERSION\"_g" $1
-  sed -i "s_\"@medplum/core\": \"$OLD_VERSION\"_\"@medplum/core\": \"$NEW_VERSION\"_g" $1
-  sed -i "s_\"@medplum/fhirpath\": \"$OLD_VERSION\"_\"@medplum/fhirpath\": \"$NEW_VERSION\"_g" $1
-  sed -i "s_\"@medplum/fhirtypes\": \"$OLD_VERSION\"_\"@medplum/fhirtypes\": \"$NEW_VERSION\"_g" $1
-  sed -i "s_\"@medplum/ui\": \"$OLD_VERSION\"_\"@medplum/ui\": \"$NEW_VERSION\"_g" $1
+  sed -i "s_\"version\": \"$OLD_VERSION\"_\"version\": \"$NEW_VERSION\"_g" "$1"
+  sed -i "s_\"@medplum/definitions\": \"$OLD_VERSION\"_\"@medplum/definitions\": \"$NEW_VERSION\"_g" "$1"
+  sed -i "s_\"@medplum/core\": \"$OLD_VERSION\"_\"@medplum/core\": \"$NEW_VERSION\"_g" "$1"
+  sed -i "s_\"@medplum/fhirpath\": \"$OLD_VERSION\"_\"@medplum/fhirpath\": \"$NEW_VERSION\"_g" "$1"
+  sed -i "s_\"@medplum/fhirtypes\": \"$OLD_VERSION\"_\"@medplum/fhirtypes\": \"$NEW_VERSION\"_g" "$1"
+  sed -i "s_\"@medplum/mock\": \"$OLD_VERSION\"_\"@medplum/mock\": \"$NEW_VERSION\"_g" "$1"
+  sed -i "s_\"@medplum/ui\": \"$OLD_VERSION\"_\"@medplum/ui\": \"$NEW_VERSION\"_g" "$1"
 }
 
 # Update package.json files
@@ -31,6 +32,7 @@ set_version "packages/fhirtypes/package.json"
 set_version "packages/generator/package.json"
 set_version "packages/graphiql/package.json"
 set_version "packages/infra/package.json"
+set_version "packages/mock/package.json"
 set_version "packages/server/package.json"
 set_version "packages/ui/package.json"
 
