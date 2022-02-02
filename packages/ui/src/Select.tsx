0b3d0dd6d9ec18fed5618e98b2a4429d1bfb1dd0
Cody Ebberson <cody@ebberson.com>
2022-02-02 13:37:51 -0800

    Fixed sonar warnings

--- packages/ui/src/Select.tsx
+++ packages/ui/src/Select.tsx
@@ -1,7 +1,7 @@
 import { OperationOutcome } from '@medplum/fhirtypes';
 import React, { RefObject } from 'react';
-import './Select.css';
 import { getIssuesForExpression } from './utils/outcomes';
+import './Select.css';
 
 export interface SelectProps {
   name?: string;
@@ -9,8 +9,6 @@ export interface SelectProps {
   required?: boolean;
   autoFocus?: boolean;
   inputRef?: RefObject<HTMLSelectElement>;
-  // onChange?: (e: React.ChangeEvent) => void;
-  // ref?: RefObject<HTMLSelectElement>;
   onChange?: (newValue: string) => void;
   children: React.ReactNode;
   outcome?: OperationOutcome;
@@ -19,7 +17,6 @@ export interface SelectProps {
 }
 
 export function Select(props: SelectProps): JSX.Element {
-  // const className = props.size || '';
   const className = 'medplum-select';
   const issues = getIssuesForExpression(props.outcome, props.name);
   const invalid = issues && issues.length > 0;
@@ -32,16 +29,15 @@ export function Select(props: SelectProps): JSX.Element {
       required={props.required}
       autoFocus={props.autoFocus}
       ref={props.inputRef}
-      // onChange={props.onChange}
       style={props.style}
+      aria-invalid={invalid}
+      aria-describedby={invalid ? props.name + '-errors' : ''}
+      data-testid={props.testid}
       onChange={(e) => {
         if (props.onChange) {
           props.onChange(e.currentTarget.value);
         }
       }}
-      aria-invalid={invalid}
-      aria-describedby={invalid ? props.name + '-errors' : ''}
-      data-testid={props.testid}
     >
       {props.children}
     </select>
