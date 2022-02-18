dc4df3241d25d04a705997dc72f2b31eb02ce989
Cody Ebberson <cody@ebberson.com>
2022-02-18 09:22:41 -0800

    Fixed build

--- packages/app/src/CodeEditor.tsx
+++ packages/app/src/CodeEditor.tsx
@@ -1,9 +1,11 @@
 import React, { Suspense } from 'react';
 
 const AceEditor = React.lazy(async () => {
-  const result = await import('react-ace');
-  await import('ace-builds/webpack-resolver');
-  return result;
+  const ace = await import('react-ace');
+  await import('ace-builds/src-noconflict/ace');
+  await import('ace-builds/src-noconflict/mode-javascript');
+  await import('ace-builds/src-noconflict/theme-github');
+  return ace;
 });
 
 export interface CodeEditorProps {
