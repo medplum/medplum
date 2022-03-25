9698488115c04ed68b2538a210a569c90ad046d6
Cody Ebberson <cody@ebberson.com>
2022-03-25 11:47:16 -0700

    PDF bot proof-of-concept (#432)

---
+++ packages/server/src/util/pdf.ts
@@ -0,0 +1,72 @@
+import { assertOk } from '@medplum/core';
+import { Binary } from '@medplum/fhirtypes';
+import PdfPrinter from 'pdfmake';
+import { TDocumentDefinitions } from 'pdfmake/interfaces';
+import { PassThrough } from 'stream';
+import { getBinaryStorage, Repository } from '../fhir';
+
+export async function createPdf(
+  repo: Repository,
+  filename: string,
+  docDefinition: TDocumentDefinitions
+): Promise<Binary> {
+  if (!repo) {
+    return Promise.reject('Missing repository');
+  }
+
+  if (!filename) {
+    return Promise.reject('Missing filename');
+  }
+
+  if (!docDefinition) {
+    return Promise.reject('Missing document definition');
+  }
+
+  // Setup standard fonts
+  // See: https://pdfmake.github.io/docs/0.1/fonts/standard-14-fonts/
+  const fonts = {
+    Helvetica: {
+      normal: 'Helvetica',
+      bold: 'Helvetica-Bold',
+      italics: 'Helvetica-Oblique',
+      bolditalics: 'Helvetica-BoldOblique',
+    },
+  };
+
+  // Setup sane defaults
+  // See: https://pdfmake.github.io/docs/0.1/document-definition-object/styling/
+  docDefinition.pageSize = docDefinition.pageSize || 'LETTER';
+  docDefinition.pageMargins = docDefinition.pageMargins || [60, 60, 60, 60];
+  docDefinition.pageOrientation = docDefinition.pageOrientation || 'portrait';
+  docDefinition.defaultStyle = docDefinition.defaultStyle || {};
+  docDefinition.defaultStyle.font = docDefinition.defaultStyle.font || 'Helvetica';
+  docDefinition.defaultStyle.fontSize = docDefinition.defaultStyle.fontSize || 11;
+  docDefinition.defaultStyle.lineHeight = docDefinition.defaultStyle.lineHeight || 2.0;
+
+  const contentType = 'application/pdf';
+  const [outcome, binary] = await repo.createResource<Binary>({
+    resourceType: 'Binary',
+    contentType,
+  });
+  assertOk(outcome, binary);
+
+  // Setup the stream
+  const stream = new PassThrough();
+
+  // Start the stream consumer
+  // This will write to S3 or the file system as the bytes come in
+  const writePromise = getBinaryStorage().writeBinary(binary, filename, contentType, stream);
+
+  // Start the stream producer
+  // This will print the PDF to the stream buffer
+  const printer = new PdfPrinter(fonts);
+  const pdfDoc = printer.createPdfKitDocument(docDefinition, {});
+  pdfDoc.pipe(stream);
+  pdfDoc.end();
+
+  // Wait for the stream to finish
+  await writePromise;
+
+  // Return the binary resource
+  return binary;
+}
