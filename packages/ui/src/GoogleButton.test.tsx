25390d007e5f78277cfcc134e9914fc14adac397
Cody Ebberson <cody@ebberson.com>
2022-04-10 11:19:17 -0700

    Updated google auth button flow

---
+++ packages/ui/src/GoogleButton.test.tsx
@@ -0,0 +1,20 @@
+import { getGoogleClientId } from './GoogleButton';
+
+describe('GoogleButton', () => {
+  test('googleClientId', () => {
+    expect(getGoogleClientId('foo')).toBeDefined();
+
+    Object.defineProperty(window, 'location', {
+      value: {
+        protocol: 'https:',
+        host: 'app.medplum.com',
+      },
+    });
+    process.env.GOOGLE_AUTH_ORIGINS = 'https://app.medplum.com';
+    process.env.GOOGLE_CLIENT_ID = 'foo';
+    expect(getGoogleClientId(undefined)).toBeDefined();
+
+    window.location.host = 'evil.com';
+    expect(getGoogleClientId(undefined)).toBeUndefined();
+  });
+});
--- packages/ui/src/utils.test.ts
+++ packages/ui/src/utils.test.ts
@@ -1,4 +1,4 @@
-import { getGoogleClientId, initGoogleAuth } from './utils';
+import { createScriptTag } from './utils';
 
 describe('Utils', () => {
   beforeEach(() => {
@@ -6,36 +6,9 @@ describe('Utils', () => {
     document.getElementsByTagName('html')[0].innerHTML = '';
   });
 
-  test('googleClientId', () => {
-    expect(getGoogleClientId('foo')).toBeDefined();
-
-    Object.defineProperty(window, 'location', {
-      value: {
-        protocol: 'https:',
-        host: 'app.medplum.com',
-      },
-    });
-    process.env.GOOGLE_AUTH_ORIGINS = 'https://app.medplum.com';
-    process.env.GOOGLE_CLIENT_ID = 'foo';
-    expect(getGoogleClientId(undefined)).toBeDefined();
-
-    window.location.host = 'evil.com';
-    expect(getGoogleClientId(undefined)).toBeUndefined();
-  });
-
-  test('initGoogleAuth', () => {
+  test('createScriptTag', () => {
     expect(document.getElementsByTagName('script').length).toBe(0);
-
-    // Init Google Auth
-    // Should create a <script> tag for the Google Auth script.
-    initGoogleAuth();
-    expect(document.getElementsByTagName('script').length).toBe(1);
-
-    // Simulate loading the script
-    Object.defineProperty(global, 'google', { value: {} });
-
-    // Initializing again should not create more <script> tags
-    initGoogleAuth();
+    createScriptTag('test.js');
     expect(document.getElementsByTagName('script').length).toBe(1);
   });
 });
