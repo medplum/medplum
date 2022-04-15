5e6792c35321e891f24cd0174344395501f851cf
Cody Ebberson <cody@ebberson.com>
2022-04-15 14:15:14 -0700

    Support partial ranges in RangeDisplay

--- packages/ui/src/RangeDisplay.tsx
+++ packages/ui/src/RangeDisplay.tsx
@@ -12,6 +12,24 @@ export function RangeDisplay(props: RangeDisplayProps): JSX.Element | null {
     return null;
   }
 
+  if (value.low && !value.high) {
+    return (
+      <span>
+        &gt;=&nbsp;
+        <QuantityDisplay value={value.low} />
+      </span>
+    );
+  }
+
+  if (!value.low && value.high) {
+    return (
+      <span>
+        &lt;=&nbsp;
+        <QuantityDisplay value={value.high} />
+      </span>
+    );
+  }
+
   return (
     <span>
       <QuantityDisplay value={value.low} />
