05e438b3a067f550096716c0ea79c082979307df
Cody Ebberson <cody@ebberson.com>
2022-05-03 16:45:52 -0700

    Fixed storybook deploy content-type

--- scripts/deploy-storybook.sh
+++ scripts/deploy-storybook.sh
@@ -2,15 +2,82 @@
 
 pushd packages/react
 npm run storybook
-aws s3 cp storybook-static/ s3://medplum-docs/storybook/ \
+
+LOCAL_PATH="storybook-static/"
+S3_URI="s3://medplum-docs/storybook/"
+
+# HTML files
+aws s3 cp "$LOCAL_PATH" "$S3_URI" \
   --region us-east-1 \
   --recursive \
-  --cache-control "public, max-age=31536000" \
-  --exclude "*.html"
-aws s3 cp storybook-static/ s3://medplum-docs/storybook/ \
-  --region us-east-1 \
-  --recursive \
+  --content-type "text/html" \
   --cache-control "no-cache" \
   --exclude "*" \
   --include "*.html"
+
+# CSS files
+aws s3 cp "$LOCAL_PATH" "$S3_URI" \
+  --region us-east-1 \
+  --recursive \
+  --content-type "text/css" \
+  --cache-control "public, max-age=31536000" \
+  --exclude "*" \
+  --include "*.css"
+
+# JS files
+aws s3 cp "$LOCAL_PATH" "$S3_URI" \
+  --region us-east-1 \
+  --recursive \
+  --content-type "application/javascript" \
+  --cache-control "public, max-age=31536000" \
+  --exclude "*" \
+  --include "*.js"
+
+# JSON files
+aws s3 cp "$LOCAL_PATH" "$S3_URI" \
+  --region us-east-1 \
+  --recursive \
+  --content-type "application/json" \
+  --cache-control "public, max-age=31536000" \
+  --exclude "*" \
+  --include "*.json" \
+  --include "*.js.map"
+
+# PNG files
+aws s3 cp "$LOCAL_PATH" "$S3_URI" \
+  --region us-east-1 \
+  --recursive \
+  --content-type "image/png" \
+  --cache-control "public, max-age=31536000" \
+  --exclude "*" \
+  --include "*.png"
+
+
+# wav files
+aws s3 cp "$LOCAL_PATH" "$S3_URI" \
+  --region us-east-1 \
+  --recursive \
+  --content-type "audio/wav" \
+  --cache-control "public, max-age=31536000" \
+  --exclude "*" \
+  --include "*.wav"
+
+# ICO files
+aws s3 cp "$LOCAL_PATH" "$S3_URI" \
+  --region us-east-1 \
+  --recursive \
+  --content-type "image/vnd.microsoft" \
+  --cache-control "public, max-age=31536000" \
+  --exclude "*" \
+  --include "*.ico"
+
+# TXT files
+aws s3 cp "$LOCAL_PATH" "$S3_URI" \
+  --region us-east-1 \
+  --recursive \
+  --content-type "text/plain" \
+  --cache-control "public, max-age=31536000" \
+  --exclude "*" \
+  --include "*.txt"
+
 popd
