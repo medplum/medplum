76f0c2f36d1d772abe1e74edc280561d3a332089
Rahul Agarwal <rahul@medplum.com>
2022-05-10 22:28:02 -0700

    Updated handling of multiple results

--- packages/ui/src/FhirPathDisplay.tsx
+++ packages/ui/src/FhirPathDisplay.tsx
@@ -7,13 +7,19 @@ import { ResourcePropertyDisplay } from '.';
 export interface FhirPathDisplayProps {
   resource: Resource;
   path: string;
+  propertyType: PropertyType;
 }
 
-export function FhirPathDisplay(props: FhirPathDisplayProps): JSX.Element | null {
+export function FhirPathDisplay(props: FhirPathDisplayProps): JSX.Element {
   const value = evalFhirPath(props.path, props.resource);
 
-  if (value.length === 1) {
-    return <ResourcePropertyDisplay value={value?.[0] || ''} propertyType={PropertyType.string} />;
+  if (value.length > 1) {
+    console.log('Error!');
+    throw new Error(
+      `Component "path" for "FhirPathDisplay" must resolve to a single element. \
+       Received ${value.length} elements \
+       [${JSON.stringify(value, null, 2)}]`
+    );
   }
-  return null;
+  return <ResourcePropertyDisplay value={value[0] || ''} propertyType={props.propertyType} />;
 }
--- packages/ui/src/stories/FhirPathDisplay.stories.tsx
+++ packages/ui/src/stories/FhirPathDisplay.stories.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { FhirPathDisplay, FhirPathDisplayProps } from '../FhirPathDisplay';
+import { PropertyType } from '@medplum/core';
 import { HomerServiceRequest, HomerSimpson } from '@medplum/mock';
 import { ComponentStory, Meta } from '@storybook/react';
 
 export default {
   component: FhirPathDisplay,
-  title: 'Medplum/SmartResourcePropertyDisplay',
+  title: 'Medplum/FhirPathDisplay',
 } as Meta;
 
 const Story: ComponentStory<typeof FhirPathDisplay> = (args: FhirPathDisplayProps) => <FhirPathDisplay {...args} />;
@@ -20,10 +21,12 @@ export const ArrayElement = Story.bind({});
 ArrayElement.args = {
   path: 'orderDetail[0].text',
   resource: HomerServiceRequest,
+  propertyType: PropertyType.string,
 };
 
 export const CompositeElement = Story.bind({});
 CompositeElement.args = {
   path: "name[0].family&', '&name[0].given[0]",
   resource: HomerSimpson,
+  propertyType: PropertyType.string,
 };
