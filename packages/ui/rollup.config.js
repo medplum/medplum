bba1244a08ab9e0e5f2891198a72f630927727b6
Cody Ebberson <cody@ebberson.com>
2022-05-28 14:00:02 -0700

    Removed fhirpath dep from rollup.config.js

--- packages/ui/rollup.config.js
+++ packages/ui/rollup.config.js
@@ -13,7 +13,6 @@ const extensions = ['.ts', '.tsx'];
 
 const globals = {
   '@medplum/core': 'medplum.core',
-  '@medplum/fhirpath': 'medplum.fhirpath',
   '@medplum/mock': 'medplum.mock',
   react: 'React',
   'react-dom': 'ReactDOM',
