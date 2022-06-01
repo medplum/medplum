b5f40f3fcaa32c66e4343c4afc939ae8cd8d2a73
Cody Ebberson <cody@ebberson.com>
2022-05-31 20:53:00 -0700

    Added more named table layouts

--- packages/server/src/util/pdf.test.ts
+++ packages/server/src/util/pdf.test.ts
@@ -1,6 +1,6 @@
 import { mkdtempSync, rmSync } from 'fs';
 import { sep } from 'path';
-import { TDocumentDefinitions } from 'pdfmake/interfaces';
+import { Content, TDocumentDefinitions } from 'pdfmake/interfaces';
 import { loadTestConfig } from '../config';
 import { closeDatabase, initDatabase } from '../database';
 import { initBinaryStorage, Repository, systemRepo } from '../fhir';
@@ -42,6 +42,42 @@ describe('Binary', () => {
       content: ['Hello world'],
     };
     const binary = await createPdf(systemRepo, 'custom-font.pdf', custom);
+    expect(binary).toBeDefined();
+  });
+
+  test('Custom table layouts', async () => {
+    const layoutNames = [
+      'noBorders',
+      'headerLineOnly',
+      'lightHorizontalLines',
+      'medplumNoBorders',
+      'medplumMetadata',
+      'medplumObservations',
+    ];
+
+    const content: Content[] = layoutNames
+      .map((name) => [
+        {
+          text: name,
+        },
+        {
+          layout: name,
+          table: {
+            body: [
+              ['First', 'Second', 'Third', 'The last one'],
+              ['Value 1', 'Value 2', 'Value 3', 'Value 4'],
+              ['Value 1', 'Value 2', 'Value 3', 'Value 4'],
+            ],
+          },
+        },
+      ])
+      .flat();
+
+    const docDefinition: TDocumentDefinitions = {
+      content,
+    };
+
+    const binary = await createPdf(systemRepo, 'custom-table.pdf', docDefinition);
     expect(binary).toBeDefined();
   });
 });
