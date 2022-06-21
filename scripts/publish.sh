cddbddfc1efd36853f4e845393033cab294861a5
Cody Ebberson <cody@ebberson.com>
2022-06-21 16:37:24 -0700

    Release Version 0.9.14 (#677)

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 
-OTP=$1
-if [[ -z "$OTP" ]]; then
-  echo "Usage: publish.sh [2fa-token]"
-  exit 1
-fi
-
 PACKAGES=("cli" "core" "definitions" "fhirtypes" "mock" "react")
 for package in ${PACKAGES[@]}; do
   echo "Publish $package"
   pushd packages/$package
-  npm publish --access public --otp=$OTP
+  npm publish --access public
   popd
 done
