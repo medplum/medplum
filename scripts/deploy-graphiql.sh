d582ed588159cbb43112f73c79e0622bbf106d2f
Cody Ebberson <cody@ebberson.com>
2022-06-30 11:20:32 -0700

    Fixed google auth on GraphiQL

--- scripts/deploy-graphiql.sh
+++ scripts/deploy-graphiql.sh
@@ -5,11 +5,28 @@ aws s3 cp dist/ s3://medplum-docs/graphiql/ \
   --region us-east-1 \
   --recursive \
   --cache-control "public, max-age=31536000" \
-  --exclude "*.html"
+  --exclude "*.html" \
+  --exclude "*.css" \
+  --exclude "*.js"
 aws s3 cp dist/ s3://medplum-docs/graphiql/ \
   --region us-east-1 \
   --recursive \
+  --content-type "text/html" \
   --cache-control "no-cache" \
   --exclude "*" \
   --include "*.html"
+aws s3 cp dist/ s3://medplum-docs/graphiql/ \
+  --region us-east-1 \
+  --recursive \
+  --content-type "text/css" \
+  --cache-control "no-cache" \
+  --exclude "*" \
+  --include "*.css"
+aws s3 cp dist/ s3://medplum-docs/graphiql/ \
+  --region us-east-1 \
+  --recursive \
+  --content-type "application/javascript" \
+  --cache-control "no-cache" \
+  --exclude "*" \
+  --include "*.js"
 popd
