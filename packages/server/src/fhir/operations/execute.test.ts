744f0f4b07a94306c2bc10cec2ebaaba3041b8c7
Cody Ebberson <cody@ebberson.com>
2022-07-23 15:30:17 -0700

    Remove vmcontext bot

--- packages/server/src/fhir/operations/execute.test.ts
+++ packages/server/src/fhir/operations/execute.test.ts
@@ -13,16 +13,25 @@ jest.mock('@aws-sdk/client-lambda', () => {
   const original = jest.requireActual('@aws-sdk/client-lambda');
 
   class LambdaClient {
-    async send(): Promise<any> {
-      // LogResult:
-      // START RequestId: 146fcfcf-c32b-43f5-82a6-ee0f3132d873 Version: $LATEST
-      // 2022-05-30T16:12:22.685Z	146fcfcf-c32b-43f5-82a6-ee0f3132d873	INFO test
-      // END RequestId: 146fcfcf-c32b-43f5-82a6-ee0f3132d873
-      // REPORT RequestId: 146fcfcf-c32b-43f5-82a6-ee0f3132d873
-      return {
-        LogResult: `U1RBUlQgUmVxdWVzdElkOiAxNDZmY2ZjZi1jMzJiLTQzZjUtODJhNi1lZTBmMzEzMmQ4NzMgVmVyc2lvbjogJExBVEVTVAoyMDIyLTA1LTMwVDE2OjEyOjIyLjY4NVoJMTQ2ZmNmY2YtYzMyYi00M2Y1LTgyYTYtZWUwZjMxMzJkODczCUlORk8gdGVzdApFTkQgUmVxdWVzdElkOiAxNDZmY2ZjZi1jMzJiLTQzZjUtODJhNi1lZTBmMzEzMmQ4NzMKUkVQT1JUIFJlcXVlc3RJZDogMTQ2ZmNmY2YtYzMyYi00M2Y1LTgyYTYtZWUwZjMxMzJkODcz`,
-        Payload: '',
-      };
+    async send(command: any): Promise<any> {
+      if (command instanceof original.InvokeCommand) {
+        const decoder = new TextDecoder();
+        const event = JSON.parse(decoder.decode(command.input.Payload));
+        const output = typeof event.input === 'string' ? event.input : JSON.stringify(event.input);
+        const encoder = new TextEncoder();
+
+        // console.log('mock lambda client input', decoder.decode(command.input.Payload));
+        // LogResult:
+        // START RequestId: 146fcfcf-c32b-43f5-82a6-ee0f3132d873 Version: $LATEST
+        // 2022-05-30T16:12:22.685Z	146fcfcf-c32b-43f5-82a6-ee0f3132d873	INFO test
+        // END RequestId: 146fcfcf-c32b-43f5-82a6-ee0f3132d873
+        // REPORT RequestId: 146fcfcf-c32b-43f5-82a6-ee0f3132d873
+        return {
+          LogResult: `U1RBUlQgUmVxdWVzdElkOiAxNDZmY2ZjZi1jMzJiLTQzZjUtODJhNi1lZTBmMzEzMmQ4NzMgVmVyc2lvbjogJExBVEVTVAoyMDIyLTA1LTMwVDE2OjEyOjIyLjY4NVoJMTQ2ZmNmY2YtYzMyYi00M2Y1LTgyYTYtZWUwZjMxMzJkODczCUlORk8gdGVzdApFTkQgUmVxdWVzdElkOiAxNDZmY2ZjZi1jMzJiLTQzZjUtODJhNi1lZTBmMzEzMmQ4NzMKUkVQT1JUIFJlcXVlc3RJZDogMTQ2ZmNmY2YtYzMyYi00M2Y1LTgyYTYtZWUwZjMxMzJkODcz`,
+          Payload: encoder.encode(output),
+        };
+      }
+      return {};
     }
   }
 
@@ -53,7 +62,14 @@ describe('Execute', () => {
       .send({
         resourceType: 'Bot',
         name: 'Test Bot',
-        code: `console.log('input', input); return input;`,
+        runtimeVersion: 'awslambda',
+        // code: `console.log('input', input); return input;`,
+        code: `
+          export async function handler(medplum, event) {
+            console.log('input', event.input);
+            return event.input;
+          }
+        `,
       });
     expect(res.status).toBe(201);
     bot = res.body as Bot;
