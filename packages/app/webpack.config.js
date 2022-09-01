1eade87b1d937acb760ff91777f0647f2a7d22a6
Cody Ebberson <cody@ebberson.com>
2022-09-01 13:15:52 -0700

    Fixed all lighthouse pwa warnings (#926)

--- packages/app/webpack.config.js
+++ packages/app/webpack.config.js
@@ -1,10 +1,13 @@
+import CopyWebpackPlugin from 'copy-webpack-plugin';
 import dotenv from 'dotenv';
 import DotenvPlugin from 'dotenv-webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import { WebpackManifestPlugin } from 'webpack-manifest-plugin';
 import { SubresourceIntegrityPlugin } from 'webpack-subresource-integrity';
+import WorkboxPlugin from 'workbox-webpack-plugin';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -18,6 +21,7 @@ export default (env, argv) => ({
     filename: 'js/[name].[contenthash].js',
     publicPath: '/',
     crossOriginLoading: 'anonymous',
+    clean: true,
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
@@ -27,6 +31,35 @@ export default (env, argv) => ({
       defaults: true,
       systemvars: true,
     }),
+    new WebpackManifestPlugin({
+      fileName: 'manifest.webmanifest',
+      seed: {
+        name: 'Medplum',
+        short_name: 'Medplum',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            'src': '/img/medplum-logo.svg',
+            'type': 'image/svg+xml',
+            'sizes': '512x512'
+          },
+          {
+            'src': '/img/medplum-logo-512x512.png',
+            'type': 'image/png',
+            'sizes': '512x512',
+          },
+          {
+            'src': '/img/medplum-logo-maskable.png',
+            'type': 'image/png',
+            'sizes': '512x512',
+            'purpose': 'maskable'
+          }
+        ],
+      }
+    }),
     new HtmlWebpackPlugin({
       template: 'index.html.ejs',
       favicon: 'favicon.ico',
@@ -35,6 +68,17 @@ export default (env, argv) => ({
       filename: 'css/[name].[contenthash].css',
     }),
     new SubresourceIntegrityPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'static',
+        },
+      ]
+    }),
+    new WorkboxPlugin.GenerateSW({
+      clientsClaim: true,
+      skipWaiting: true,
+    }),
   ],
   module: {
     rules: [
