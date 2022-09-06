5f4cf5732b3e9127afaa36540bd8209c0f14e3f9
Cody Ebberson <cody@ebberson.com>
2022-09-06 14:46:16 -0700

    Update docs s3 bucket (#948)

--- scripts/deploy-docs.sh
+++ scripts/deploy-docs.sh
@@ -11,7 +11,7 @@ pushd packages/docs/build
 
 # Copy all non-HTML assets
 # Mark as public and cached forever
-aws s3 cp ./ s3://medplum-docs/ \
+aws s3 cp ./ s3://www.medplum.com/ \
   --region us-east-1 \
   --recursive \
   --cache-control "public, max-age=31536000" \
@@ -21,14 +21,14 @@ aws s3 cp ./ s3://medplum-docs/ \
 
 # Copy index.html
 # Set non-cache so clients revalidate
-aws s3 cp index.html s3://medplum-docs/index.html \
+aws s3 cp index.html s3://www.medplum.com/index.html \
   --region us-east-1 \
   --content-type "text/html; charset=utf-8" \
   --cache-control "no-cache"
 
 # Copy sitemap.xml
 # Set non-cache so clients revalidate
-aws s3 cp sitemap.xml s3://medplum-docs/sitemap.xml \
+aws s3 cp sitemap.xml s3://www.medplum.com/sitemap.xml \
   --region us-east-1 \
   --content-type "application/xml; charset=utf-8" \
   --cache-control "no-cache"
@@ -39,7 +39,7 @@ aws s3 cp sitemap.xml s3://medplum-docs/sitemap.xml \
 shopt -s globstar
 for input_file in **/*.html; do
   output_file=${input_file%.html}
-  aws s3 cp "$input_file" "s3://medplum-docs/$output_file" \
+  aws s3 cp "$input_file" "s3://www.medplum.com/$output_file" \
     --region us-east-1 \
     --content-type "text/html; charset=utf-8" \
     --cache-control "no-cache"
