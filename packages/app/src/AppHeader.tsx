55b0fb81665dbc190b03e57f1ed62241d6a5d5f0
Cody Ebberson <cody@ebberson.com>
2022-09-27 15:48:11 -0700

    Fixed profile selector

--- packages/app/src/AppHeader.tsx
+++ packages/app/src/AppHeader.tsx
@@ -1,7 +1,7 @@
-import { createStyles, Group, Header, Menu, Text, UnstyledButton } from '@mantine/core';
+import { Avatar, createStyles, Group, Header, Menu, Modal, Stack, Text, UnstyledButton } from '@mantine/core';
 import { formatHumanName, getReferenceString, ProfileResource } from '@medplum/core';
 import { HumanName } from '@medplum/fhirtypes';
-import { Logo, ResourceAvatar, useMedplum, useMedplumProfile } from '@medplum/react';
+import { Logo, ResourceAvatar, useMedplumContext } from '@medplum/react';
 import { IconChevronDown, IconLogout, IconSettings, IconSwitchHorizontal } from '@tabler/icons';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
@@ -57,61 +57,105 @@ interface HeaderTabsProps {
 }
 
 export function AppHeader({ navbarToggle }: HeaderTabsProps): JSX.Element {
-  const medplum = useMedplum();
-  const profile = useMedplumProfile() as ProfileResource;
+  const context = useMedplumContext();
+  const medplum = context.medplum;
+  const profile = context.profile as ProfileResource;
+  const logins = medplum.getLogins();
   const { classes, cx } = useStyles();
   const [userMenuOpened, setUserMenuOpened] = useState(false);
+  const [profileDialogOpened, setProfileDialogOpened] = useState(false);
   const navigate = useNavigate();
 
   return (
-    <Header height={60} p={8} style={{ zIndex: 101 }}>
-      <Group position="apart">
-        <Group spacing="xs">
-          <UnstyledButton className={classes.logoButton} onClick={navbarToggle}>
-            <Logo size={24} />
-          </UnstyledButton>
-          <HeaderSearchInput />
-        </Group>
-
-        <Menu
-          width={260}
-          position="bottom-end"
-          transition="pop-top-right"
-          onClose={() => setUserMenuOpened(false)}
-          onOpen={() => setUserMenuOpened(true)}
-        >
-          <Menu.Target>
-            <UnstyledButton className={cx(classes.user, { [classes.userActive]: userMenuOpened })}>
-              <Group spacing={7}>
-                <ResourceAvatar value={profile} radius="xl" size={24} />
-                <Text size="sm" className={classes.userName}>
-                  {formatHumanName(profile.name?.[0] as HumanName)}
-                </Text>
-                <IconChevronDown size={12} stroke={1.5} />
-              </Group>
+    <>
+      <Header height={60} p={8} style={{ zIndex: 101 }}>
+        <Group position="apart">
+          <Group spacing="xs">
+            <UnstyledButton className={classes.logoButton} onClick={navbarToggle}>
+              <Logo size={24} />
             </UnstyledButton>
-          </Menu.Target>
-          <Menu.Dropdown>
-            <Menu.Label>Settings</Menu.Label>
-            <Menu.Item
-              icon={<IconSettings size={14} stroke={1.5} />}
-              onClick={() => navigate(`/${getReferenceString(profile)}`)}
-            >
-              Account settings
-            </Menu.Item>
-            <Menu.Item icon={<IconSwitchHorizontal size={14} stroke={1.5} />}>Change account</Menu.Item>
-            <Menu.Item
-              icon={<IconLogout size={14} stroke={1.5} />}
-              onClick={() => {
-                medplum.signOut();
-                navigate('/signin');
-              }}
-            >
-              Sign out
-            </Menu.Item>
-          </Menu.Dropdown>
-        </Menu>
-      </Group>
-    </Header>
+            <HeaderSearchInput />
+          </Group>
+
+          <Menu
+            width={260}
+            shadow="xl"
+            position="bottom-end"
+            transition="pop-top-right"
+            onClose={() => setUserMenuOpened(false)}
+            onOpen={() => setUserMenuOpened(true)}
+          >
+            <Menu.Target>
+              <UnstyledButton className={cx(classes.user, { [classes.userActive]: userMenuOpened })}>
+                <Group spacing={7}>
+                  <ResourceAvatar value={profile} radius="xl" size={24} />
+                  <Text size="sm" className={classes.userName}>
+                    {formatHumanName(profile.name?.[0] as HumanName)}
+                  </Text>
+                  <IconChevronDown size={12} stroke={1.5} />
+                </Group>
+              </UnstyledButton>
+            </Menu.Target>
+            <Menu.Dropdown>
+              <Menu.Label>Settings</Menu.Label>
+              <Menu.Item
+                icon={<IconSettings size={14} stroke={1.5} />}
+                onClick={() => navigate(`/${getReferenceString(profile)}`)}
+              >
+                Account settings
+              </Menu.Item>
+              <Menu.Item
+                icon={<IconSwitchHorizontal size={14} stroke={1.5} />}
+                onClick={() => setProfileDialogOpened(true)}
+              >
+                Change account
+              </Menu.Item>
+              <Menu.Item
+                icon={<IconLogout size={14} stroke={1.5} />}
+                onClick={() => {
+                  medplum.signOut();
+                  navigate('/signin');
+                }}
+              >
+                Sign out
+              </Menu.Item>
+            </Menu.Dropdown>
+          </Menu>
+        </Group>
+      </Header>
+      <Modal opened={profileDialogOpened} withCloseButton onClose={() => setProfileDialogOpened(false)}>
+        <Stack>
+          {logins.map(
+            (login) =>
+              login.profile?.reference !== getReferenceString(context.profile as ProfileResource) && (
+                <UnstyledButton
+                  key={login.profile?.reference}
+                  onClick={() => {
+                    medplum
+                      .setActiveLogin(login)
+                      .then(() => {
+                        setProfileDialogOpened(false);
+                        window.location.reload();
+                      })
+                      .catch(console.log);
+                  }}
+                >
+                  <Group>
+                    <Avatar radius="xl" />
+                    <div style={{ flex: 1 }}>
+                      <Text size="sm" weight={500}>
+                        {login.profile?.display}
+                      </Text>
+                      <Text color="dimmed" size="xs">
+                        {login.project?.display}
+                      </Text>
+                    </div>
+                  </Group>
+                </UnstyledButton>
+              )
+          )}
+        </Stack>
+      </Modal>
+    </>
   );
 }
