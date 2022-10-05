b09bc8dedcf10edf97f0de2cbe5e626cf3d46199
Cody Ebberson <cody@ebberson.com>
2022-10-05 16:23:41 -0700

    Use Mantine badge for status badges

--- packages/react/src/StatusBadge.tsx
+++ packages/react/src/StatusBadge.tsx
@@ -1,10 +1,65 @@
+import { Badge, DefaultMantineColor } from '@mantine/core';
 import React from 'react';
-import './StatusBadge.css';
+
+/*
+ * Request status: https://hl7.org/fhir/valueset-request-status.html
+ * draft, active, on-hold, revoked, completed, entered-in-error, unknown
+ *
+ * Publication status: https://hl7.org/fhir/valueset-publication-status.html
+ * draft, active, retired, unknown
+ *
+ * Observation status: https://www.hl7.org/fhir/valueset-observation-status.html
+ * registered, preliminary, final, amended, cancelled, entered-in-error, unknown
+ *
+ * DiagnosticReport status: https://hl7.org/fhir/valueset-diagnostic-report-status.html
+ * registered, preliminary, final, amended, corrected, appended, cancelled, entered-in-error, unknown
+ *
+ * Task status: https://hl7.org/fhir/valueset-task-status.html
+ * draft, requested, received, accepted, rejected, ready, cancelled, in-progress, on-hold, failed, completed, entered-in-error
+ *
+ * Appointment status: https://www.hl7.org/fhir/valueset-appointmentstatus.html
+ * proposed, pending, booked, arrived, fulfilled, cancelled, noshow, entered-in-error, chcked-in, waitlist
+ */
+
+const statusToColor: Record<string, DefaultMantineColor> = {
+  draft: 'blue',
+  active: 'blue',
+  'on-hold': 'yellow',
+  revoked: 'red',
+  completed: 'green',
+  'entered-in-error': 'red',
+  unknown: 'gray',
+  retired: 'gray',
+  registered: 'blue',
+  preliminary: 'blue',
+  final: 'green',
+  amended: 'yellow',
+  cancelled: 'red',
+  requested: 'blue',
+  received: 'blue',
+  accepted: 'blue',
+  rejected: 'red',
+  ready: 'blue',
+  'in-progress': 'blue',
+  failed: 'red',
+  proposed: 'blue',
+  pending: 'blue',
+  booked: 'blue',
+  arrived: 'blue',
+  fulfilled: 'green',
+  noshow: 'red',
+  'checked-in': 'blue',
+  waitlist: 'gray',
+  routine: 'gray',
+  urgent: 'red',
+  asap: 'red',
+  stat: 'red',
+};
 
 export interface StatusBadgeProps {
   readonly status: string;
 }
 
 export function StatusBadge(props: StatusBadgeProps): JSX.Element {
-  return <span className={`medplum-status medplum-status-${props.status}`}>{props.status}</span>;
+  return <Badge color={statusToColor[props.status]}>{props.status}</Badge>;
 }
