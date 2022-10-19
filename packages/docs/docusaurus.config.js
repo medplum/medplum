86bfdc2435035478d5672daf9cd45a609a012119
Cody Ebberson <cody@ebberson.com>
2022-10-19 09:46:29 -0700

    Use esbuild-loader (#1066)

--- packages/docs/docusaurus.config.js
+++ packages/docs/docusaurus.config.js
@@ -235,6 +235,17 @@ const config = {
         //... other Algolia params
       },
     }),
+
+    webpack: {
+      jsLoader: (isServer) => ({
+        loader: require.resolve('esbuild-loader'),
+        options: {
+          loader: 'tsx',
+          format: isServer ? 'cjs' : undefined,
+          target: isServer ? 'node12' : 'es2017',
+        },
+      }),
+    },
 };
 
 module.exports = config;
