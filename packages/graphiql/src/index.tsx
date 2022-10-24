63c8b6c965b7de3d7219e2fbb47bd0ba024b7c8c
Cody Ebberson <cody@ebberson.com>
2022-10-24 16:49:00 -0700

    Fixed graphiql index.tsx

--- packages/graphiql/src/index.tsx
+++ packages/graphiql/src/index.tsx
@@ -1,4 +1,4 @@
-import { MantineProvider, Text } from '@mantine/core';
+import { MantineProvider, MantineThemeOverride, Title } from '@mantine/core';
 import { MedplumClient } from '@medplum/core';
 import { Logo, MedplumProvider, SignInForm, useMedplumProfile } from '@medplum/react';
 import GraphiQL from 'graphiql';
@@ -39,6 +39,25 @@ const medplum = new MedplumClient({
   baseUrl: process.env.MEDPLUM_BASE_URL,
 });
 
+const theme: MantineThemeOverride = {
+  headings: {
+    sizes: {
+      h1: {
+        fontSize: 18,
+        fontWeight: 500,
+        lineHeight: 2.0,
+      },
+    },
+  },
+  fontSizes: {
+    xs: 11,
+    sm: 14,
+    md: 14,
+    lg: 16,
+    xl: 18,
+  },
+};
+
 function App(): JSX.Element {
   const profile = useMedplumProfile();
   return profile ? (
@@ -58,7 +77,7 @@ const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <MedplumProvider medplum={medplum}>
-      <MantineProvider withGlobalStyles withNormalizeCSS>
+      <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
         <App />
       </MantineProvider>
     </MedplumProvider>
