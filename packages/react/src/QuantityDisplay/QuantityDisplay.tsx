293153fa321da7040c7d5fc4155028862ba3822e
Rahul Agarwal <rahul@medplum.com>
2022-11-04 11:55:44 -0700

    Removing Duplicated Code

--- packages/react/src/QuantityDisplay/QuantityDisplay.tsx
+++ packages/react/src/QuantityDisplay/QuantityDisplay.tsx
@@ -1,3 +1,4 @@
+import { formatQuantityString } from '@medplum/core';
 import { Quantity } from '@medplum/fhirtypes';
 import React from 'react';
 
@@ -7,30 +8,4 @@ export interface QuantityDisplayProps {
 
 export function QuantityDisplay(props: QuantityDisplayProps): JSX.Element | null {
   return <>{formatQuantityString(props.value)}</>;
-}
-
-export function formatQuantityString(quantity: Quantity | undefined): string {
-  if (!quantity) {
-    return '';
-  }
-
-  const result = [];
-
-  if (quantity.comparator) {
-    result.push(quantity.comparator);
-    result.push(' ');
-  }
-
-  if (quantity.value !== undefined) {
-    result.push(quantity.value);
-  }
-
-  if (quantity.unit) {
-    if (quantity.unit !== '%') {
-      result.push(' ');
-    }
-    result.push(quantity.unit);
-  }
-
-  return result.join('');
 }
