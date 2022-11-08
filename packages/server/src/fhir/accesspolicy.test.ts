3457d3c4a1bf147b0b8d0ee56e4c075581b23613
Cody Ebberson <cody@ebberson.com>
2022-11-08 15:57:08 -0800

    Support AccessPolicy with overlapping resource type rules

--- packages/server/src/fhir/accesspolicy.test.ts
+++ packages/server/src/fhir/accesspolicy.test.ts
@@ -1,4 +1,4 @@
-import { createReference, Operator } from '@medplum/core';
+import { createReference, normalizeErrorString, Operator } from '@medplum/core';
 import {
   AccessPolicy,
   ClientApplication,
@@ -1154,5 +1154,78 @@ describe('AccessPolicy', () => {
     const historyBundle = await repo2.readHistory<Questionnaire>('Questionnaire', questionnaire?.id as string);
     expect(historyBundle.entry).toHaveLength(1);
     expect(historyBundle.entry?.[0]?.resource?.id).toBe(questionnaire.id);
+  });
+
+  test('Overlapping resource policies', async () => {
+    const accessPolicy: AccessPolicy = {
+      resourceType: 'AccessPolicy',
+      resource: [
+        {
+          // ServiceRequest is readonly by default
+          resourceType: 'ServiceRequest',
+          readonly: true,
+        },
+        {
+          // ServiceRequest is read/write when in 'active' status
+          resourceType: 'ServiceRequest',
+          criteria: 'ServiceRequest?status=active',
+          readonly: false,
+        },
+      ],
+    };
+
+    const repo2 = new Repository({
+      author: {
+        reference: 'Practitioner/123',
+      },
+      accessPolicy,
+    });
+
+    // Can create in "active" status
+    let sr = await repo2.createResource<ServiceRequest>({
+      resourceType: 'ServiceRequest',
+      status: 'active',
+      intent: 'order',
+      subject: { reference: 'Patient/' + randomUUID() },
+      code: { text: 'test' },
+    });
+    expect(sr.id).toBeDefined();
+
+    // Can update in "active" status
+    sr = await repo2.updateResource<ServiceRequest>({
+      ...sr,
+      priority: 'stat',
+    });
+
+    // Cannot put into "completed" status
+    try {
+      await repo2.updateResource<ServiceRequest>({
+        ...sr,
+        status: 'completed',
+      });
+      throw new Error('Should not be able to update resource');
+    } catch (err) {
+      expect(normalizeErrorString(err)).toEqual('Forbidden');
+    }
+
+    // As admin, set the status
+    sr = await systemRepo.updateResource<ServiceRequest>({
+      ...sr,
+      status: 'completed',
+    });
+
+    // Can still read
+    sr = await repo2.readResource<ServiceRequest>('ServiceRequest', sr.id as string);
+
+    // Cannot update
+    try {
+      await repo2.updateResource<ServiceRequest>({
+        ...sr,
+        priority: 'routine',
+      });
+      throw new Error('Should not be able to update resource');
+    } catch (err) {
+      expect(normalizeErrorString(err)).toEqual('Forbidden');
+    }
   });
 });
