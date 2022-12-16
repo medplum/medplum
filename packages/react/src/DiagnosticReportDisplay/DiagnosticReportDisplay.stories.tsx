2c0e423c756782a5890de4b95fba74d797cd9524
Rahul Agarwal <rahul@medplum.com>
2022-12-16 13:37:22 -0800

    Added "category" and "status" fields for DiagnosticReportDisplay component

--- packages/react/src/DiagnosticReportDisplay/DiagnosticReportDisplay.stories.tsx
+++ packages/react/src/DiagnosticReportDisplay/DiagnosticReportDisplay.stories.tsx
@@ -1,9 +1,11 @@
 import { HomerDiagnosticReport } from '@medplum/mock';
 import { Meta } from '@storybook/react';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { DiagnosticReportDisplay } from './DiagnosticReportDisplay';
 import { Document } from '../Document/Document';
-
+import { CreatinineObservation, ExampleReport } from '../stories/referenceLab';
+import { useMedplum } from '../MedplumProvider/MedplumProvider';
+import { createReference } from '@medplum/core';
 export default {
   title: 'Medplum/DiagnosticReportDisplay',
   component: DiagnosticReportDisplay,
@@ -14,3 +16,29 @@ export const Simple = (): JSX.Element => (
     <DiagnosticReportDisplay value={HomerDiagnosticReport} />
   </Document>
 );
+
+export const WithCategories = (): JSX.Element => {
+  const medplum = useMedplum();
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    (async (): Promise<boolean> => {
+      const obs = await medplum.createResource(CreatinineObservation);
+      ExampleReport.result = [createReference(obs)];
+      await medplum.updateResource(ExampleReport);
+      return true;
+    })()
+      .then(setLoaded)
+      .catch(console.log);
+  }, [medplum]);
+
+  if (!loaded) {
+    return <></>;
+  }
+
+  return (
+    <Document>
+      <DiagnosticReportDisplay value={ExampleReport} />
+    </Document>
+  );
+};
