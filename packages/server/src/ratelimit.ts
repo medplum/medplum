126e98738f374436956eb06f8bb96924936b480f
Cody Ebberson <cody@ebberson.com>
2023-02-06 13:02:52 -0800

    Increase auth rate limit max (#1481)

--- packages/server/src/ratelimit.ts
+++ packages/server/src/ratelimit.ts
@@ -12,7 +12,7 @@ export function getRateLimiter(): RateLimitRequestHandler {
     store = new MemoryStore();
     handler = rateLimit({
       windowMs: 15 * 60 * 1000, // 15 minutes
-      max: 100, // limit each IP to 100 requests per windowMs
+      max: 300, // limit each IP to 300 requests per windowMs
       store,
     });
   }
