a096bd6507a21a34cc67e18c5d5c2bd07901ca17
Cody Ebberson <cody@ebberson.com>
2023-02-11 18:15:27 -0800

    Removed ServiceWorker debug messages

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -11,14 +11,8 @@ if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
       .register('/service-worker.js')
-      .then((reg) => {
-        console.log('SW registered: ', reg);
-        return reg.update();
-      })
-      .then((reg) => {
-        console.log('SW updated: ', reg);
-      })
-      .catch((regError) => console.log('SW registration failed: ', regError));
+      .then((reg) => reg.update())
+      .catch((regError) => console.error('SW registration failed: ', regError));
   });
 }
 
