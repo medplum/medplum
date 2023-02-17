5d45104ebb739254d6e316d819dba8f59c97a3a1
Cody Ebberson <cody@ebberson.com>
2023-02-17 12:50:00 -0800

    Fixed prettier errors

--- packages/server/src/fhir/signer.test.ts
+++ packages/server/src/fhir/signer.test.ts
@@ -1,47 +1,29 @@
-import { generateKeyPairSync } from 'crypto';
-import { Signer } from './signer';
+import { Binary } from '@medplum/fhirtypes';
+import { randomUUID } from 'crypto';
+import { loadTestConfig } from '../config';
+import { getPresignedUrl } from './signer';
 
 describe('Signer', () => {
-
-  let signer: Signer;
-
-  beforeAll(() => {
-
+  beforeAll(async () => {
     jest.useFakeTimers();
-
-    const keyId = 'xyz';
-    const passphrase = 'top secret';
-
-    const { privateKey } = generateKeyPairSync('rsa', {
-      modulusLength: 4096,
-      publicKeyEncoding: {
-        type: 'spki',
-        format: 'pem',
-      },
-      privateKeyEncoding: {
-        type: 'pkcs8',
-        format: 'pem',
-        cipher: 'aes-256-cbc',
-        passphrase,
-      },
-    });
-
-    signer = new Signer(keyId, privateKey, passphrase);
-  })
+    await loadTestConfig();
+  });
 
   afterAll(() => {
     jest.useRealTimers();
-  })
+  });
 
   test('Presign URL', () => {
-    jest.setSystemTime(new Date("2023-02-10T00:00:00.000Z"));
-    const withoutDate = signer.sign('https://example.com/test');
-    expect(withoutDate).toMatch(/^https:\/\/example\.com\/test\?Expires=1675990800&Key-Pair-Id=/);
-  },);
-    
-  test('Presign URL with date', () => {
-    const signDate = new Date("2023-02-10T00:00:00.000Z");
-    const withDate = signer.sign('https://example.com/test', signDate);
-    expect(withDate).toMatch(/^https:\/\/example\.com\/test\?Expires=1675987200&Key-Pair-Id=/);
+    jest.setSystemTime(new Date('2023-02-10T00:00:00.000Z'));
+
+    const binary: Binary = {
+      resourceType: 'Binary',
+      id: randomUUID(),
+      meta: {
+        versionId: randomUUID(),
+      },
+    };
+
+    expect(getPresignedUrl(binary)).toMatch(/\?Expires=1675990800&Key-Pair-Id=/);
   });
 });
--- packages/server/src/fhir/signer.ts
+++ packages/server/src/fhir/signer.ts
@@ -1,49 +1,13 @@
+import { getSignedUrl } from '@aws-sdk/cloudfront-signer';
 import { Binary } from '@medplum/fhirtypes';
 import { getConfig } from '../config';
-import { getSignedUrl } from "@aws-sdk/cloudfront-signer";
-
-/**
- * Creates a signed URL for a given URL.
- *
- * Reference:
- * https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/modules/_aws_sdk_cloudfront_signer.html
- */
-export class Signer {
-  readonly #keyPairId: string;
-  readonly #privateKey: string;
-  readonly #passphrase: string;
-
-  constructor(keyPairId: string, privateKey: string, passphrase: string) {
-    this.#keyPairId = keyPairId;
-    this.#privateKey = privateKey;
-    this.#passphrase = passphrase;
-  }
-
-  /**
-   * Creates a signed URL.
-   * @param url Input URL.
-   * @param dateLessThan The date for when the signed URL can no longer be accessed. Default is one hour in the future
-   * @returns Presigned URL.
-   */
-  sign(url: string, dateLessThan?: Date): string {
-
-    if (!dateLessThan) {
-      dateLessThan = new Date();
-      dateLessThan.setHours(dateLessThan.getHours() + 1);
-    }
-
-    return getSignedUrl({
-      url,
-      keyPairId: this.#keyPairId,
-      dateLessThan: dateLessThan.toISOString(),
-      privateKey: this.#privateKey,
-      passphrase: this.#passphrase,
-    })
-  }
-}
 
 /**
  * Returns a presigned URL for the Binary resource content.
+ *
+ * Reference:
+ * https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/modules/_aws_sdk_cloudfront_signer.html
+ *
  * @param binary Binary resource.
  * @returns Presigned URL to access the binary data.
  */
@@ -51,7 +15,13 @@ export function getPresignedUrl(binary: Binary): string {
   const config = getConfig();
   const storageBaseUrl = config.storageBaseUrl;
   const unsignedUrl = `${storageBaseUrl}${binary.id}/${binary.meta?.versionId}`;
-  const signer = new Signer(config.signingKeyId, config.signingKey, config.signingKeyPassphrase);
-
-  return signer.sign(unsignedUrl);
+  const dateLessThan = new Date();
+  dateLessThan.setHours(dateLessThan.getHours() + 1);
+  return getSignedUrl({
+    url: unsignedUrl,
+    keyPairId: config.signingKeyId,
+    dateLessThan: dateLessThan.toISOString(),
+    privateKey: config.signingKey,
+    passphrase: config.signingKeyPassphrase,
+  });
 }
