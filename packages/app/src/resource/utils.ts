6a0046e6fcf4917c3329a2e22cea585d1240b061
rahul1 <rahul@medplum.com>
2023-03-09 19:57:15 -0800

    Workaround for JsonInput deserialization bug (#1639)

--- packages/app/src/resource/utils.ts
+++ packages/app/src/resource/utils.ts
@@ -4,7 +4,7 @@ import { Resource } from '@medplum/fhirtypes';
  * Cleans a resource of unwanted meta values.
  * For most users, this will not matter, because meta values are set by the server.
  * However, some administrative users are allowed to specify some meta values.
- * The admin use case is sepcial though, and unwanted here on the resource page.
+ * The admin use case is special though, and unwanted here on the resource page.
  * @param resource The input resource.
  * @returns The cleaned output resource.
  */
