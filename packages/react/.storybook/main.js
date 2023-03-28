5717c39babb865a3301d6a1977a158cbd3b91cb6
Cody Ebberson <cody@ebberson.com>
2023-03-27 23:21:10 -0700

    Fixes #1732 - storybook build with typescript 5

--- packages/react/.storybook/main.js
+++ packages/react/.storybook/main.js
@@ -2,6 +2,10 @@ module.exports = {
   core: {
     builder: 'webpack5',
   },
+  typescript: {
+    // Remove this after upgrading to Storybook 7
+    reactDocgen: 'react-docgen-typescript-plugin'
+  },
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   staticDirs: ['../public'],
