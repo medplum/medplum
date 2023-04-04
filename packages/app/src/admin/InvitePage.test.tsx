a6c6dc29482710b94ae4a5903732d4140405185b
James Touri <j_touri@yahoo.com>
2023-04-04 16:58:44 -0700

    Remove accidental import

--- packages/app/src/admin/InvitePage.test.tsx
+++ packages/app/src/admin/InvitePage.test.tsx
@@ -4,7 +4,6 @@ import { act, fireEvent, render, screen, waitFor } from '@testing-library/react'
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { AppRoutes } from '../AppRoutes';
-import exp from 'constants';
 
 const medplum = new MockClient();
 
