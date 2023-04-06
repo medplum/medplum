e44f745d5634deec41e15d4082cd232957cc2b07
Cody Ebberson <cody@ebberson.com>
2023-04-06 10:59:19 -0700

    Fixed storybook issues

--- packages/react/.storybook/main.js
+++ packages/react/.storybook/main.js
@@ -1,16 +1,16 @@
 module.exports = {
-  typescript: {
-    // Remove this after upgrading to Storybook 7
-    reactDocgen: 'react-docgen-typescript-plugin'
-  },
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: [
+    '../src/stories/Introduction.stories.mdx',
+    '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   staticDirs: ['../public'],
   framework: {
     name: '@storybook/react-webpack5',
-    options: {}
+    options: {},
   },
   docs: {
-    autodocs: true
-  }
+    autodocs: true,
+  },
 };
