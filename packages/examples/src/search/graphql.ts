71539fb29eb4785ef9276f21bff35669a8db59ba
Rahul Agarwal <rahul@medplum.com>
2023-04-15 17:42:05 -0700

    Completing all prose and code examples

--- packages/examples/src/search/graphql.ts
+++ packages/examples/src/search/graphql.ts
@@ -46,6 +46,7 @@ curl -X POST 'https://api.medplum.com/$graphql' \
 // start-block GetPatientByIdTS
 const patientId = 'example-id';
 await medplum.graphql(`
+{
   Patient(id: "${patientId}") {
     resourceType
     id
@@ -55,7 +56,8 @@ await medplum.graphql(`
     address {
       text
     }
-  }`);
+  }
+}`);
 // end-block GetPatientByIdTS
 
 let response: any =
@@ -84,7 +86,55 @@ let response: any =
 /*
 // start-block SearchPatientsByNameAndCityGraphQL
 query {
-  patients: PatientList(filter: { name: "Eve", addressCity: "Philadelphia" }) {
+  patients: PatientList(name: "Eve", address_city: "Philadelphia") {
+    resourceType
+    id
+    name {
+      family
+      given
+    }
+    address {
+      line
+      city
+      state
+      postal_code
+    }
+  }
+}
+// end-block SearchPatientsByNameAndCityGraphQL
+*/
+
+/*
+// start-block SearchPatientsByNameAndCityCurl
+curl -X POST "https://api.medplum.com/$graphql" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "query": "
+      query {
+        patients: PatientList(name: \"Eve\", address_city: \"Philadelphia\") {
+          resourceType
+          id
+          name {
+            family
+            given
+          }
+          address {
+            line
+            city
+            state
+            postal_code
+          }
+        }
+      }"
+  }'
+// end-block SearchPatientsByNameAndCityCurl
+*/
+
+// start-block SearchPatientsByNameAndCityTS
+
+await medplum.graphql(`
+{
+  patients: PatientList(name: "Eve", address_city: "Philadelphia") {
     resourceType
     id
     name {
@@ -98,19 +148,295 @@ query {
       postalCode
     }
   }
-}
+}`);
+// end-block SearchPatientsByNameAndCityTS
 
-// end-block SearchPatientsByNameAndCityGraphQL
+response = {
+  // start-block SearchPatientsByNameAndCityResponse
+  data: {
+    PatientList: [
+      {
+        resourceType: 'Patient',
+        id: 'example-id-1',
+        name: [
+          {
+            family: 'Johnson',
+            given: ['Eve'],
+          },
+        ],
+        address: [
+          {
+            line: ['456 Market St'],
+            city: 'Philadelphia',
+            state: 'PA',
+            postalCode: '19104',
+          },
+        ],
+      },
+      {
+        resourceType: 'Patient',
+        id: 'example-id-2',
+        name: [
+          {
+            family: 'Smith',
+            given: ['Eve'],
+          },
+        ],
+        address: [
+          {
+            line: ['789 Broad St'],
+            city: 'Philadelphia',
+            state: 'PA',
+            postalCode: '19107',
+          },
+        ],
+      },
+    ],
+  },
+  // end-block SearchPatientsByNameAndCityResponse
+};
+
+console.log(response);
+
+/* -- GetDiagnosticReportAndObservations -- */
+
+/* --- DiagnosticReportWithObservations -- */
+/*
+// start-block DiagnosticReportWithObservationsGraphQL
+query {
+  DiagnosticReport(id: "example-id-1") {
+    resourceType
+    id
+    result {
+      resource {
+        ... on Observation {
+          resourceType
+          id
+          valueQuantity {
+            value
+            unit
+          }
+        }
+      }
+    }
+  }
+}
+// end-block DiagnosticReportWithObservationsGraphQL
 */
 
 /*
-// start-block SearchPatientsByNameAndCityCurl
+// start-block DiagnosticReportWithObservationsCurl
 curl -X POST "https://api.medplum.com/$graphql" \
   -H "Content-Type: application/json" \
   -d '{
     "query": "
       query {
-        patients: PatientList(name: \"Eve\", addressCity: \"Philadelphia\" ) {
+        DiagnosticReport(id: \"example-id-1\") {
+          resourceType
+          id
+          result {
+            resource {
+              ... on Observation {
+                resourceType
+                id
+                valueQuantity {
+                  value
+                  unit
+                }
+              }
+            }
+          }
+        }
+      }"
+  }'
+// end-block DiagnosticReportWithObservationsCurl
+*/
+
+// start-block DiagnosticReportWithObservationsTS
+await medplum.graphql(`
+{
+  DiagnosticReport(id: "example-id-1") {
+    resourceType
+    id
+    result {
+      resource {
+        ... on Observation {
+          resourceType
+          id
+          valueQuantity {
+            value
+            unit
+          }
+        }
+      }
+    }
+  }
+}`);
+// end-block DiagnosticReportWithObservationsTS
+
+response = {
+  // start-block DiagnosticReportWithObservationsResponse
+  data: {
+    DiagnosticReport: {
+      resourceType: 'DiagnosticReport',
+      id: 'example-id-1',
+      result: [
+        {
+          resource: {
+            resourceType: 'Observation',
+            id: 'observation-id-1',
+            valueQuantity: {
+              value: 5.5,
+              unit: 'mg/dL',
+            },
+          },
+        },
+        {
+          resource: {
+            resourceType: 'Observation',
+            id: 'observation-id-2',
+            valueQuantity: {
+              value: 3.2,
+              unit: 'mg/dL',
+            },
+          },
+        },
+      ],
+    },
+  },
+  // end-block DiagnosticReportWithObservationsResponse
+};
+
+console.log(response);
+
+/* --- PatientWithRelatedEncounters -- */
+
+/*
+// start-block PatientWithRelatedEncountersGraphQL
+query {
+  {
+    Patient(id: "example-patient-id") {
+      resourceType
+      id
+      encounters: EncounterList(_reference: patient) {
+        resourceType
+        id
+      }
+    }
+  }
+}
+// end-block PatientWithRelatedEncountersGraphQL
+*/
+
+/*
+// start-block PatientWithRelatedEncountersCurl
+curl -X POST "https://api.medplum.com/$graphql" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "query": "
+      query {
+        {
+          Patient(id: "example-patient-id") {
+            resourceType
+            id
+            encounters: EncounterList(_reference: patient) {
+              resourceType
+              id
+            }
+          }
+        }
+      "
+  }'
+// end-block PatientWithRelatedEncountersCurl
+*/
+
+// start-block PatientWithRelatedEncountersTS
+await medplum.graphql(`
+{
+  Patient(id: "example-patient-id") {
+    resourceType
+    id
+    encounters: EncounterList(_reference: patient) {
+      resourceType
+      id
+    }
+  }
+}`);
+
+// end-block PatientWithRelatedEncountersTS
+
+response = {
+  // start-block PatientWithRelatedEncountersResponse
+  data: {
+    Patient: {
+      resourceType: 'Patient',
+      id: 'example-patient-id',
+      encounters: [
+        {
+          resourceType: 'Encounter',
+          id: 'encounter-id-1',
+        },
+        {
+          resourceType: 'Encounter',
+          id: 'encounter-id-2',
+        },
+      ],
+    },
+  },
+  // end-block PatientWithRelatedEncountersResponse
+};
+
+console.log(response);
+
+/* --- PatientsWithReports -- */
+/*
+// start-block PatientsWithReportsGraphQL
+query {
+  // Search for a list of Patients named "Eve", living in "Philadelphia"
+  patients: PatientList(name: "Eve", address_city: "Philadelphia") {
+    resourceType
+    id
+    name {
+      family
+      given
+    }
+    address {
+      line
+      city
+      state
+      postalCode
+    }
+    // Search for DiagnosticReports linked to each Patient
+    reports: DiagnosticReportList(_reference: subject) {
+      resourceType
+      id
+      // Resolve the Observations referenced by DiagnosticReport.result
+      result {
+        resource {
+          ... on Observation {
+            resourceType
+            id
+            valueQuantity {
+              value
+              unit
+            }
+          }
+        }
+      }
+    }
+  }
+}
+// end-block PatientsWithReportsGraphQL
+*/
+
+/*
+// start-block PatientsWithReportsCurl
+curl -X POST "https://api.medplum.com/graphql" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "query": "
+      query {
+        patients: PatientList(name: \"Eve\", address_city: \"Philadelphia\") {
           resourceType
           id
           name {
@@ -123,291 +449,145 @@ curl -X POST "https://api.medplum.com/$graphql" \
             state
             postalCode
           }
+          reports: DiagnosticReportList(_reference: subject) {
+            resourceType
+            id
+
+            result {
+              resource {
+                ... on Observation {
+                  resourceType
+                  id
+                  valueQuantity {
+                    value
+                    unit
+                  }
+                }
+              }
+            }
+          }
         }
       }"
   }'
-// end-block SearchPatientsByNameAndCityCurl
+// end-block PatientsWithReportsCurl
 */
 
-// start-block SearchPatientsByNameAndCityTS
-const name = 'Eve';
-const city = 'Philadelphia';
+// start-block PatientsWithReportsTS
 await medplum.graphql(`
-patients: PatientList(filter: { name: "${name}", addressCity: "${city}" }) {
-  resourceType
-  id
-  name {
-    family
-    given
-  }
-  address {
-    line
-    city
-    state
-    postalCode
+{
+  patients: PatientList(name: "Eve", address_city: "Philadelphia") {
+    resourceType
+    id
+    name {
+      family
+      given
+    }
+    address {
+      line
+      city
+      state
+      postalCode
+    }
+    reports: DiagnosticReportList(_reference: subject) {
+      resourceType
+      id
+      result {
+        resource {
+          ... on Observation {
+            resourceType
+            id
+            valueQuantity {
+              value
+              unit
+            }
+          }
+        }
+      }
+    }
   }
 }
 `);
-// end-block SearchPatientsByNameAndCityTS
+// end-block PatientsWithReportsTS
 
-response =
-  // start-block SearchPatientsByNameAndCityResponse
-  {
-    data: {
-      patients: [
-        {
-          resourceType: 'Patient',
-          id: 'example-id-1',
-          name: [
-            {
-              family: 'Johnson',
-              given: ['Eve'],
-            },
-          ],
-          address: [
-            {
-              line: ['456 Market St'],
-              city: 'Philadelphia',
-              state: 'PA',
-              postalCode: '19104',
-            },
-          ],
-        },
-        {
-          resourceType: 'Patient',
-          id: 'example-id-2',
-          name: [
-            {
-              family: 'Smith',
-              given: ['Eve'],
-            },
-          ],
-          address: [
-            {
-              line: ['789 Broad St'],
-              city: 'Philadelphia',
-              state: 'PA',
-              postalCode: '19107',
-            },
-          ],
-        },
-      ],
-    },
-  };
-// end-block SearchPatientsByNameAndCityResponse
-
-console.log(response);
-
-/* -- GetDiagnosticReportAndObservations -- */
-
-/*
-// start-block GetDiagnosticReportAndObservationsGraphQL
-query {
-  DiagnosticReport(id: "example-id") {
-    resourceType
-    id
-    result {
-      resource {
-        ... on Observation {
-          resourceType
-          id
-        }
-      }
-    }
-  }
-}
-// end-block GetDiagnosticReportAndObservationsGraphQL
-*/
-
-/*
-// start-block GetDiagnosticReportAndObservationsCurl
-curl -X POST 'https://api.medplum.com/$graphql' \
--H 'Content-Type: application/json' \
--H 'Authorization: Bearer your-access-token' \
--d '{
-  "query": "
-    query {
-      DiagnosticReport(id: \"example-id\") {
-        resourceType
-        id
-        result {
-          resource {
-            ... on Observation {
-              resourceType
-              id
-            }
-          }
-        }
-      }
-    }
-  "
-}'
-// end-block GetDiagnosticReportAndObservationsCurl
-*/
-
-// start-block GetDiagnosticReportAndObservationsTS
-const reportId = 'example-report-1';
-await medplum.graphql(`
-    query {
-      DiagnosticReport(id: "${reportId}") {
-        resourceType
-        id
-        result {
-          resource {
-            ... on Observation {
-              resourceType
-              id
-            }
-          }
-        }
-      }
-    }
-  `);
-// end-block GetDiagnosticReportAndObservationsTS
-
-response =
-  // start-block GetDiagnosticReportAndObservationsResponse
-
-  {
-    data: {
-      DiagnosticReport: {
-        resourceType: 'DiagnosticReport',
-        id: 'example-id',
-        result: [
+response = {
+  // start-block PatientsWithReportsResponse
+  data: {
+    patients: [
+      {
+        resourceType: 'Patient',
+        id: 'patient-id-1',
+        name: [
           {
-            resource: {
-              resourceType: 'Observation',
-              id: 'observation-id-1',
-            },
+            family: 'Smith',
+            given: ['Eve'],
           },
+        ],
+        address: [
           {
-            resource: {
-              resourceType: 'Observation',
-              id: 'observation-id-2',
-            },
+            line: ['123 Main St'],
+            city: 'Philadelphia',
+            state: 'PA',
+            postalCode: '19107',
+          },
+        ],
+        reports: [
+          {
+            resourceType: 'DiagnosticReport',
+            id: 'report-id-1',
+            result: [
+              {
+                resource: {
+                  resourceType: 'Observation',
+                  id: 'observation-id-1',
+                  valueQuantity: {
+                    value: 5.5,
+                    unit: 'mg/dL',
+                  },
+                },
+              },
+            ],
           },
         ],
       },
-    },
-  };
-// end-block GetDiagnosticReportAndObservationsResponse
-
-/* -- SearchEncountersByPatient -- */
-/*
-// start-block SearchEncountersByPatientGraphQL
-query {
-  EncounterList(filter: { _reference: "subject:Patient/example-id" }) {
-    entries {
-      resource {
-        ... on Encounter {
-          resourceType
-          id
-          subject {
-            resource {
-              ... on Patient {
-                resourceType
-                id
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-}
-
-// end-block SearchEncountersByPatientGraphQL
- */
-
-/*
-// start-block SearchEncountersByPatientCurl
-curl -X POST 'https://api.medplum.com/graphql' \
--H 'Content-Type: application/json' \
--H 'Authorization: Bearer your-access-token' \
--d '{
-  "query": "
-    query {
-      EncounterList(filter: { _reference: \"subject:Patient/example-id\" }) {
-        entries {
-          resource {
-            ... on Encounter {
-              resourceType
-              id
-              subject {
-                resource {
-                  ... on Patient {
-                    resourceType
-                    id
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  "
-}'
-// end-block SearchEncountersByPatientCurl
-*/
-
-// start-block SearchEncountersByPatientTS
-await medplum.graphql(`
-    query {
-      EncounterList(filter: { _reference: "subject:Patient/${patientId}" }) {
-        entries {
-          resource {
-            ... on Encounter {
-              resourceType
-              id
-              subject {
-                resource {
-                  ... on Patient {
-                    resourceType
-                    id
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  `);
-// end-block SearchEncountersByPatientTS
-
-response =
-  // start-block SearchEncountersByPatientResponse
-  {
-    data: {
-      Patient: {
+      {
         resourceType: 'Patient',
-        id: 'example-id',
-        encounters: {
-          resource: [
-            {
-              resourceType: 'Encounter',
-              id: 'encounter-id-1',
-              subject: {
+        id: 'patient-id-2',
+        name: [
+          {
+            family: 'Johnson',
+            given: ['Eve'],
+          },
+        ],
+        address: [
+          {
+            line: ['456 Oak St'],
+            city: 'Philadelphia',
+            state: 'PA',
+            postalCode: '19107',
+          },
+        ],
+        reports: [
+          {
+            resourceType: 'DiagnosticReport',
+            id: 'report-id-2',
+            result: [
+              {
                 resource: {
-                  resourceType: 'Patient',
-                  id: 'example-id',
+                  resourceType: 'Observation',
+                  id: 'observation-id-2',
+                  valueQuantity: {
+                    value: 6.7,
+                    unit: 'mg/dL',
+                  },
                 },
               },
-            },
-            {
-              resourceType: 'Encounter',
-              id: 'encounter-id-2',
-              subject: {
-                resource: {
-                  resourceType: 'Patient',
-                  id: 'example-id',
-                },
-              },
-            },
-          ],
-        },
+            ],
+          },
+        ],
       },
-    },
-  };
-// end-block SearchEncountersByPatientResponse
+    ],
+  },
+  // end-block PatientsWithReportsResponse
+};
+
+console.log(response);
