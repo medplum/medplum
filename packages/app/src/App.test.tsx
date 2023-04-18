8d2642796beadba927b7c7e7f0f4f0bc7576b491
TONY LEE <tonylee80@gmail.com>
2023-04-17 19:25:54 -0700

    remove unused code

--- packages/app/src/App.test.tsx
+++ packages/app/src/App.test.tsx
@@ -26,12 +26,6 @@ async function setup(url = '/'): Promise<void> {
 }
 
 describe('App', () => {
-  // beforeAll(() => {
-  //   indexStructureDefinitionBundle(readJson('fhir/r4/profiles-types.json') as Bundle);
-  //   indexStructureDefinitionBundle(readJson('fhir/r4/profiles-resources.json') as Bundle);
-  //   indexSearchParameterBundle(readJson('fhir/r4/search-parameters.json') as Bundle<SearchParameter>);
-  // });
-
   beforeEach(() => {
     jest.useFakeTimers();
   });
