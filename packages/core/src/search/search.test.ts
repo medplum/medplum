7c1cf12ce6092efe889d729dec1227ab3d0281d5
Cody Ebberson <cody@ebberson.com>
2023-04-19 18:13:42 -0700

    Fixed search URL parse bug with multiple filters (#1869)

--- packages/core/src/search/search.test.ts
+++ packages/core/src/search/search.test.ts
@@ -98,6 +98,27 @@ describe('Search Utils', () => {
     });
   });
 
+  test('Parse multiple filters same code', () => {
+    const result = parseSearchDefinition(
+      'Patient?_lastUpdated=ge2023-04-01T07%3A00%3A00.000Z&_lastUpdated=le2023-05-01T06%3A59%3A59.999Z'
+    );
+    expect(result).toMatchObject({
+      resourceType: 'Patient',
+      filters: [
+        {
+          code: '_lastUpdated',
+          operator: Operator.GREATER_THAN_OR_EQUALS,
+          value: '2023-04-01T07:00:00.000Z',
+        },
+        {
+          code: '_lastUpdated',
+          operator: Operator.LESS_THAN_OR_EQUALS,
+          value: '2023-05-01T06:59:59.999Z',
+        },
+      ],
+    });
+  });
+
   test('Format Patient search', () => {
     const result = formatSearchQuery({
       resourceType: 'Patient',
