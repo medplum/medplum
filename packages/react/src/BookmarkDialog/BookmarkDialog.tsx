f371ca0437ecf9a386747bdbf90638f9eab1273b
TONY LEE <tonylee80@gmail.com>
2023-04-27 14:58:44 -0700

    update existing UserConfiguration

--- packages/react/src/BookmarkDialog/BookmarkDialog.tsx
+++ packages/react/src/BookmarkDialog/BookmarkDialog.tsx
@@ -1,7 +1,8 @@
-import { Box, Button, Input, Modal } from '@mantine/core';
+import { Box, Button, Modal, Select, TextInput } from '@mantine/core';
 import React from 'react';
 import { Form } from '../Form/Form';
 import { useMedplum } from '../MedplumProvider/MedplumProvider';
+import { UserConfiguration } from '@medplum/fhirtypes';
 
 interface bookmarkDialog {
   visible: boolean;
@@ -11,34 +12,41 @@ interface bookmarkDialog {
 }
 export function BookmarkDialog(props: bookmarkDialog): JSX.Element | null {
   const medplum = useMedplum();
-
+  const config = medplum.getUserConfiguration() as UserConfiguration;
+  console.log(config);
   function submitHandler(formData: Record<string, string>): void {
     console.log(formData);
-    addBookmark(formData.bookmarkname);
+
+    console.log('adding bookmark', formData);
+
+    const { menuname, bookmarkname: name } = formData;
+    const target = '/Patient';
+    const menu = config?.menu?.find(({ title }) => title === menuname);
+
+    if (menu) {
+      menu?.link?.push({ name, target });
+    }
+
+    if (config?.id) {
+      medplum
+        .updateResource(config)
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } else {
+      console.log(config);
+      medplum
+        .createResource(config)
+        .then((response) => console.log('success!!!', response))
+        .catch((error) => console.log(error));
+    }
     props.onOk();
     props.onCancel();
   }
 
-  function addBookmark(value: string): void {
-    console.log('adding bookmark', value);
-    medplum
-      .updateResource({
-        resourceType: 'Patient',
-        id: '123',
-        name: [
-          {
-            family: value,
-            given: ['John'],
-          },
-        ],
-      })
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
   return (
     <Modal
       title="Add Bookmark"
@@ -48,10 +56,29 @@ export function BookmarkDialog(props: bookmarkDialog): JSX.Element | null {
     >
       <Box display="flex" sx={{ justifyContent: 'space-between' }}>
         <Form onSubmit={submitHandler}>
-          <Input name="bookmarkname" placeholder="bookmark name" />
+          <SelectMenu config={config}></SelectMenu>
+          <TextInput label="Bookmark Name" type="text" name="bookmarkname" placeholder="bookmark name" withAsterisk />
           <Button type="submit">Save</Button>
         </Form>
       </Box>
     </Modal>
+  );
+}
+
+interface selectMenuProps {
+  config: UserConfiguration | undefined;
+}
+function SelectMenu(props: selectMenuProps): JSX.Element {
+  function userConfigToMenu(config: UserConfiguration | undefined): [] {
+    return config?.menu?.map((menu) => menu.title) as [];
+  }
+  return (
+    <Select
+      name="menuname"
+      label="Select Menu Option"
+      placeholder="Menu"
+      data={userConfigToMenu(props.config)}
+      withAsterisk
+    />
   );
 }
