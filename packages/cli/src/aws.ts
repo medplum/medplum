d9c2ea0b5dcbf65147927d3e91224fcdba0738f3
Cody Ebberson <cody@ebberson.com>
2023-04-26 22:28:46 -0700

    Draft of CLI update-app command

--- packages/cli/src/aws.ts
+++ packages/cli/src/aws.ts
@@ -8,7 +8,19 @@ import {
   StackSummary,
 } from '@aws-sdk/client-cloudformation';
 import { ECSClient, UpdateServiceCommand } from '@aws-sdk/client-ecs';
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { CloudFrontClient, CreateInvalidationCommand } from '@aws-sdk/client-cloudfront';
 import { Command } from 'commander';
+import fastGlob from 'fast-glob';
+import { createReadStream, mkdtempSync, readFileSync, readdirSync, rmSync, writeFileSync } from 'fs';
+import fetch from 'node-fetch';
+import { tmpdir } from 'os';
+import { join, sep } from 'path';
+import { pipeline } from 'stream';
+import tar from 'tar';
+import { promisify } from 'util';
+
+const pipelineAsync = promisify(pipeline);
 
 interface MedplumStackDetails {
   stack: Stack;
@@ -16,11 +28,14 @@ interface MedplumStackDetails {
   ecsCluster?: StackResource;
   ecsService?: StackResource;
   appBucket?: StackResource;
+  appDistribution?: StackResource;
   storageBucket?: StackResource;
 }
 
 export const aws = new Command('aws').description('Commands to manage AWS resources');
 const client = new CloudFormationClient({});
+const s3Client = new S3Client({});
+const cloudFrontClient = new CloudFrontClient({});
 const tagKey = 'medplum:environment';
 
 aws
@@ -83,6 +98,76 @@ aws
     console.log(`Service "${ecsService}" updated successfully.`);
   });
 
+aws
+  .command('update-app')
+  .description('Update the app site')
+  .argument('<tag>')
+  .action(async (tag) => {
+    const details = await getStackByTag(tag);
+    if (!details) {
+      console.log('Stack not found');
+      return;
+    }
+    const appBucket = details.appBucket;
+    if (!appBucket) {
+      console.log('App bucket not found');
+      return;
+    }
+
+    const tmpDir = await downloadNpmPackage('@medplum/app/', 'latest');
+
+    // TODO: Load these values from a local config file
+    replaceVariables(tmpDir, {
+      MEDPLUM_BASE_URL: 'https://api.staging.medplum.com/',
+      MEDPLUM_CLIENT_ID: '',
+      GOOGLE_CLIENT_ID: '659647315343-c0p9rkl3pq38q18r13bkrchs4iqjogv1.apps.googleusercontent.com',
+      RECAPTCHA_SITE_KEY: '6LfXscQdAAAAAKlNFAoXqjliz0xbR8hvQw_pZfb2',
+      MEDPLUM_REGISTER_ENABLED: 'true',
+    });
+
+    // Manually iterate and upload files
+    // Automatic content-type detection is not reliable on Microsoft Windows
+    // So we explicitly set content-type
+    const uploadPatterns: [string, string, boolean][] = [
+      // Cached
+      // These files generally have a hash, so they can be cached forever
+      // It is important to upload them first to avoid broken references from index.html
+      ['css/**/*.css', 'text/css', true],
+      ['css/**/*.css.map', 'application/json', true],
+      ['img/**/*.png', 'image/png', true],
+      ['img/**/*.svg', 'image/svg+xml', true],
+      ['js/**/*.js', 'application/javascript', true],
+      ['js/**/*.js.map', 'application/json', true],
+      ['js/**/*.txt', 'text/plain', true],
+      ['favicon.ico', 'image/vnd.microsoft.icon', true],
+      ['robots.txt', 'text/plain', true],
+      ['workbox-*.js', 'application/javascript', true],
+      ['workbox-*.js.map', 'application/json', true],
+
+      // Not cached
+      ['manifest.webmanifest', 'application/manifest+json', false],
+      ['service-worker.js', 'application/javascript', false],
+      ['service-worker.js.map', 'application/json', false],
+      ['index.html', 'text/html', false],
+    ];
+    for (const uploadPattern of uploadPatterns) {
+      await uploadFolderToS3({
+        rootDir: tmpDir,
+        bucketName: appBucket.PhysicalResourceId as string,
+        fileNamePattern: uploadPattern[0],
+        contentType: uploadPattern[1],
+        cached: uploadPattern[2],
+      });
+    }
+
+    // Create a CloudFront invalidation to clear any cached resources
+    if (details.appDistribution?.PhysicalResourceId) {
+      await createInvalidation(details.appDistribution.PhysicalResourceId);
+    }
+
+    console.log('Done');
+  });
+
 async function listStacks(): Promise<(StackSummary & { StackName: string })[]> {
   const listResult = await client.send(new ListStacksCommand({}));
   return (
@@ -138,6 +223,11 @@ async function getStackDetails(stackName: string): Promise<MedplumStackDetails |
       resource.LogicalResourceId?.startsWith('StorageStorageBucket')
     ) {
       result.storageBucket = resource;
+    } else if (
+      resource.ResourceType === 'AWS::CloudFront::Distribution' &&
+      resource.LogicalResourceId?.startsWith('FrontEndAppDistribution')
+    ) {
+      result.appDistribution = resource;
     }
   }
 
@@ -157,4 +247,130 @@ function printStackDetails(details: MedplumStackDetails): void {
 
 function getEcsServiceName(resource: StackResource | undefined): string | undefined {
   return resource?.PhysicalResourceId?.split('/')?.pop() || '';
+}
+
+/**
+ * Returns NPM package metadata for a given package name.
+ * See: https://github.com/npm/registry/blob/master/docs/REGISTRY-API.md#getpackageversion
+ * @param packageName The npm package name.
+ */
+async function getNpmPackageMetadata(packageName: string, version: string): Promise<any> {
+  const url = `https://registry.npmjs.org/${packageName}/${version}`;
+  const response = await fetch(url);
+  return response.json();
+}
+
+/**
+ * Downloads and extracts an NPM package.
+ * @param packageName The NPM package name.
+ * @param version The NPM package version or "latest".
+ * @returns Path to temporary directory where the package was downloaded and extracted.
+ */
+async function downloadNpmPackage(packageName: string, version: string): Promise<string> {
+  const packageMetadata = await getNpmPackageMetadata(packageName, version);
+  const tarballUrl = packageMetadata.dist.tarball as string;
+  const tmpDir = mkdtempSync(join(tmpdir(), 'tarball-'));
+  try {
+    const response = await fetch(tarballUrl);
+    const extractor = tar.x({ cwd: tmpDir });
+    await pipelineAsync(response.body, extractor);
+    return join(tmpDir, 'package', 'dist');
+  } catch (error) {
+    rmSync(tmpDir, { recursive: true, force: true });
+    throw error;
+  }
+}
+
+/**
+ * Replaces variables in all JS files in the given folder.
+ * @param folderName The folder name of the files.
+ * @param replacements The collection of variable placeholders and replacements.
+ */
+function replaceVariables(folderName: string, replacements: Record<string, string>): void {
+  for (const item of readdirSync(folderName, { withFileTypes: true })) {
+    const itemPath = join(folderName, item.name);
+    if (item.isDirectory()) {
+      replaceVariables(itemPath, replacements);
+    } else if (item.isFile() && itemPath.endsWith('.js')) {
+      replaceVariablesInFile(itemPath, replacements);
+    }
+  }
+}
+
+/**
+ * Replaces variables in the JS file.
+ * @param fileName The file name.
+ * @param replacements The collection of variable placeholders and replacements.
+ */
+function replaceVariablesInFile(fileName: string, replacements: Record<string, string>): void {
+  let contents = readFileSync(fileName, 'utf-8');
+  for (const [placeholder, replacement] of Object.entries(replacements)) {
+    contents = contents.replaceAll(`process.env.${placeholder}`, `'${replacement}'`);
+  }
+  writeFileSync(fileName, contents);
+}
+
+/**
+ * Uploads a directory of files to S3.
+ * @param options The upload options such as bucket name, content type, and cache control.
+ */
+async function uploadFolderToS3(options: {
+  rootDir: string;
+  bucketName: string;
+  fileNamePattern: string;
+  contentType: string;
+  cached: boolean;
+}): Promise<void> {
+  const items = fastGlob.sync(options.fileNamePattern, { cwd: options.rootDir });
+  for (const item of items) {
+    await uploadFileToS3(join(options.rootDir, item), options);
+  }
+}
+
+/**
+ * Uploads a file to S3.
+ * @param filePath The file path.
+ * @param options The upload options such as bucket name, content type, and cache control.
+ */
+async function uploadFileToS3(
+  filePath: string,
+  options: {
+    rootDir: string;
+    bucketName: string;
+    contentType: string;
+    cached: boolean;
+  }
+): Promise<void> {
+  const fileStream = createReadStream(filePath);
+  const s3Key = filePath
+    .substring(options.rootDir.length + 1)
+    .split(sep)
+    .join('/');
+
+  const putObjectParams = {
+    Bucket: options.bucketName,
+    Key: s3Key,
+    Body: fileStream,
+    ContentType: options.contentType,
+    CacheControl: options.cached ? 'public, max-age=31536000' : 'no-cache, no-store, must-revalidate',
+  };
+
+  console.log(`Uploading ${s3Key} to ${options.bucketName}...`);
+  await s3Client.send(new PutObjectCommand(putObjectParams));
+}
+
+async function createInvalidation(distributionId: string): Promise<void> {
+  const params = {
+    DistributionId: distributionId,
+    InvalidationBatch: {
+      CallerReference: `invalidate-all-${Date.now()}`,
+      Paths: {
+        Quantity: 1,
+        Items: ['/*'],
+      },
+    },
+  };
+
+  const response = await cloudFrontClient.send(new CreateInvalidationCommand(params));
+  console.log(`Created invalidation with ID: ${response.Invalidation?.Id}`);
 }
