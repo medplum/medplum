86ea32aa3457de58e701326aa60d8d09a96e4377
Cody Ebberson <cody@ebberson.com>
2023-05-01 19:15:44 -0700

    Fixes #1958 - add Tasks to timelines

--- packages/react/src/PatientTimeline/PatientTimeline.tsx
+++ packages/react/src/PatientTimeline/PatientTimeline.tsx
@@ -8,15 +8,18 @@ export interface PatientTimelineProps {
 }
 
 export function PatientTimeline(props: PatientTimelineProps): JSX.Element {
-  const loadTimelineResources = useCallback((medplum: MedplumClient, _resourceType: ResourceType, id: string) => {
+  const loadTimelineResources = useCallback((medplum: MedplumClient, resourceType: ResourceType, id: string) => {
+    const ref = `${resourceType}/${id}`;
+    const _count = 100;
     return Promise.allSettled([
       medplum.readHistory('Patient', id),
-      medplum.search('Communication', 'subject=Patient/' + id),
-      medplum.search('Device', 'patient=Patient/' + id),
-      medplum.search('DeviceRequest', 'patient=Patient/' + id),
-      medplum.search('DiagnosticReport', 'subject=Patient/' + id),
-      medplum.search('Media', 'subject=Patient/' + id),
-      medplum.search('ServiceRequest', 'subject=Patient/' + id),
+      medplum.search('Communication', { subject: ref, _count }),
+      medplum.search('Device', { patient: ref, _count }),
+      medplum.search('DeviceRequest', { patient: ref, _count }),
+      medplum.search('DiagnosticReport', { subject: ref, _count }),
+      medplum.search('Media', { subject: ref, _count }),
+      medplum.search('ServiceRequest', { subject: ref, _count }),
+      medplum.search('Task', { subject: ref, _count }),
     ]);
   }, []);
 
