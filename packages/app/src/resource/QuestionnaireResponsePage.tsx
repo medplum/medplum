4a55e0a89d5a7644120908adaec33e5f8c630e19
TONY LEE <tonylee80@gmail.com>
2023-05-02 00:20:47 -0700

    remove unused variables

--- packages/app/src/resource/QuestionnaireResponsePage.tsx
+++ packages/app/src/resource/QuestionnaireResponsePage.tsx
@@ -5,18 +5,13 @@ import React, { useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
 export function QuestionnaireResponsePage(): JSX.Element | null {
-  const { resourceType, id } = useParams() as { resourceType: ResourceType; id: string };
-  const resource = useResource({ reference: resourceType + '/' + id });
+  const { id } = useParams() as { resourceType: ResourceType; id: string };
   const navigate = useNavigate();
   const [search, setSearch] = useState<SearchRequest>({
     resourceType: 'QuestionnaireResponse',
     filters: [{ code: 'questionnaire', operator: Operator.EQUALS, value: 'Questionnaire/' + id }],
     fields: ['id', '_lastUpdated'],
   });
-
-  if (!resource) {
-    return null;
-  }
 
   return (
     <Document>
