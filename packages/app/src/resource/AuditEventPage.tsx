7b85300903277df06abd26fea8331c995725efa5
Tony Lee <tonylee80@gmail.com>
2023-05-10 15:01:11 -0700

    Fixes 1947: Add a tab for Audit Events for a resource (#1993)

---
+++ packages/app/src/resource/AuditEventPage.tsx
@@ -0,0 +1,28 @@
+import { Operator, SearchRequest } from '@medplum/core';
+import { ResourceType } from '@medplum/fhirtypes';
+import { Document, MemoizedSearchControl } from '@medplum/react';
+import React, { useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+
+export function AuditEventPage(): JSX.Element | null {
+  const { resourceType, id } = useParams() as { resourceType: ResourceType; id: string };
+  const navigate = useNavigate();
+  const [search, setSearch] = useState<SearchRequest>({
+    resourceType: 'AuditEvent',
+    filters: [{ code: 'entity', operator: Operator.EQUALS, value: `${resourceType}/${id}` }],
+    fields: ['id', 'outcome', 'outcomeDesc', '_lastUpdated'],
+    sortRules: [{ code: '-_lastUpdated' }],
+    count: 20,
+  });
+
+  return (
+    <Document>
+      <MemoizedSearchControl
+        search={search}
+        onClick={(e) => navigate(`/${e.resource.resourceType}/${e.resource.id}`)}
+        onChange={(e) => setSearch(e.definition)}
+        hideFilters
+      />
+    </Document>
+  );
+}
