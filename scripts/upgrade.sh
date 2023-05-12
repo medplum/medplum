4092693ade232476b2153b1faffd61b656ed93c2
James Touri <j_touri@yahoo.com>
2023-05-12 16:16:36 -0700

    address suggestions

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -2,8 +2,8 @@
 
 npx npm-check-updates -u --packageFile package.json
 
-for dir in `ls packages`; do
-  if test -f "packages/$dir/package.json"; then
-    npx npm-check-updates -u --packageFile "packages/$dir/package.json"
+for dir in `ls examples`; do
+  if test -f "examples/$dir/package.json"; then
+    npx npm-check-updates -u --packageFile "examples/$dir/package.json"
   fi
 done
