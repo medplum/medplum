1f726400595c08c3679940de845dbc7d5aae254a
James Touri <j_touri@yahoo.com>
2023-05-12 16:36:54 -0700

    Uprade

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -2,6 +2,12 @@
 
 npx npm-check-updates -u --packageFile package.json
 
+for dir in `ls packages`; do
+  if test -f "packages/$dir/package.json"; then
+    npx npm-check-updates -u --packageFile "packages/$dir/package.json"
+  fi
+done
+
 for dir in `ls examples`; do
   if test -f "examples/$dir/package.json"; then
     npx npm-check-updates -u --packageFile "examples/$dir/package.json"
