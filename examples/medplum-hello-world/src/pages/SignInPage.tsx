3dc5c4e70d65b20ee746d2f4b95c6fcc20c753d3
James Touri <j_touri@yahoo.com>
2023-05-17 09:19:23 -0700

    Change comment

--- examples/medplum-hello-world/src/pages/SignInPage.tsx
+++ examples/medplum-hello-world/src/pages/SignInPage.tsx
@@ -3,11 +3,11 @@ import { Logo, SignInForm } from '@medplum/react';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-// Login page
 export function SignInPage(): JSX.Element {
   const navigate = useNavigate();
   return (
     <SignInForm
+      // Configure according to your settings
       googleClientId="921088377005-3j1sa10vr6hj86jgmdfh2l53v3mp7lfi.apps.googleusercontent.com"
       onSuccess={() => navigate('/')}
     >
