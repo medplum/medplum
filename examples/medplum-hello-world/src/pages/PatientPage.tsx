11aa58f0eb9353d4bb53bf674eb996602c6e8a7c
James Touri <j_touri@yahoo.com>
2023-05-22 15:08:42 -0700

    Link Component

--- examples/medplum-hello-world/src/pages/PatientPage.tsx
+++ examples/medplum-hello-world/src/pages/PatientPage.tsx
@@ -1,18 +1,11 @@
 import { Loader, Tabs } from '@mantine/core';
-import { formatDate, getReferenceString } from '@medplum/core';
+import { getReferenceString } from '@medplum/core';
 import { DiagnosticReport, Patient, ServiceRequest } from '@medplum/fhirtypes';
-import {
-  AddressDisplay,
-  ContactPointDisplay,
-  Document,
-  PatientTimeline,
-  ResourceHistoryTable,
-  ResourceName,
-  useMedplum,
-} from '@medplum/react';
+import { useMedplum } from '@medplum/react';
 import React, { useContext, useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Outlet, useParams } from 'react-router-dom';
 import { PatientHeader } from './PatientHeader';
+import { Link } from 'react-router-dom';
 
 interface PatientGraphQLResponse {
   data: {
@@ -27,7 +20,7 @@ const PatientContext = React.createContext({} as PatientGraphQLResponse);
 export function PatientPage(): JSX.Element {
   const medplum = useMedplum();
   const { id } = useParams();
-  const [tab, setTab] = useState<string | null>('overview');
+  const [tab, setTab] = useState<string | null>('');
 
   const [response, setResponse] = useState<PatientGraphQLResponse>();
 
@@ -88,97 +81,18 @@ export function PatientPage(): JSX.Element {
     return <Loader />;
   }
 
-  const { patient, orders, reports } = response.data;
-
+  const { patient } = response.data;
   return (
     <PatientContext.Provider value={response}>
       <PatientHeader patient={patient} key={getReferenceString(patient)} />
-      {/* Use the Mantine Tabs components to implement a simple tabbed layout */}
       <Tabs value={tab} onTabChange={setTab}>
         <Tabs.List bg="white">
-          <Tabs.Tab value="overview">Overview</Tabs.Tab>
-          <Tabs.Tab value="timeline">Timeline</Tabs.Tab>
-          <Tabs.Tab value="history">History</Tabs.Tab>
+          <TabLink value="overview" />
+          <TabLink value="timeline" />
+          <TabLink value="history" />
         </Tabs.List>
-        <Tabs.Panel value="overview">
-          {/*
-           * You can combine Medplum components with plain HTML to quickly display patient data.
-           * Medplum has out of the box components to render common data types such as
-           *   - Addresses
-           *   - Phone numbers
-           *   - Patient/Provider names
-           *   - Patient/Provider profile photo
-           * */}
-          <Document>
-            <div className="patient-sidebar">
-              <div className="patient-title">
-                <ResourceName value={patient} />
-              </div>
-              <h3>Birth Date</h3>
-              <div>{patient.birthDate}</div>
-              <h3>Address</h3>
-              {patient.address?.map((a, i) => (
-                <div key={`address-${i}`}>
-                  <AddressDisplay value={a} />
-                </div>
-              ))}
-              <h3>Contact</h3>
-              {patient.telecom?.map((t, i) => (
-                <div key={`contact-${i}`}>
-                  <ContactPointDisplay value={t} />
-                </div>
-              ))}
-            </div>
-            <div className="patient-demographics">
-              <h3>Demographics</h3>
-              <div>Created Date: {formatDate(patient.meta?.lastUpdated)}</div>
-              <table>
-                <tbody>
-                  <tr>
-                    <td>Prefix: {patient?.name?.[0]?.prefix}</td>
-                    <td>First: {patient?.name?.[0]?.given?.[0]}</td>
-                    <td>Middle: {patient?.name?.[0]?.given?.[1]}</td>
-                    <td>Last: {patient?.name?.[0]?.family}</td>
-                    <td>Suffix: {patient?.name?.[0]?.suffix}</td>
-                  </tr>
-                </tbody>
-              </table>
-              <h3>Orders ({orders?.length})</h3>
-              <ul>
-                {orders?.map((o, i) => (
-                  <li key={`order-${i}`}>
-                    <a href={`/ServiceRequest/${o.id}`}>{o.code?.text}</a> ({formatDate(o.meta?.lastUpdated)})
-                  </li>
-                ))}
-              </ul>
-              <h3>Reports ({reports?.length})</h3>
-              <ul>
-                {reports?.map((o, i) => (
-                  <li key={`report-${i}`}>
-                    <a href={`/DiagnosticReport/${o.id}`}>
-                      {o.code?.text} ({formatDate(o.meta?.lastUpdated)})
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </Document>
-        </Tabs.Panel>
-        {/*
-         * The PatientTimeline component displays relevant events related to the patient
-         */}
-        <Tabs.Panel value="timeline">
-          <PatientTimeline patient={patient} />
-        </Tabs.Panel>
-        {/*
-         * The ResourceHistoryTable allows you to audit all the changes that have been made to the Patient resource
-         */}
-        <Tabs.Panel value="history">
-          <Document>
-            <ResourceHistoryTable resourceType="Patient" id={patient.id} />
-          </Document>
-        </Tabs.Panel>
       </Tabs>
+      <Outlet />
     </PatientContext.Provider>
   );
 }
@@ -189,4 +103,13 @@ export function usePatientInfo() {
     throw new Error('Missing PatientContext');
   }
   return context;
+}
+
+function TabLink(props: { value: string }): JSX.Element {
+  const capitalizedFirstLetterValue = props.value.charAt(0).toUpperCase() + props.value.slice(1);
+  return (
+    <Link to={props.value} style={{ textDecoration: 'none' }}>
+      <Tabs.Tab value={props.value}>{capitalizedFirstLetterValue}</Tabs.Tab>
+    </Link>
+  );
 }
