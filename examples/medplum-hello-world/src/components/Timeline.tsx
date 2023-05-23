4d2d48844af5e4a456bb481e368c36306f6c8d0f
Cody Ebberson <cody@ebberson.com>
2023-05-23 16:34:34 -0700

    Hello world suggestions (#2098)

--- examples/medplum-hello-world/src/components/Timeline.tsx
+++ examples/medplum-hello-world/src/components/Timeline.tsx
@@ -1,19 +1,11 @@
-import { usePatientInfo } from '../pages/PatientPage';
 import { PatientTimeline } from '@medplum/react';
 import React from 'react';
-import { Document } from '@medplum/react';
+import { useParams } from 'react-router-dom';
 
 /*
  * The PatientTimeline component displays relevant events related to the patient
  */
 export function Timeline(): JSX.Element {
-  const { patient } = usePatientInfo().data;
-  return (
-    <>
-      <Document>
-        <h3>Timeline</h3>
-      </Document>
-      <PatientTimeline patient={patient} />
-    </>
-  );
+  const { id } = useParams();
+  return <PatientTimeline patient={{ reference: `Patient/${id}` }} />;
 }
