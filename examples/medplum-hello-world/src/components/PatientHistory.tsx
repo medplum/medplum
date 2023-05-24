2cab1cbc5e7c2a36db0149675ce9a4eee5d990c3
James Touri <j_touri@yahoo.com>
2023-05-24 08:58:02 -0700

    Add Outlet components to ResourcePage in the examples/medplum-hello-world (#2096)
    * Add Outlet to Tab change in Hello World
    
    * Link Component
    
    * Add title to each page
    
    * Hello world suggestions (#2098)
    
    ---------
    
    Co-authored-by: Cody Ebberson <cody@ebberson.com>

---
+++ examples/medplum-hello-world/src/components/PatientHistory.tsx
@@ -0,0 +1,16 @@
+import { Title } from '@mantine/core';
+import { Document, ResourceHistoryTable } from '@medplum/react';
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+export function PatientHistory(): JSX.Element {
+  const { id } = useParams();
+  return (
+    <Document>
+      <Title order={3} mb="xl">
+        Patient History
+      </Title>
+      <ResourceHistoryTable resourceType="Patient" id={id} />
+    </Document>
+  );
+}
