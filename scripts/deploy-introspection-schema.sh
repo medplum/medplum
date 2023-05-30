66065681ee43bd5912b578de59f7ead9ccad0d73
James Touri <j_touri@yahoo.com>
2023-05-30 14:46:00 -0700

    Add script

--- scripts/deploy-introspection-schema.sh
+++ scripts/deploy-introspection-schema.sh
@@ -1,5 +1,14 @@
-aws s3 cp dist/ "s3://graphiq.medplum.com/" \
-  --content-type "text/plain" \
-  --cache-control "public, max-age=31536000" \
-  --include "../introspection-schema.json" \ 
+# Taking a compressed introspection schema file and uploading it to S3
+# This is specifically for s3://graphiq.medplum.com/schema/
+
+curl 'http://localhost:8103/fhir/R4/$graphql' \
+  -u f3efaacc-2eb4-4d04-8a7f-edf907c18f72:ace9325e9a0c03a3b999f0713bb7aaf984d4223ad9ab2ac46cdeeefd91f861fe \
+  -H 'Content-Type: application/json' \
+  --data-raw '{"query":"\n    query IntrospectionQuery {\n      __schema {\n        \n        queryType { name }\n        mutationType { name }\n        subscriptionType { name }\n        types {\n          ...FullType\n        }\n        directives {\n          name\n          description\n          \n          locations\n          args {\n            ...InputValue\n          }\n        }\n      }\n    }\n\n    fragment FullType on __Type {\n      kind\n      name\n      description\n      \n      fields(includeDeprecated: true) {\n        name\n        description\n        args {\n          ...InputValue\n        }\n        type {\n          ...TypeRef\n        }\n        isDeprecated\n        deprecationReason\n      }\n      inputFields {\n        ...InputValue\n      }\n      interfaces {\n        ...TypeRef\n      }\n      enumValues(includeDeprecated: true) {\n        name\n        description\n        isDeprecated\n        deprecationReason\n      }\n      possibleTypes {\n        ...TypeRef\n      }\n    }\n\n    fragment InputValue on __InputValue {\n      name\n      description\n      type { ...TypeRef }\n      defaultValue\n      \n      \n    }\n\n    fragment TypeRef on __Type {\n      kind\n      name\n      ofType {\n        kind\n        name\n        ofType {\n          kind\n          name\n          ofType {\n            kind\n            name\n            ofType {\n              kind\n              name\n              ofType {\n                kind\n                name\n                ofType {\n                  kind\n                  name\n                  ofType {\n                    kind\n                    name\n                  }\n                }\n              }\n            }\n          }\n        }\n      }\n    }\n  ","operationName":"IntrospectionQuery"}' \
+  > schema-v4.json
+
+gzip schema-v4.json
+
+aws s3 cp ../schema-v4.json "s3://graphiq.medplum.com/schema/" \
+  --content-type "application/json" \
   --content-encoding "gzip"
