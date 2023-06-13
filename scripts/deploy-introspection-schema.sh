32e37791a457bd3fd60592b13fea04b4167a70e7
James Touri <j_touri@yahoo.com>
2023-06-13 13:07:43 -0700

    Remove the aws-cors

--- scripts/deploy-introspection-schema.sh
+++ scripts/deploy-introspection-schema.sh
@@ -4,19 +4,14 @@
 version=$1
 client_id=$2
 client_secret=$3
-cors_file=$4
 
 curl 'http://localhost:8103/fhir/R4/$graphql' \
   -u $client_id:$client_secret \
   -H 'Content-Type: application/json' \
-  --data-raw '{"query":"    query IntrospectionQuery {      __schema {                queryType { name }        mutationType { name }        subscriptionType { name }        types {          ...FullType        }        directives {          name          description                    locations          args {            ...InputValue          }        }      }    }    fragment FullType on __Type {      kind      name      description            fields(includeDeprecated: true) {        name        description        args {          ...InputValue        }        type {          ...TypeRef        }        isDeprecated        deprecationReason      }      inputFields {        ...InputValue      }      interfaces {        ...TypeRef      }      enumValues(includeDeprecated: true) {        name        description        isDeprecated        deprecationReason      }      possibleTypes {        ...TypeRef      }    }    fragment InputValue on __InputValue {      name      description      type { ...TypeRef }      defaultValue                }    fragment TypeRef on __Type {      kind      name      ofType {        kind        name        ofType {          kind          name          ofType {            kind            name            ofType {              kind              name              ofType {                kind                name                ofType {                  kind                  name                  ofType {                    kind                    name                  }                }              }            }          }        }      }    }  ","operationName":"IntrospectionQuery"}' \
+  --data-raw '{"query":" query IntrospectionQuery { __schema { queryType { name } mutationType { name } subscriptionType { name } types { ...FullType } directives { name description locations args { ...InputValue } } } } fragment FullType on __Type { kind name description fields(includeDeprecated: true) { name description args { ...InputValue } type { ...TypeRef } isDeprecated deprecationReason } inputFields { ...InputValue } interfaces { ...TypeRef } enumValues(includeDeprecated: true) { name description isDeprecated deprecationReason } possibleTypes { ...TypeRef } } fragment InputValue on __InputValue { name description type { ...TypeRef } defaultValue } fragment TypeRef on __Type { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name ofType { kind name } } } } } } } } ","operationName":"IntrospectionQuery"}' \
   > schema-$version.json
 
 gzip schema-$version.json
-
-aws s3api put-bucket-cors \
-  --bucket graphiq.medplum.com \
-  --cors-configuration $cors_file
 
 aws s3 cp ../schema-$version.json.gz "s3://graphiq.medplum.com/schema/" \
   --content-type "application/json" \
