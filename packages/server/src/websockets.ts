485f68f5d36caaa67743726c20ad427090192d90
Cody Ebberson <cody@ebberson.com>
2023-07-02 17:46:30 -0700

    Use redis for pub sub

--- packages/server/src/websockets.ts
+++ packages/server/src/websockets.ts
@@ -1,6 +1,8 @@
+import { randomUUID } from 'crypto';
 import http from 'http';
 import ws from 'ws';
 import { logger } from './logger';
+import { getRedis } from './redis';
 
 let wsServer: ws.Server | undefined = undefined;
 
@@ -11,21 +13,38 @@ let wsServer: ws.Server | undefined = undefined;
 export function initWebSockets(server: http.Server): void {
   wsServer = new ws.Server({ noServer: true });
 
-  wsServer.on('connection', (socket: ws.WebSocket) => {
+  wsServer.on('connection', async (socket: ws.WebSocket) => {
     logger.debug('[WS] connection');
 
-    socket.on('message', (data: ws.RawData, binary: boolean) => {
-      logger.debug('[WS] message', data, binary);
-      socket.send(data, { binary });
+    const redis = getRedis();
+    const channel = randomUUID();
+
+    redis.on('message', (channel: string, message: string) => {
+      logger.debug('[WS] redis message', channel, message);
+      socket.send(message, { binary: false });
     });
 
-    socket.on('error', (err: Error) => {
+    redis.on('messageBuffer', (channel: string, message: Buffer) => {
+      logger.debug('[WS] redis messageBuffer', channel, message);
+      socket.send(message, { binary: true });
+    });
+
+    await redis.subscribe(channel);
+
+    socket.on('message', async (data: ws.RawData, binary: boolean) => {
+      logger.debug('[WS] message', data, binary);
+      await redis.publish(channel, rawDataToBuffer(data));
+    });
+
+    socket.on('error', async (err: Error) => {
       logger.debug('[WS] error', err);
+      await redis.unsubscribe(channel);
       socket.close();
     });
 
-    socket.on('close', () => {
+    socket.on('close', async () => {
       logger.debug('[WS] close');
+      await redis.unsubscribe(channel);
     });
   });
 
@@ -45,4 +64,15 @@ export function closeWebSockets(): void {
     wsServer.close();
     wsServer = undefined;
   }
+}
+
+function rawDataToBuffer(data: ws.RawData): Buffer {
+  if (Buffer.isBuffer(data)) {
+    return data;
+  }
+  if (data instanceof ArrayBuffer) {
+    return Buffer.from(data);
+  }
+  // Otherwise data is Buffer[]
+  return Buffer.concat(data);
 }
