9ee14a991145afb80a772452d89234711ee9c902
James Touri <j_touri@yahoo.com>
2023-07-07 14:41:56 -0700

    Skip tests

--- examples/medplum-demo-bots/src/examples/sftp-upload.test.ts
+++ examples/medplum-demo-bots/src/examples/sftp-upload.test.ts
@@ -1,6 +1,8 @@
 import { expect, test } from 'vitest';
 import { handler } from './sftp-upload';
 
+vi.mock('ssh2-sftp-client');
+
 test('Hello SFTP', async () => {
   const result = await handler();
   expect(result).toBeDefined();
