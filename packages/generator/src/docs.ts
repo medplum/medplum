72e2cb4ca65e67be4ad610776785f9323dc31ae3
rahul1 <rahul@medplum.com>
2023-07-17 15:10:33 -0700

    Adding FHIR Spec Intro Text (#2363)
    * First checkpoint of working parser
    Usage, Background, Relationships from fhir spec HTML
    
    * Handling tables inside descriptions
    
    * Better handling of html content in description
    
    Better handling of links and comments
    
    * Stripping style tags
    
    WIP removing style tags
    
    * Fixed broken links
    
    * Some improvements on tabs
    
    Hide tabs without content
    
    * Initial commit of docusaurus files
    
    * remove dead code
    
    * Commenbts
    
    * Reformatting to a "tabs maximalist" approach
    
    * renamed query param
    
    * package-lock update
    
    * Minor formatting chagnes on output
    
    * Adding custom description text for ONC compliance
    
    * re-run generator
    
    * Add description text for ONC compliance

--- packages/generator/src/docs.ts
+++ packages/generator/src/docs.ts
@@ -1,8 +1,14 @@
 import { getExpressionForResourceType, isLowerCase } from '@medplum/core';
 import { readJson } from '@medplum/definitions';
 import { Bundle, BundleEntry, ElementDefinition, SearchParameter, StructureDefinition } from '@medplum/fhirtypes';
-import { writeFileSync } from 'fs';
+import fs, { writeFileSync } from 'fs';
+import { JSDOM, DOMWindow } from 'jsdom';
+import * as mkdirp from 'mkdirp';
+import fetch from 'node-fetch';
+import * as path from 'path';
 import { resolve } from 'path/posix';
+import * as unzipper from 'unzipper';
+
 import {
   DocumentationLocation,
   PropertyDocInfo,
@@ -23,8 +29,15 @@ for (const entry of readJson('fhir/r4/search-parameters-medplum.json').entry as 
 }
 
 let documentedTypes: Record<string, DocumentationLocation>;
+// a map from lowercase resource names (e.g. appointmentresponse) to camelcase names (e.g. AppointmentResponse)
+let lowerCaseResourceNames: Record<string, string>;
 
-export function main(): void {
+export async function main(): Promise<void> {
+  const outputFolder = path.resolve(__dirname, '..', 'output');
+  if (!fs.existsSync(outputFolder)) {
+    fs.mkdirSync(outputFolder);
+  }
+
   const indexedSearchParams = indexSearchParameters(searchParams);
   // Definitions for FHIR Spec resources
   const fhirCoreDefinitions = filterDefinitions(readJson(`fhir/r4/profiles-resources.json`));
@@ -32,7 +45,6 @@ export function main(): void {
   const medplumResourceDefinitions = filterDefinitions(readJson(`fhir/r4/profiles-medplum.json`));
   // StructureDefinitions for FHIR "Datatypes" (e.g. Address, ContactPoint, Identifier...)
   const fhirDatatypes = filterDefinitions(readJson(`fhir/r4/profiles-types.json`));
-
   // Map from resource/datatype name -> documented location
   documentedTypes = {
     ...Object.fromEntries(
@@ -44,16 +56,21 @@ export function main(): void {
     ),
   };
 
+  lowerCaseResourceNames = Object.fromEntries(Object.keys(documentedTypes).map((e) => [e.toLowerCase(), e]));
+
   const fhirResourceDocs = buildDocsDefinitions(fhirCoreDefinitions, 'resource', indexedSearchParams);
   const medplumResourceDocs = buildDocsDefinitions(medplumResourceDefinitions, 'medplum', indexedSearchParams);
   const fhirDatatypeDocs = buildDocsDefinitions(fhirDatatypes, 'datatype');
-  writeDocs(fhirResourceDocs, 'resource');
+
+  const resourceIntroductions = await fetchHtmlSpecContent(fhirCoreDefinitions);
+
+  writeDocs(fhirResourceDocs, 'resource', resourceIntroductions);
   writeDocs(fhirDatatypeDocs, 'datatype');
   writeDocs(medplumResourceDocs, 'medplum');
 }
 
 /**
- * Indexes searcch parameters by "base" resource type.
+ * Indexes search parameters by "base" resource type.
  * @param searchParams The bundle of SearchParameter resources.
  * @returns A map from resourceType -> an array of associated SearchParameters
  */
@@ -133,15 +150,17 @@ function buildDocsDefinition(
   return result;
 }
 
-function buildDocsMarkdown(position: number, definition: ResourceDocsProps): string {
+function buildDocsMarkdown(position: number, definition: ResourceDocsProps, resourceIntroduction?: any): string {
   const resourceName = definition.name;
-  const description = rewriteLinks(definition.description);
+  const description = rewriteLinksText(definition.description);
+
   return `\
 ---
 title: ${resourceName}
 sidebar_position: ${position}
 ---
-
+import Tabs from '@theme/Tabs';
+import TabItem from '@theme/TabItem';
 import definition from '@site/static/data/${definition.location}Definitions/${resourceName.toLowerCase()}.json';
 import { ResourcePropertiesTable, SearchParamsTable } from '@site/src/components/ResourceTables';
 
@@ -149,7 +168,12 @@ import { ResourcePropertiesTable, SearchParamsTable } from '@site/src/components
 
 ${description}
 
+<Tabs queryString="section">
+${`<TabItem value="schema" label="Schema" default>
+
+
 ## Properties
+
 
 <ResourcePropertiesTable properties={definition.properties.filter((p) => !(p.inherited && p.base.includes('Resource')))} />
 
@@ -165,22 +189,58 @@ ${
 `
     : ''
 }
+</TabItem>
+  ${
+    resourceIntroduction?.scopeAndUsage
+      ? `<TabItem value="usage" label="Usage">
+    ${rewriteLinksText(resourceIntroduction.scopeAndUsage)}
+    </TabItem>`
+      : ''
+  }
+  ${
+    resourceIntroduction?.boundariesAndRelationships
+      ? `<TabItem value="relationships" label="Relationships">
+    ${rewriteLinksText(resourceIntroduction.boundariesAndRelationships)}
+    </TabItem>`
+      : ''
+  }
+  ${
+    resourceIntroduction?.backgroundAndContext
+      ? `<TabItem value="backgroundAndContext" label="Background and Context">
+    ${rewriteLinksText(resourceIntroduction.backgroundAndContext)}
+    </TabItem>`
+      : ''
+  }
+  ${
+    resourceIntroduction?.referencedBy?.length > 0
+      ? `<TabItem value="referencedBy" label="Referenced By">
+    <ul>${resourceIntroduction.referencedBy.map((e: string) => `<li>${e}</li>`).join('\n')}</ul>
+    </TabItem>`
+      : ''
+  }`}
 
+</Tabs>
 `;
 }
 
-function writeDocs(definitions: ResourceDocsProps[], location: DocumentationLocation): void {
+function writeDocs(
+  definitions: ResourceDocsProps[],
+  location: DocumentationLocation,
+  resourceIntroductions?: Record<string, any>
+): void {
+  console.info('Writing JS and Markdown files...');
   definitions.forEach((definition, i) => {
-    const resourceName = definition.name.toLowerCase();
+    const resourceType = definition.name;
+    printProgress(Math.round((i / definitions.length) * 100));
     writeFileSync(
-      resolve(__dirname, `../../docs/static/data/${location}Definitions/${resourceName}.json`),
+      resolve(__dirname, `../../docs/static/data/${location}Definitions/${resourceType.toLowerCase()}.json`),
       JSON.stringify(definition, null, 2),
-      // JSON.stringify(definition),
       'utf8'
     );
+
     writeFileSync(
-      resolve(__dirname, `../../docs/docs/api/fhir/${pluralize(location)}/${resourceName}.mdx`),
-      buildDocsMarkdown(i, definition),
+      resolve(__dirname, `../../docs${getMedplumDocsPath(resourceType)}.mdx`),
+      buildDocsMarkdown(i, definition, resourceIntroductions?.[resourceType]),
       'utf8'
     );
   });
@@ -256,39 +316,289 @@ function getInheritance(property: ElementDefinition): { inherited: boolean; base
   return { inherited, base: inheritanceBase };
 }
 
-function rewriteLinks(description: string): string {
-  description = description
-    .replace('(operations.html)', '(/api/fhir/operations)')
+/**
+ * Rewrite internal links from official FHIR site to medplum internal links
+ * @param text text which contains internal links
+ * @returns returns text with internal links rewritten
+ */
+function rewriteLinksText(text: string | undefined): string {
+  if (!text) {
+    return '';
+  }
+  // Replace relative links in StructureDefinition.description with absolute links
+  text = text
+    .replace('(operations.html)', '(/docs/api/fhir/operations)')
     .replace('(terminologies.html)', '(https://www.hl7.org/fhir/terminologies.html)');
 
   // Replace datatype internal links
   const datatypeLinkPattern = /datatypes.html#([a-zA-Z-]+)/g;
-  const dtMatches = description.matchAll(datatypeLinkPattern);
+  const dtMatches = text.matchAll(datatypeLinkPattern);
 
   for (const match of dtMatches) {
     if (match[1] in documentedTypes) {
-      description = description.replace(match[0], `/api/fhir/datatypes/${match[1].toLowerCase()}`);
+      text = text.replace(match[0], `/api/fhir/datatypes/${match[1].toLowerCase()}`);
     } else {
-      description = description.replace(match[0], `https://www.hl7.org/fhir/datatypes.html#${match[1]}`);
+      text = text.replace(match[0], `https://www.hl7.org/fhir/datatypes.html#${match[1]}`);
     }
   }
 
   // Replace all the links of [[[Type]]] with internal links
-  const typeLinks = Array.from(description.matchAll(/\[\[\[([A-Z][a-z]*)*\]\]\]/gi));
+  const typeLinks = Array.from(text.matchAll(/\[\[\[([A-Z][a-z]*)*\]\]\]/gi));
   for (const match of typeLinks) {
-    description = description.replace(match[0], `[${match[1]}](./${match[1].toLowerCase()})`);
+    text = text.replace(match[0], `[${match[1]}](./${match[1].toLowerCase()})`);
   }
 
-  return description;
+  // Replace names of all Resources/datatypes with internal links
+  const documentedTypeNames = Object.keys(documentedTypes);
+  const resourceTypeExp = new RegExp(`\\s((${documentedTypeNames.join('|')})[s]?)\\b`, 'g');
+  text = text.replace(
+    resourceTypeExp,
+    (_, resourceText, resourceName) => ` <a href="${getMedplumDocsPath(resourceName)}">${resourceText}</a>`
+  );
+
+  return text;
 }
 
-function pluralize(location: DocumentationLocation): string {
-  if (location !== 'medplum' && location.endsWith('e')) {
-    return `${location}s`;
+/**
+ * Download a Zip file from the given URL, and unzip to the given path
+ * @param downloadURL Source URL
+ * @param zipFilePath Destination path for *.zip
+ * @param outputFolder Destination path for extracted contents
+ */
+async function downloadAndUnzip(downloadURL: string, zipFilePath: string, outputFolder: string): Promise<void> {
+  console.info('Downloading FHIR Spec...');
+  return new Promise((resolve, reject) => {
+    fetch(downloadURL)
+      .then((response) => {
+        if (!response.ok) {
+          reject(new Error(`Error downloading file: ${response.status} ${response.statusText}`));
+          return;
+        }
+
+        const fileStream = fs.createWriteStream(zipFilePath);
+        response.body.pipe(fileStream);
+
+        // Inside your 'downloadAndUnzip' function, replace the extraction part with this:
+        fileStream.on('finish', async () => {
+          fs.createReadStream(zipFilePath)
+            .pipe(unzipper.Parse())
+            .on('entry', function (entry) {
+              const fileName = entry.path;
+              const type = entry.type; // 'Directory' or 'File'
+              const fullPath = path.join(outputFolder, fileName).replaceAll('\\', '/');
+
+              if (type === 'Directory') {
+                mkdirp.sync(fullPath);
+                entry.autodrain();
+              } else {
+                mkdirp.sync(path.dirname(fullPath));
+                entry.pipe(fs.createWriteStream(fullPath));
+              }
+              console.info('\rDownloading FHIR Spec...');
+            })
+            .on('close', resolve)
+            .on('error', reject);
+        });
+      })
+      .catch(() => {
+        reject(new Error('Error downloading or unzipping file'));
+      });
+  });
+}
+/**
+ * For each core FHIR resource type, find the corresponding HTML page and extract the relevant introductory sections.
+ * @param htmlDirectory Directory containing HTML files for each resource ([resourceType].html)
+ * @param definitions Array of all core FHIR StructureDefinitions
+ * @returns A map from resourcename to html description data
+ */
+function extractResourceDescriptions(
+  htmlDirectory: string,
+  definitions: StructureDefinition[]
+): Record<string, Record<string, string | string[] | undefined>> {
+  const results: Record<string, Record<string, string | string[] | undefined>> = {};
+
+  console.info('Extracting HTML descriptions...');
+  for (let i = 0; i < definitions.length; i++) {
+    printProgress(Math.round((i / definitions.length) * 100));
+    const definition = definitions[i];
+    const resourceType = definition.name;
+    const fileName = path.resolve(htmlDirectory, `${resourceType?.toLowerCase()}.html`);
+    if (resourceType && fs.existsSync(fileName)) {
+      const fileContent = fs.readFileSync(fileName, 'utf-8');
+      const dom = new JSDOM(fileContent);
+      const document = dom.window.document;
+
+      const resourceContents: Record<string, string | string[] | undefined> = { referencedBy: [] };
+
+      // find the divs
+      const divs = document.getElementsByTagName('div');
+      for (const div of divs) {
+        const h2 = div.querySelector('h2');
+        if (h2) {
+          const h2Text = h2.textContent?.toLowerCase().replace(/\s/g, '') || '';
+
+          const paragraphHTML = sanitizeNodeContent(div, dom.window);
+
+          if (h2Text.includes('scopeandusage')) {
+            resourceContents.scopeAndUsage = paragraphHTML;
+          } else if (h2Text.includes('backgroundandcontext')) {
+            resourceContents.backgroundAndContext = paragraphHTML;
+          } else if (h2Text.includes('boundariesandrelationships')) {
+            resourceContents.boundariesAndRelationships = paragraphHTML;
+          }
+        }
+      }
+
+      // find referencedBy
+      const pElements = document.querySelectorAll('p');
+      for (const p of pElements) {
+        if (p.textContent?.trim().startsWith('This resource is referenced by')) {
+          const aElements = p.querySelectorAll('a');
+          resourceContents['referencedBy'] = Array.from(aElements).map((a) => {
+            rewriteHtmlSpecHref(a);
+            return a.outerHTML;
+          });
+        }
+      }
+      results[resourceType] = resourceContents;
+    }
   }
-  return location;
+
+  console.info('Done');
+
+  return results;
+}
+
+/**
+ * Converts local links to FHIR resources from the FHIR site to relative links in the medplum documentation site
+ * @param anchorElement An Anchor tag, potentially referring to a resource page (e.g. "appointmentresponse.html")
+ */
+function rewriteHtmlSpecHref(anchorElement: HTMLAnchorElement): void {
+  const href = anchorElement.getAttribute('href'); // Get the href attribute of the anchor tag
+
+  // Try to match the href to the expected format
+  const match = href?.match(/^(http[s]?:\/\/)?(\S+?)(\.html)?(#\w+)?$/);
+  if (match) {
+    const http = match[1];
+    const resourceType = match[2];
+    // If the URL is absolute, don't do anything
+    if (http || match[0]?.startsWith('/')) {
+      return;
+    }
+
+    // If it is a relative URL, check if it is a link to a resource page.
+    // If yes, rewrite to medplum docs URL
+    // If no, convert to an absolute URL
+    let newHref = match[0];
+    const resourceName = lowerCaseResourceNames[resourceType];
+    if (resourceName) {
+      // Update the href attribute of the anchor tag
+      newHref = getMedplumDocsPath(resourceName);
+    } else {
+      newHref = `https://www.hl7.org/fhir/${newHref}`;
+    }
+    anchorElement.setAttribute('href', newHref);
+  }
+}
+
+function sanitizeNodeContent(node: HTMLElement, window: DOMWindow): string {
+  // Clone the div to keep original div intact.
+  const clonedDiv = node.cloneNode(true) as HTMLElement;
+
+  // Recursive function to remove comment nodes and style attributes.
+  function removeUnwantedNodes(node: Node): void {
+    Array.from(node.childNodes).forEach((child: Node) => {
+      if (child.nodeType === 8) {
+        // Node.COMMENT_NODE
+        node.removeChild(child);
+      } else {
+        if (node instanceof window.Element) {
+          if (node.getAttribute('style')) {
+            node.removeAttribute('style');
+          }
+
+          if (node.tagName.toLowerCase() === 'a') {
+            rewriteHtmlSpecHref(node as HTMLAnchorElement);
+          }
+        }
+        removeUnwantedNodes(child);
+      }
+    });
+  }
+
+  // Remove img tags.
+  const imgElements = clonedDiv.getElementsByTagName('img');
+  for (const img of Array.from(imgElements)) {
+    img.parentNode?.removeChild(img);
+  }
+
+  // Remove svg tags.
+  const svgElements = clonedDiv.getElementsByTagName('svg');
+  for (const svg of Array.from(svgElements)) {
+    svg.parentNode?.removeChild(svg);
+  }
+
+  // Remove h2 tags.
+  const h2Elements = clonedDiv.getElementsByTagName('h2');
+  for (const h2 of Array.from(h2Elements)) {
+    h2.parentNode?.removeChild(h2);
+  }
+
+  // Remove span elements with class 'sectioncount'.
+  const spanElements = clonedDiv.querySelectorAll('span.sectioncount');
+  for (const span of Array.from(spanElements)) {
+    span.parentNode?.removeChild(span);
+  }
+
+  // Remove p elements containing the text "Trial-Use Note".
+  if (clonedDiv.nodeName.toLowerCase() === 'p' && clonedDiv.textContent?.includes('Trial-Use Note')) {
+    clonedDiv.parentNode?.removeChild(node);
+  }
+
+  // Remove comment nodes and style attributes.
+  removeUnwantedNodes(node);
+
+  // Replace br tags with closed ones.
+  return clonedDiv.outerHTML.replaceAll('<br>', '<br/>').replace(/[\n\t]/g, ' ');
+}
+
+/**
+ * Download the "entire fhir spec" as a zip file, and parse the HTML file for each resource to extract the detailed
+ * information about usage and scope for each resource
+ * @param definitions FHIR core profile definitions
+ * @returns Map from resource name to extracted HTML data
+ */
+async function fetchHtmlSpecContent(
+  definitions: StructureDefinition[]
+): Promise<Record<string, Record<string, string | string[] | undefined>>> {
+  const downloadURL = 'http://hl7.org/fhir/R4/fhir-spec.zip';
+  const zipFile = path.resolve(__dirname, '..', 'output', 'fhir-spec.zip');
+  const outputFolder = path.resolve(__dirname, '..', 'output', 'fhir-spec');
+  const siteDir = path.resolve(outputFolder, 'site');
+  if (!fs.existsSync(outputFolder)) {
+    return downloadAndUnzip(downloadURL, zipFile, outputFolder).then(() => {
+      return extractResourceDescriptions(siteDir, definitions);
+    });
+  } else {
+    const results = extractResourceDescriptions(siteDir, definitions);
+    return results;
+  }
+}
+
+function getMedplumDocsPath(typeName: string): string {
+  let location = documentedTypes[typeName] as string;
+  if (location !== 'medplum' && location.endsWith('e')) {
+    location = location + 's';
+  }
+  return `/docs/api/fhir/${location}/${typeName.toLowerCase()}`;
+}
+
+function printProgress(progress: number): void {
+  process.stdout.clearLine(0);
+  process.stdout.cursorTo(0);
+  process.stdout.write(progress + '%');
 }
 
 if (process.argv[1].endsWith('docs.ts')) {
-  main();
+  main().catch(console.error);
 }
