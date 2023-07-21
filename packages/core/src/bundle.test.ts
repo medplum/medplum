4bb15b1ca1ceb88b033b873e88772870ef112e0e
Rahul Agarwal <rahul@medplum.com>
2023-07-21 13:24:23 -0700

    Fixed unit tests

--- packages/core/src/bundle.test.ts
+++ packages/core/src/bundle.test.ts
@@ -1,39 +1,9 @@
-import { Bundle, BundleEntry, Resource } from '@medplum/fhirtypes';
+import { Bundle, BundleEntry, Resource, Specimen } from '@medplum/fhirtypes';
+import { webcrypto } from 'crypto';
 import { convertToTransactionBundle } from './bundle';
+import { isUUID } from './utils';
 
 let jsonFile: any;
-describe('FHIR Bundle Download', () => {
-  beforeEach(() => {
-    jsonFile = {
-      entry: [
-        {
-          fullUrl: 'medplum.com',
-          resource: {
-            meta: {},
-            id: '123',
-            resourceType: 'Patient',
-          },
-        },
-        {
-          fullUrl: 'app.medplum.com/123',
-          resource: {
-            meta: {
-              id: '123',
-            },
-            id: '456',
-            resourceType: 'Patient',
-          },
-        },
-      ],
-    };
-  });
-
-  test('create a FHIR bundle from JSON File', () => {
-    const transactionBundle = convertToTransactionBundle(jsonFile);
-    const firstEntry = transactionBundle.entry?.[0];
-    expect(firstEntry?.request?.url).toEqual('Patient');
-  });
-});
 
 function createResourceWithReference<T extends Resource>(
   resourceType: T['resourceType'],
@@ -52,114 +22,156 @@ function createResourceWithReference<T extends Resource>(
   };
 }
 
-describe('convertToTransactionBundle', () => {
-  test('reorders a 2-element bundle', () => {
-    const inputBundle: Bundle = {
-      resourceType: 'Bundle',
-      type: 'transaction',
-      entry: [
-        createResourceWithReference('DiagnosticReport', 'urn:uuid:3d8b6e96-6de4-48c1-b7ff-e2c26c924620', {
-          subject: { reference: 'urn:uuid:70653c8f-95e1-4b4e-84e8-8d64c15e4a13' },
-        }),
-        createResourceWithReference('Patient', 'urn:uuid:70653c8f-95e1-4b4e-84e8-8d64c15e4a13'),
-      ],
-    };
-
-    const reorderedBundle = convertToTransactionBundle(inputBundle);
-
-    expect(reorderedBundle?.entry?.map((e) => e.fullUrl)).toEqual([
-      'urn:uuid:70653c8f-95e1-4b4e-84e8-8d64c15e4a13',
-      'urn:uuid:3d8b6e96-6de4-48c1-b7ff-e2c26c924620',
-    ]);
+describe('Bundle tests', () => {
+  beforeAll(() => {
+    Object.defineProperty(globalThis, 'crypto', { value: webcrypto });
   });
 
-  test('reorders a bundle with a cycle', () => {
-    const inputBundle: Bundle = {
-      resourceType: 'Bundle',
-      type: 'transaction',
-      entry: [
-        createResourceWithReference('ServiceRequest', 'urn:uuid:c3d8f926-1f10-41b5-bd20-1d3d6e1f63b5', {
-          subject: { reference: 'urn:uuid:b3e7d3f5-f7c0-41c3-b1c2-8b39e271b2c8' },
-        }),
-        createResourceWithReference('Specimen', 'urn:uuid:b3e7d3f5-f7c0-41c3-b1c2-8b39e271b2c8', {
-          request: [{ reference: 'urn:uuid:c3d8f926-1f10-41b5-bd20-1d3d6e1f63b5' }],
-        }),
-      ],
-    };
-
-    const reorderedBundle = convertToTransactionBundle(inputBundle);
-
-    expect(reorderedBundle?.entry?.map((e) => e.fullUrl)).toEqual([
-      'urn:uuid:c3d8f926-1f10-41b5-bd20-1d3d6e1f63b5',
-      'urn:uuid:b3e7d3f5-f7c0-41c3-b1c2-8b39e271b2c8',
-      'urn:uuid:c3d8f926-1f10-41b5-bd20-1d3d6e1f63b5',
-      'urn:uuid:b3e7d3f5-f7c0-41c3-b1c2-8b39e271b2c8',
-    ]);
-    expect(reorderedBundle?.entry?.map((e) => e.request?.method)).toEqual(['POST', 'POST', 'PUT', 'PUT']);
-  });
-
-  test('Reorders Lab bundle', () => {
-    const inputBundle: Bundle = {
-      resourceType: 'Bundle',
-      type: 'transaction',
-      entry: [
-        createResourceWithReference('Patient', 'urn:uuid:ca760a2b-3f5d-4c85-9087-b8b6422970a8'),
-        createResourceWithReference('ServiceRequest', 'urn:uuid:76cdff91-2a4d-4c57-8922-2f2ea17f6756', {
-          subject: { reference: 'urn:uuid:ca760a2b-3f5d-4c85-9087-b8b6422970a8' },
-        }),
-        createResourceWithReference('DiagnosticReport', 'urn:uuid:9e1fe992-1e45-4a0e-8dae-cbb8490f449e', {
-          subject: { reference: 'urn:uuid:ca760a2b-3f5d-4c85-9087-b8b6422970a8' },
-          basedOn: [{ reference: 'urn:uuid:76cdff91-2a4d-4c57-8922-2f2ea17f6756' }],
-          result: [{ reference: 'urn:uuid:e2d7f292-1e1d-4d5c-9f3a-fae792856f71' }],
-        }),
-        createResourceWithReference('Observation', 'urn:uuid:e2d7f292-1e1d-4d5c-9f3a-fae792856f71', {
-          subject: { reference: 'urn:uuid:ca760a2b-3f5d-4c85-9087-b8b6422970a8' },
-        }),
-      ],
-    };
-
-    const reorderedBundle = convertToTransactionBundle(inputBundle);
-
-    expect(reorderedBundle.entry?.map((e) => e.fullUrl)).toEqual([
-      'urn:uuid:ca760a2b-3f5d-4c85-9087-b8b6422970a8',
-      'urn:uuid:e2d7f292-1e1d-4d5c-9f3a-fae792856f71',
-      'urn:uuid:76cdff91-2a4d-4c57-8922-2f2ea17f6756',
-      'urn:uuid:9e1fe992-1e45-4a0e-8dae-cbb8490f449e',
-    ]);
-  });
-
-  test('Cancer Pathology Example', () => {
-    const bundle: Bundle = {
-      resourceType: 'Bundle',
-      id: 'us-pathology-content-bundle-example',
-      type: 'collection',
-      entry: [
-        {
-          fullUrl: 'http://hl7.org/fhir/us/cancer-reporting/Specimen/adrenal-example',
-          resource: {
-            resourceType: 'Specimen',
-            id: 'adrenal-example',
-            subject: { reference: 'Patient/JoelAlexPatient' },
+  describe('FHIR Bundle Download', () => {
+    beforeEach(() => {
+      jsonFile = {
+        entry: [
+          {
+            fullUrl: 'medplum.com',
+            resource: {
+              meta: {},
+              id: '123',
+              resourceType: 'Patient',
+            },
           },
-        },
-        {
-          fullUrl: 'http://hl7.org/fhir/us/cancer-reporting/Patient/JoelAlexPatient',
-          resource: {
-            resourceType: 'Patient',
-            id: 'JoelAlexPatient',
-            name: [
-              {
-                family: 'Joel',
-                given: ['Alex'],
+          {
+            fullUrl: 'app.medplum.com/123',
+            resource: {
+              meta: {
+                id: '123',
               },
-            ],
-            gender: 'male',
+              id: '456',
+              resourceType: 'Patient',
+            },
           },
-        },
-      ],
-    };
+        ],
+      };
+    });
 
-    const transaction = convertToTransactionBundle(bundle);
-    expect(transaction.entry?.map((e) => e.resource?.resourceType)).toMatchObject(['Patient', 'Specimen']);
+    test('create a FHIR bundle from JSON File', () => {
+      const transactionBundle = convertToTransactionBundle(jsonFile);
+      const firstEntry = transactionBundle.entry?.[0];
+      expect(firstEntry?.request?.url).toEqual('Patient');
+    });
+  });
+
+  describe('convertToTransactionBundle', () => {
+    test('reorders a 2-element bundle', () => {
+      const inputBundle: Bundle = {
+        resourceType: 'Bundle',
+        type: 'transaction',
+        entry: [
+          createResourceWithReference('DiagnosticReport', 'urn:uuid:3d8b6e96-6de4-48c1-b7ff-e2c26c924620', {
+            subject: { reference: 'urn:uuid:70653c8f-95e1-4b4e-84e8-8d64c15e4a13' },
+          }),
+          createResourceWithReference('Patient', 'urn:uuid:70653c8f-95e1-4b4e-84e8-8d64c15e4a13'),
+        ],
+      };
+
+      const reorderedBundle = convertToTransactionBundle(inputBundle);
+
+      expect(reorderedBundle?.entry?.map((e) => e.fullUrl)).toEqual([
+        'urn:uuid:70653c8f-95e1-4b4e-84e8-8d64c15e4a13',
+        'urn:uuid:3d8b6e96-6de4-48c1-b7ff-e2c26c924620',
+      ]);
+    });
+
+    test('reorders a bundle with a cycle', () => {
+      const inputBundle: Bundle = {
+        resourceType: 'Bundle',
+        type: 'transaction',
+        entry: [
+          createResourceWithReference('ServiceRequest', 'urn:uuid:c3d8f926-1f10-41b5-bd20-1d3d6e1f63b5', {
+            subject: { reference: 'urn:uuid:b3e7d3f5-f7c0-41c3-b1c2-8b39e271b2c8' },
+          }),
+          createResourceWithReference('Specimen', 'urn:uuid:b3e7d3f5-f7c0-41c3-b1c2-8b39e271b2c8', {
+            request: [{ reference: 'urn:uuid:c3d8f926-1f10-41b5-bd20-1d3d6e1f63b5' }],
+          }),
+        ],
+      };
+
+      const reorderedBundle = convertToTransactionBundle(inputBundle);
+
+      expect(reorderedBundle?.entry?.map((e) => e.fullUrl)).toEqual([
+        'urn:uuid:c3d8f926-1f10-41b5-bd20-1d3d6e1f63b5',
+        'urn:uuid:b3e7d3f5-f7c0-41c3-b1c2-8b39e271b2c8',
+        'urn:uuid:c3d8f926-1f10-41b5-bd20-1d3d6e1f63b5',
+        'urn:uuid:b3e7d3f5-f7c0-41c3-b1c2-8b39e271b2c8',
+      ]);
+      expect(reorderedBundle?.entry?.map((e) => e.request?.method)).toEqual(['POST', 'POST', 'PUT', 'PUT']);
+    });
+
+    test('Reorders Lab bundle', () => {
+      const inputBundle: Bundle = {
+        resourceType: 'Bundle',
+        type: 'transaction',
+        entry: [
+          createResourceWithReference('Patient', 'urn:uuid:ca760a2b-3f5d-4c85-9087-b8b6422970a8'),
+          createResourceWithReference('ServiceRequest', 'urn:uuid:76cdff91-2a4d-4c57-8922-2f2ea17f6756', {
+            subject: { reference: 'urn:uuid:ca760a2b-3f5d-4c85-9087-b8b6422970a8' },
+          }),
+          createResourceWithReference('DiagnosticReport', 'urn:uuid:9e1fe992-1e45-4a0e-8dae-cbb8490f449e', {
+            subject: { reference: 'urn:uuid:ca760a2b-3f5d-4c85-9087-b8b6422970a8' },
+            basedOn: [{ reference: 'urn:uuid:76cdff91-2a4d-4c57-8922-2f2ea17f6756' }],
+            result: [{ reference: 'urn:uuid:e2d7f292-1e1d-4d5c-9f3a-fae792856f71' }],
+          }),
+          createResourceWithReference('Observation', 'urn:uuid:e2d7f292-1e1d-4d5c-9f3a-fae792856f71', {
+            subject: { reference: 'urn:uuid:ca760a2b-3f5d-4c85-9087-b8b6422970a8' },
+          }),
+        ],
+      };
+
+      const reorderedBundle = convertToTransactionBundle(inputBundle);
+
+      expect(reorderedBundle.entry?.map((e) => e.fullUrl)).toEqual([
+        'urn:uuid:ca760a2b-3f5d-4c85-9087-b8b6422970a8',
+        'urn:uuid:e2d7f292-1e1d-4d5c-9f3a-fae792856f71',
+        'urn:uuid:76cdff91-2a4d-4c57-8922-2f2ea17f6756',
+        'urn:uuid:9e1fe992-1e45-4a0e-8dae-cbb8490f449e',
+      ]);
+    });
+
+    test('Cancer Pathology Example', () => {
+      const bundle: Bundle = {
+        resourceType: 'Bundle',
+        id: 'us-pathology-content-bundle-example',
+        type: 'collection',
+        entry: [
+          {
+            fullUrl: 'http://hl7.org/fhir/us/cancer-reporting/Specimen/adrenal-example',
+            resource: {
+              resourceType: 'Specimen',
+              id: 'adrenal-example',
+              subject: { reference: 'Patient/JoelAlexPatient' },
+            },
+          },
+          {
+            fullUrl: 'http://hl7.org/fhir/us/cancer-reporting/Patient/JoelAlexPatient',
+            resource: {
+              resourceType: 'Patient',
+              id: 'JoelAlexPatient',
+              name: [
+                {
+                  family: 'Joel',
+                  given: ['Alex'],
+                },
+              ],
+              gender: 'male',
+            },
+          },
+        ],
+      };
+
+      const transaction = convertToTransactionBundle(bundle);
+      expect(transaction.entry?.map((e) => e.resource?.resourceType)).toMatchObject(['Patient', 'Specimen']);
+
+      const specimen = transaction.entry?.find((e) => e.resource?.resourceType === 'Specimen')?.resource as Specimen;
+      expect(isUUID(specimen?.subject?.reference?.split(':')[2] ?? '')).toBeTruthy();
+    });
   });
 });
