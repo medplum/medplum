a92167fb61652d7dd983a4fbdc4d43d00e3a1c5f
Rahul Agarwal <rahul@medplum.com>
2023-07-21 10:01:28 -0700

    topologically sort transaction bundles

--- packages/core/src/bundle.test.ts
+++ packages/core/src/bundle.test.ts
@@ -1,3 +1,4 @@
+import { Bundle, BundleEntry, Resource } from '@medplum/fhirtypes';
 import { convertToTransactionBundle } from './bundle';
 
 let jsonFile: any;
@@ -31,5 +32,83 @@ describe('FHIR Bundle Download', () => {
     const transactionBundle = convertToTransactionBundle(jsonFile);
     const firstEntry = transactionBundle.entry?.[0];
     expect(firstEntry?.request?.url).toEqual('Patient');
+  });
+});
+
+function createResourceWithReference<T extends Resource>(
+  resourceType: T['resourceType'],
+  fullUrl: string,
+  referenceField?: Partial<T>
+): BundleEntry {
+  let resource = { resourceType, id: fullUrl.split(':').pop() } as T;
+  if (referenceField) {
+    resource = { ...resource, ...referenceField };
+  }
+
+  return {
+    fullUrl,
+    resource,
+    request: { method: 'POST', url: resourceType },
+  };
+}
+
+describe('convertToTransactionBundle', () => {
+  test('reorders a 2-element bundle', () => {
+    const inputBundle: Bundle = {
+      resourceType: 'Bundle',
+      type: 'transaction',
+      entry: [
+        createResourceWithReference('DiagnosticReport', 'urn:uuid:B', { subject: { reference: 'urn:uuid:A' } }),
+        createResourceWithReference('Patient', 'urn:uuid:A'),
+      ],
+    };
+
+    const reorderedBundle = convertToTransactionBundle(inputBundle);
+
+    expect(reorderedBundle?.entry?.map((e) => e.fullUrl)).toEqual(['urn:uuid:A', 'urn:uuid:B']);
+  });
+
+  test('reorders a bundle with a cycle', () => {
+    const inputBundle: Bundle = {
+      resourceType: 'Bundle',
+      type: 'transaction',
+      entry: [
+        createResourceWithReference('ServiceRequest', 'urn:uuid:A', { subject: { reference: 'urn:uuid:B' } }),
+        createResourceWithReference('Specimen', 'urn:uuid:B', { request: [{ reference: 'urn:uuid:A' }] }),
+      ],
+    };
+
+    const reorderedBundle = convertToTransactionBundle(inputBundle);
+
+    expect(reorderedBundle?.entry?.map((e) => e.fullUrl)).toEqual(['urn:uuid:A', 'urn:uuid:B', 'urn:uuid:B']);
+    expect(reorderedBundle?.entry?.map((e) => e.request?.method)).toEqual(['POST', 'POST', 'PUT']);
+  });
+
+  test('Reorders Lab bundle', () => {
+    const inputBundle: Bundle = {
+      resourceType: 'Bundle',
+      type: 'transaction',
+      entry: [
+        createResourceWithReference('Patient', 'urn:uuid:A'),
+        createResourceWithReference('ServiceRequest', 'urn:uuid:B', { subject: { reference: 'urn:uuid:A' } }),
+        createResourceWithReference('DiagnosticReport', 'urn:uuid:C', {
+          subject: { reference: 'urn:uuid:A' },
+          basedOn: [{ reference: 'urn:uuid:B' }],
+          result: [{ reference: 'urn:uuid:D' }],
+        }),
+        createResourceWithReference('Observation', 'urn:uuid:D', {
+          subject: { reference: 'urn:uuid:A' },
+        }),
+      ],
+    };
+
+    const reorderedBundle = convertToTransactionBundle(inputBundle);
+
+    expect(reorderedBundle.entry?.map((e) => e.fullUrl)).toEqual([
+      'urn:uuid:A',
+      'urn:uuid:D',
+      'urn:uuid:B',
+      'urn:uuid:C',
+    ]);
   });
 });
--- packages/core/src/bundle.ts
+++ packages/core/src/bundle.ts
@@ -1,4 +1,5 @@
-import { Bundle } from '@medplum/fhirtypes';
+import { Bundle, BundleEntry } from '@medplum/fhirtypes';
+import { isReference } from './types';
 
 /**
  * More on Bundles can be found here
@@ -30,7 +31,7 @@ export function convertToTransactionBundle(bundle: Bundle): Bundle {
     replacer,
     2
   );
-  return JSON.parse(jsonString);
+  return reorderBundle(JSON.parse(jsonString) as Bundle);
 }
 
 function replacer(key: string, value: string): string {
@@ -38,4 +39,152 @@ function replacer(key: string, value: string): string {
     return 'urn:uuid:' + value.split('/')[1];
   }
   return value;
+}
+
+/**
+ * Topologically sorts a `batch` or `transaction` bundle to improve reference resolution.
+ * The bundle is sorted such that a resource is created _before_ references to that resource appear in the bundle.
+ *
+ * In the event of cycles, this function will first create a POST request for each resource in the cycle, and then will
+ * append a PUT request to the bundle. This ensures that each resources in the cycle is visited twice, and all
+ * references can be resolved
+ * @param bundle Input bundle with type `batch` or `transaction`
+ * @returns Bundle of the same type, with Bundle.entry reordered
+ */
+export function reorderBundle(bundle: Bundle): Bundle {
+  const adjacencyList = buildAdjacencyList(bundle);
+  const { sorted: sortedFullUrls, cycles } = topologicalSortWithCycles(adjacencyList);
+
+  const entryMap: Record<string, BundleEntry> = {};
+
+  for (const entry of bundle.entry || []) {
+    if (entry.fullUrl) {
+      entryMap[entry.fullUrl] = entry;
+    }
+  }
+
+  const reorderedEntries = sortedFullUrls.map((fullUrl) => entryMap[fullUrl]);
+
+  // Handle cycles by appending additional entries with a method of 'PUT'
+  for (const cycle of cycles) {
+    for (const fullUrl of cycle) {
+      const originalEntry = entryMap[fullUrl];
+      const putEntry: BundleEntry = {
+        ...originalEntry,
+        request: {
+          ...originalEntry.request,
+          method: 'PUT',
+        },
+      };
+      reorderedEntries.push(putEntry);
+    }
+  }
+
+  return { ...bundle, entry: reorderedEntries };
+}
+
+type AdjacencyList = Record<string, string[]>;
+
+enum VertexState {
+  NotVisited,
+  Visiting,
+  Visited,
+}
+
+function topologicalSortWithCycles(graph: AdjacencyList): { sorted: string[]; cycles: string[][] } {
+  const sorted: string[] = [];
+  const state: Record<string, VertexState> = {};
+  const cycles: string[][] = [];
+
+  // Initialize all vertices to NotVisited state
+  for (const vertex of Object.keys(graph)) {
+    state[vertex] = VertexState.NotVisited;
+  }
+
+  function visit(vertex: string, path: string[]): boolean {
+    // If this vertex is already visited, return true
+    if (state[vertex] === VertexState.Visited) {
+      return true;
+    }
+
+    // If this vertex is currently being visited, we have a cycle
+    if (state[vertex] === VertexState.Visiting) {
+      const cycleStartIndex = path.lastIndexOf(vertex);
+      if (cycleStartIndex !== -1) {
+        cycles.push(path.slice(cycleStartIndex));
+      }
+      return true; // return true for vertices that are part of cycles
+    }
+
+    // Mark the vertex as visiting and add it to the path
+    state[vertex] = VertexState.Visiting;
+    path.push(vertex);
+
+    // Visit all neighbors
+    let hasCycle = false;
+    for (const neighbor of graph[vertex]) {
+      if (!visit(neighbor, path)) {
+        hasCycle = true;
+      }
+    }
+
+    // Mark the vertex as visited, remove it from the path, and add it to the sorted list
+    state[vertex] = VertexState.Visited;
+    path.pop();
+    sorted.unshift(vertex);
+
+    return !hasCycle;
+  }
+
+  for (const vertex in graph) {
+    if (state[vertex] === VertexState.NotVisited) {
+      const path: string[] = [];
+      visit(vertex, path);
+    }
+  }
+
+  return { sorted, cycles };
+}
+
+function findReferences(resource: any, callback: (reference: string) => void): void {
+  for (const key in resource) {
+    if (resource[key] && typeof resource[key] === 'object') {
+      const value = resource[key];
+
+      if (isReference(value)) {
+        const reference = value.reference;
+        if (reference.startsWith('urn:uuid:')) {
+          callback(reference);
+        }
+      } else {
+        findReferences(value, callback);
+      }
+    }
+  }
+}
+
+function buildAdjacencyList(bundle: Bundle): AdjacencyList {
+  const adjacencyList: AdjacencyList = {};
+
+  // Initialize adjacency list with empty arrays for each entry's fullUrl
+  for (const entry of bundle.entry || []) {
+    if (entry.fullUrl) {
+      adjacencyList[entry.fullUrl] = [];
+    }
+  }
+
+  for (const entry of bundle.entry || []) {
+    const fullUrl = entry.fullUrl;
+
+    if (entry.resource) {
+      findReferences(entry.resource, (reference: string) => {
+        // Add an incoming reference to the adjacency list
+        if (adjacencyList[reference]) {
+          adjacencyList[reference].push(fullUrl as string);
+        }
+      });
+    }
+  }
+
+  return adjacencyList;
 }
