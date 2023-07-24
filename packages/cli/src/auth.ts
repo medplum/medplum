200c29b8dedc6c4314d32f0eddd04805a43ac50b
James Touri <j_touri@yahoo.com>
2023-07-24 13:40:22 -0700

    Logic for token exchange

--- packages/cli/src/auth.ts
+++ packages/cli/src/auth.ts
@@ -4,7 +4,7 @@ import { exec } from 'child_process';
 import { createServer } from 'http';
 import { createMedplumCommand } from './util/command';
 import { createMedplumClient } from './util/client';
-import { checkIfProfileExists, createProfile, getProfileOptions } from './utils';
+import { checkForProfile, createProfile, getProfileOptions } from './utils';
 
 const clientId = 'medplum-cli';
 const redirectUri = 'http://localhost:9615';
@@ -13,15 +13,15 @@ export const login = createMedplumCommand('login');
 export const whoami = createMedplumCommand('whoami');
 
 login.action(async (options) => {
-  const medplum = await createMedplumClient(options);
   if (options.authType === 'basic') {
     console.log('Basic authentication does not require login');
     return;
   }
-  if (options.profile && !checkIfProfileExists(options.profile)) {
+  if (!checkForProfile(options)) {
     console.log(`Creating new profile...`);
     createProfile(options.profile, options);
   }
+  const medplum = await createMedplumClient(options);
   const profile = getProfileOptions(options.profile);
   await startLogin(medplum, profile);
 });
@@ -32,20 +32,24 @@ whoami.action(async (options) => {
 });
 
 async function startLogin(medplum: MedplumClient, profile: any): Promise<void> {
-  await startWebServer(medplum);
-
-  if (profile.authType && profile.authType === 'jwt-bearer') {
-    await medplum.startJwtBearerLogin(profile.clientId, profile.assertion, profile.scope);
-    return;
-  } 
-
-  const loginUrl = new URL(medplum.getAuthorizeUrl());
-  loginUrl.searchParams.set('client_id', clientId);
-  loginUrl.searchParams.set('redirect_uri', redirectUri);
-  loginUrl.searchParams.set('scope', 'openid');
-  loginUrl.searchParams.set('response_type', 'code');
-  loginUrl.searchParams.set('prompt', 'login');
-  await openBrowser(loginUrl.toString());
+  switch (profile.authType) {
+    case 'jwt-bearer':
+      console.log('Starting JWT login...');
+      await medplum.startJwtBearerLogin(profile.clientId, profile.assertion, profile.scope);
+      return;
+    case 'token-exchange':
+      await medplum.exchangeExternalAccessToken(profile.accessToken);
+      return;
+    default:
+      await startWebServer(medplum);
+      const loginUrl = new URL(medplum.getAuthorizeUrl());
+      loginUrl.searchParams.set('client_id', clientId);
+      loginUrl.searchParams.set('redirect_uri', redirectUri);
+      loginUrl.searchParams.set('scope', 'openid');
+      loginUrl.searchParams.set('response_type', 'code');
+      loginUrl.searchParams.set('prompt', 'login');
+      await openBrowser(loginUrl.toString());
+  }
 }
 
 async function startWebServer(medplum: MedplumClient): Promise<void> {
