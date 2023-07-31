d5dc96893ac66caeeac88a073ee975c8eddb9dfe
rahul1 <rahul@medplum.com>
2023-07-31 11:58:05 -0700

    Document both POST and GET requests for $execute (#2580)
    * WIP  Bot GET
    
    * Documenting both POST and GET requests for $execute

--- packages/examples/src/api/fhir/operations/bot-execute.ts
+++ packages/examples/src/api/fhir/operations/bot-execute.ts
@@ -4,9 +4,14 @@ const medplum = new MedplumClient();
 
 export async function executeById(id: string): Promise<void> {
   // start-block execute-by-id
-  const result = await medplum.executeBot(id, { input: '...' });
+  const result = await medplum.executeBot(id, { input: { foo: 'bar' } });
   console.log(result);
   // end-block execute-by-id
+
+  // start-block execute-by-id-get
+  const getResult = await medplum.get(medplum.fhirUrl('Bot', id, '$execute').toString() + '?foo=bar');
+  console.log(getResult);
+  // end-block execute-by-id-get
 }
 
 export async function executeByIdentifier(): Promise<void> {
@@ -17,10 +22,15 @@ export async function executeByIdentifier(): Promise<void> {
       value: '1234',
     },
     {
-      input1: '...',
-      input2: '...',
+      foo: 'bar',
     }
   );
   console.log(result);
   // end-block execute-by-identifier
+  // start-block execute-by-identifier-get
+  const getResult = await medplum.get(
+    medplum.fhirUrl('Bot', '$execute').toString() + '?identifier=https://example.com/bots|1234&foo=bar'
+  );
+  console.log(getResult);
+  // end-block execute-by-identifier-get
 }
