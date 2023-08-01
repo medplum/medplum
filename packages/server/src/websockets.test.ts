eabf50d2620b5e12dbaa555c8619760f0cd1b6ae
Cody Ebberson <cody@ebberson.com>
2023-08-01 15:13:46 -0700

    Test transmit before connect

--- packages/server/src/websockets.test.ts
+++ packages/server/src/websockets.test.ts
@@ -73,6 +73,21 @@ describe('WebSockets', () => {
 
     await request(server)
       .ws('/ws/agent')
+      // Try to transmit before connecting
+      // This should fail
+      .sendText(
+        JSON.stringify({
+          type: 'transmit',
+          message: Hl7Message.parse(
+            'MSH|^~\\&|ADT1|MCM|LABADT|MCM|198808181126|SECURITY|ADT^A01|MSG00001|P|2.2\r' +
+              'PID|||PATID1234^5^M11||JONES^WILLIAM^A^III||19610615|M-\r' +
+              'NK1|1|JONES^BARBARA^K|SPO|||||20011105\r' +
+              'PV1|1|I|2000^2012^01||||004777^LEBAUER^SIDNEY^J.|||SUR||-||1|A0-'
+          ).toString(),
+        })
+      )
+      .expectText('{"type":"error","message":"Not connected"}')
+      // Now connect
       .sendText(
         JSON.stringify({
           type: 'connect',
@@ -81,6 +96,8 @@ describe('WebSockets', () => {
         })
       )
       .expectText(JSON.stringify({ type: 'connected' }))
+      // Now transmit
+      // This should succeed
       .sendText(
         JSON.stringify({
           type: 'transmit',
