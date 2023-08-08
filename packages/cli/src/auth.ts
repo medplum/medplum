668123980a25f274feea407fdbabf3dd56970873
James Touri <j_touri@yahoo.com>
2023-08-08 09:11:52 -0700

    JWT Bearer Login for CLI  (#2587)
    * Bearer methods
    
    * tests
    
    * Tests
    
    * Lint
    
    * Add profile property to Profile interface
    
    * Change profile property to name
    
    * Changes
    
    * Jwt bearer works with health gorilla test
    
    * Flags in place
    
    * Fix tests
    
    * Lint
    
    * Addd lint
    
    * address suggestions
    
    * lint
    
    * Remove method param

--- packages/cli/src/auth.ts
+++ packages/cli/src/auth.ts
@@ -1,9 +1,12 @@
-import { ContentType, getDisplayString, MedplumClient, normalizeErrorString } from '@medplum/core';
+import { ContentType, MedplumClient, encodeBase64, getDisplayString, normalizeErrorString } from '@medplum/core';
 import { exec } from 'child_process';
 import { createServer } from 'http';
 import { platform } from 'os';
 import { createMedplumClient } from './util/client';
 import { createMedplumCommand } from './util/command';
+import { Profile, saveProfile, loadProfile, profileExists } from './utils';
+import { FileSystemStorage } from './storage';
+import { createHmac } from 'crypto';
 
 const clientId = 'medplum-cli';
 const redirectUri = 'http://localhost:9615';
@@ -12,12 +15,19 @@ export const login = createMedplumCommand('login');
 export const whoami = createMedplumCommand('whoami');
 
 login.action(async (options) => {
-  const medplum = await createMedplumClient(options);
+  const profileName = options.profile ?? 'default';
+  const storage = new FileSystemStorage(profileName);
+  if (!profileExists(storage, profileName)) {
+    console.log(`Creating new profile...`);
+    saveProfile(profileName, options);
+  }
   if (options.authType === 'basic') {
     console.log('Basic authentication does not require login');
     return;
   }
-  await startLogin(medplum);
+  const profile = loadProfile(profileName);
+  const medplum = await createMedplumClient(options);
+  await startLogin(medplum, profile);
 });
 
 whoami.action(async (options) => {
@@ -25,15 +35,21 @@ whoami.action(async (options) => {
   printMe(medplum);
 });
 
-async function startLogin(medplum: MedplumClient): Promise<void> {
-  await startWebServer(medplum);
-  const loginUrl = new URL(medplum.getAuthorizeUrl());
-  loginUrl.searchParams.set('client_id', clientId);
-  loginUrl.searchParams.set('redirect_uri', redirectUri);
-  loginUrl.searchParams.set('scope', 'openid');
-  loginUrl.searchParams.set('response_type', 'code');
-  loginUrl.searchParams.set('prompt', 'login');
-  await openBrowser(loginUrl.toString());
+async function startLogin(medplum: MedplumClient, profile: Profile): Promise<void> {
+  if (!profile?.authType) {
+    await medplumAuthorizationCodeLogin(medplum);
+    return;
+  }
+  if (profile.authType === 'jwt-bearer') {
+    if (!profile.clientId || !profile.clientSecret) {
+      throw new Error('Missing values, make sure to add --client-id, and --client-secret for JWT Bearer login');
+    }
+    console.log('Starting JWT login...');
+    const accessToken = await jwtBearerLogin(medplum, profile);
+    const storage = new FileSystemStorage(profile.name as string);
+    storage.setObject('activeLogin', { accessToken });
+    console.log('Login successful');
+  }
 }
 
 async function startWebServer(medplum: MedplumClient): Promise<void> {
@@ -96,4 +112,52 @@ function printMe(medplum: MedplumClient): void {
   } else {
     console.log('Not logged in');
   }
+}
+
+async function medplumAuthorizationCodeLogin(medplum: MedplumClient): Promise<void> {
+  await startWebServer(medplum);
+  const loginUrl = new URL(medplum.getAuthorizeUrl());
+  loginUrl.searchParams.set('client_id', clientId);
+  loginUrl.searchParams.set('redirect_uri', redirectUri);
+  loginUrl.searchParams.set('scope', 'openid');
+  loginUrl.searchParams.set('response_type', 'code');
+  loginUrl.searchParams.set('prompt', 'login');
+  await openBrowser(loginUrl.toString());
+}
+
+async function jwtBearerLogin(medplum: MedplumClient, profile: Profile): Promise<string> {
+  const header = {
+    typ: 'JWT',
+    alg: 'HS256',
+  };
+
+  const currentTimestamp = Math.floor(Date.now() / 1000);
+  const data = {
+    aud: `${profile.baseUrl}${profile.audience}`,
+    iss: profile.issuer,
+    sub: profile.subject,
+    nbf: currentTimestamp,
+    iat: currentTimestamp,
+    exp: currentTimestamp + 604800, // expiry time is 7 days from time of creation
+  };
+  const encodedHeader = encodeBase64(JSON.stringify(header));
+  const encodedData = encodeBase64(JSON.stringify(data));
+  const token = `${encodedHeader}.${encodedData}`;
+  const signature = createHmac('sha256', profile.clientSecret as string)
+    .update(token)
+    .digest('base64url');
+  const signedToken = `${token}.${signature}`;
+
+  const formBody = new URLSearchParams();
+  formBody.set('grant_type', 'urn:ietf:params:oauth:grant-type:jwt-bearer');
+  formBody.set('client_id', profile.clientId as string);
+  formBody.set('assertion', signedToken);
+  formBody.set('scope', profile.scope ?? '');
+
+  const res = await medplum.post(profile.tokenUrl as string, formBody.toString(), 'application/x-www-form-urlencoded', {
+    credentials: 'include',
+  });
+
+  const obj = await JSON.parse(res);
+  return obj.access_token;
 }
---
+++ packages/cli/src/profile-auth.test.ts
@@ -0,0 +1,126 @@
+import { main } from '.';
+import os from 'os';
+import { mkdtempSync, rmSync } from 'fs';
+import { sep } from 'path';
+import { createMedplumClient } from './util/client';
+import { FileSystemStorage } from './storage';
+import { FetchLike, MedplumClient, getStatus, isOperationOutcome } from '@medplum/core';
+import { OperationOutcome } from '@medplum/fhirtypes';
+
+jest.mock('os');
+jest.mock('fast-glob', () => ({
+  sync: jest.fn(() => []),
+}));
+jest.mock('./util/client');
+jest.mock('fs', () => ({
+  ...jest.requireActual('fs'),
+  writeFile: jest.fn((path, data, callback) => {
+    callback();
+  }),
+}));
+
+const testHomeDir = mkdtempSync(__dirname + sep + 'storage-');
+
+const originalWindow = globalThis.window;
+
+describe('Profiles Auth', () => {
+  beforeEach(async () => {
+    console.log = jest.fn();
+  });
+  beforeAll(async () => {
+    Object.defineProperty(globalThis, 'window', { get: () => originalWindow });
+    (os.homedir as unknown as jest.Mock).mockReturnValue(testHomeDir);
+  });
+
+  afterAll(async () => {
+    rmSync(testHomeDir, { recursive: true, force: true });
+  });
+
+  test('JWT Bearer', async () => {
+    const profileName = 'jwtProfile';
+    const jwtObj = {
+      authType: 'jwt-bearer',
+      baseUrl: 'https://valid.gov',
+      fhirUrlPath: 'api/v2',
+      tokenUrl: '/oauth2/token',
+      clientId: 'validClientId',
+      clientSecret: 'validClientSecret',
+      scope: 'validScope',
+      audience: '/oauth2/token',
+      authorizeUrl: 'https://valid.gov/authorize',
+      subject: 'john_doe',
+      issuer: 'https://valid.gov',
+    };
+
+    const accessTokenFromClientId = createFakeJwt({ client_id: 'test-client-id', login_id: '123' });
+
+    const fetch = mockFetch(200, (url) => {
+      if (url.includes('oauth2/token')) {
+        return JSON.stringify({
+          access_token: accessTokenFromClientId,
+        });
+      }
+      return {};
+    });
+    const profile = new FileSystemStorage(profileName);
+
+    const medplum = new MedplumClient({ fetch, storage: profile });
+    (createMedplumClient as unknown as jest.Mock).mockImplementation(async () => medplum);
+
+    await main([
+      'node',
+      'index.js',
+      'login',
+      '-p',
+      profileName,
+      '--auth-type',
+      jwtObj.authType,
+      '--client-id',
+      jwtObj.clientId,
+      '--scope',
+      jwtObj.scope,
+      '--authorize-url',
+      jwtObj.authorizeUrl,
+      '--subject',
+      jwtObj.subject,
+      '--audience',
+      jwtObj.audience,
+      '--client-secret',
+      jwtObj.clientSecret,
+      '--issuer',
+      jwtObj.issuer,
+      '--token-url',
+      jwtObj.tokenUrl,
+    ]);
+
+    expect(profile.getObject('activeLogin')).toEqual({
+      accessToken: accessTokenFromClientId,
+    });
+  });
+});
+
+function createFakeJwt(claims: Record<string, string | number>): string {
+  const payload = JSON.stringify(claims);
+  const encodedPayload = Buffer.from(payload).toString('base64');
+
+  return `header.${encodedPayload}.signature`;
+}
+
+function mockFetch(
+  status: number,
+  body: OperationOutcome | Record<string, unknown> | ((url: string, options?: any) => any),
+  contentType = 'application/fhir+json'
+): FetchLike & jest.Mock {
+  const bodyFn = typeof body === 'function' ? body : () => body;
+  return jest.fn((url: string, options?: any) => {
+    const response = bodyFn(url, options);
+    const responseStatus = isOperationOutcome(response) ? getStatus(response) : status;
+    return Promise.resolve({
+      ok: responseStatus < 400,
+      status: responseStatus,
+      headers: { get: () => contentType },
+      blob: () => Promise.resolve(response),
+      json: () => Promise.resolve(response),
+    });
+  });
+}
