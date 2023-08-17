a2e5e199f5071c7a7b80347bb34b3d39cae35b57
Matthew Willer <matt@medplum.com>
2023-08-17 09:54:10 -0700

    Add test coverage

--- packages/react/src/ReferenceRangeEditor/ReferenceRangeEditor.test.tsx
+++ packages/react/src/ReferenceRangeEditor/ReferenceRangeEditor.test.tsx
@@ -211,12 +211,22 @@ describe('ReferenceRangeEditor', () => {
     const genderDropdown = screen.getByLabelText('Gender:');
     fireEvent.change(genderDropdown, { target: { value: 'female' } });
     fireEvent.change(genderDropdown, { target: { value: '' } });
-    fireEvent.click(screen.getByText('Save'));
 
+    const endocrineDropdown = screen.getByLabelText('Endocrine:');
+    fireEvent.change(endocrineDropdown, { target: { value: 'luteal' } });
+    fireEvent.change(endocrineDropdown, { target: { value: '' } });
+
+    const categoryDropdown = screen.getByLabelText('Category:');
+    fireEvent.change(categoryDropdown, { target: { value: 'reference' } });
+    fireEvent.change(categoryDropdown, { target: { value: '' } });
+
+    fireEvent.click(screen.getByText('Save'));
     const checkSubmitted = onSubmit.mock.calls[0][0] as ObservationDefinition;
     expect(checkSubmitted.qualifiedInterval).toMatchObject(
       Array(3).fill({
         gender: undefined,
+        context: undefined,
+        category: undefined,
       })
     );
   });
