9b9138cb943194458dd31653fb5b3b566fbf88b1
Charlie Thomas <contact@charliebthomas.com>
2023-08-21 13:33:59 -0500

    add: app version to <meta> tag in app (#2692)

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -2,10 +2,15 @@
 import react from '@vitejs/plugin-react';
 import { copyFileSync, existsSync } from 'fs';
 import { defineConfig } from 'vite';
+import packageJson from './package.json' assert { type: 'json' };
+import { execSync } from 'child_process';
 
 if (!existsSync('.env')) {
   copyFileSync('.env.defaults', '.env');
 }
+
+const gitHash = execSync('git rev-parse --short HEAD').toString().trim();
+process.env.MEDPLUM_VERSION = packageJson.version + '-' + gitHash;
 
 export default defineConfig({
   envPrefix: ['MEDPLUM_', 'GOOGLE_', 'RECAPTCHA_'],
