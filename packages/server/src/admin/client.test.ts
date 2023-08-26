37a22f242bed0bf3ade1c7c43bef89f03cbc33cc
Derrick Farris <mr.dcfarris@gmail.com>
2023-08-25 19:15:42 -0500

    fix(admin/client): use `:projectId` param to create client as superadmin (#2722)
    * fix(admin/client): use `:projectId` param to create client as superadmin
    
    * fix(test/admin/client): move check to where it makes sense
    
    * test(client): `parseReference()` -> `resolveId()`
    
    * test(client): give superadmin test client a better name
    
    * chore(admin/client): organize test imports
    
    * perf(admin/client): reuse var instead of reaccessing nested prop

--- packages/server/src/admin/client.test.ts
+++ packages/server/src/admin/client.test.ts
@@ -1,9 +1,12 @@
+import { getReferenceString, resolveId } from '@medplum/core';
+import type { BundleEntry, ClientApplication, ProjectMembership } from '@medplum/fhirtypes';
 import { randomUUID } from 'crypto';
 import express from 'express';
 import request from 'supertest';
 import { initApp, shutdownApp } from '../app';
 import { registerNew } from '../auth/register';
 import { loadTestConfig } from '../config';
+import { createTestProject, initTestAuth } from '../test.setup';
 
 const app = express();
 
@@ -57,5 +60,43 @@ describe('Client admin', () => {
       .type('json')
       .send({ foo: 'bar' });
     expect(res4.status).toBe(400);
+  });
+
+  test('Create client as superadmin - verify projectID', async () => {
+    const { project } = await createTestProject();
+
+    // As a superadmin, create a new client
+    const superAdminAccessToken = await initTestAuth({ superAdmin: true });
+    const res = await request(app)
+      .post('/admin/projects/' + project.id + '/client')
+      .set('Authorization', 'Bearer ' + superAdminAccessToken)
+      .send({
+        name: "Super Admin's test client",
+        description: 'A client for testing creating a client with superadmin privileges.',
+      });
+
+    expect(res.status).toBe(201);
+    expect((res.body as ClientApplication).resourceType).toBe('ClientApplication');
+    expect((res.body as ClientApplication).id).toBeDefined();
+
+    // Get the client membership
+    const res2 = await request(app)
+      .get('/fhir/R4/ProjectMembership?profile=' + getReferenceString(res.body as ClientApplication))
+      .set('Authorization', 'Bearer ' + superAdminAccessToken);
+    expect(res2.status).toBe(200);
+    expect(res2.body.resourceType).toBe('Bundle');
+
+    // Find the membership
+    const clientMembership = res2.body.entry.find(
+      (e: BundleEntry<ProjectMembership>) =>
+        e.resource?.profile?.reference === getReferenceString(res.body as ClientApplication)
+    )?.resource;
+    expect(clientMembership).toBeDefined();
+    expect(clientMembership.resourceType).toBe('ProjectMembership');
+
+    // Get the id of the project attached to this membership
+    const projectId = resolveId(clientMembership.project);
+
+    expect(projectId).toBe(project.id);
   });
 });
