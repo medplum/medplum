db7e32c43b002052eb2d3f5761173d4c4c6901dc
Derrick Farris <mr.dcfarris@gmail.com>
2023-08-29 17:56:39 -0500

    perf(types): refactor `.forEach` to numeric for loops

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -159,11 +159,17 @@ export function indexStructureDefinition(structureDefinition: StructureDefinitio
 
   const elements = structureDefinition.snapshot?.element;
   if (elements) {
-    // First pass, build types
-    elements.forEach((element) => indexType(structureDefinition, element));
+    // @TODO(ThatOneBro 29 Aug 2023): any reason why this can't be done in one loop?
+    // Using numeric for loops since they avoid overhead of iterators that you get with for...of
+    for (let i = 0; i < elements.length; i++) {
+      // First pass, build types
+      indexType(structureDefinition, elements[i]);
+    }
 
-    // Second pass, build properties
-    elements.forEach((element) => indexProperty(structureDefinition, element));
+    for (let i = 0; i < elements.length; i++) {
+      // Second pass, build properties
+      indexProperty(structureDefinition, elements[i]);
+    }
   }
 }
 
