30147fd7cf38aeb6d53e5642862ed66a068972f0
Matt Long <matt@mattlong.org>
2023-09-18 16:15:23 -0700

    Support creating a new resource from JSON

---
+++ packages/app/src/resource/FormCreatePage.tsx
@@ -0,0 +1,17 @@
+import { OperationOutcome } from '@medplum/fhirtypes';
+import { Document, ResourceForm } from '@medplum/react';
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useCreateResource } from './useCreateResource';
+
+export function FormCreatePage(): JSX.Element {
+  const { resourceType } = useParams();
+  const [outcome, setOutcome] = useState<OperationOutcome | undefined>();
+  const { defaultValue, handleSubmit } = useCreateResource(resourceType, setOutcome);
+
+  return (
+    <Document>
+      <ResourceForm defaultValue={defaultValue} onSubmit={handleSubmit} outcome={outcome} />
+    </Document>
+  );
+}
