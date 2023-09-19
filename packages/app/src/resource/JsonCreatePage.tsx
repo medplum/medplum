dd6d9dc1d0dfe98f477c8dac2b76eeb204333633
Matt Long <mattlong@users.noreply.github.com>
2023-09-19 11:47:15 -0700

    Fixes #2847 - Create new resources from JSON input (#2870)
    * Support creating a new resource from JSON
    
    * Add tests for new resource creation routes
    
    * Callback should not be async

---
+++ packages/app/src/resource/JsonCreatePage.tsx
@@ -0,0 +1,39 @@
+import { stringify } from '@medplum/core';
+import { Button, Group, JsonInput } from '@mantine/core';
+import { OperationOutcome } from '@medplum/fhirtypes';
+import { Document, Form, OperationOutcomeAlert } from '@medplum/react';
+import React, { useCallback, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useCreateResource } from './useCreateResource';
+
+export function JsonCreatePage(): JSX.Element {
+  const { resourceType } = useParams();
+  const [outcome, setOutcome] = useState<OperationOutcome | undefined>();
+  const { defaultValue, handleSubmit } = useCreateResource(resourceType, setOutcome);
+
+  const handleFormSubmit = useCallback(
+    (formData: Record<string, string>) => {
+      handleSubmit(JSON.parse(formData['new-resource']));
+    },
+    [handleSubmit]
+  );
+
+  return (
+    <Document>
+      {outcome && <OperationOutcomeAlert outcome={outcome} />}
+      <Form onSubmit={handleFormSubmit}>
+        <JsonInput
+          name="new-resource"
+          data-testid="create-resource-json"
+          minRows={24}
+          defaultValue={stringify(defaultValue, true)}
+          formatOnBlur
+          deserialize={JSON.parse}
+        />
+        <Group position="right" mt="xl" noWrap>
+          <Button type="submit">OK</Button>
+        </Group>
+      </Form>
+    </Document>
+  );
+}
---
+++ packages/app/src/resource/useCreateResource.ts
@@ -0,0 +1,42 @@
+import { normalizeOperationOutcome } from '@medplum/core';
+import { OperationOutcome, Resource } from '@medplum/fhirtypes';
+import { useMedplum } from '@medplum/react';
+import { useNavigate } from 'react-router-dom';
+
+/**
+ * React Hook providing helpers to create a FHIR resource.
+ * @param resourceType The FHIR resource type.
+ * @param setOutcome Optional callback to set the OperationOutcome.
+ * @returns An object with `defaultValue` to seed a creation view and `handleSubmit`
+ * to create the new resource on submission.
+ */
+export function useCreateResource<T extends Resource>(
+  resourceType: string | undefined,
+  setOutcome?: (outcome: OperationOutcome | undefined) => void
+): {
+  defaultValue: T;
+  handleSubmit: (newResource: T) => void;
+} {
+  const medplum = useMedplum();
+  const navigate = useNavigate();
+  const defaultValue = { resourceType } as T;
+
+  const handleSubmit = (newResource: T): void => {
+    if (setOutcome) {
+      setOutcome(undefined);
+    }
+    medplum
+      .createResource(newResource)
+      .then((result) => navigate('/' + result.resourceType + '/' + result.id))
+      .catch((err) => {
+        if (setOutcome) {
+          setOutcome(normalizeOperationOutcome(err));
+        }
+      });
+  };
+
+  return {
+    defaultValue,
+    handleSubmit,
+  };
+}
