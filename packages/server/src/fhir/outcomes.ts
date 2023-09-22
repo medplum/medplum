d383652e7545d68fc22ab34b45ca9adfadd75a13
Matt Willer <matt@medplum.com>
2023-09-22 11:26:31 -0700

    Add request IDs to error responses (#2907)

--- packages/server/src/fhir/outcomes.ts
+++ packages/server/src/fhir/outcomes.ts
@@ -3,6 +3,7 @@ import { OperationOutcome } from '@medplum/fhirtypes';
 import { randomUUID } from 'crypto';
 import { Response } from 'express';
 import { Result, ValidationError } from 'express-validator';
+import { getRequestContext } from '../context';
 
 export function invalidRequest(errors: Result<ValidationError>): OperationOutcome {
   return {
@@ -26,8 +27,20 @@ function getValidationErrorExpression(error: ValidationError): string[] | undefi
 }
 
 export function sendOutcome(res: Response, outcome: OperationOutcome): Response {
+  const ctx = getRequestContext();
   if (isAccepted(outcome) && outcome.issue?.[0].diagnostics) {
     res.set('Content-Location', outcome.issue[0].diagnostics);
   }
-  return res.status(getStatus(outcome)).json(outcome);
+  return res.status(getStatus(outcome)).json({
+    ...outcome,
+    extension: [
+      {
+        url: 'https://medplum.com/fhir/StructureDefinition/tracing',
+        extension: [
+          { url: 'requestId', valueUuid: ctx.requestId },
+          { url: 'traceId', valueUuid: ctx.traceId },
+        ],
+      },
+    ],
+  } as OperationOutcome);
 }
