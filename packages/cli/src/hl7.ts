6452d712299e8f45f3e8a5f5feccd48b274cfe48
Cody Ebberson <cody@ebberson.com>
2023-09-28 13:50:02 -0700

    Tests

--- packages/cli/src/hl7.ts
+++ packages/cli/src/hl7.ts
@@ -2,15 +2,7 @@ import { formatHl7DateTime, Hl7Message } from '@medplum/core';
 import { Hl7Client, Hl7Server } from '@medplum/hl7';
 import { Command } from 'commander';
 import { readFileSync } from 'fs';
-import { resolve } from 'path';
 import { createMedplumCommand } from './util/command';
-
-const HL7_NOW = formatHl7DateTime(new Date());
-const HL7_CONTROL_ID = Date.now().toString();
-const SAMPLE_HL7_MESSAGE = `MSH|^~\\&|ADTSYS|HOSPITAL|RECEIVER|DEST|${HL7_NOW}||ADT^A01|${HL7_CONTROL_ID}|P|2.5|
-EVN|A01|${HL7_NOW}||
-PID|1|12345|12345^^^HOSP^MR|123456|DOE^JOHN^MIDDLE^SUFFIX|19800101|M|||123 STREET^APT 4B^CITY^ST^12345-6789||555-555-5555||S|
-PV1|1|I|2000^2012^01||||12345^DOCTOR^DOC||||||||||1234567^DOCTOR^DOC||AMB|||||||||||||||||||||||||202309280900|`;
 
 const send = createMedplumCommand('send')
   .description('Send an HL7 v2 message via MLLP')
@@ -18,12 +10,12 @@ const send = createMedplumCommand('send')
   .argument('<port>', 'The destination port number')
   .argument('[body]', 'Optional HL7 message body')
   .option('--generate', 'Generate a sample HL7 message')
-  .option('--file', 'Read the HL7 message from a file')
+  .option('--file <file>', 'Read the HL7 message from a file')
   .action(async (host, port, body, options) => {
     if (options.generate) {
-      body = SAMPLE_HL7_MESSAGE;
+      body = generateSampleHl7Message();
     } else if (options.file) {
-      body = readFileSync(resolve(process.cwd(), body), 'utf8');
+      body = readFileSync(options.file, 'utf8');
     }
 
     if (!body) {
@@ -56,3 +48,12 @@ const listen = createMedplumCommand('listen')
   });
 
 export const hl7 = new Command('hl7').addCommand(send).addCommand(listen);
+
+export function generateSampleHl7Message(): string {
+  const now = formatHl7DateTime(new Date());
+  const controlId = Date.now().toString();
+  return `MSH|^~\\&|ADTSYS|HOSPITAL|RECEIVER|DEST|${now}||ADT^A01|${controlId}|P|2.5|
+EVN|A01|${now}||
+PID|1|12345|12345^^^HOSP^MR|123456|DOE^JOHN^MIDDLE^SUFFIX|19800101|M|||123 STREET^APT 4B^CITY^ST^12345-6789||555-555-5555||S|
+PV1|1|I|2000^2012^01||||12345^DOCTOR^DOC||||||||||1234567^DOCTOR^DOC||AMB|||||||||||||||||||||||||202309280900|`;
+}
