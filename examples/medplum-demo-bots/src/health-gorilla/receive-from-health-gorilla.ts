7965741e3f181c25ddbd01ac6c3a775bc9b05efb
Cody Ebberson <cody@ebberson.com>
2023-10-02 13:08:54 -0700

    Updated Health Gorilla callback bot (#2960)
    * Health Gorilla transaction bundle
    
    * Bundle transaction fix
    
    * Cleanup

--- examples/medplum-demo-bots/src/health-gorilla/receive-from-health-gorilla.ts
+++ examples/medplum-demo-bots/src/health-gorilla/receive-from-health-gorilla.ts
@@ -1,6 +1,16 @@
-import { allOk, BotEvent, getIdentifier, getReferenceString, MedplumClient, normalizeErrorString } from '@medplum/core';
+import {
+  allOk,
+  BotEvent,
+  convertContainedResourcesToBundle,
+  getIdentifier,
+  getReferenceString,
+  MedplumClient,
+  normalizeErrorString,
+} from '@medplum/core';
 import {
   Account,
+  Bundle,
+  BundleEntryRequest,
   DiagnosticReport,
   Observation,
   OperationOutcome,
@@ -27,23 +37,56 @@ const HEALTH_GORILLA_SYSTEM = 'https://www.healthgorilla.com';
 
 const referenceMap = new Map<string, string>();
 
+/**
+ * Main entry point for the Health Gorilla webhook bot.
+ *
+ * This bot receives a Health Gorilla resource and syncs it to Medplum.
+ *
+ * Health Gorilla uses "contained resources" to represent related resources.
+ *
+ * In Medplum, we recommend that you use references instead of contained resources,
+ * which is often better for data quality and analytics.
+ *
+ * The majority of this bot is dedicated to rewriting references from Health Gorilla.
+ *
+ * @param medplum The Medplum client.
+ * @param event The Bot execution event with a Health Gorilla resource.
+ * @returns Returns OK OperationOutcome on success, or an error OperationOutcome on failure.
+ */
 export async function handler(
   medplum: MedplumClient,
   event: BotEvent<HealthGorillaResource>
 ): Promise<OperationOutcome> {
+  const resource = event.input;
+
+  // Move the Health Gorilla resource ID to an identifier
+  if (resource.id) {
+    const identifiers = resource.identifier ?? [];
+    identifiers.push({ system: HEALTH_GORILLA_SYSTEM, value: resource.id });
+    resource.identifier = identifiers;
+  }
+
+  // Convert the Health Gorilla resource to a Medplum bundle.
+  // This moves contained resources to separate create/update operations.
+  const healthGorillaBundle = convertContainedResourcesToBundle(resource) as Bundle<HealthGorillaResource>;
+
+  // Touch up the bundle before executing
+  // This adds identifiers and ifNoneExist to the bundle entries
+  touchUpBundle(healthGorillaBundle);
+
   try {
-    const resource = event.input;
+    // Rewrite references to other resources
+    // For example, convert references to Patient and Organization resources
+    // from Health Gorilla references to Medplum references
+    await rewriteReferences(medplum, healthGorillaBundle);
 
-    if (resource.contained) {
-      for (const containedResource of resource.contained) {
-        await syncResource(medplum, containedResource as HealthGorillaResource, true);
+    // Execute the bundle
+    const result = await medplum.executeBatch(healthGorillaBundle);
+    for (const entry of result.entry ?? []) {
+      if (entry.response) {
+        console.log(entry.response.status, entry.response.location);
       }
-
-      // Remove contained resources before syncing the parent resource
-      resource.contained = undefined;
     }
-
-    await syncResource(medplum, resource);
   } catch (err) {
     console.log(normalizeErrorString(err));
   }
@@ -51,57 +94,65 @@ export async function handler(
   return allOk;
 }
 
-export async function syncResource<T extends HealthGorillaResource>(
-  medplum: MedplumClient,
-  healthGorillaResource: T,
-  contained = false
-): Promise<void> {
-  if (healthGorillaResource.resourceType === 'Account' && !healthGorillaResource.status) {
-    // Health Gorilla drops Account.status which is a required field
-    healthGorillaResource.status = 'active';
-  }
+/**
+ * Touches up the bundle before executing.
+ *
+ * As part of the conversion from Health Gorilla to Medplum, we need to add identifiers to the resources,
+ * so that we can connect/reuse resources that already exist in Medplum.
+ *
+ * We also take advantage of the "ifNoneExist" feature of FHIR to avoid creating duplicate resources.
+ *
+ * @param bundle The Health Gorilla bundle.
+ */
+function touchUpBundle(bundle: Bundle<HealthGorillaResource>): void {
+  for (const entry of bundle.entry ?? []) {
+    const resource = entry.resource as HealthGorillaResource;
+    const request = entry.request as BundleEntryRequest;
 
-  // Rewrite references to other resources
-  // For example, convert references to Patient and Organization resources
-  // from Health Gorilla references to Medplum references
-  await rewriteReferences(medplum, healthGorillaResource);
-
-  let existingResource: HealthGorillaResource | undefined = undefined;
-  let healthGorillaId: string | undefined = undefined;
-
-  const mergeResourceTypes: HealthGorillaResourceType[] = ['Account'];
-  if (mergeResourceTypes.includes(healthGorillaResource.resourceType)) {
-    // For some resource types, we attempt a "merge" operation with existing resources.
-    // For other resource types, we always create a new resource.
-    healthGorillaId = getHealthGorillaId(healthGorillaResource, contained) as string;
-    existingResource = await searchByHealthGorillaId(medplum, healthGorillaResource.resourceType, healthGorillaId);
-  }
-
-  if (existingResource) {
-    // Update the existing resource
-    const updatedResource = await medplum.updateResource({
-      ...healthGorillaResource,
-      id: existingResource.id,
-      identifier: existingResource.identifier,
-    });
-    console.log('Updated', updatedResource.resourceType, updatedResource.id);
-    if (healthGorillaResource.id) {
-      referenceMap.set('#' + healthGorillaResource.id, getReferenceString(updatedResource));
+    if (resource.resourceType === 'Account') {
+      // In Health Gorilla, Account connects a Patient and a payment method
+      // So we use the combination of those references as the Account identifier
+      const identifier = resource.guarantor?.[0]?.party?.reference + '-' + resource?.type?.coding?.[0]?.code;
+      if (!resource.identifier) {
+        resource.identifier = [];
+      }
+      resource.identifier.push({ system: HEALTH_GORILLA_SYSTEM, value: identifier });
+      request.ifNoneExist = 'identifier=' + identifier;
     }
-  } else {
-    // Create a new resource
-    const createdResource = await medplum.createResource({
-      ...healthGorillaResource,
-      id: undefined,
-      identifier: [{ system: HEALTH_GORILLA_SYSTEM, value: healthGorillaId }],
-    });
-    console.log('Created', createdResource.resourceType, createdResource.id);
-    if (healthGorillaResource.id) {
-      referenceMap.set('#' + healthGorillaResource.id, getReferenceString(createdResource));
+
+    if (resource.resourceType === 'PractitionerRole') {
+      // In Health Gorilla, PractitionerRole connects a Practitioner and an Organization
+      // So we use the combination of those references as the PractitionerRole identifier
+      const identifier = resource.practitioner?.reference + '-' + resource.organization?.reference;
+      if (!resource.identifier) {
+        resource.identifier = [];
+      }
+      resource.identifier.push({ system: HEALTH_GORILLA_SYSTEM, value: identifier });
+      request.ifNoneExist = 'identifier=' + identifier;
+    }
+
+    if (resource.resourceType === 'RequestGroup' || resource.resourceType === 'DiagnosticReport') {
+      const identifier = getIdentifier(resource, HEALTH_GORILLA_SYSTEM);
+      if (identifier) {
+        request.ifNoneExist = 'identifier=' + identifier;
+      }
+    }
+
+    if (resource.resourceType === 'ServiceRequest') {
+      const requisition = resource.requisition?.value;
+      if (requisition) {
+        request.ifNoneExist = 'requisition=' + requisition;
+      }
     }
   }
 }
 
+/**
+ * Rewrites Health Gorilla references to Medplum references.
+ *
+ * @param medplum The Medplum client.
+ * @param value An unknown value.
+ */
 async function rewriteReferences(medplum: MedplumClient, value: unknown): Promise<void> {
   if (!value) {
     return;
@@ -145,17 +196,30 @@ async function rewriteReferencesInObject(medplum: MedplumClient, obj: Record<str
   }
 }
 
-function getHealthGorillaId(resource: HealthGorillaResource, contained: boolean): string | undefined {
-  if (!contained && resource.id) {
-    return resource.id;
-  }
-  return getIdentifier(resource, HEALTH_GORILLA_SYSTEM);
-}
-
+/**
+ * Tries to find a Medplum resource by Health Gorilla ID.
+ *
+ * In most cases, this is a matter of search by "identifier" rather than "id".
+ *
+ * There are some special cases where "identifier" is not available.
+ *
+ * @param medplum The Medplum client.
+ * @param resourceType The FHIR resource type.
+ * @param id The Health Gorilla resource ID.
+ * @returns The Medplum resource, or undefined if not found.
+ */
 async function searchByHealthGorillaId(
   medplum: MedplumClient,
   resourceType: HealthGorillaResourceType,
   id: string
 ): Promise<HealthGorillaResource | undefined> {
-  return medplum.searchOne(resourceType, { identifier: id });
+  if (resourceType === 'ServiceRequest') {
+    // Special case for ServiceRequest - search by requisition instead of identifier
+    // Because Health Gorilla does not include the identifier
+    const requisition = id.split('-')[0];
+    return medplum.searchOne(resourceType, { requisition });
+  }
+
+  // Default case - search by identifier
+  return medplum.searchOne(resourceType, { identifier: `${HEALTH_GORILLA_SYSTEM}|${id}` });
 }
