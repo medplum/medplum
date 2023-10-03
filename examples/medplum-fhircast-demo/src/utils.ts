48957f34628ca0492f3fe5f5745fd4cd369aab35
Derrick Farris <derrick@medplum.com>
2023-10-03 15:08:19 -0700

    cleanup(fhircast-demo): cleanup more unused utils and types

--- examples/medplum-fhircast-demo/src/utils.ts
+++ examples/medplum-fhircast-demo/src/utils.ts
@@ -9,16 +9,6 @@ export type SubscriptionRequest = {
   endpoint?: URL;
 };
 
-export type FHIRResource = {
-  resourceType: 'Patient';
-  id: string;
-  identifier: {
-    type: {
-      coding: any[];
-    };
-  }[];
-};
-
 export type FHIRcastHubEventContext = {
   key: string;
   resource: Resource;
@@ -47,52 +37,3 @@ export function serializeHubSubscriptionRequest(subscriptionRequest: Subscriptio
   };
   return new URLSearchParams(formattedSubRequest).toString();
 }
-
-export function deserializeHubSubscriptionRequest(urlEncodedSubRequest: string): SubscriptionRequest {
-  const searchParams = new URLSearchParams(urlEncodedSubRequest);
-  if (!searchParams.has('hub.channel.type')) {
-    throw new Error('Must include `hub.channel.type` in hub subscription!');
-  }
-  if (!searchParams.has('hub.mode')) {
-    throw new Error('Must include `hub.mode` in hub subscription!');
-  }
-  if (!searchParams.has('hub.topic')) {
-    throw new Error('Must include `hub.topic` in hub subscription!');
-  }
-  if (!searchParams.has('hub.events')) {
-    throw new Error('Must include `hub.events` in hub subscription!');
-  }
-  return {
-    channelType: searchParams.get('hub.channel.type') as 'websocket',
-    mode: searchParams.get('hub.mode') as 'subscribe' | 'unsubscribe',
-    topic: searchParams.get('hub.topic') as string,
-    events: searchParams.get('hub.events')?.split(',') as string[],
-  };
-}
-
-export function isWebSocketMessage(value: unknown): value is WebSocketMessage {
-  // @ts-expect-error unknown type
-  if (!(value?.type && value?.payload)) {
-    return false;
-  }
-  return true;
-}
-
-export type WebSocketMessage = {
-  type: string;
-  payload: Record<string, any>;
-};
-
-export type WebSocketLike = {
-  send: (data: string | ArrayBufferLike) => void;
-};
-
-export type WrappedWebSocket<T extends WebSocketLike> = {
-  ws: T;
-  sendMessage: (message: WebSocketMessage) => void;
-};
-
-export type WrappedWebSocketFhir<T extends WebSocketLike> = {
-  ws: T;
-  sendMessage: (message: FHIRcastMessagePayload) => void;
-};
