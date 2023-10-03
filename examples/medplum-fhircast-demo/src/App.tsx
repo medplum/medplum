9f8bf1f47922cb34d9104bd25f049920a0ead3f4
Derrick Farris <derrick@medplum.com>
2023-10-03 11:03:25 -0700

    cleanup(fhircast-demo): remove unused styling

--- examples/medplum-fhircast-demo/src/App.tsx
+++ examples/medplum-fhircast-demo/src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
 import Publisher from './components/Publisher';
 import Subscriber from './components/Subscriber';
 
