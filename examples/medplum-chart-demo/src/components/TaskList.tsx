323f2afff1f26d28cb6d66ed79a0f3e154b0e775
James Touri <j_touri@yahoo.com>
2023-10-09 17:30:41 -0700

    Add task list

--- examples/medplum-chart-demo/src/components/TaskList.tsx
+++ examples/medplum-chart-demo/src/components/TaskList.tsx
@@ -1,111 +1,134 @@
-import { Box } from '@mantine/core';
-import { createReference } from '@medplum/core';
-import { Practitioner, Task } from '@medplum/fhirtypes';
-import { CodeableConceptDisplay, Timeline, TimelineItem, useMedplumProfile, useResource } from '@medplum/react';
-import React from 'react';
+import { Box, Text, Group, Card, Divider, Title } from '@mantine/core';
+import { formatDateTime } from '@medplum/core';
+import { Task, Resource } from '@medplum/fhirtypes';
+import {
+  CodeableConceptDisplay,
+  Timeline,
+  useResource,
+  useMedplum,
+  MedplumLink,
+  ResourceAvatar,
+  ResourceName,
+  StatusBadge,
+  ErrorBoundary,
+  AttachmentDisplay,
+} from '@medplum/react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-
-const tasks: Task[] = [
-  {
-    resourceType: 'Task',
-    id: 'example1',
-    meta: {
-      lastUpdated: '2016-03-10T22:39:32-04:00',
-    },
-    status: 'draft',
-    intent: 'order',
-    code: {
-      text: 'Fill out questionnaire',
-    },
-    focus: {
-      reference: 'MedicationRequest/medrx002',
-    },
-    for: {
-      reference: 'Patient/f001',
-    },
-    authoredOn: '2016-03-10T22:39:32-04:00',
-    lastModified: '2016-03-10T22:39:32-04:00',
-    requester: {
-      reference: 'Patient/example',
-    },
-    owner: {
-      reference: 'Practitioner/example',
-    },
-  },
-  {
-    resourceType: 'Task',
-    id: 'example2',
-    meta: {
-      lastUpdated: '2016-03-10T22:39:32-04:00',
-    },
-    status: 'draft',
-    intent: 'order',
-    code: {
-      text: 'Approve lab imaging for patient',
-    },
-    focus: {
-      reference: 'MedicationRequest/medrx002',
-    },
-    for: {
-      reference: 'Patient/f001',
-    },
-    authoredOn: '2016-03-10T22:39:32-04:00',
-    lastModified: '2016-03-10T22:39:32-04:00',
-    requester: {
-      reference: 'Patient/example',
-    },
-    owner: {
-      reference: 'Practitioner/example',
-    },
-  },
-  {
-    resourceType: 'Task',
-    id: 'example3',
-    meta: {
-      lastUpdated: '2016-03-10T22:39:32-04:00',
-    },
-    status: 'draft',
-    intent: 'order',
-    code: {
-      text: 'Refill Request',
-    },
-    focus: {
-      reference: 'MedicationRequest/medrx002',
-    },
-    for: {
-      reference: 'Patient/f001',
-    },
-    authoredOn: '2016-03-10T22:39:32-04:00',
-    lastModified: '2016-03-10T22:39:32-04:00',
-    requester: {
-      reference: 'Patient/example',
-    },
-    owner: {
-      reference: 'Practitioner/example',
-    },
-  },
-];
 
 export function TaskList(): JSX.Element | null {
   const { id } = useParams();
-  const author = createReference(useMedplumProfile() as Practitioner);
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const medplum = useMedplum();
   const patient = useResource({ reference: `Patient/${id}` });
+  useEffect(() => {
+    medplum.searchResources('Task', `patient=${id}`).then((response) => {
+      setTasks(response);
+    });
+  });
 
   if (!patient) {
     return null;
   }
-
   return (
-    <Box sx={{ width: 600 }}>
-      <Timeline>
-        {tasks.map((task) => (
-          <TimelineItem key={task.id} profile={author} resource={task}>
-            <Box pt="sm" px="xl" pb="xl">
-              <CodeableConceptDisplay value={task.code} />
+    <Card sx={{ width: 600 }} withBorder padding="sm" radius="md" mx="md" my="xl" shadow="xs">
+      <Title>Required Action</Title>
+      <Box>
+        <Timeline>
+          {tasks.map((task, idx) => (
+            <>
+              <FocusTimeline key={task.id} task={task} />
+              {idx !== tasks.length - 1 ? <Divider /> : null}
+            </>
+          ))}
+        </Timeline>
+      </Box>
+    </Card>
+  );
+}
+
+function FocusTimeline(props: { task: Task }): JSX.Element | undefined {
+  const task = props.task;
+
+  const focusedResource = useResource(task.focus);
+  if (!focusedResource) {
+    return;
+  }
+  return (
+    <TaskItem key={task.id} profile={task.owner} resource={focusedResource} task={task}>
+      <Box pt="sm" px="xl" pb="xl">
+        <TaskType resource={focusedResource} task={task} />
+      </Box>
+    </TaskItem>
+  );
+}
+
+function TaskType(props: { resource: Resource; task: Task }): JSX.Element {
+  const resource = props.resource;
+  function renderResourceContent(resource: Resource) {
+    switch (resource.resourceType) {
+      case 'Communication':
+        if (resource.topic) {
+          return <CodeableConceptDisplay value={resource.topic} />;
+        }
+        return resource.payload?.[0].contentString ? (
+          <Text>{resource.payload?.[0].contentString}</Text>
+        ) : (
+          <AttachmentDisplay value={resource.payload?.[0].contentAttachment} />
+        );
+      case 'MedicationRequest':
+        return <CodeableConceptDisplay value={resource.medicationCodeableConcept} />;
+      case 'Questionnaire':
+        return <Text>{resource.name}</Text>;
+      case 'DiagnosticReport':
+        return <CodeableConceptDisplay value={resource.code} />;
+      default:
+        return <div />;
+    }
+  }
+
+  if (!resource) {
+    return <div />;
+  }
+  return <MedplumLink to={props.task}>{renderResourceContent(resource)}</MedplumLink>;
+}
+
+function TaskItem(props: any): JSX.Element {
+  const { task, resource, profile, padding } = props;
+  const author = profile ?? resource.meta?.author;
+  const dateTime = props.dateTime ?? resource.meta?.lastUpdated;
+  console.log(props.task.status);
+  console.log(task.input[0].type)
+  return (
+    <>
+      <Group position="apart" spacing={8} my="sm">
+        <ResourceAvatar value={author} link={true} size="md" />
+        <div style={{ flex: 1 }}>
+          <Text size="sm">
+            <ResourceName color="dark" weight={500} value={author} link={true} />
+          </Text>
+          {'status' in props.resource && (
+            <Box mt={2} mb={2}>
+              <StatusBadge status={task.status as string} />
             </Box>
-          </TimelineItem>
-        ))}
-      </Timeline>
-    </Box>
+          )}
+          <Text size="xs">
+            <MedplumLink color="dimmed" to={props.task}>
+              {formatDateTime(dateTime)}
+            </MedplumLink>
+            <Text component="span" color="dimmed" mx={8}>
+              &middot;
+            </Text>
+            <MedplumLink color="dimmed" to={props.resource}>
+              <CodeableConceptDisplay value={task.input[0].type} />
+            </MedplumLink>
+          </Text>
+        </div>
+      </Group>
+      <ErrorBoundary>
+        {padding && <div style={{ padding: '0 16px 16px 16px' }}>{props.children}</div>}
+        {!padding && <>{props.children}</>}
+      </ErrorBoundary>
+    </>
   );
 }
