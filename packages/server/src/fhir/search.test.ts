a305a74aa5d7b2331f5cf2e7ffd980caea667146
Cody Ebberson <cody@ebberson.com>
2023-10-17 11:26:12 -0700

    Fixes #3027 - added re operator for _filter

--- packages/server/src/fhir/search.test.ts
+++ packages/server/src/fhir/search.test.ts
@@ -2821,6 +2821,29 @@ describe('FHIR Search', () => {
       expect(result.entry).toHaveLength(0);
     }));
 
+  test('_filter re', () =>
+    withTestContext(async () => {
+      const patient = await systemRepo.createResource<Patient>({
+        resourceType: 'Patient',
+        name: [{ given: ['Eve'] }],
+        managingOrganization: { reference: 'Organization/' + randomUUID() },
+      });
+
+      const result = await systemRepo.search({
+        resourceType: 'Patient',
+        filters: [
+          {
+            code: '_filter',
+            operator: Operator.EQUALS,
+            value: 'organization re ' + patient.managingOrganization?.reference,
+          },
+        ],
+      });
+
+      expect(result.entry).toHaveLength(1);
+      expect(result.entry?.[0]?.resource?.id).toEqual(patient.id);
+    }));
+
   test('Lookup table exact match with comma disjunction', () =>
     withTestContext(async () => {
       const family = randomUUID();
