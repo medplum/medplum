ede4681053ff89333ed8dc5c0d0238d8b545a741
Matthew Willer <matt@medplum.com>
2023-10-18 15:12:12 -0700

    Add expression (path) to OutcomeError message display

--- packages/react/src/OperationOutcomeAlert/OperationOutcomeAlert.tsx
+++ packages/react/src/OperationOutcomeAlert/OperationOutcomeAlert.tsx
@@ -17,7 +17,7 @@ export function OperationOutcomeAlert(props: OperationOutcomeAlertProps): JSX.El
     <Alert icon={<IconAlertCircle size={16} />} color="red">
       {issues.map((issue) => (
         <div data-testid="text-field-error" key={issue.details?.text}>
-          {issue.details?.text}
+          {issue.details?.text} {issue.expression && `(${issue.expression[0]})`}
         </div>
       ))}
     </Alert>
