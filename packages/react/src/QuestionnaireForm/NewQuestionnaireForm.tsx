074847e561cc3aa0157e8fce3553350cc98e84cc
James Touri <j_touri@yahoo.com>
2023-10-18 14:51:21 -0700

    new implementation

---
+++ packages/react/src/QuestionnaireForm/NewQuestionnaireForm.tsx
@@ -0,0 +1,298 @@
+import { Anchor, Button, Group, Stack, Stepper, Title } from '@mantine/core';
+import { createReference, getExtension, getReferenceString, ProfileResource } from '@medplum/core';
+import {
+  Questionnaire,
+  QuestionnaireItem,
+  QuestionnaireItemInitial,
+  QuestionnaireResponse,
+  QuestionnaireResponseItem,
+  QuestionnaireResponseItemAnswer,
+  Reference,
+} from '@medplum/fhirtypes';
+import React, { useEffect, useState } from 'react';
+import { Form } from '../Form/Form';
+import { useMedplum } from '../MedplumProvider/MedplumProvider.context';
+import { useResource } from '../useResource/useResource';
+import { isQuestionEnabled, QuestionnaireItemType } from '../utils/questionnaire';
+import { QuestionnaireFormItem } from './QuestionnaireFormItem/NewQuestionnaireFormItem';
+
+export interface QuestionnaireFormProps {
+  questionnaire: Questionnaire;
+  subject?: Reference;
+  submitButtonText?: string;
+  onSubmit?: (response: QuestionnaireResponse) => void;
+}
+
+export function QuestionnaireForm(props: QuestionnaireFormProps): JSX.Element | null {
+  const medplum = useMedplum();
+  const source = medplum.getProfile();
+  const [schemaLoaded, setSchemaLoaded] = useState(false);
+  const questionnaire = useResource(props.questionnaire);
+  const [response, setResponse] = useState<QuestionnaireResponse | undefined>();
+  const [activePage, setActivePage] = useState(0);
+
+  useEffect(() => {
+    medplum
+      .requestSchema('Questionnaire')
+      .then(() => medplum.requestSchema('QuestionnaireResponse'))
+      .then(() => setSchemaLoaded(true))
+      .catch(console.log);
+  }, [medplum]);
+
+  useEffect(() => {
+    setResponse(questionnaire ? buildInitialResponse(questionnaire) : undefined);
+  }, [questionnaire]);
+
+  function setItems(newResponseItems: QuestionnaireResponseItem[]): void {
+    // const currentItems = response?.item ?? [];
+    // const mergedItems = mergeItems(currentItems, newResponseItems);
+
+    const newResponse: QuestionnaireResponse = {
+      resourceType: 'QuestionnaireResponse',
+      item: newResponseItems,
+    };
+
+    setResponse(newResponse);
+  }
+
+  if (!schemaLoaded || !questionnaire) {
+    return null;
+  }
+
+  const numberOfPages = getNumberOfPages(questionnaire);
+  const nextStep = (): void => setActivePage((current) => current + 1);
+  const prevStep = (): void => setActivePage((current) => current - 1);
+
+  return (
+    <Form
+      testid="questionnaire-form"
+      onSubmit={() => {
+        if (props.onSubmit && response) {
+          props.onSubmit({
+            ...response,
+            questionnaire: getReferenceString(questionnaire),
+            subject: props.subject,
+            source: createReference(source as ProfileResource),
+            authored: new Date().toISOString(),
+            status: 'completed',
+          });
+        }
+      }}
+    >
+      {questionnaire.title && <Title>{questionnaire.title}</Title>}
+      {questionnaire.item && (
+        <QuestionnairePageSequence
+          items={questionnaire.item ?? []}
+          response={response}
+          onChange={setItems}
+          renderPages={numberOfPages > 1}
+          activePage={activePage}
+          numberOfPages={numberOfPages}
+          nextStep={nextStep}
+          prevStep={prevStep}
+        />
+      )}
+    </Form>
+  );
+}
+
+interface QuestionnairePageSequenceProps {
+  items: QuestionnaireItem[];
+  response?: QuestionnaireResponse;
+  renderPages: boolean;
+  activePage?: number;
+  numberOfPages: number;
+  submitButtonText?: string;
+  onChange: (items: QuestionnaireResponseItem[]) => void;
+  nextStep: () => void;
+  prevStep: () => void;
+}
+
+function QuestionnairePageSequence(props: QuestionnairePageSequenceProps): JSX.Element {
+  const { items, response, activePage, onChange, nextStep, prevStep, numberOfPages, renderPages, submitButtonText } =
+    props;
+  const form = items.map((item) => {
+    const itemResponse = response?.item?.find((i) => i.linkId === item.linkId);
+
+    const repeatedGroup = <QuestionnaireRepeatedGroup item={item} response={itemResponse} onChange={onChange} />;
+
+    if (renderPages) {
+      return (
+        <Stepper.Step key={item.linkId} label={item.text}>
+          {repeatedGroup}
+        </Stepper.Step>
+      );
+    }
+    return repeatedGroup;
+  });
+
+  return (
+    <>
+      {renderPages && (
+        <Stepper active={activePage ?? 0} allowNextStepsSelect={false} p={6}>
+          {form}
+        </Stepper>
+      )}
+      {!renderPages && <Stack>{form}</Stack>}
+      <ButtonGroup
+        activePage={activePage ?? 0}
+        numberOfPages={numberOfPages}
+        nextStep={nextStep}
+        prevStep={prevStep}
+        submitButtonText={submitButtonText}
+      />
+    </>
+  );
+}
+
+interface QuestionnaireGroupProps {
+  item: QuestionnaireItem;
+  response?: QuestionnaireResponseItem;
+  onChange: (items: QuestionnaireResponseItem[]) => void;
+}
+
+function QuestionnaireRepeatedGroup(props: QuestionnaireGroupProps): JSX.Element {
+  const [items, setItems] = useState([props.item]);
+  const [responses, setResponses] = useState([props.response]);
+
+  return (
+    <>
+      {items.map((item, index) => (
+        <QuestionnaireGroup item={item} response={responses[index]} onChange={props.onChange} />
+      ))}
+      {props.item.repeats && <Anchor onClick={() => console.log('')}>Add Group</Anchor>}
+    </>
+  );
+}
+
+function QuestionnaireGroup(props: QuestionnaireGroupProps): JSX.Element | null {
+  const { item, response, onChange } = props;
+  // use context + hook 
+  if (!isQuestionEnabled(item, [])) {
+    return null;
+  }
+  return (
+    <>
+      {(item.item ?? []).map((item, index) => {
+        if (item.type === QuestionnaireItemType.group) {
+          return item.repeats ? (
+            <QuestionnaireRepeatedGroup
+              key={index}
+              item={item}
+              response={response?.item?.find((i) => i.linkId === item.linkId)}
+              onChange={onChange}
+            />
+          ) : (
+            <QuestionnaireGroup key={index} item={item} response={response} onChange={onChange} />
+          );
+        }
+        return <QuestionnaireRepeatableItem item={item} response={response} onChange={onChange} key={index} />;
+      })}
+    </>
+  );
+}
+
+interface QuestionnaireRepeatableItemProps {
+  item: QuestionnaireItem;
+  response: QuestionnaireResponseItem;
+  onChange: (items: QuestionnaireResponseItem[]) => void;
+}
+
+function QuestionnaireRepeatableItem(props: QuestionnaireRepeatableItemProps): JSX.Element | null {
+  const { item, response, onChange } = props;
+  const [number, setNumber] = useState(1);
+  if (isQuestionEnabled(item, [])) {
+    return null;
+  }
+  const showAddButton =
+    item?.repeats && item.type !== QuestionnaireItemType.choice && item.type !== QuestionnaireItemType.openChoice;
+
+  return (
+    <>
+      {[...Array(number)].map((item, index) => (
+        <QuestionnaireFormItem item={item} response={response} onChange={onChange} index={index} />
+      ))}
+      {showAddButton && <Anchor onClick={() => setNumber((n) => n + 1)}>Add Item</Anchor>}
+    </>
+  );
+}
+
+function buildInitialResponse(questionnaire: Questionnaire): QuestionnaireResponse {
+  const response: QuestionnaireResponse = {
+    resourceType: 'QuestionnaireResponse',
+    questionnaire: getReferenceString(questionnaire),
+    item: buildInitialResponseItems(questionnaire.item),
+  };
+
+  return response;
+}
+
+function buildInitialResponseItems(items: QuestionnaireItem[] | undefined): QuestionnaireResponseItem[] {
+  return items?.map(buildInitialResponseItem) ?? [];
+}
+
+function buildInitialResponseItem(item: QuestionnaireItem): QuestionnaireResponseItem {
+  return {
+    id: generateId(),
+    linkId: item.linkId,
+    text: item.text,
+    item: buildInitialResponseItems(item.item),
+    answer: item.initial?.map(buildInitialResponseAnswer) ?? [],
+  };
+}
+
+let nextId = 1;
+function generateId(): string {
+  return 'id-' + nextId++;
+}
+
+function buildInitialResponseAnswer(answer: QuestionnaireItemInitial): QuestionnaireResponseItemAnswer {
+  // This works because QuestionnaireItemInitial and QuestionnaireResponseItemAnswer
+  // have the same properties.
+  return { ...answer };
+}
+
+/**
+ * Returns the number of pages in the questionnaire.
+ *
+ * By default, a questionnaire is represented as a simple single page questionnaire,
+ * so the default return value is 1.
+ *
+ * If the questionnaire has a page extension on the first item, then the number of pages
+ * is the number of top level items in the questionnaire.
+ *
+ * @param questionnaire The questionnaire to get the number of pages for.
+ * @returns The number of pages in the questionnaire. Default is 1.
+ */
+function getNumberOfPages(questionnaire: Questionnaire): number {
+  const firstItem = questionnaire?.item?.[0];
+  if (firstItem) {
+    const extension = getExtension(firstItem, 'http://hl7.org/fhir/StructureDefinition/questionnaire-itemControl');
+    if (extension?.valueCodeableConcept?.coding?.[0]?.code === 'page') {
+      return (questionnaire.item as QuestionnaireItem[]).length;
+    }
+  }
+  return 1;
+}
+
+interface ButtonGroupProps {
+  activePage: number;
+  numberOfPages: number;
+  submitButtonText?: string;
+  nextStep: () => void;
+  prevStep: () => void;
+}
+
+function ButtonGroup(props: ButtonGroupProps): JSX.Element {
+  const showBackButton = props.activePage > 0;
+  const showNextButton = props.activePage < props.numberOfPages - 1;
+  const showSubmitButton = props.activePage === props.numberOfPages - 1;
+
+  return (
+    <Group position="right" mt="xl" spacing="xs">
+      {showBackButton && <Button onClick={props.prevStep}>Back</Button>}
+      {showNextButton && <Button onClick={props.nextStep}>Next</Button>}
+      {showSubmitButton && <Button type="submit">{props.submitButtonText ?? 'Submit'}</Button>}
+    </Group>
+  );
+}
---
+++ packages/react/src/QuestionnaireForm/QuestionnaireFormItem/NewQuestionnaireFormItem.tsx
@@ -0,0 +1,435 @@
+import { Checkbox, Group, MultiSelect, NativeSelect, Radio, Textarea, TextInput } from '@mantine/core';
+import {
+  TypedValue,
+  capitalize,
+  deepEquals,
+  formatCoding,
+  getElementDefinition,
+  getTypedPropertyValue,
+  stringify,
+} from '@medplum/core';
+import {
+  QuestionnaireItem,
+  QuestionnaireItemAnswerOption,
+  QuestionnaireItemInitial,
+  QuestionnaireResponseItem,
+  QuestionnaireResponseItemAnswer,
+} from '@medplum/fhirtypes';
+import React, { ChangeEvent } from 'react';
+import { AttachmentInput } from '../../AttachmentInput/AttachmentInput';
+import { CheckboxFormSection } from '../../CheckboxFormSection/CheckboxFormSection';
+import { DateTimeInput } from '../../DateTimeInput/DateTimeInput';
+import { QuantityInput } from '../../QuantityInput/QuantityInput';
+import { ReferenceInput } from '../../ReferenceInput/ReferenceInput';
+import { ResourcePropertyDisplay } from '../../ResourcePropertyDisplay/ResourcePropertyDisplay';
+import {
+  getNewMultiSelectValues,
+  getQuestionnaireItemReferenceTargetTypes,
+  QuestionnaireItemType,
+} from '../../utils/questionnaire';
+import { ValueSetAutocomplete } from '../../ValueSetAutocomplete/ValueSetAutocomplete';
+
+export interface QuestionnaireFormItemProps {
+  item: QuestionnaireItem;
+  index: number;
+  response: QuestionnaireResponseItem;
+  onChange: (newResponseItem: QuestionnaireResponseItem) => void;
+}
+
+export function QuestionnaireFormItem(props: QuestionnaireFormItemProps): JSX.Element | null {
+  const item = props.item;
+  const response = props.response;
+
+  function onChangeAnswer(
+    newResponseAnswer: QuestionnaireResponseItemAnswer | QuestionnaireResponseItemAnswer[]
+  ): void {
+    let updatedAnswers: QuestionnaireResponseItemAnswer[];
+    if (Array.isArray(newResponseAnswer)) {
+      // It's a multi-select case, so use the array directly.
+      updatedAnswers = newResponseAnswer;
+    } else {
+      // It's a single answer case.
+      updatedAnswers = [newResponseAnswer];
+    }
+    props.onChange({
+      id: response.id,
+      linkId: response.linkId,
+      text: item.text,
+      answer: updatedAnswers,
+    });
+  }
+
+  const type = item.type;
+  if (!type) {
+    return null;
+  }
+
+  const name = item.linkId;
+  if (!name) {
+    return null;
+  }
+
+  const initial = item.initial && item.initial.length > 0 ? item.initial[0] : undefined;
+  const defaultValue =
+    getCurrentAnswer(response) ?? getTypedPropertyValue({ type: 'QuestionnaireItemInitial', value: initial }, 'value');
+
+  switch (type) {
+    case QuestionnaireItemType.display:
+      return <p key={props.item.linkId}>{props.item.text}</p>;
+    case QuestionnaireItemType.boolean:
+      return (
+        <CheckboxFormSection key={props.item.linkId} title={props.item.text} htmlFor={props.item.linkId}>
+          <Checkbox
+            id={props.item.linkId}
+            name={props.item.linkId}
+            defaultChecked={defaultValue?.value}
+            onChange={(e) => onChangeAnswer({ valueBoolean: e.currentTarget.checked })}
+          />
+        </CheckboxFormSection>
+      );
+    case QuestionnaireItemType.decimal:
+      return (
+        <TextInput
+          type="number"
+          step="any"
+          id={name}
+          name={name}
+          defaultValue={defaultValue?.value}
+          onChange={(e) => onChangeAnswer({ valueDecimal: e.currentTarget.valueAsNumber })}
+        />
+      );
+    case QuestionnaireItemType.integer:
+      return (
+        <TextInput
+          type="number"
+          step={1}
+          id={name}
+          name={name}
+          defaultValue={defaultValue?.value}
+          onChange={(e) => onChangeAnswer({ valueInteger: e.currentTarget.valueAsNumber })}
+        />
+      );
+    case QuestionnaireItemType.date:
+      return (
+        <TextInput
+          type="date"
+          id={name}
+          name={name}
+          defaultValue={defaultValue?.value}
+          onChange={(e) => onChangeAnswer({ valueDate: e.currentTarget.value })}
+        />
+      );
+    case QuestionnaireItemType.dateTime:
+      return (
+        <DateTimeInput
+          name={name}
+          defaultValue={defaultValue?.value}
+          onChange={(newValue: string) => onChangeAnswer({ valueDateTime: newValue })}
+        />
+      );
+    case QuestionnaireItemType.time:
+      return (
+        <TextInput
+          type="time"
+          id={name}
+          name={name}
+          defaultValue={defaultValue?.value}
+          onChange={(e) => onChangeAnswer({ valueTime: e.currentTarget.value })}
+        />
+      );
+    case QuestionnaireItemType.string:
+    case QuestionnaireItemType.url:
+      return (
+        <TextInput
+          id={name}
+          name={name}
+          defaultValue={defaultValue?.value}
+          onChange={(e) => onChangeAnswer({ valueString: e.currentTarget.value })}
+        />
+      );
+    case QuestionnaireItemType.text:
+      return (
+        <Textarea
+          id={name}
+          name={name}
+          defaultValue={defaultValue?.value}
+          onChange={(e) => onChangeAnswer({ valueString: e.currentTarget.value })}
+        />
+      );
+    case QuestionnaireItemType.attachment:
+      return (
+        <Group py={4}>
+          <AttachmentInput
+            name={name}
+            defaultValue={defaultValue?.value}
+            onChange={(newValue) => onChangeAnswer({ valueAttachment: newValue })}
+          />
+        </Group>
+      );
+    case QuestionnaireItemType.reference:
+      return (
+        <ReferenceInput
+          name={name}
+          targetTypes={getQuestionnaireItemReferenceTargetTypes(item)}
+          defaultValue={defaultValue?.value}
+          onChange={(newValue) => onChangeAnswer({ valueReference: newValue })}
+        />
+      );
+    case QuestionnaireItemType.quantity:
+      return (
+        <QuantityInput
+          name={name}
+          defaultValue={defaultValue?.value}
+          onChange={(newValue) => onChangeAnswer({ valueQuantity: newValue })}
+          disableWheel
+        />
+      );
+    case QuestionnaireItemType.choice:
+    case QuestionnaireItemType.openChoice:
+      if (isDropDownChoice(item)) {
+        return (
+          <QuestionnaireChoiceDropDownInput
+            name={name}
+            item={item}
+            initial={initial}
+            response={response}
+            onChangeAnswer={(e) => onChangeAnswer(e)}
+          />
+        );
+      } else {
+        return (
+          <QuestionnaireChoiceSetInput
+            name={name}
+            item={item}
+            initial={initial}
+            response={response}
+            onChangeAnswer={(e) => onChangeAnswer(e)}
+          />
+        );
+      }
+    default:
+      return null;
+  }
+}
+
+interface QuestionnaireChoiceInputProps {
+  name: string;
+  item: QuestionnaireItem;
+  initial: QuestionnaireItemInitial | undefined;
+  response: QuestionnaireResponseItem;
+  onChangeAnswer: (newResponseAnswer: QuestionnaireResponseItemAnswer | QuestionnaireResponseItemAnswer[]) => void;
+}
+
+function QuestionnaireChoiceDropDownInput(props: QuestionnaireChoiceInputProps): JSX.Element {
+  const { name, item, initial, response } = props;
+  const initialValue = getTypedPropertyValue({ type: 'QuestionnaireItemInitial', value: initial }, 'value') as
+    | TypedValue
+    | undefined;
+
+  const data = [''];
+  if (item.answerOption) {
+    for (const option of item.answerOption) {
+      const optionValue = getTypedPropertyValue(
+        { type: 'QuestionnaireItemAnswerOption', value: option },
+        'value'
+      ) as TypedValue;
+      data.push(typedValueToString(optionValue) as string);
+    }
+  }
+
+  const defaultValue =
+    getCurrentAnswer(response) ?? getTypedPropertyValue({ type: 'QuestionnaireItemInitial', value: initial }, 'value');
+
+  if (item.repeats) {
+    const { propertyName, data } = formatSelectData(props.item);
+    const currentAnswer = getCurrentMultiSelectAnswer(response);
+
+    return (
+      <MultiSelect
+        data={data}
+        placeholder="Select items"
+        searchable
+        defaultValue={currentAnswer || [typedValueToString(initialValue)]}
+        onChange={(selected) => {
+          const values = getNewMultiSelectValues(selected, propertyName, item);
+          props.onChangeAnswer(values);
+        }}
+      />
+    );
+  }
+
+  return (
+    <NativeSelect
+      id={name}
+      name={name}
+      onChange={(e: ChangeEvent<HTMLSelectElement>) => {
+        const index = e.currentTarget.selectedIndex;
+        if (index === 0) {
+          props.onChangeAnswer({});
+          return;
+        }
+        const option = (item.answerOption as QuestionnaireItemAnswerOption[])[index - 1];
+        const optionValue = getTypedPropertyValue(
+          { type: 'QuestionnaireItemAnswerOption', value: option },
+          'value'
+        ) as TypedValue;
+        const propertyName = 'value' + capitalize(optionValue.type);
+        props.onChangeAnswer({ [propertyName]: optionValue.value });
+      }}
+      defaultValue={(formatCoding(defaultValue?.value) || defaultValue?.value) ?? typedValueToString(initialValue)}
+      data={data}
+    />
+  );
+}
+
+function QuestionnaireChoiceSetInput(props: QuestionnaireChoiceInputProps): JSX.Element {
+  const { name, item, initial, onChangeAnswer, response } = props;
+  if (item.answerValueSet) {
+    return <ValueSetAutocomplete binding={item.answerValueSet} onChange={onChangeAnswer} />;
+  }
+  return (
+    <QuestionnaireChoiceRadioInput
+      name={name}
+      item={item}
+      initial={initial}
+      response={response}
+      onChangeAnswer={onChangeAnswer}
+    />
+  );
+}
+
+function QuestionnaireChoiceRadioInput(props: QuestionnaireChoiceInputProps): JSX.Element {
+  const { name, item, initial, onChangeAnswer, response } = props;
+  const valueElementDefinition = getElementDefinition('QuestionnaireItemAnswerOption', 'value[x]');
+  const initialValue = getTypedPropertyValue({ type: 'QuestionnaireItemInitial', value: initial }, 'value') as
+    | TypedValue
+    | undefined;
+
+  const options: [string, TypedValue][] = [];
+  let defaultValue = undefined;
+  if (item.answerOption) {
+    for (let i = 0; i < item.answerOption.length; i++) {
+      const option = item.answerOption[i];
+      const optionName = `${name}-option-${i}`;
+      const optionValue = getTypedPropertyValue(
+        { type: 'QuestionnaireItemAnswerOption', value: option },
+        'value'
+      ) as TypedValue;
+
+      if (initialValue && stringify(optionValue) === stringify(initialValue)) {
+        defaultValue = optionName;
+      }
+      options.push([optionName, optionValue]);
+    }
+  }
+
+  const defaultAnswer = getCurrentAnswer(response);
+  const answerLinkId = getCurrentRadioAnswer(options, defaultAnswer);
+
+  return (
+    <Radio.Group
+      name={name}
+      value={answerLinkId ?? defaultValue}
+      onChange={(newValue) => {
+        const option = options.find((option) => option[0] === newValue);
+        if (option) {
+          const optionValue = option[1];
+          const propertyName = 'value' + capitalize(optionValue.type);
+          onChangeAnswer({ [propertyName]: optionValue.value });
+        }
+      }}
+    >
+      {options.map(([optionName, optionValue]) => (
+        <Radio
+          key={optionName}
+          id={optionName}
+          value={optionName}
+          py={4}
+          label={
+            <ResourcePropertyDisplay
+              property={valueElementDefinition}
+              propertyType={optionValue.type}
+              value={optionValue.value}
+            />
+          }
+        />
+      ))}
+    </Radio.Group>
+  );
+}
+
+function getItemValue(answer: QuestionnaireResponseItemAnswer): TypedValue {
+  const itemValue = getTypedPropertyValue({ type: 'QuestionnaireItemAnswer', value: answer }, 'value') as TypedValue;
+  return itemValue;
+}
+
+function getCurrentAnswer(response: QuestionnaireResponseItem, index: number = 0): TypedValue {
+  const results = response.answer;
+  return getItemValue(results?.[index] ?? {});
+}
+
+function getCurrentMultiSelectAnswer(response: QuestionnaireResponseItem): string[] {
+  const results = response.answer;
+  if (!results) {
+    return [];
+  }
+  const typedValues = results.map((a) => getItemValue(a));
+  return typedValues.map((type) => formatCoding(type?.value) || type?.value);
+}
+
+function getCurrentRadioAnswer(options: [string, TypedValue][], defaultAnswer: TypedValue): string | undefined {
+  return options.find((option) => deepEquals(option[1].value, defaultAnswer?.value))?.[0];
+}
+
+function typedValueToString(typedValue: TypedValue | undefined): string | undefined {
+  if (!typedValue) {
+    return undefined;
+  }
+  if (typedValue.type === 'CodeableConcept') {
+    return typedValue.value.coding[0].display;
+  }
+  if (typedValue.type === 'Coding') {
+    return typedValue.value.display;
+  }
+  return typedValue.value.toString();
+}
+
+function isDropDownChoice(item: QuestionnaireItem): boolean {
+  return !!item.extension?.some(
+    (e) =>
+      e.url === 'http://hl7.org/fhir/StructureDefinition/questionnaire-itemControl' &&
+      e.valueCodeableConcept?.coding?.[0]?.code === 'drop-down'
+  );
+}
+
+interface MultiSelect {
+  value: any;
+  label: any;
+}
+
+interface FormattedData {
+  propertyName: string;
+  data: MultiSelect[];
+}
+
+function formatSelectData(item: QuestionnaireItem): FormattedData {
+  if (item.answerOption?.length === 0) {
+    return { propertyName: '', data: [] };
+  }
+  const option = (item.answerOption as QuestionnaireItemAnswerOption[])[0];
+  const optionValue = getTypedPropertyValue(
+    { type: 'QuestionnaireItemAnswerOption', value: option },
+    'value'
+  ) as TypedValue;
+  const propertyName = 'value' + capitalize(optionValue.type);
+
+  const data = (item.answerOption ?? []).map((a) => ({
+    value: getValueAndLabel(a, propertyName),
+    label: getValueAndLabel(a, propertyName),
+  }));
+  return { propertyName, data };
+}
+
+function getValueAndLabel(option: QuestionnaireItemAnswerOption, propertyName: string): string | undefined {
+  return formatCoding(option.valueCoding) || option[propertyName as keyof QuestionnaireItemAnswerOption]?.toString();
+}
