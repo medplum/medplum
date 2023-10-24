948fac27e953b6791223badde2fe6030a737c929
James Touri <j_touri@yahoo.com>
2023-10-24 11:07:04 -0700

    Fix nested choices

--- packages/react/src/QuestionnaireForm/QuestionnaireForm.tsx
+++ packages/react/src/QuestionnaireForm/QuestionnaireForm.tsx
@@ -42,6 +42,8 @@ export function QuestionnaireForm(props: QuestionnaireFormProps): JSX.Element | 
 
   function setItems(newResponseItems: QuestionnaireResponseItem | QuestionnaireResponseItem[]): void {
     const currentItems = response?.item ?? [];
+    console.log('currentItems', currentItems);
+    console.log('newResponseItems', newResponseItems);
     const mergedItems = mergeItems(
       currentItems,
       Array.isArray(newResponseItems) ? newResponseItems : [newResponseItems]
@@ -117,6 +119,7 @@ function mergeItems(
   prevItems: QuestionnaireResponseItem[],
   newItems: QuestionnaireResponseItem[]
 ): QuestionnaireResponseItem[] {
+  // Using responseId as the key because it is guaranteed to be unique
   const result: QuestionnaireResponseItem[] = [];
   const usedIds = new Set<string>();
 
@@ -132,7 +135,6 @@ function mergeItems(
     }
   }
 
-  // Add items from newItems that were not in prevItems.
   for (const newItem of newItems) {
     if (!usedIds.has(newItem.id as string)) {
       result.push(newItem);
