d7ebd4a203cbcf2656cea0123228819cac1d4db5
Derrick Farris <derrick@medplum.com>
2023-10-31 09:28:26 -0700

    feat(sever/fhircast): add `GetCurrentContext` (`PowerCast`)

--- packages/server/src/fhircast/routes.test.ts
+++ packages/server/src/fhircast/routes.test.ts
@@ -1,4 +1,4 @@
-import { ContentType } from '@medplum/core';
+import { ContentType, createFhircastMessagePayload } from '@medplum/core';
 import { randomUUID } from 'crypto';
 import express from 'express';
 import request from 'supertest';
@@ -25,6 +25,7 @@ describe('FHIRCast routes', () => {
 
     expect(res.status).toBe(200);
     expect(res.body.eventsSupported).toBeDefined();
+    expect(res.body.getCurrentSupport).toBe(true);
     expect(res.body.websocketSupport).toBe(true);
     expect(res.body.webhookSupport).toBe(false);
     expect(res.body.fhircastVersion).toBe('STU3');
@@ -122,5 +123,26 @@ describe('FHIRCast routes', () => {
       .set('Authorization', 'Bearer ' + accessToken);
     expect(res.status).toBe(200);
     expect(res.body).toEqual([]);
+  });
+
+  test('Get context after *-open event', async () => {
+    const payload = createFhircastMessagePayload('my-topic', 'imagingstudy-open', [
+      { key: 'study', resource: { id: 'def-456', resourceType: 'ImagingStudy' } },
+      { key: 'patient', resource: { id: 'xyz-789', resourceType: 'Patient' } },
+    ]);
+    const publishRes = await request(app)
+      .post('/fhircast/STU3/my-topic')
+      .set('Content-Type', ContentType.JSON)
+      .set('Authorization', 'Bearer ' + accessToken)
+      .send(payload);
+    expect(publishRes.status).toBe(201);
+
+    // Get the current subscription status
+    // Non-standard FHIRCast extension to support Nuance PowerCast Hub
+    const contextRes = await request(app)
+      .get('/fhircast/STU3/my-topic')
+      .set('Authorization', 'Bearer ' + accessToken);
+    expect(contextRes.status).toBe(200);
+    expect(contextRes.body).toEqual(payload.event.context);
   });
 });
--- packages/server/src/fhircast/routes.ts
+++ packages/server/src/fhircast/routes.ts
@@ -1,4 +1,4 @@
-import { badRequest } from '@medplum/core';
+import { FhircastMessagePayload, badRequest } from '@medplum/core';
 import { Request, Response, Router } from 'express';
 import { body, validationResult } from 'express-validator';
 import { asyncWrap } from '../async';
@@ -19,6 +19,7 @@ publicRoutes.get('/.well-known/fhircast-configuration', (_req: Request, res: Res
   res.status(200).json({
     eventsSupported: [
       'syncerror',
+      'heartbeat',
       'userlogout',
       'userhibernate',
       'patient-open',
@@ -30,6 +31,7 @@ publicRoutes.get('/.well-known/fhircast-configuration', (_req: Request, res: Res
       'diagnosticreport-open',
       'diagnosticreport-close',
     ],
+    getCurrentSupport: true,
     websocketSupport: true,
     webhookSupport: false,
     fhircastVersion: 'STU3',
@@ -92,13 +94,28 @@ protectedRoutes.post(
       return;
     }
 
-    await getRedis().publish(req.params.topic as string, JSON.stringify(req.body));
+    const { event } = req.body as FhircastMessagePayload;
+    const stringifiedBody = JSON.stringify(req.body);
+    // Check if this an open event
+    if (event['hub.event'].endsWith('-open')) {
+      // NOTE: We get the topic from the event since per the spec, the URL should eventually NOT contain the topic but another slug linked to topic
+      await getRedis().set(`${event['hub.topic']}-latest`, stringifiedBody);
+    }
+    await getRedis().publish(req.params.topic as string, stringifiedBody);
     res.status(201).json({ success: true, event: body });
   })
 );
 
 // Get the current subscription status
-// Non-standard FHIRCast extension to support Nuance PowerCast Hub
-protectedRoutes.get('/:topic', (req: Request, res: Response) => {
-  res.status(200).json([]);
-});
+protectedRoutes.get(
+  '/:topic',
+  asyncWrap(async (req: Request, res: Response) => {
+    const latestEvent = await getRedis().get(`${req.params.topic}-latest`);
+    // Non-standard FHIRCast extension to support Nuance PowerCast Hub
+    if (!latestEvent) {
+      res.status(200).json([]);
+      return;
+    }
+    res.status(200).json(JSON.parse(latestEvent).event.context);
+  })
+);
