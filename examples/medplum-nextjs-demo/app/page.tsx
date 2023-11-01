cb99375242fa7ed10e175043f5fcfe8549783bf4
James Touri <j_touri@yahoo.com>
2023-11-01 15:52:42 -0700

    Update Next Example project to use App Router (#3197)
    * 1st
    
    * change to app router
    
    * Use metadata
    
    * Remove types in tsconfig
    
    * prettier

--- examples/medplum-nextjs-demo/app/page.tsx
+++ examples/medplum-nextjs-demo/app/page.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import { Button, Title } from '@mantine/core';
 import { Container, ResourceTable, SignInForm, useMedplum, useMedplumProfile } from '@medplum/react';
+import { Suspense } from 'react';
 
 // Medplum can autodetect Google Client ID from origin, but only if using window.location.host.
 // Because window.location.host is not available on the server, we must use a constant value.
@@ -7,7 +10,7 @@ import { Container, ResourceTable, SignInForm, useMedplum, useMedplumProfile } f
 // You will need to register your own Google Client ID for your own domain.
 const googleClientId = '921088377005-3j1sa10vr6hj86jgmdfh2l53v3mp7lfi.apps.googleusercontent.com';
 
-export default function IndexPage(): JSX.Element {
+export default function HomePage(): JSX.Element {
   const medplum = useMedplum();
   const profile = useMedplumProfile();
   return (
@@ -17,13 +20,13 @@ export default function IndexPage(): JSX.Element {
       </Title>
       {!profile && <SignInForm googleClientId={googleClientId}>Sign in</SignInForm>}
       {profile && (
-        <>
+        <Suspense fallback={<div>Loading...</div>}>
           <Title order={3} my="xl">
             Profile
           </Title>
           <ResourceTable value={profile} ignoreMissingValues />
           <Button onClick={() => medplum.signOut()}>Sign out</Button>
-        </>
+        </Suspense>
       )}
     </Container>
   );
---
+++ examples/medplum-nextjs-demo/app/root.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { MantineProvider } from '@mantine/core';
+import { MedplumClient } from '@medplum/core';
+import { MedplumProvider } from '@medplum/react';
+
+const medplum = new MedplumClient({
+  // Uncomment this to run against the server on your localhost
+  // baseUrl: 'http://localhost:8103/',
+
+  // Handle unauthenticated requests
+  onUnauthenticated: () => (window.location.href = '/'),
+
+  // Use Next.js fetch
+  fetch: (url: string, options?: any) => fetch(url, options),
+});
+
+export default function Root(props: { children: React.ReactNode }): JSX.Element {
+  return (
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <MedplumProvider medplum={medplum}>{props.children}</MedplumProvider>
+    </MantineProvider>
+  );
+}
