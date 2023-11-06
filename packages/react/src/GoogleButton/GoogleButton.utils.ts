dc8074e3ab1adf9e96abe841e3b7441dd2908af7
Matt Long <matt@mattlong.org>
2023-11-06 15:58:57 -0800

    process is a reference error

--- packages/react/src/GoogleButton/GoogleButton.utils.ts
+++ packages/react/src/GoogleButton/GoogleButton.utils.ts
@@ -5,9 +5,20 @@ export function getGoogleClientId(clientId: string | undefined): string | undefi
 
   if (typeof window !== 'undefined') {
     const origin = window.location.protocol + '//' + window.location.host;
-    const authorizedOrigins = process.env.GOOGLE_AUTH_ORIGINS?.split(',') ?? [];
-    if (authorizedOrigins.includes(origin)) {
-      return process.env.GOOGLE_CLIENT_ID;
+
+    try {
+      const authorizedOrigins = process.env.GOOGLE_AUTH_ORIGINS?.split(',') ?? [];
+
+      if (authorizedOrigins.includes(origin)) {
+        return process.env.GOOGLE_CLIENT_ID;
+      }
+    } catch (e) {
+      if (e instanceof ReferenceError) {
+        // static replacement of process.env.GOOGLE_AUTH_ORIGINS by Vite did not occur,
+        // https://vitejs.dev/guide/env-and-mode.html
+        return undefined;
+      }
+      throw e;
     }
   }
 
