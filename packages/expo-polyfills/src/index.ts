27f2fea65ba1a97522ab74550df42b035ffeead9
Derrick Farris <derrick@medplum.com>
2023-11-06 16:21:15 -0800

    refactor(expo-polyfills): remove inlined `MemoryStorage`

--- packages/expo-polyfills/src/index.ts
+++ packages/expo-polyfills/src/index.ts
@@ -13,6 +13,7 @@ type ExtendedExpoCrypto = typeof expoWebCrypto & {
 
 export function polyfillMedplumWebAPIs(): void {
   if (typeof window.crypto === 'undefined') {
+    // eslint-disable-next-line no-inner-declarations
     async function polyfilledDigest(algorithm: AlgorithmIdentifier, data: BufferSource): Promise<ArrayBuffer> {
       return digest(algorithm as CryptoDigestAlgorithm, data);
     }
@@ -38,71 +39,6 @@ export function polyfillMedplumWebAPIs(): void {
 
   let _sessionStorage: Storage;
   if (typeof window.sessionStorage === 'undefined') {
-    // /**
-    //  * The MemoryStorage class is a minimal in-memory implementation of the Storage interface.
-    //  */
-    // class MemoryStorage implements Storage {
-    //   private data: Map<string, string>;
-
-    //   constructor() {
-    //     this.data = new Map<string, string>();
-    //   }
-
-    //   /**
-    //    * Returns the number of key/value pairs.
-    //    * @returns The number of key/value pairs.
-    //    */
-    //   get length(): number {
-    //     return this.data.size;
-    //   }
-
-    //   /**
-    //    * Removes all key/value pairs, if there are any.
-    //    */
-    //   clear(): void {
-    //     this.data.clear();
-    //   }
-
-    //   /**
-    //    * Returns the current value associated with the given key, or null if the given key does not exist.
-    //    * @param key - The specified storage key.
-    //    * @returns The current value associated with the given key, or null if the given key does not exist.
-    //    */
-    //   getItem(key: string): string | null {
-    //     return this.data.get(key) ?? null;
-    //   }
-
-    //   /**
-    //    * Sets the value of the pair identified by key to value, creating a new key/value pair if none existed for key previously.
-    //    * @param key - The storage key.
-    //    * @param value - The new value.
-    //    */
-    //   setItem(key: string, value: string | null): void {
-    //     if (value) {
-    //       this.data.set(key, value);
-    //     } else {
-    //       this.data.delete(key);
-    //     }
-    //   }
-
-    //   /**
-    //    * Removes the key/value pair with the given key, if a key/value pair with the given key exists.
-    //    * @param key - The storage key.
-    //    */
-    //   removeItem(key: string): void {
-    //     this.data.delete(key);
-    //   }
-
-    //   /**
-    //    * Returns the name of the nth key, or null if n is greater than or equal to the number of key/value pairs.
-    //    * @param index - The numeric index.
-    //    * @returns The nth key.
-    //    */
-    //   key(index: number): string | null {
-    //     return Array.from(this.data.keys())[index];
-    //   }
-    // }
-
     Object.defineProperty(window, 'sessionStorage', {
       configurable: true,
       enumerable: true,
