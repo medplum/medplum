6d8a0b89e937a769a2427ebc02dd779d94cda5c7
dillonstreator <dillonstreator@gmail.com>
2023-11-09 15:20:36 -0600

    use existing type

--- packages/server/src/util/validator.ts
+++ packages/server/src/util/validator.ts
@@ -1,13 +1,10 @@
 import { RequestHandler, Request, Response, NextFunction } from 'express';
-import { ContextRunner, ValidationChain, validationResult } from 'express-validator';
-import { Middleware } from 'express-validator/src/base';
+import { ContextRunner, validationResult } from 'express-validator';
 import { sendOutcome, invalidRequest } from '../fhir/outcomes';
 
-type ExpressValidator = ValidationChain | (Middleware & ContextRunner);
-
-export function makeValidationMiddleware(expressValidators: ExpressValidator[]): RequestHandler {
+export function makeValidationMiddleware(runners: ContextRunner[]): RequestHandler {
   return async function (req: Request, res: Response, next: NextFunction) {
-    await Promise.all(expressValidators.map(ev => ev.run(req)));
+    await Promise.all(runners.map(runner => runner.run(req)));
 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
