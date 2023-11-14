9ee0efde6147dfdd851e6b09893cc42b5b5c48e0
Derrick Farris <derrick@medplum.com>
2023-11-13 16:20:52 -0800

    cleanup: revert change of `baseUrl`

--- examples/medplum-react-native-example/src/App.tsx
+++ examples/medplum-react-native-example/src/App.tsx
@@ -6,7 +6,7 @@ import Home from './Home';
 const medplum = new MedplumClient({
   // Enter your Medplum connection details here
   // See MedplumClient docs for more details
-  baseUrl: 'http://172.16.102.3:8103',
+  baseUrl: 'http://localhost:8103',
   // ------------------------------------------------------------------------------
   // If you are testing this out with your physical Android / iOS device and not an emulator,
   // you will need to put your computer's local IP address here, for example:
