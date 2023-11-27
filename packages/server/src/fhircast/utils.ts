1e006cb25f248a3125f97b14678031f84aca2c8c
Derrick Farris <derrick@medplum.com>
2023-11-27 15:28:08 -0800

    cleanup(fhircast): fix find-and-replace

--- packages/server/src/fhircast/utils.ts
+++ packages/server/src/fhircast/utils.ts
@@ -5,11 +5,11 @@ export async function getTopicForUser(userId: string): Promise<string> {
   const newTopic = generateId();
   const topicKey = `::fhircast::topic:${userId}`;
 
-  // Sets the topic topic to the new topic if it doesn't exist, then gets either existing or the new topic
+  // Sets the topic key to the new topic if it doesn't exist, then gets either existing or the new topic
   // 3600 seconds is the configured expiry time for the associated token, so this should work
   // Per the spec, the lease time of a subscription should not exceed the token expiry time
   // Source: https://fhircast.org/specification/STU2/#session-discovery:~:text=.%20If%20using%20OAuth%202.0%2C%20the%20Hub%20SHALL%20limit%20the%20subscription%20lease%20seconds%20to%20be%20less%20than%20or%20equal%20to%20the%20access%20token%27s%20expiration.
   const topic = await getRedis().set(topicKey, newTopic, 'EX', 3600, 'NX', 'GET');
-  // If topic is null, it means there was no value at the given topic, so we should return the new topic
+  // If topic is null, it means there was no value at the given key, so we should return the new topic
   return topic || newTopic;
 }
