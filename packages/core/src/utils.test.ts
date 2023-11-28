8c94b8d0e61c7f1ad3630bbfd4fa3d6a812f8723
Derrick Farris <derrick@medplum.com>
2023-11-28 14:48:23 -0800

    feat(fhircast): add `heartbeat` event (#3350)
    * feat(fhircast): add `heartbeat` event
    
    * refactor(fhircast): `HeartbeatStore` to store heartbeats
    
    * test(fhircast): fix test for `heartbeat`
    
    * test(fhircast): add back `advanceTimers`
    
    * refactor(fhircast): `heartbeat` on one timer
    
    * cleanup(fhircast): fix find-and-replace
    
    * refactor(fhircast/ws): reduce complexity
    
    * cleanup: fix comment
    
    * refactor(fhircast/ws): reuse scoped `redis`
    
    * refactor(fhircast/ws): cleanup `cleanupPromises` on close of WS

--- packages/core/src/utils.test.ts
+++ packages/core/src/utils.test.ts
@@ -1,11 +1,13 @@
 import { Attachment, CodeableConcept, ObservationDefinition, Patient, Resource } from '@medplum/fhirtypes';
 import { ContentType } from './contenttype';
 import {
+  ResourceWithCode,
   arrayBufferToBase64,
   arrayBufferToHex,
   calculateAge,
   calculateAgeString,
   capitalize,
+  createDeferredPromise,
   createReference,
   deepClone,
   deepEquals,
@@ -34,7 +36,6 @@ import {
   preciseLessThanOrEquals,
   preciseRound,
   resolveId,
-  ResourceWithCode,
   setCodeBySystem,
   setIdentifier,
   splitN,
@@ -1017,5 +1018,26 @@ describe('Core Utils', () => {
       splitN('_has:Observation:subject:encounter:Encounter._has:DiagnosticReport:encounter:result.status', ':', 3)
     ).toEqual(['_has', 'Observation', 'subject:encounter:Encounter._has:DiagnosticReport:encounter:result.status']);
     expect(splitN('organization', ':', 2)).toEqual(['organization']);
+  });
+
+  describe('createDeferredPromise', () => {
+    test('Created promise has all props', () => {
+      const deferredPromise = createDeferredPromise();
+      expect(deferredPromise.promise).toBeInstanceOf(Promise);
+      expect(deferredPromise.resolve).toBeInstanceOf(Function);
+      expect(deferredPromise.reject).toBeInstanceOf(Function);
+    });
+
+    test('Calling `resolve` resolves promise', async () => {
+      const deferredPromise = createDeferredPromise();
+      expect(() => deferredPromise.resolve()).not.toThrow();
+      await expect(deferredPromise.promise).resolves;
+    });
+
+    test('Calling `reject` rejects promise', async () => {
+      const deferredPromise = createDeferredPromise();
+      expect(() => deferredPromise.reject(new Error('Rejected!'))).not.toThrow();
+      await expect(deferredPromise.promise).rejects.toThrow(/Rejected!/);
+    });
   });
 });
