d670fe35a76db8cb208ebd63b1377fdf7e24e1cb
dillonstreator <dillonstreator@gmail.com>
2023-11-29 12:58:24 -0600

    Revert "`BinarySource` supporting `ReadableStream` (#3411)" (#3421)
    This reverts commit 7c80d0f26b772ab4d716051e613b6e42265b0a5e.

--- packages/core/src/client.ts
+++ packages/core/src/client.ts
@@ -434,7 +434,7 @@ export interface PatchOperation {
 /**
  * Source for a FHIR Binary.
  */
-export type BinarySource = string | File | Blob | Uint8Array | ReadableStream;
+export type BinarySource = string | File | Blob | Uint8Array;
 
 /**
  * Email address definition.
@@ -1885,17 +1885,7 @@ export class MedplumClient extends EventTarget {
       xhr.setRequestHeader('Cache-Control', 'no-cache, no-store, max-age=0');
       xhr.setRequestHeader('Content-Type', contentType);
       xhr.setRequestHeader('X-Medplum', 'extended');
-      if (data instanceof ReadableStream) {
-        const writableStream = new WritableStream({
-          write(chunk) {
-            xhr.send(chunk);
-          },
-        });
-
-        data.pipeTo(writableStream).catch(console.error);
-      } else {
-        xhr.send(data);
-      }
+      xhr.send(data);
     });
   }
 
