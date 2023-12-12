64a269fe44856e90b91b9ab82e8a49192d9d18db
Matt Long <matt@mattlong.org>
2023-12-12 15:18:05 -0800

    Use centralized resources from mock

--- packages/react/src/BackboneElementInput/BackboneElementInput.test.tsx
+++ packages/react/src/BackboneElementInput/BackboneElementInput.test.tsx
@@ -1,12 +1,9 @@
 import { globalSchema, indexStructureDefinitionBundle, InternalSchemaElement, TypeInfo } from '@medplum/core';
-import { MockClient } from '@medplum/mock';
+import { FishPatientResources, MockClient } from '@medplum/mock';
 import { MedplumProvider } from '@medplum/react-hooks';
 import { act, render, screen, within } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import { BackboneElementInput, BackboneElementInputProps } from './BackboneElementInput';
-import { readFileSync } from 'fs';
-import { resolve } from 'path';
-import { Patient } from '@medplum/fhirtypes';
 
 const valueSetComposeProperty: InternalSchemaElement = {
   path: 'ValueSet.compose',
@@ -61,34 +58,6 @@ globalSchema.types['ValueSetCompose'] = {
 
 const medplum = new MockClient();
 
-const shortyTheFish: Patient = {
-  resourceType: 'Patient',
-  id: 'Shorty',
-  meta: {
-    profile: ['http://example.org/fhir/fish/StructureDefinition/fish-patient'],
-  },
-  extension: [
-    {
-      url: 'http://example.org/fhir/fish/StructureDefinition/fish-species',
-      valueCodeableConcept: {
-        coding: [
-          {
-            code: '47978005',
-            system: 'http://snomed.info/sct',
-            display: 'Carpiodes cyprinus (organism)',
-          },
-        ],
-      },
-    },
-  ],
-  name: [
-    {
-      given: ['Shorty'],
-      family: 'Koi-Fish',
-    },
-  ],
-};
-
 describe('BackboneElementInput', () => {
   async function setup(args: BackboneElementInputProps): Promise<void> {
     await act(async () => {
@@ -120,20 +89,18 @@ describe('BackboneElementInput', () => {
     expect(screen.getByText('Foo not implemented')).toBeInTheDocument();
   });
 
-  function readJson(testFilename: string): any {
-    return JSON.parse(readFileSync(resolve('src', '__test__', testFilename), 'utf8'));
-  }
-
   test('Resource with profile', async () => {
-    const fishPatientProfile = readJson('StructureDefinition-fish-patient.json');
-    const fishSpeciesProfile = readJson('StructureDefinition-fish-species.json');
+    const fishPatientProfile = FishPatientResources.getFishPatientProfileSD();
+    const fishSpeciesProfile = FishPatientResources.getFishSpeciesExtensionSD();
+    const fishPatient = FishPatientResources.getSampleFishPatient();
+
     for (const profile of [fishPatientProfile, fishSpeciesProfile]) {
       indexStructureDefinitionBundle([profile], profile.url);
     }
     await setup({
       typeName: fishPatientProfile.name,
       profileUrl: fishPatientProfile.url,
-      defaultValue: shortyTheFish,
+      defaultValue: fishPatient,
       onChange: () => {},
     });
 
@@ -156,7 +123,7 @@ describe('BackboneElementInput', () => {
     expect(screen.getByText('Species')).toBeInTheDocument();
     expect(screen.getByText('The species of the fish.')).toBeInTheDocument();
 
-    // Shorty's species
+    // fishPatient's species
     expect(screen.getByText('Carpiodes cyprinus (organism)')).toBeInTheDocument();
   });
 });
