74286a4317549ae6b9a6b1c2b1dea058dc4a2d34
Matt Long <ml@medplum.com>
2023-12-14 16:14:41 -0800

    Handles existing as well

--- packages/react/src/ReferenceInput/ReferenceInput.tsx
+++ packages/react/src/ReferenceInput/ReferenceInput.tsx
@@ -1,5 +1,5 @@
 import { Group, NativeSelect } from '@mantine/core';
-import { MedplumClient, createReference } from '@medplum/core';
+import { MedplumClient, createReference, isEmpty } from '@medplum/core';
 import { Reference, Resource, ResourceType, StructureDefinition } from '@medplum/fhirtypes';
 import { useEffect, useMemo, useState } from 'react';
 import { ResourceInput } from '../ResourceInput/ResourceInput';
@@ -30,47 +30,63 @@ export function ReferenceInput(props: ReferenceInputProps): JSX.Element {
   const [targetType, setTargetType] = useState<TargetType | undefined>(() =>
     getInitialTargetType(props.defaultValue, targetTypes)
   );
-  const [resourceType, setResourceType] = useState<ResourceType | undefined>(() =>
-    getInitialResourceType(props.defaultValue, targetTypes)
-  );
+  const [resourceType, setResourceType] = useState<ResourceType | undefined>();
+
   const searchCriteria = useMemo<ReferenceInputProps['searchCriteria']>(() => {
-    console.log(targetType);
     if (targetType?.type !== 'profile' || !targetType?.value) {
       return props.searchCriteria;
     }
 
-    console.log({ ...props.searchCriteria, _profile: targetType.value });
     return { ...props.searchCriteria, _profile: targetType.value };
   }, [props.searchCriteria, targetType]);
 
   useEffect(() => {
-    // if targetType not set or resourceType is already known, nothing to do
-    if (!targetType || targetType.resourceType) {
-      return;
+    const promises: Promise<TargetType>[] = [];
+    let fetchedAtLeastOne: boolean = false;
+    for (const tt of targetTypes) {
+      if (tt.type === 'profile' && isEmpty(tt.resourceType)) {
+        console.log(`fetching ${tt.value}`);
+        const promise = fetchResourceType(medplum, tt.value)
+          .then((partialSD) => {
+            if (partialSD?.type) {
+              return { ...tt, resourceType: partialSD.type satisfies string };
+            }
+            return tt;
+          })
+          .catch((reason) => {
+            console.error(reason);
+            return tt;
+          });
+        promises.push(promise);
+        fetchedAtLeastOne ||= true;
+      } else {
+        promises.push(Promise.resolve(tt));
+      }
     }
 
-    fetchResourceType(medplum, targetType.value)
-      .then((resp) => {
-        if (resp?.type) {
-          const newTargetType = { ...targetType, resourceType: resp.type satisfies string };
-          setTargetType(newTargetType);
-          setResourceType(newTargetType.resourceType satisfies string as ResourceType);
-
-          // given the way getInitialTargetType handles defaultValue, i.e. the function
-          // will create a TargetType even if defaultValue is not found in targetTypes,
-          // it's okay if targetType isn't found in targetTypes
-          const index = targetTypes.indexOf(targetType);
-          if (index >= 0) {
-            const newTargetTypes = [...targetTypes];
-            newTargetTypes[index] = newTargetType;
-            setTargetTypes(newTargetTypes);
-          } else {
-            console.log('not in array');
+    Promise.all(promises)
+      .then((results) => {
+        if (fetchedAtLeastOne) {
+          if (targetType) {
+            const index = results.findIndex((tt) => tt.resourceType === targetType.resourceType);
+            if (index >= 0) {
+              console.log('updated target type', results[index] === targetType);
+              setTargetType(results[index]);
+            } else {
+              console.log(`defaultValue had unexpected resourceType: ${targetType.resourceType}`);
+            }
           }
+          setTargetTypes(results);
         }
       })
       .catch(console.error);
   }, [medplum, targetType, targetTypes]);
+
+  useEffect(() => {
+    if (targetType?.resourceType !== resourceType) {
+      setResourceType(targetType?.resourceType as ResourceType);
+    }
+  }, [resourceType, targetType]);
 
   function setValueHelper(newValue: Reference | undefined): void {
     setValue(newValue);
@@ -78,7 +94,6 @@ export function ReferenceInput(props: ReferenceInputProps): JSX.Element {
       props.onChange(newValue);
     }
   }
-  console.log(targetType);
 
   return (
     <Group spacing="xs" grow noWrap>
@@ -121,15 +136,19 @@ export function ReferenceInput(props: ReferenceInputProps): JSX.Element {
   );
 }
 
-function getTargetTypes(targetTypes: string[] | undefined): TargetType[] {
+function getTargetTypes(resourceTypesOrProfileUrls: string[] | undefined): TargetType[] {
   const results: TargetType[] = [];
-  if (!targetTypes || targetTypes.length === 0 || (targetTypes.length === 1 && targetTypes[0] === 'Resource')) {
+  if (
+    !resourceTypesOrProfileUrls ||
+    resourceTypesOrProfileUrls.length === 0 ||
+    (resourceTypesOrProfileUrls.length === 1 && resourceTypesOrProfileUrls[0] === 'Resource')
+  ) {
     return results;
   }
 
-  for (const type of targetTypes) {
+  for (const type of resourceTypesOrProfileUrls) {
+    // TODO is there a less hacky way to distinguish resourceType from URLs?
     if (type.includes('/')) {
-      // TODO is there a better way to
       results.push({ type: 'profile', value: type });
     } else {
       results.push({ type: 'resourceType', value: type, resourceType: type });
@@ -138,33 +157,21 @@ function getTargetTypes(targetTypes: string[] | undefined): TargetType[] {
   return results;
 }
 
-function getInitialResourceType(
-  defaultValue: Reference | undefined,
-  targetTypes: TargetType[]
-): ResourceType | undefined {
-  const defaultValueResourceType = defaultValue?.reference?.split('/')[0];
-  if (defaultValueResourceType) {
-    return defaultValueResourceType as ResourceType;
-  }
-
-  if (targetTypes.length > 0 && targetTypes[0].type === 'resourceType') {
-    return targetTypes[0].resourceType as ResourceType;
-  }
-
-  return undefined;
-}
-
 function getInitialTargetType(defaultValue: Reference | undefined, targetTypes: TargetType[]): TargetType | undefined {
   const defaultValueResourceType = defaultValue?.reference?.split('/')[0];
-  console.log(defaultValue, defaultValueResourceType);
   if (defaultValueResourceType) {
-    return (
-      targetTypes.find((tt) => tt.resourceType === defaultValueResourceType) ?? {
-        type: 'resourceType',
-        value: defaultValueResourceType,
-        resourceType: defaultValueResourceType as ResourceType,
-      }
-    );
+    const targetType = targetTypes.find((tt) => tt.resourceType === defaultValueResourceType);
+    if (targetType) {
+      return targetType;
+    }
+
+    const orphan: TargetType = {
+      type: 'resourceType',
+      value: defaultValueResourceType,
+      resourceType: defaultValueResourceType,
+    };
+    console.log(`initializing with orphaned targetType: ${JSON.stringify(orphan)}`);
+    return orphan;
   }
 
   if (targetTypes.length > 0) {
@@ -173,7 +180,8 @@ function getInitialTargetType(defaultValue: Reference | undefined, targetTypes: 
 
   return undefined;
 }
-interface ResourceTypeResponse {
+
+interface ResourceTypeGraphQLResponse {
   readonly data: {
     readonly StructureDefinitionList: Pick<StructureDefinition, 'type'>[];
   };
@@ -182,13 +190,14 @@ async function fetchResourceType(
   medplum: MedplumClient,
   profileUrl: string
 ): Promise<Pick<StructureDefinition, 'type'> | undefined> {
+  // TODO{profiles}: centralize _sort for profiles
   const query = `{
       StructureDefinitionList(url: "${profileUrl}", _sort: "_lastUpdated", _count: 1) {
         type
       }
     }`.replace(/\s+/g, ' ');
 
-  const response = (await medplum.graphql(query)) as ResourceTypeResponse;
+  const response = (await medplum.graphql(query)) as ResourceTypeGraphQLResponse;
 
   return response.data.StructureDefinitionList[0];
 }
