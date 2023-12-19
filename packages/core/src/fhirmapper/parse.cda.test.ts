f3ad9aeab1a643eaf4232e68314edfb35dc36435
Cody Ebberson <cody@ebberson.com>
2023-12-18 19:04:53 -0800

    FHIR Mapper - nested objects and dependent rules (#3577)
    * FHIR Mapper - nested objects and dependent rules
    
    * Fixed prototype-polluting assignment warning

---
+++ packages/core/src/fhirmapper/parse.cda.test.ts
@@ -0,0 +1,33 @@
+import { readJson } from '@medplum/definitions';
+import { Bundle } from '@medplum/fhirtypes';
+import { indexStructureDefinitionBundle } from '../typeschema/types';
+import { parseMappingLanguage } from './parse';
+
+describe('FHIR Mapping Language parser - C-CDA maps', () => {
+  beforeAll(() => {
+    indexStructureDefinitionBundle(readJson('fhir/r4/profiles-types.json') as Bundle);
+    indexStructureDefinitionBundle(readJson('fhir/r4/profiles-resources.json') as Bundle);
+  });
+
+  test('Simplest possible transform', () => {
+    // https://build.fhir.org/mapping-tutorial.html#step1
+
+    const map = `
+    group CdaToBundle(source cda : ClinicalDocument, target bundle : Bundle) {
+      cda ->  bundle.entry as e, 
+        e.resource = create('Composition') as composition, 
+        composition.id = uuid() as uuid,
+        e.fullUrl = append('urn:uuid:',uuid),
+        bundle.entry as e2, 
+        e2.resource = create('Patient') as patient,
+        patient.id = uuid() as uuid2,
+        e2.fullUrl = append('urn:uuid:',uuid2)
+        then {
+          cda then ClinicalDocumentToBundle(cda, patient, composition, bundle) "cdatobundle";
+        } "ClinicalDocumentToBody";
+    }
+    `;
+
+    expect(parseMappingLanguage(map)).toBeDefined();
+  });
+});
--- packages/core/src/fhirmapper/transform.ts
+++ packages/core/src/fhirmapper/transform.ts
@@ -3,129 +3,177 @@ import {
   StructureMapGroup,
   StructureMapGroupInput,
   StructureMapGroupRule,
+  StructureMapGroupRuleDependent,
   StructureMapGroupRuleSource,
   StructureMapGroupRuleTarget,
 } from '@medplum/fhirtypes';
 import { evalFhirPathTyped } from '../fhirpath/parse';
 import { getTypedPropertyValue, toJsBoolean, toTypedValue } from '../fhirpath/utils';
+import { TypedValue } from '../types';
 
 interface TransformContext {
   parent?: TransformContext;
   variables?: Record<string, any>;
 }
 
-export function structureMapTransform(structureMap: StructureMap, input: any): any {
+export function structureMapTransform(structureMap: StructureMap, input: any[]): any[] {
   return evalStructureMap({}, structureMap, input);
 }
 
-function evalStructureMap(ctx: TransformContext, structureMap: StructureMap, input: any): any {
+function evalStructureMap(ctx: TransformContext, structureMap: StructureMap, input: any[]): any[] {
   const groups = structureMap.group as StructureMapGroup[];
+
+  // Hoist groups by name
   for (const group of groups) {
-    input = evalGroup(ctx, group, input);
+    setVariable(ctx, group.name as string, group);
   }
-  return input;
+
+  // Only execute the first group - other groups can be called by name
+  return evalGroup(ctx, groups[0], input);
 }
 
-function evalGroup(ctx: TransformContext, group: StructureMapGroup, input: any): any {
-  let sourceInput = undefined;
-  let targetInput = undefined;
+function evalGroup(ctx: TransformContext, group: StructureMapGroup, input: any[]): any[] {
+  const sourceDefinitions: StructureMapGroupInput[] = [];
+  const targetDefinitions: StructureMapGroupInput[] = [];
 
-  for (const input of group.input as StructureMapGroupInput[]) {
-    if (input.mode === 'source') {
-      sourceInput = input;
+  for (const inputDefinition of group.input as StructureMapGroupInput[]) {
+    if (inputDefinition.mode === 'source') {
+      sourceDefinitions.push(inputDefinition);
     }
-    if (input.mode === 'target') {
-      targetInput = input;
+    if (inputDefinition.mode === 'target') {
+      targetDefinitions.push(inputDefinition);
     }
   }
 
-  if (!sourceInput) {
-    throw new Error('Missing source input');
+  if (sourceDefinitions.length === 0) {
+    throw new Error('Missing source definitions');
   }
 
-  if (!targetInput) {
-    throw new Error('Missing target input');
+  if (targetDefinitions.length === 0) {
+    throw new Error('Missing target definitions');
+  }
+
+  if (input.length < sourceDefinitions.length) {
+    throw new Error(`Not enough arguments (got ${input.length}, min ${sourceDefinitions.length})`);
+  }
+
+  if (input.length > sourceDefinitions.length + targetDefinitions.length) {
+    throw new Error(
+      `Too many arguments (got ${input.length}, max ${sourceDefinitions.length + targetDefinitions.length})`
+    );
   }
 
   const variables: Record<string, any> = {};
-  const result = {};
+  const outputs = [];
+  let inputIndex = 0;
 
-  if (sourceInput) {
-    variables[sourceInput.name as string] = input;
+  for (const sourceDefinition of sourceDefinitions) {
+    safeAssign(variables, sourceDefinition.name as string, input[inputIndex++]);
   }
 
-  if (targetInput) {
-    variables[targetInput.name as string] = result;
+  for (const targetDefinition of targetDefinitions) {
+    const output = input[inputIndex++] ?? {};
+    safeAssign(variables, targetDefinition.name as string, output);
+    outputs.push(output);
   }
 
   const newContext: TransformContext = { parent: ctx, variables };
 
-  for (const rule of group.rule as StructureMapGroupRule[]) {
-    evalRule(newContext, rule);
+  if (group.rule) {
+    for (const rule of group.rule) {
+      evalRule(newContext, rule);
+    }
   }
 
-  return result;
+  return outputs;
 }
 
 function evalRule(ctx: TransformContext, rule: StructureMapGroupRule): void {
-  for (const source of rule.source as StructureMapGroupRuleSource[]) {
-    evalSource(ctx, source);
+  if (rule.source) {
+    for (const source of rule.source) {
+      evalSource(ctx, source);
+    }
   }
-  for (const target of rule.target as StructureMapGroupRuleTarget[]) {
-    evalTarget(ctx, target);
+  if (rule.target) {
+    for (const target of rule.target) {
+      evalTarget(ctx, target);
+    }
+  }
+  if (rule.rule) {
+    for (const childRule of rule.rule) {
+      evalRule(ctx, childRule);
+    }
+  }
+  if (rule.dependent) {
+    for (const dependent of rule.dependent) {
+      evalDependent(ctx, dependent);
+    }
   }
 }
 
 function evalSource(ctx: TransformContext, source: StructureMapGroupRuleSource): void {
   const sourceContext = getVariable(ctx, source.context as string);
-  const sourceElement = source.element as string;
+  const sourceElement = source.element;
+  if (!sourceElement) {
+    return;
+  }
+
   const sourceValue = evalFhirPathTyped(sourceElement, [toTypedValue(sourceContext)]);
   if (!sourceValue || sourceValue.length === 0) {
     return;
   }
 
   if (source.condition) {
-    const conditionExpression = source.condition as string;
-    const conditionInput = [toTypedValue(sourceContext)];
-    const conditionVariables = { [source.variable as string]: sourceValue[0] };
-    const conditionResult = evalFhirPathTyped(conditionExpression, conditionInput, conditionVariables);
-    if (!toJsBoolean(conditionResult)) {
+    if (!evalCondition(sourceContext, { [source.variable as string]: sourceValue[0] }, source.condition)) {
       return;
     }
   }
 
   if (source.check) {
-    const checkExpression = source.check as string;
-    const checkInput = [toTypedValue(sourceContext)];
-    const checkVariables = { [source.variable as string]: sourceValue[0] };
-    const checkResult = evalFhirPathTyped(checkExpression, checkInput, checkVariables);
-    if (!toJsBoolean(checkResult)) {
-      throw new Error('Check failed: ' + checkExpression);
+    if (!evalCondition(sourceContext, { [source.variable as string]: sourceValue[0] }, source.check)) {
+      throw new Error('Check failed: ' + source.check);
     }
   }
 
-  if (!ctx.variables) {
-    ctx.variables = {};
+  if (source.variable) {
+    setVariable(ctx, source.variable, sourceValue[0].value);
   }
+}
 
-  ctx.variables[source.variable as string] = sourceValue[0].value;
+function evalCondition(input: any, variables: Record<string, TypedValue>, condition: string): boolean {
+  return toJsBoolean(evalFhirPathTyped(condition, [toTypedValue(input)], variables));
 }
 
 function evalTarget(ctx: TransformContext, target: StructureMapGroupRuleTarget): void {
-  switch (target.transform as string) {
-    case 'copy':
-      evalCopy(ctx, target);
-      break;
-    case 'truncate':
-      evalTruncate(ctx, target);
-      break;
-    default:
-      throw new Error('Unsupported transform: ' + target.transform);
+  const targetContext = getVariable(ctx, target.context as string);
+  if (!targetContext) {
+    throw new Error('Target not found: ' + target.context);
+  }
+
+  let targetValue;
+
+  if (!target.transform) {
+    targetValue = {};
+    safeAssign(targetContext, target.element as string, targetValue);
+  } else {
+    switch (target.transform) {
+      case 'copy':
+        targetValue = evalCopy(ctx, target, targetContext);
+        break;
+      case 'truncate':
+        targetValue = evalTruncate(ctx, target, targetContext);
+        break;
+      default:
+        console.warn('Unsupported transform: ' + target.transform);
+    }
+  }
+
+  if (target.variable) {
+    setVariable(ctx, target.variable, targetValue);
   }
 }
 
-function evalCopy(ctx: TransformContext, target: StructureMapGroupRuleTarget): void {
-  const targetContext = getVariable(ctx, target.context as string);
+function evalCopy(ctx: TransformContext, target: StructureMapGroupRuleTarget, targetContext: any): any {
   const targetElement = target.element as string;
   let targetParameter = getTypedPropertyValue(
     { type: 'StructureMapGroupRuleTargetParameter', value: target.parameter?.[0] },
@@ -135,24 +183,40 @@ function evalCopy(ctx: TransformContext, target: StructureMapGroupRuleTarget): v
     targetParameter = targetParameter[0];
   }
   if (!targetParameter) {
-    return;
+    throw new Error('Missing target parameter: ' + targetElement);
   }
   let targetValue = targetParameter.value;
   if (targetParameter.type === 'id') {
     targetValue = getVariable(ctx, targetParameter.value as string);
   }
-  targetContext[targetElement] = targetValue;
+  safeAssign(targetContext, targetElement, targetValue);
+  return targetValue;
 }
 
-function evalTruncate(ctx: TransformContext, target: StructureMapGroupRuleTarget): void {
-  const targetContext = getVariable(ctx, target.context as string);
+function evalTruncate(ctx: TransformContext, target: StructureMapGroupRuleTarget, targetContext: any): any {
   const targetElement = target.element as string;
   let targetValue = getVariable(ctx, target.parameter?.[0]?.valueId as string);
   const targetLength = target.parameter?.[1]?.valueInteger as number;
   if (targetValue && typeof targetValue === 'string') {
     targetValue = targetValue.substring(0, targetLength);
   }
-  targetContext[targetElement] = targetValue;
+  safeAssign(targetContext, targetElement, targetValue);
+  return targetValue;
+}
+
+function evalDependent(ctx: TransformContext, dependent: StructureMapGroupRuleDependent): void {
+  const dependentGroup = getVariable(ctx, dependent.name as string);
+  if (!dependentGroup) {
+    throw new Error('Dependent group not found: ' + dependent.name);
+  }
+
+  const variables = dependent.variable as string[];
+  const args = [];
+  for (const variable of variables) {
+    args.push(getVariable(ctx, variable));
+  }
+
+  evalGroup(ctx, dependentGroup as StructureMapGroup, args);
 }
 
 function getVariable(ctx: TransformContext, name: string): any {
@@ -164,4 +228,18 @@ function getVariable(ctx: TransformContext, name: string): any {
     return getVariable(ctx.parent, name);
   }
   return undefined;
+}
+
+function setVariable(ctx: TransformContext, name: string, value: any): void {
+  if (!ctx.variables) {
+    ctx.variables = {};
+  }
+  safeAssign(ctx.variables, name, value);
+}
+
+function safeAssign(target: Record<string, unknown>, key: string, value: unknown): void {
+  if (key === '__proto__' || key === 'constructor' || key === 'prototype') {
+    throw new Error('Invalid key: ' + key);
+  }
+  target[key] = value;
 }
