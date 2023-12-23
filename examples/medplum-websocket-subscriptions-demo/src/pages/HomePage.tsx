b1314e867cdffe023facde36d6c3d5d1a5d8e7d1
Derrick Farris <derrick@medplum.com>
2023-12-22 17:21:37 -0800

    fix(subscriptions-demo): reverse notification sorting order

--- examples/medplum-websocket-subscriptions-demo/src/pages/HomePage.tsx
+++ examples/medplum-websocket-subscriptions-demo/src/pages/HomePage.tsx
@@ -174,7 +174,7 @@ export function HomePage(): JSX.Element {
             .catch(console.error);
         }
       }
-      setBundles((s) => [...s, bundle]);
+      setBundles((s) => [bundle, ...s]);
     });
 
     setWebSocket(ws);
