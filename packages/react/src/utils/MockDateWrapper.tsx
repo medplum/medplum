cde772230c52b7cfac51ac9cc852276676d46e75
Matt Long <ml@medplum.com>
2024-01-02 16:29:20 -0800

    Working with mockdate I think?

--- packages/react/src/utils/MockDateWrapper.tsx
+++ packages/react/src/utils/MockDateWrapper.tsx
@@ -1,32 +1,37 @@
-import sinon from 'sinon';
-import { ReactNode, useEffect, createContext, useMemo, useRef } from 'react';
+import mockdate from 'mockdate';
+import { ReactNode, useEffect, createContext, useMemo, useState } from 'react';
 import { Decorator } from '@storybook/react';
 
 type MockDateContextType = {
-  clock: sinon.SinonFakeTimers;
   advanceSystemTime: (seconds?: number) => void;
 };
 
 // cast undefined so that attempting to use this context without the withMockedDate decorator will crash
 export const MockDateContext = createContext<MockDateContextType>(undefined as unknown as MockDateContextType);
 
-function MockDateWrapper({ children }: { children: ReactNode }): JSX.Element {
-  const clockRef = useRef<sinon.SinonFakeTimers>(sinon.useFakeTimers(new Date('2020-05-04T12:00:00.000Z')));
-  const clock = clockRef.current;
+function advanceSystemTime(seconds?: number): void {
+  const milliseconds = (seconds ?? 60) * 1000;
+  const now = new Date();
+  mockdate.set(new Date(now.getTime() + milliseconds));
+}
+
+function MockDateWrapper({ children }: { children: ReactNode }): JSX.Element | null {
+  const [ready, setReady] = useState(false);
   useEffect(() => {
+    mockdate.set(new Date('2020-05-04T12:00:00.000Z'));
+    setReady(true);
     return () => {
-      clock.restore();
+      mockdate.reset();
     };
-  }, [clock]);
+  }, []);
 
   const contextValue = useMemo(() => {
-    function advanceSystemTime(clock: sinon.SinonFakeTimers, seconds?: number): void {
-      const milliseconds = (seconds ?? 60) * 1000;
-      const now = new Date();
-      clock.setSystemTime(new Date(now.getTime() + milliseconds));
-    }
-    return { clock, advanceSystemTime: advanceSystemTime.bind(undefined, clock) };
-  }, [clock]);
+    return { advanceSystemTime };
+  }, []);
+
+  if (!ready) {
+    return null;
+  }
 
   return <MockDateContext.Provider value={contextValue}>{children}</MockDateContext.Provider>;
 }
