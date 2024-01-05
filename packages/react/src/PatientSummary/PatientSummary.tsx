e6e4cb6b6a655922154a6d7249d76f9837ef912a
Cody Ebberson <cody@ebberson.com>
2024-01-04 16:46:48 -0800

    Convert graphql to batch search

--- packages/react/src/PatientSummary/PatientSummary.tsx
+++ packages/react/src/PatientSummary/PatientSummary.tsx
@@ -1,5 +1,5 @@
-import { Anchor, Card, Divider, Flex, Group, Paper, Stack, Text } from '@mantine/core';
-import { calculateAgeString, formatHumanName, getDisplayString, resolveId } from '@medplum/core';
+import { Anchor, Card, CardProps, Divider, Flex, Group, Paper, Stack, Text } from '@mantine/core';
+import { calculateAgeString, formatHumanName, resolveId } from '@medplum/core';
 import {
   AllergyIntolerance,
   Condition,
@@ -9,7 +9,7 @@ import {
   Patient,
   Reference,
 } from '@medplum/fhirtypes';
-import { useMedplum, useResource } from '@medplum/react-hooks';
+import { useMedplum } from '@medplum/react-hooks';
 import { IconGenderFemale, IconStethoscope, IconUserSquare } from '@tabler/icons-react';
 import { useEffect, useState } from 'react';
 import { ResourceAvatar } from '../ResourceAvatar/ResourceAvatar';
@@ -19,12 +19,14 @@ import { ProblemList } from './ProblemList';
 import { SmokingStatus } from './SmokingStatus';
 import { Vitals } from './Vitals';
 
-export interface PatientSummaryProps {
+export interface PatientSummaryProps extends Omit<CardProps, 'children'> {
   readonly patient: Patient | Reference<Patient>;
+  readonly background?: string;
 }
 
 export function PatientSummary(props: PatientSummaryProps): JSX.Element | null {
   const medplum = useMedplum();
+  const { patient: propsPatient, background, ...rest } = props;
   const [patient, setPatient] = useState<Patient>();
   const [allergies, setAllergies] = useState<AllergyIntolerance[]>();
   const [problems, setProblems] = useState<Condition[]>();
@@ -33,79 +35,37 @@ export function PatientSummary(props: PatientSummaryProps): JSX.Element | null {
   const [medicationRequest, setMedicationRequest] = useState<MedicationRequest[]>();
 
   useEffect(() => {
-    const query = `{
-      Patient(id: "${resolveId(props.patient)}") {
-        resourceType,
-        id,
-        birthDate,
-        gender,
-        generalPractitioner { reference },
-        name { given, family },
-        address { line, city, state }
-        photo { contentType, url, title },
-        AllergyIntoleranceList(_reference: patient, _count: 100, _sort: "-_lastUpdated") {
-          id,
-          meta { lastUpdated },
-          code { coding { code, display } },
-          criticality
-        }
-        ConditionList(_reference: patient, _count: 100, _sort: "-_lastUpdated") {
-          id,
-          meta { lastUpdated },
-          category { coding { code } },
-          clinicalStatus { coding { display } },
-          code { coding { display } },
-          onsetDateTime
-        }
-        MedicationRequestList(_reference: subject, _count: 100, _sort: "-_lastUpdated") {
-          id,
-          status,
-          medicationCodeableConcept { coding { code, display }},
-        }
-        ObservationList(_reference: subject, _count: 100, _sort: "-_lastUpdated") {
-          id,
-          category { coding { code } },
-          code { coding { code, system, display } },
-          valueCodeableConcept { coding { code, display } text },
-          valueQuantity { value, unit },
-          component {
-            code { coding { code, system, display } },
-            valueCodeableConcept { coding { code, display } text },
-            valueQuantity { value, unit },
-          }
-          effectiveDateTime
-        }
-      }
-    }`;
+    const id = resolveId(propsPatient) as string;
+    const ref = `Patient/${id}`;
+    const searchMeta = { _count: 100, _sort: '-_lastUpdated' };
 
-    medplum
-      .graphql(query)
-      .then((response) => {
-        const newPatient = response.data.Patient;
-        const observations = (newPatient.ObservationList ?? []) as Observation[];
-        setPatient(newPatient as Patient);
-        setAllergies((newPatient.AllergyIntoleranceList ?? []) as AllergyIntolerance[]);
-        setProblems(newPatient.ConditionList as Condition[]);
-        setMedicationRequest(newPatient.MedicationRequestList as MedicationRequest[]);
+    Promise.all([
+      medplum.readResource('Patient', id),
+      medplum.searchResources('AllergyIntolerance', { patient: ref, ...searchMeta }),
+      medplum.searchResources('Condition', { patient: ref, ...searchMeta }),
+      medplum.searchResources('MedicationRequest', { subject: ref, ...searchMeta }),
+      medplum.searchResources('Observation', { subject: ref, ...searchMeta }),
+    ])
+      .then((results) => {
+        setPatient(results[0] as Patient);
+        setAllergies(results[1] as AllergyIntolerance[]);
+        setProblems(results[2] as Condition[]);
+        setMedicationRequest(results[3] as MedicationRequest[]);
+
+        const observations = results[4] as Observation[];
         setSmokingStatus(observations.find((obs) => obs.code?.coding?.[0].code === '72166-2'));
         setVitals(observations.filter((obs) => obs.category?.[0]?.coding?.[0].code === 'vital-signs'));
       })
       .catch(console.error);
-  }, [medplum, props]);
+  }, [medplum, propsPatient]);
 
   if (!patient) {
     return null;
   }
 
   return (
-    <Card w="25%" withBorder padding="lg" radius="md" mx="md" my="xl" shadow="xs">
-      <Card.Section
-        h={100}
-        style={{
-          backgroundImage:
-            'url(https://images.unsplash.com/photo-1535961652354-923cb08225a7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bmF0dXJlJTIwc21hbGx8ZW58MHwwfDB8fHww&auto=format&fit=crop&w=800&q=60)',
-        }}
-      />
+    <Card {...rest}>
+      <Card.Section h={100} style={{ background }} />
       <ResourceAvatar value={patient} size={80} radius={80} mx="auto" mt={-50} sx={{ border: '2px solid white' }} />
       <Text ta="center" fz="lg" fw={500}>
         {formatHumanName(patient.name?.[0] as HumanName)}
@@ -118,19 +78,19 @@ export function PatientSummary(props: PatientSummaryProps): JSX.Element | null {
           <Flex justify="center" align="center" direction="column" gap={0} maw="33%">
             <IconUserSquare size={24} color="gray" />
             <Text fz="xs" align="center" sx={{ whiteSpace: 'nowrap' }}>
-              {formatHumanName(patient.name?.[0] as HumanName)}
+              Self
             </Text>
           </Flex>
           <Flex justify="center" align="center" direction="column" gap={0}>
             <IconStethoscope size={24} color="gray" />
             <Text fz="xs" sx={{ whiteSpace: 'nowrap' }}>
-              <ProviderName patient={patient} />
+              {patient?.generalPractitioner?.[0]?.display ?? 'No provider'}
             </Text>
           </Flex>
           <Flex justify="center" align="center" direction="column" gap={0}>
             <IconGenderFemale size={24} color="gray" />
             <Text fz="xs" sx={{ whiteSpace: 'nowrap' }}>
-              Female
+              {patient.gender}
             </Text>
           </Flex>
         </Group>
@@ -151,13 +111,4 @@ export function PatientSummary(props: PatientSummaryProps): JSX.Element | null {
       </Stack>
     </Card>
   );
-}
-
-function ProviderName(props: { patient: Patient }): JSX.Element | null {
-  const patient = props.patient;
-  const provider = useResource(patient?.generalPractitioner?.[0]);
-  if (provider) {
-    return <>{getDisplayString(provider)}</>;
-  }
-  return null;
 }
