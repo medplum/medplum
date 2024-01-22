1e3dc5e2fe6e1f7a2be0b0fee6e9d4b530ba7ce6
Matt Long <ml@medplum.com>
2024-01-21 23:42:19 -0800

    Extract profile tabs component

--- packages/app/src/resource/ProfilesPage.tsx
+++ packages/app/src/resource/ProfilesPage.tsx
@@ -1,25 +1,18 @@
-import { Button, Group, Stack, Switch, Tabs, Text, ThemeIcon, Title } from '@mantine/core';
+import { Button, Group, Stack, Switch, Text, Title } from '@mantine/core';
 import { showNotification } from '@mantine/notifications';
 import { deepClone, normalizeErrorString, normalizeOperationOutcome } from '@medplum/core';
 import { OperationOutcome, Resource, ResourceType } from '@medplum/fhirtypes';
-import {
-  Document,
-  ResourceForm,
-  SupportedProfileStructureDefinition,
-  isSupportedProfileStructureDefinition,
-  useMedplum,
-} from '@medplum/react';
-import { IconCircleFilled } from '@tabler/icons-react';
+import { Document, ResourceForm, SupportedProfileStructureDefinition, useMedplum } from '@medplum/react';
 import React, { useCallback, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { addProfileToResource, cleanResource, removeProfileFromResource } from './utils';
+import { ProfileTabs } from './ProfileTabs';
 
 export function ProfilesPage(): JSX.Element | null {
   const medplum = useMedplum();
   const { resourceType, id } = useParams() as { resourceType: ResourceType; id: string };
   const [resource, setResource] = useState<Resource | undefined>();
   const [currentProfile, setCurrentProfile] = useState<SupportedProfileStructureDefinition>();
-  const [availableProfiles, setAvailableProfiles] = useState<SupportedProfileStructureDefinition[]>();
 
   useEffect(() => {
     medplum
@@ -30,19 +23,6 @@ export function ProfilesPage(): JSX.Element | null {
       });
   }, [medplum, resourceType, id]);
 
-  // This is a bit inefficient since the entire structure definition
-  // for each available profile is being fetched. All that is really needed is the title & url
-  // The SD is useful for the time being to populate the Snapshot and JSON debugging tabs;
-  // but those will likely be removed before deploying
-  useEffect(() => {
-    medplum
-      .searchResources('StructureDefinition', { type: resourceType, derivation: 'constraint', _count: 50 })
-      .then((results) => {
-        setAvailableProfiles(results.filter(isSupportedProfileStructureDefinition));
-      })
-      .catch(console.error);
-  }, [medplum, resourceType]);
-
   if (!resource) {
     return null;
   }
@@ -52,35 +32,7 @@ export function ProfilesPage(): JSX.Element | null {
       <Title order={2}>Available {resourceType} profiles</Title>
       <Stack>
         <>
-          <Tabs
-            value={currentProfile?.url}
-            onChange={(newProfileUrl) => setCurrentProfile(availableProfiles?.find((p) => p.url === newProfileUrl))}
-          >
-            <Tabs.List>
-              {availableProfiles?.map((profile) => {
-                const isActive = resource.meta?.profile?.includes(profile.url);
-                const title = isActive
-                  ? `This profile is present in this resource's meta.profile property.`
-                  : `This profile is not included in this resource's meta.profile property.`;
-                return (
-                  <Tabs.Tab
-                    key={profile.url}
-                    value={profile.url}
-                    title={title}
-                    rightSection={
-                      isActive && (
-                        <ThemeIcon variant="outline" color="green" size="xs" style={{ borderStyle: 'none' }}>
-                          <IconCircleFilled size="90%" />
-                        </ThemeIcon>
-                      )
-                    }
-                  >
-                    {profile.title || profile.name}
-                  </Tabs.Tab>
-                );
-              })}
-            </Tabs.List>
-          </Tabs>
+          <ProfileTabs resource={resource} currentProfile={currentProfile} onChange={setCurrentProfile} />
           {currentProfile ? (
             <ProfileDetail
               key={currentProfile.url}
