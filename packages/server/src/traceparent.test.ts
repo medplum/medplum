e5d9e0bd0d5afb651a1402b68af170d08b907808
dillonstreator <dillonstreator@gmail.com>
2024-02-03 12:41:49 -0600

    allow some grace on flags format

--- packages/server/src/traceparent.test.ts
+++ packages/server/src/traceparent.test.ts
@@ -25,6 +25,14 @@ describe('parseTraceparent', () => {
     expect(parseTraceparent(`${tp.traceId}-${tp.parentId}`)).toEqual({ ...tp, version: null, flags: null });
   });
 
+  it('allow 1 character for flags', () => {
+    expect(parseTraceparent(`${tp.traceId}-${tp.parentId}-1`)).toEqual({ ...tp, version: null, flags: '1' });
+  });
+
+  it('no more than 2 characters for flags', () => {
+    expect(parseTraceparent(`${tp.traceId}-${tp.parentId}-001`)).toEqual({ valid: false });
+  });
+
   it('reports invalid', () => {
     expect(parseTraceparent(`invalid-traceparent`)).toEqual({ valid: false });
   });
--- packages/server/src/traceparent.ts
+++ packages/server/src/traceparent.ts
@@ -14,7 +14,7 @@ export type Traceparent =
     };
 
 export function parseTraceparent(traceparent: string): Traceparent {
-  const traceparentRegex = /^([0-9a-f]{2})?-?([0-9a-f]{32})-([0-9a-f]{16})-?([0-9a-f]{2})?$/i;
+  const traceparentRegex = /^([0-9a-f]{2})?-?([0-9a-f]{32})-([0-9a-f]{16})-?([0-9a-f]{1,2})?$/i;
 
   const match = traceparent.match(traceparentRegex);
   if (!match) {
