a9436f4d8915edd281dff9795c9676aeca469b0b
Derrick Farris <derrick@medplum.com>
2024-02-11 13:23:08 -0800

    test(useSubscription): more testing

--- packages/react-hooks/src/useSubscription/useSubscription.test.tsx
+++ packages/react-hooks/src/useSubscription/useSubscription.test.tsx
@@ -8,16 +8,24 @@ import { MemoryRouter } from 'react-router-dom';
 import { MedplumProvider } from '../MedplumProvider/MedplumProvider';
 import { useSubscription } from './useSubscription';
 
-function TestComponent(): JSX.Element {
+function TestComponent({ callback }: { callback?: (bundle: Bundle) => void }): JSX.Element {
   const [lastReceived, setLastReceived] = useState<Bundle>();
-  useSubscription('Communication', (bundle: Bundle) => {
-    setLastReceived(bundle);
-  });
+  useSubscription(
+    'Communication',
+    callback ??
+      ((bundle: Bundle) => {
+        setLastReceived(bundle);
+      })
+  );
   return (
     <div>
       <div data-testid="bundle">{JSON.stringify(lastReceived)}</div>
     </div>
   );
+}
+
+function RenderToggleComponent({ render }: { render: boolean }): JSX.Element {
+  return <>{render ? <TestComponent /> : null}</>;
 }
 
 describe('useSubscription()', () => {
@@ -32,16 +40,21 @@ describe('useSubscription()', () => {
     jest.useRealTimers();
   });
 
-  function setup(children: ReactNode): ReturnType<typeof render> {
+  function setup(children: ReactNode): {
+    unmount: ReturnType<typeof render>['unmount'];
+    rerender: (element: JSX.Element) => void;
+  } {
     medplum = new MockClient();
-    return render(
+    const wrapper = (children: ReactNode): JSX.Element => (
       <MemoryRouter>
         <MedplumProvider medplum={medplum}>{children}</MedplumProvider>
       </MemoryRouter>
     );
+    const { unmount, rerender } = render(wrapper(children));
+    return { unmount, rerender: (element: JSX.Element) => rerender(wrapper(element)) };
   }
 
-  test('Happy path', async () => {
+  test('Mount and unmount completely', async () => {
     const { unmount } = setup(<TestComponent />);
 
     act(() => {
@@ -62,5 +75,73 @@ describe('useSubscription()', () => {
     unmount();
     jest.advanceTimersByTime(5000);
     expect(medplum.getSubscriptionManager().getCriteriaCount()).toEqual(0);
+  });
+
+  test('Mount and remount before debounce timeout', async () => {
+    expect(medplum.getSubscriptionManager().getCriteriaCount()).toEqual(0);
+    const { rerender } = setup(<RenderToggleComponent render={true} />);
+
+    expect(medplum.getSubscriptionManager().getCriteriaCount()).toEqual(1);
+    rerender(<RenderToggleComponent render={false} />);
+    jest.advanceTimersByTime(1000);
+    expect(medplum.getSubscriptionManager().getCriteriaCount()).toEqual(1);
+    rerender(<RenderToggleComponent render={true} />);
+    jest.advanceTimersByTime(4000);
+    expect(medplum.getSubscriptionManager().getCriteriaCount()).toEqual(1);
+
+    // Make sure we fully unmount later when actually unmounting
+    rerender(<RenderToggleComponent render={false} />);
+    jest.advanceTimersByTime(5000);
+    expect(medplum.getSubscriptionManager().getCriteriaCount()).toEqual(0);
+  });
+
+  test('Callback changed', async () => {
+    let lastFromCb1: Bundle | undefined;
+    let lastFromCb2: Bundle | undefined;
+    const id1 = generateId();
+    const id2 = generateId();
+
+    const { rerender } = setup(
+      <TestComponent
+        callback={(bundle: Bundle) => {
+          lastFromCb1 = bundle;
+        }}
+      />
+    );
+
+    act(() => {
+      medplum.getSubscriptionManager().emitEventForCriteria<'message'>('Communication', {
+        type: 'message',
+        payload: { resourceType: 'Bundle', id: id1, type: 'history' },
+      });
+    });
+
+    expect(lastFromCb1?.resourceType).toEqual('Bundle');
+    expect(lastFromCb1?.type).toEqual('history');
+    expect(lastFromCb1?.id).toEqual(id1);
+    expect(lastFromCb2).not.toBeDefined();
+
+    rerender(
+      <TestComponent
+        callback={(bundle: Bundle) => {
+          lastFromCb2 = bundle;
+        }}
+      />
+    );
+
+    act(() => {
+      medplum.getSubscriptionManager().emitEventForCriteria<'message'>('Communication', {
+        type: 'message',
+        payload: { resourceType: 'Bundle', id: id2, type: 'history' },
+      });
+    });
+
+    expect(lastFromCb1?.resourceType).toEqual('Bundle');
+    expect(lastFromCb1?.type).toEqual('history');
+    expect(lastFromCb1?.id).toEqual(id1);
+
+    expect(lastFromCb2?.resourceType).toEqual('Bundle');
+    expect(lastFromCb2?.type).toEqual('history');
+    expect(lastFromCb2?.id).toEqual(id2);
   });
 });
