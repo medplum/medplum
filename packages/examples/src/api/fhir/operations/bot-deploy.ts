2c1a888e0e3bdb95d456e18596891aac77e5ee94
ksmith94 <102421938+ksmith94@users.noreply.github.com>
2024-02-16 13:15:31 -0500

    Update example with hello-patient bot

--- packages/examples/src/api/fhir/operations/bot-deploy.ts
+++ packages/examples/src/api/fhir/operations/bot-deploy.ts
@@ -4,14 +4,14 @@ const medplum = new MedplumClient();
 
 // start-block deployTs
 await medplum.post(medplum.fhirUrl('Bot', '[id]', '$deploy').toString(), {
-  filename: 'example-bot',
-  code: 'example-bot-code',
+  filename: 'hello-patient.js',
+  code: "import { BotEvent, MedplumClient } from '@medplum/core';\nimport { Patient } from '@medplum/fhirtypes';\n\nexport async function handler(medplum: MedplumClient, event: BotEvent): Promise<any> {\n  const patient = event.input as Patient;\n  const firstName = patient.name?.[0]?.given?.[0];\n  const lastName = patient.name?.[0]?.family;\n  console.log(`Hello ${firstName} ${lastName}!`);\n  return true;\n}\n",
 });
 // end-block deployTs
 
 /*
 // start-block deployCli
-medplum post 'Bot/[id]/$deploy' '{ "filename": "example-bot", "code": "example-bot-code" }'
+medplum post 'Bot/[id]/$deploy' '{ "filename": "hello-patient.js", "code": "import { BotEvent, MedplumClient } from '@medplum/core';\nimport { Patient } from '@medplum/fhirtypes';\n\nexport async function handler(medplum: MedplumClient, event: BotEvent): Promise<any> {\n  const patient = event.input as Patient;\n  const firstName = patient.name?.[0]?.given?.[0];\n  const lastName = patient.name?.[0]?.family;\n  console.log(`Hello ${firstName} ${lastName}!`);\n  return true;\n}\n" }'
 // end-block deployCli
 
 // start-block deployCurl
@@ -19,6 +19,6 @@ curl 'https://api.medplum.com/fhir/R4/Bot/[id]/$deploy' \
   -X POST \
   -H "Content-Type: application/json" \
   -H "Authorization: Bearer $MY_ACCESS_TOKEN" \
-  -d '{"filename":"example-bot","code":"example-bot-code"}'
+  -d '{"filename":"hello-patient.js","code":"import { BotEvent, MedplumClient } from '@medplum/core';\nimport { Patient } from '@medplum/fhirtypes';\n\nexport async function handler(medplum: MedplumClient, event: BotEvent): Promise<any> {\n  const patient = event.input as Patient;\n  const firstName = patient.name?.[0]?.given?.[0];\n  const lastName = patient.name?.[0]?.family;\n  console.log(`Hello ${firstName} ${lastName}!`);\n  return true;\n}\n"}'
 // end-block deployCurl
 */
