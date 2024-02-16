719c1db1ba44956c1b181ecf8081564de7bc3a99
ksmith94 <102421938+ksmith94@users.noreply.github.com>
2024-02-16 11:48:53 -0500

    Add bots folder and basic implementation for eligibility request bot

---
+++ examples/medplum-eligibility-demo/src/bots/process-eligibility-request.ts
@@ -0,0 +1,44 @@
+import { BotEvent, getReferenceString, MedplumClient } from '@medplum/core';
+import { CoverageEligibilityRequest, CoverageEligibilityResponse } from '@medplum/fhirtypes';
+
+export async function handler(medplum: MedplumClient, event: BotEvent<CoverageEligibilityRequest>): Promise<void> {
+  const request = event.input as CoverageEligibilityRequest;
+
+  const response = processRequest(request);
+  await medplum.createResource(response);
+}
+
+function processRequest(request: CoverageEligibilityRequest) {
+  console.log(request);
+
+  const coverage = request.insurance?.[0].coverage;
+
+  if (!coverage) {
+    throw new Error('Error: No linked coverage for request');
+  }
+
+  const response: CoverageEligibilityResponse = {
+    resourceType: 'CoverageEligibilityResponse',
+    status: 'active',
+    purpose: request.purpose,
+    patient: request.patient,
+    created: new Date().toDateString(),
+    request: {
+      reference: getReferenceString(request),
+    },
+    outcome: 'complete',
+    insurer: request.insurer,
+    insurance: [
+      {
+        coverage,
+        item: [
+          {
+            excluded: Math.floor(Math.random()) % 2 === 0 ? true : false,
+          },
+        ],
+      },
+    ],
+  };
+
+  return response;
+}
