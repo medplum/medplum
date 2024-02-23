7a35c1fdd5b170cdc8336326714b1a80fdcf92aa
ksmith94 <102421938+ksmith94@users.noreply.github.com>
2024-02-23 12:49:33 -0500

    Disable eslint for line to allow template literal in string

--- packages/examples/src/api/fhir/operations/bot-deploy.ts
+++ packages/examples/src/api/fhir/operations/bot-deploy.ts
@@ -5,6 +5,7 @@ const medplum = new MedplumClient();
 // start-block deployTs
 await medplum.post(medplum.fhirUrl('Bot', '[id]', '$deploy').toString(), {
   filename: 'hello-patient.js',
+  // eslint-disable-next-line no-template-curly-in-string
   code: "import { BotEvent, MedplumClient } from '@medplum/core';\nimport { Patient } from '@medplum/fhirtypes';\n\nexport async function handler(medplum: MedplumClient, event: BotEvent): Promise<any> {\n  const patient = event.input as Patient;\n  const firstName = patient.name?.[0]?.given?.[0];\n  const lastName = patient.name?.[0]?.family;\n  console.log(`Hello ${firstName} ${lastName}!`);\n  return true;\n}\n",
 });
 // end-block deployTs
