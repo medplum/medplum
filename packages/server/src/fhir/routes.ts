a38b8411354c6023cd25321284532ac3a9799ce0
Matt Willer <matt@medplum.com>
2024-02-26 09:19:27 -0800

    Log batch processing statistics (#4018)
    * Log batch processing statistics
    
    * Use EventTarget

--- packages/server/src/fhir/routes.ts
+++ packages/server/src/fhir/routes.ts
@@ -31,6 +31,7 @@ import { sendOutcome } from './outcomes';
 import { isFhirJsonContentType, sendResponse } from './response';
 import { smartConfigurationHandler, smartStylingHandler } from './smart';
 import { structureDefinitionExpandProfileHandler } from './operations/structuredefinitionexpandprofile';
+import { recordHistogramValue } from '../otel/otel';
 
 export const fhirRouter = Router();
 
@@ -209,6 +210,16 @@ protectedRoutes.use(
     const ctx = getAuthenticatedContext();
     if (!internalFhirRouter) {
       internalFhirRouter = new FhirRouter({ introspectionEnabled: getConfig().introspectionEnabled });
+      internalFhirRouter.addEventListener('warn', (e: any) => ctx.logger.warn(e.message));
+      internalFhirRouter.addEventListener('batch', ({ count, errors, size, bundleType }: any) => {
+        recordHistogramValue('medplum.batch.entries', count, { bundleType });
+        recordHistogramValue('medplum.batch.errors', errors, { bundleType });
+        recordHistogramValue('medplum.batch.size', size, { bundleType });
+
+        if (errors > 0 && bundleType === 'transaction') {
+          ctx.logger.warn('Error processing transaction Bundle', { count, errors, size });
+        }
+      });
     }
     const request: FhirRequest = {
       method: req.method as HttpMethod,
