1d4ca003c5ddd36cd24a46592dc2b9ddbe1ef090
Matt Willer <matt@medplum.com>
2024-02-26 14:05:59 -0800

    Enable filtering out abstract codes from ValueSet expansion (#4032)
    * Use operation input and output utilities
    
    * Refactor params passing
    
    * Allow filtering out abstract codes from ValueSet expansion
    
    * Remove redundant block

--- packages/server/src/fhir/operations/expand.ts
+++ packages/server/src/fhir/operations/expand.ts
@@ -1,5 +1,5 @@
-import { badRequest, OperationOutcomeError, Operator, Operator as SearchOperator } from '@medplum/core';
-import { CodeSystem, Coding, ValueSet, ValueSetComposeInclude, ValueSetExpansionContains } from '@medplum/fhirtypes';
+import { allOk, badRequest, OperationOutcomeError, Operator, Operator as SearchOperator } from '@medplum/core';
+import { CodeSystem, ValueSet, ValueSetComposeInclude, ValueSetExpansionContains } from '@medplum/fhirtypes';
 import { Request, Response } from 'express';
 import { asyncWrap } from '../../async';
 import { sendOutcome } from '../outcomes';
@@ -7,10 +7,21 @@ import { getSystemRepo } from '../repo';
 import { Column, Condition, Conjunction, SelectQuery, Expression, Disjunction, Union } from '../sql';
 import { getAuthenticatedContext } from '../../context';
 import { parentProperty } from './codesystemimport';
-import { clamp } from './utils/parameters';
+import { clamp, parseInputParameters, sendOutputParameters } from './utils/parameters';
 import { validateCode } from './codesystemvalidatecode';
 import { getDatabasePool } from '../../database';
 import { r4ProjectId } from '../../seed';
+import { getOperationDefinition } from './definitions';
+
+const operation = getOperationDefinition('ValueSet', 'expand');
+
+type ValueSetExpandParameters = {
+  url?: string;
+  filter?: string;
+  offset?: number;
+  count?: number;
+  excludeNotForUI?: boolean;
+};
 
 // Implements FHIR "Value Set Expansion"
 // https://www.hl7.org/fhir/operation-valueset-expand.html
@@ -22,13 +33,15 @@ import { r4ProjectId } from '../../seed';
 // 4) Optional count for pagination (default is 10, can be 1-1000)
 
 export const expandOperator = asyncWrap(async (req: Request, res: Response) => {
-  let url = req.query.url as string | undefined;
-  if (typeof url !== 'string') {
+  const params = parseInputParameters<ValueSetExpandParameters>(operation, req);
+
+  let url = params.url;
+  if (!url) {
     sendOutcome(res, badRequest('Missing url'));
     return;
   }
 
-  const filter = req.query.filter;
+  const filter = params.filter;
   if (filter !== undefined && typeof filter !== 'string') {
     sendOutcome(res, badRequest('Invalid filter'));
     return;
@@ -46,18 +59,18 @@ export const expandOperator = asyncWrap(async (req: Request, res: Response) => {
   }
 
   let offset = 0;
-  if (req.query.offset) {
-    offset = Math.max(0, parseInt(req.query.offset as string, 10));
+  if (params.offset) {
+    offset = Math.max(0, params.offset);
   }
 
   let count = 10;
-  if (req.query.count) {
-    count = clamp(parseInt(req.query.count as string, 10), 1, 1000);
+  if (params.count) {
+    count = clamp(params.count, 1, 1000);
   }
 
   if (shouldUseLegacyTable()) {
     const elements = await queryValueSetElements(valueSet, offset, count, filter);
-    res.status(200).json({
+    await sendOutputParameters(req, res, operation, allOk, {
       resourceType: 'ValueSet',
       url,
       expansion: {
@@ -66,8 +79,8 @@ export const expandOperator = asyncWrap(async (req: Request, res: Response) => {
       },
     } as ValueSet);
   } else {
-    valueSet = await expandValueSet(valueSet, offset, count, filter);
-    res.status(200).json(valueSet);
+    valueSet = await expandValueSet(valueSet, params);
+    await sendOutputParameters(req, res, operation, allOk, valueSet);
   }
 });
 
@@ -170,12 +183,7 @@ function processInclude(systemExpressions: Expression[], include: ValueSetCompos
 
 const MAX_EXPANSION_SIZE = 1001;
 
-export async function expandValueSet(
-  valueSet: ValueSet,
-  offset: number,
-  count: number,
-  filter?: string
-): Promise<ValueSet> {
+export async function expandValueSet(valueSet: ValueSet, params: ValueSetExpandParameters): Promise<ValueSet> {
   const expansion = valueSet.expansion;
   if (expansion?.contains?.length && !expansion.parameter && expansion.total === expansion.contains.length) {
     // Full expansion is already available, use that
@@ -184,7 +192,7 @@ export async function expandValueSet(
 
   // Compute expansion
   const expandedSet = [] as ValueSetExpansionContains[];
-  await computeExpansion(valueSet, expandedSet, offset, count, filter);
+  await computeExpansion(valueSet, expandedSet, params);
   if (expandedSet.length >= MAX_EXPANSION_SIZE) {
     valueSet.expansion = {
       total: 1001,
@@ -195,7 +203,7 @@ export async function expandValueSet(
     valueSet.expansion = {
       total: expandedSet.length,
       timestamp: new Date().toISOString(),
-      contains: expandedSet.slice(0, count),
+      contains: expandedSet.slice(0, params.count),
     };
   }
   return valueSet;
@@ -204,13 +212,13 @@ export async function expandValueSet(
 async function computeExpansion(
   valueSet: ValueSet,
   expansion: ValueSetExpansionContains[],
-  offset: number,
-  count: number,
-  filter?: string
+  params: ValueSetExpandParameters
 ): Promise<void> {
   if (!valueSet.compose?.include.length) {
     throw new OperationOutcomeError(badRequest('Missing ValueSet definition', 'ValueSet.compose.include'));
   }
+
+  const { count, filter } = params;
 
   const codeSystemCache: Record<string, CodeSystem> = Object.create(null);
   for (const include of valueSet.compose.include) {
@@ -231,10 +239,10 @@ async function computeExpansion(
         }
       }
     } else {
-      await includeInExpansion(include, expansion, codeSystem, offset, count, filter);
+      await includeInExpansion(include, expansion, codeSystem, params);
     }
 
-    if (expansion.length > count) {
+    if (expansion.length > (count ?? MAX_EXPANSION_SIZE)) {
       // Return partial expansion
       return;
     }
@@ -279,19 +287,18 @@ async function includeInExpansion(
   include: ValueSetComposeInclude,
   expansion: ValueSetExpansionContains[],
   codeSystem: CodeSystem,
-  offset: number,
-  count: number,
-  filter?: string
+  params: ValueSetExpandParameters
 ): Promise<void> {
   const ctx = getAuthenticatedContext();
+  const { count, offset, filter } = params;
 
   let query = new SelectQuery('Coding')
     .column('id')
     .column('code')
     .column('display')
     .where('system', '=', codeSystem.id)
-    .limit(count + 1)
-    .offset(offset);
+    .limit((count ?? MAX_EXPANSION_SIZE) + 1)
+    .offset(offset ?? 0);
   if (filter) {
     query.where('display', '!=', null).where('display', 'TSVECTOR_ENGLISH', filterToTsvectorQuery(filter));
   }
@@ -299,20 +306,17 @@ async function includeInExpansion(
     for (const condition of include.filter) {
       switch (condition.op) {
         case 'is-a':
-          {
-            const coding = await validateCode(codeSystem, condition.value);
-            if (!coding) {
-              ctx.logger.warn('Invalid parent code in ValueSet', { codeSystem: codeSystem.id, code: condition.value });
-              return; // Invalid parent code, don't make DB query with incorrect filters
-            }
-            query = addParentCondition(query, codeSystem, coding);
-          }
+          query = addParentCondition(query, codeSystem, condition.value);
           break;
         default:
           ctx.logger.warn('Unknown filter type in ValueSet', { filter: condition });
           return; // Unknown filter type, don't make DB query with incorrect filters
       }
     }
+  }
+
+  if (params.excludeNotForUI) {
+    query = addAbstractFilter(query, codeSystem);
   }
 
   const results = await query.execute(ctx.repo.getDatabaseClient());
@@ -322,7 +326,7 @@ async function includeInExpansion(
   }
 }
 
-function addParentCondition(query: SelectQuery, codeSystem: CodeSystem, parent: Coding): SelectQuery {
+function addParentCondition(query: SelectQuery, codeSystem: CodeSystem, parentCode: string): SelectQuery {
   if (codeSystem.hierarchyMeaning !== 'is-a') {
     throw new OperationOutcomeError(
       badRequest(
@@ -337,7 +341,12 @@ function addParentCondition(query: SelectQuery, codeSystem: CodeSystem, parent: 
     property = { code: codeSystem.hierarchyMeaning ?? 'parent', uri: parentProperty, type: 'code' };
   }
 
-  const base = new SelectQuery('Coding').column('id').column('code').column('display').where('id', '=', parent.id);
+  const base = new SelectQuery('Coding')
+    .column('id')
+    .column('code')
+    .column('display')
+    .where('system', '=', codeSystem.id)
+    .where('code', '=', parentCode);
 
   const propertyTable = query.getNextJoinAlias();
   query.innerJoin(
@@ -372,4 +381,35 @@ function addParentCondition(query: SelectQuery, codeSystem: CodeSystem, parent: 
     .withRecursive('cte_descendants', new Union(base, query))
     .limit(query.limit_)
     .offset(offset);
+}
+
+export const abstractProperty = 'http://hl7.org/fhir/concept-properties#notSelectable';
+
+function addAbstractFilter(query: SelectQuery, codeSystem: CodeSystem): SelectQuery {
+  const property = codeSystem.property?.find((p) => p.uri === abstractProperty);
+  if (!property) {
+    return query;
+  }
+
+  const propertyTable = query.getNextJoinAlias();
+  query.leftJoin(
+    'Coding_Property',
+    propertyTable,
+    new Conjunction([
+      new Condition(new Column(query.tableName, 'id'), '=', new Column(propertyTable, 'coding')),
+      new Condition(new Column(propertyTable, 'value'), '=', 'true'),
+    ])
+  );
+
+  const csPropertyTable = query.getNextJoinAlias();
+  query.leftJoin(
+    'CodeSystem_Property',
+    csPropertyTable,
+    new Conjunction([
+      new Condition(new Column(propertyTable, 'property'), '=', new Column(csPropertyTable, 'id')),
+      new Condition(new Column(csPropertyTable, 'code'), '=', property.code),
+    ])
+  );
+  query.where(new Column(csPropertyTable, 'id'), '=', null);
+  return query;
 }
