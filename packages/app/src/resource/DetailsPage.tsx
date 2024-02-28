06e0120040dd8aa3b9be3da78405c3f57ffdb327
Matt Long <ml@medplum.com>
2024-02-28 11:47:02 -0800

    Better profile select UI

--- packages/app/src/resource/DetailsPage.tsx
+++ packages/app/src/resource/DetailsPage.tsx
@@ -1,21 +1,35 @@
-import { ComboboxItem, Group, NativeSelect, Stack } from '@mantine/core';
+import { Box, Combobox, Group, Stack, useCombobox, Text, UnstyledButton, Code } from '@mantine/core';
 import { isPopulated } from '@medplum/core';
 import { ResourceType } from '@medplum/fhirtypes';
 import { Document, ResourceTable, useResource } from '@medplum/react';
+import { IconChevronDown } from '@tabler/icons-react';
 import { useEffect, useMemo, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import classes from './DetailsPage.module.css';
 
 export function DetailsPage(): JSX.Element | null {
   const { resourceType, id } = useParams() as { resourceType: ResourceType; id: string };
   const resource = useResource({ reference: resourceType + '/' + id });
-  const [profileUrl, setProfileUrl] = useState<string | undefined>();
+  const [profileUrl, setProfileUrl] = useState<string>();
 
-  const profileUrlOptions: ComboboxItem[] | undefined = useMemo(() => {
+  const combobox = useCombobox({
+    onDropdownClose: () => combobox.resetSelectedOption(),
+  });
+  const profileUrlOptions = useMemo(() => {
     if (!isPopulated(resource?.meta?.profile)) {
       return undefined;
     }
 
-    return [{ label: 'none', value: '' }, ...resource.meta.profile.map((p) => ({ label: p, value: p }))];
+    return [
+      <Combobox.Option value="" key="">
+        None
+      </Combobox.Option>,
+      ...resource.meta.profile.map((p) => (
+        <Combobox.Option value={p} key={p}>
+          {p}
+        </Combobox.Option>
+      )),
+    ];
   }, [resource?.meta?.profile]);
 
   useEffect(() => {
@@ -31,21 +45,58 @@ export function DetailsPage(): JSX.Element | null {
   let profileSelect: JSX.Element | undefined;
   if (isPopulated(profileUrlOptions)) {
     profileSelect = (
-      <div style={{ maxWidth: 600 }}>
-        <NativeSelect
-          label="Profile to display"
-          data={profileUrlOptions}
-          value={profileUrl}
-          onChange={(e) => setProfileUrl(e.currentTarget.value || undefined)} // coalesce empty string to undefined
-        />
-      </div>
+      <Group justify="flex-end">
+        <Stack align="flex-end" gap="xs">
+          <Combobox
+            store={combobox}
+            width={250}
+            position="bottom-end"
+            withinPortal={false}
+            onOptionSubmit={(val) => {
+              setProfileUrl(val);
+              combobox.closeDropdown();
+            }}
+          >
+            <Combobox.Target>
+              <UnstyledButton onClick={() => combobox.toggleDropdown()}>
+                <Code fw="bold" fs="sm" className={classes.selectProfileBtn}>
+                  <span>Pick profile</span>
+                  <IconChevronDown stroke={1.5} className={classes.chevron} />
+                </Code>
+              </UnstyledButton>
+            </Combobox.Target>
+
+            <Combobox.Dropdown w="max-content">
+              <Combobox.Options>{profileUrlOptions}</Combobox.Options>
+            </Combobox.Dropdown>
+          </Combobox>
+
+          <Box>
+            {profileUrl ? (
+              <>
+                <Text span size="sm" c="dimmed">
+                  Displaying profile:&nbsp;
+                </Text>
+
+                <Text span size="sm">
+                  {profileUrl || 'Nothing selected'}
+                </Text>
+              </>
+            ) : (
+              <Text span size="sm" c="dimmed">
+                No profile displayed
+              </Text>
+            )}
+          </Box>
+        </Stack>
+      </Group>
     );
   }
 
   return (
     <Document>
       <Stack gap="xl">
-        <Group justify="flex-end">{profileSelect}</Group>
+        {profileSelect}
         <ResourceTable value={resource} profileUrl={profileUrl} />
       </Stack>
     </Document>
