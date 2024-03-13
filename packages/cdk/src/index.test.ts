662ba4db21f9d0bf62e09bf2a3aeda3fbcca0540
Cody Ebberson <cody@ebberson.com>
2024-03-13 16:12:13 -0700

    Fixes #4162 - cdk exports autoscaling settings

--- packages/cdk/src/index.test.ts
+++ packages/cdk/src/index.test.ts
@@ -609,4 +609,44 @@ describe('Infra', () => {
     expect(() => main({ config: filename })).not.toThrow();
     unlinkSync(filename);
   });
+
+  test('Autoscaling', () => {
+    const filename = resolve('./medplum.autoscaling.config.json');
+    writeFileSync(
+      filename,
+      JSON.stringify({
+        name: 'autoscaling',
+        stackName: 'MedplumAutoscalingTestStack',
+        accountNumber: '647991932601',
+        region: 'us-east-1',
+        domainName: 'medplum.com',
+        apiPort: 8103,
+        apiDomainName: 'api.medplum.com',
+        apiSslCertArn: 'arn:aws:acm:us-east-1:647991932601:certificate/08bf1daf-3a2b-4cbe-91a0-739b4364a1ec',
+        appDomainName: 'app.medplum.com',
+        appSslCertArn: 'arn:aws:acm:us-east-1:647991932601:certificate/fd21b628-b2c0-4a5d-b4f5-b5c9a6d63b1a',
+        storageBucketName: 'medplum-storage',
+        storageDomainName: 'storage.medplum.com',
+        storageSslCertArn: 'arn:aws:acm:us-east-1:647991932601:certificate/19d85245-0a1d-4bf5-9789-23082b1a15fc',
+        storagePublicKey: '-----BEGIN PUBLIC KEY-----\n-----END PUBLIC KEY-----',
+        maxAzs: 2,
+        rdsInstances: 1,
+        desiredServerCount: 1,
+        serverImage: 'medplum/medplum-server:staging',
+        serverMemory: 512,
+        serverCpu: 256,
+        fargateAutoScaling: {
+          minCapacity: 1,
+          maxCapacity: 10,
+          targetUtilizationPercent: 50,
+          scaleInCooldown: 60,
+          scaleOutCooldown: 60,
+        },
+      }),
+      { encoding: 'utf-8' }
+    );
+
+    expect(() => main({ config: filename })).not.toThrow();
+    unlinkSync(filename);
+  });
 });
