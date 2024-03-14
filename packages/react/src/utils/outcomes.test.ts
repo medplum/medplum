8055835fc6c7ec61ac8a9c9799cc7c4bbfaa98a1
Matt Long <ml@medplum.com>
2024-03-14 11:47:59 -0700

    make isExpressionMatch array-index aware

--- packages/react/src/utils/outcomes.test.ts
+++ packages/react/src/utils/outcomes.test.ts
@@ -1,5 +1,23 @@
 import { allOk, badRequest, gone } from '@medplum/core';
-import { getIssuesForExpression } from './outcomes';
+import { getErrorsForInput, getIssuesForExpression } from './outcomes';
+import { OperationOutcome } from '@medplum/fhirtypes';
+
+const MISSING_PROP = 'Missing required property';
+function missingProp(expression: string): OperationOutcome {
+  return {
+    resourceType: 'OperationOutcome',
+    issue: [
+      {
+        severity: 'error',
+        code: 'structure',
+        details: {
+          text: MISSING_PROP,
+        },
+        expression: [expression],
+      },
+    ],
+  };
+}
 
 describe('Outcome utils', () => {
   test('getIssuesForExpression', () => {
@@ -14,5 +32,24 @@ describe('Outcome utils', () => {
     expect(getIssuesForExpression(badRequest('bad', 'Patient.name'), 'name')).toHaveLength(1);
     expect(getIssuesForExpression(badRequest('bad', 'name'), 'Patient.name')).toHaveLength(1);
     expect(getIssuesForExpression(badRequest('bad', 'Practitioner.name'), 'Patient.name')).toHaveLength(0);
+  });
+
+  describe('getErrorsForInput', () => {
+    test('match without indexes', () => {
+      expect(getErrorsForInput(missingProp('identifier.system'), 'identifier.system')).toEqual(MISSING_PROP);
+    });
+
+    test('exact match with indexes', () => {
+      expect(getErrorsForInput(missingProp('identifier[1].system'), 'identifier[1].system')).toEqual(MISSING_PROP);
+    });
+    test('mismatched indexes', () => {
+      expect(getErrorsForInput(missingProp('identifier[1].system'), 'identifier[0].system')).toEqual('');
+    });
+    test('indexes only on outcome', () => {
+      expect(getErrorsForInput(missingProp('identifier[1].system'), 'identifier.system')).toEqual(MISSING_PROP);
+    });
+    test('indexes only on client', () => {
+      expect(getErrorsForInput(missingProp('identifier.system'), 'identifier[1].system')).toEqual(MISSING_PROP);
+    });
   });
 });
--- packages/react/src/utils/outcomes.ts
+++ packages/react/src/utils/outcomes.ts
@@ -17,7 +17,16 @@ export function getIssuesForExpression(
   return outcome?.issue?.filter((issue) => isExpressionMatch(issue.expression?.[0], expression));
 }
 
+const ARRAY_INDEX_REGEX = /\[\d+\]/;
 function isExpressionMatch(expr1: string | undefined, expr2: string | undefined): boolean {
+  // to make this behavior backwards compatible, if only one expression specifies array indexes, ignore all indexes
+  const isExpr1Indexed = typeof expr1 === 'string' && ARRAY_INDEX_REGEX.test(expr1);
+  const isExpr2Indexed = typeof expr2 === 'string' && ARRAY_INDEX_REGEX.test(expr2);
+  if (isExpr1Indexed !== isExpr2Indexed) {
+    expr1 = expr1?.replace(ARRAY_INDEX_REGEX, '');
+    expr2 = expr2?.replace(ARRAY_INDEX_REGEX, '');
+  }
+
   // Expression can be either "fieldName" or "resourceType.fieldName"
   if (expr1 === expr2) {
     return true;
