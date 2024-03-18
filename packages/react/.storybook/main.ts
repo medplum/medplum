73edd624bc27f9c386346b005bbaf65328a36f32
Rahul Agarwal <rahul@medplum.com>
2024-03-17 18:47:05 -0700

    Added Theme Add-on to storybook

--- packages/react/.storybook/main.ts
+++ packages/react/.storybook/main.ts
@@ -16,6 +16,7 @@ const config: StorybookConfig = {
         },
       },
     },
+    'storybook-addon-mantine',
   ],
   staticDirs: ['../public'],
   framework: {
--- packages/react/.storybook/preview.tsx
+++ packages/react/.storybook/preview.tsx
@@ -1,9 +1,10 @@
-import { MantineProvider, MantineThemeOverride } from '@mantine/core';
+import { MantineProvider, MantineThemeOverride, createTheme } from '@mantine/core';
 import '@mantine/core/styles.css';
 import { MockClient } from '@medplum/mock';
 import { MedplumProvider } from '@medplum/react-hooks';
 import { BrowserRouter } from 'react-router-dom';
 import { createGlobalTimer } from '../src/stories/MockDateWrapper.utils';
+import { withMantineThemes } from 'storybook-addon-mantine';
 
 export const parameters = {
   layout: 'fullscreen',
@@ -26,7 +27,7 @@ medplum.get('/').then(() => {
   clock.restore();
 });
 
-const theme: MantineThemeOverride = {
+const medplumDefault = createTheme({
   headings: {
     sizes: {
       h1: {
@@ -43,15 +44,112 @@ const theme: MantineThemeOverride = {
     lg: '1.0rem',
     xl: '1.125rem',
   },
-};
+});
+
+// const fooMedical = createTheme({
+//   primaryColor: 'teal',
+//   primaryShade: 8,
+//   fontFamily: 'Open Sans, sans-serif',
+//   fontSizes: {
+//     xs: '0.6875rem',
+//     sm: '0.875rem',
+//     md: '0.875rem',
+//     lg: '1rem',
+//     xl: '1.125rem',
+//   },
+//   components: {
+//     Container: {
+//       defaultProps: {
+//         size: 1200,
+//       },
+//     },
+//   },
+// });
+
+const fooMedical = createTheme({
+  colorScheme: 'light',
+  colors: {
+    // Replace or adjust with the exact colors used for your design
+    primary: [
+      '#f7f7f7', // primary[0]
+      '#eef6f4', // primary[1]
+      '#e3eff2', // primary[2]
+      '#d5ebec', // primary[3]
+      '#cfe7e9', // primary[4]
+      '#b0d7db', // primary[5]
+      '#39acbc', // primary[6]
+      '#005450', // primary[7]
+      '#004d49', // primary[8]
+      '#00353a', // primary[9] (adjusted for a darker shade)
+    ],
+    secondary: [
+      '#fff7eb', // secondary[0]
+      '#ffedce', // secondary[1]
+      '#fae3c3', // secondary[2]
+      '#e9d1b9', // secondary[3]
+      '#e8c9a6', // secondary[4]
+      '#f1dfca', // secondary[5]
+      '#ffc776', // secondary[6]
+      '#fa645a', // secondary[7]
+      '#b57931', // secondary[8]
+      '#935923', // secondary[9] (adjusted for a darker shade)
+    ],
+  },
+  primaryColor: 'primary',
+  fontFamily: 'Ginto, helvetica',
+  radius: {
+    xs: '.5rem',
+    sm: '.75rem',
+    md: '1rem',
+    lg: '1.5rem',
+    xl: '2.5rem',
+  },
+  spacing: {
+    xs: '.25rem',
+    sm: '.33rem',
+    md: '.5rem',
+    lg: '.66rem',
+    xl: '1rem',
+  },
+  defaultRadius: 'xl',
+  shadows: {
+    xs: '10px 10px 3px rgba(0, 0, 0, 0)',
+    md: '10px 10px 3px rgba(0, 0, 0, .25)',
+    xl: '5px 5px 3px rgba(0, 0, 0, .25)',
+  },
+  headings: {
+    fontFamily: 'GT Super Display, serif',
+    sizes: {
+      h1: { fontSize: '30px', lineHeight: '1.4' },
+      h2: { fontSize: '24px', lineHeight: '1.35' },
+      h3: { fontSize: '20px', lineHeight: '1.3' },
+      h4: { fontSize: '18px', lineHeight: '1.25' },
+      h5: { fontSize: '16px', lineHeight: '1.2' },
+      h6: { fontSize: '14px', lineHeight: '1.15' },
+    },
+  },
+  // Define other theme properties like spacing, breakpoints, etc.
+});
 
 export const decorators = [
+  withMantineThemes({
+    themes: [
+      {
+        id: 'medplumDefault',
+        name: 'Medplum Default',
+        ...medplumDefault,
+      },
+      {
+        id: 'foomedical',
+        name: 'Foo Medical',
+        ...fooMedical,
+      },
+    ],
+  }),
   (Story) => (
     <BrowserRouter>
       <MedplumProvider medplum={medplum}>
-        <MantineProvider theme={theme}>
-          <Story />
-        </MantineProvider>
+        <Story />
       </MedplumProvider>
     </BrowserRouter>
   ),
