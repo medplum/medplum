3bdeab7a9486a7595921359483119b940decbcd2
ksmith94 <102421938+ksmith94@users.noreply.github.com>
2024-03-18 15:27:06 -0400

    Update search page and add Communication page

---
+++ examples/medplum-chat-demo/src/components/CommunicationDetails.tsx
@@ -0,0 +1,44 @@
+import { Paper, Tabs } from '@mantine/core';
+import { Communication } from '@medplum/fhirtypes';
+import { ResourceHistoryTable, ResourceTable } from '@medplum/react';
+import { useNavigate } from 'react-router-dom';
+
+interface CommunicationDetailsProps {
+  readonly communication: Communication;
+}
+
+export function CommunicationDetails({ communication }: CommunicationDetailsProps): JSX.Element {
+  const navigate = useNavigate();
+  const id = communication.id as string;
+  const tabs = ['Chat', 'Details', 'History'];
+
+  const tab = window.location.pathname.split('/').pop();
+  const currentTab = tab && tabs.map((t) => t.toLowerCase()).includes(tab) ? tab : tabs[0].toLowerCase();
+
+  const handleTabChange = (newTab: string | null): void => {
+    navigate(`/Communication/${id}/${newTab ?? ''}`);
+  };
+
+  return (
+    <Paper>
+      <Tabs defaultValue="details" onChange={handleTabChange}>
+        <Tabs.List>
+          {tabs.map((tab) => (
+            <Tabs.Tab key={tab} value={tab.toLowerCase()}>
+              {tab}
+            </Tabs.Tab>
+          ))}
+        </Tabs.List>
+        <Tabs.Panel value="chat">
+          <p>Chat</p>
+        </Tabs.Panel>
+        <Tabs.Panel value="details">
+          <ResourceTable value={communication} />
+        </Tabs.Panel>
+        <Tabs.Panel value="history">
+          <ResourceHistoryTable resourceType="Communication" id={communication.id} />
+        </Tabs.Panel>
+      </Tabs>
+    </Paper>
+  );
+}
