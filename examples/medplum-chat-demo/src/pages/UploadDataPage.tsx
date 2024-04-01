0dfb671278d106def311fbbf928bc21beb7efad5
ksmith94 <102421938+ksmith94@users.noreply.github.com>
2024-04-01 09:24:07 -0400

    Add example valueset and implement data upload

--- examples/medplum-chat-demo/src/pages/UploadDataPage.tsx
+++ examples/medplum-chat-demo/src/pages/UploadDataPage.tsx
@@ -1,19 +1,55 @@
 import { Button } from '@mantine/core';
-import { capitalize } from '@medplum/core';
-import { useParams } from 'react-router-dom';
+import { showNotification } from '@mantine/notifications';
+import { capitalize, MedplumClient, normalizeErrorString } from '@medplum/core';
+import { Bundle } from '@medplum/fhirtypes';
+import { Document, useMedplum } from '@medplum/react';
+import { IconCircleCheck, IconCircleOff } from '@tabler/icons-react';
+import { useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import exampleDataSet from '../../data/example/example-data.json';
 
 export function UploadDataPage(): JSX.Element {
+  const medplum = useMedplum();
+  const navigate = useNavigate();
+  const [buttonDisabled, setButtonDisabled] = useState<boolean>(false);
+
   // Get the data type and capitalize the first letter
   const { dataType } = useParams();
   const dataTypeDisplay = dataType ? capitalize(dataType) : '';
 
   const handleDataUpload = (): void => {
-    console.log('No Data Yet :(');
+    setButtonDisabled(true);
+    uploadData(medplum, dataType)
+      .then(() => navigate('/'))
+      .catch((err) => {
+        showNotification({
+          color: 'red',
+          icon: <IconCircleOff />,
+          title: 'Error',
+          message: normalizeErrorString(err),
+        });
+      })
+      .finally(() => setButtonDisabled(false));
   };
 
   return (
-    <div>
+    <Document>
       <Button onClick={handleDataUpload}>Upload {dataTypeDisplay} Data</Button>
-    </div>
+    </Document>
   );
+}
+
+async function uploadData(medplum: MedplumClient, dataType?: string): Promise<void> {
+  if (dataType === 'core') {
+    console.log('no core data');
+  } else if (dataType === 'example') {
+    await medplum.executeBatch(exampleDataSet as Bundle);
+    showNotification({
+      icon: <IconCircleCheck />,
+      title: 'Success',
+      message: `${capitalize(dataType)} data uploaded`,
+    });
+  } else {
+    throw new Error('Invalid data type');
+  }
 }
