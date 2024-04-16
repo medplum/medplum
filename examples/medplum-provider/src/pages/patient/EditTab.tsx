9c5c684cd8b9e4f3a4da3be52f476b2ba48f3aca
Matt Long <ml@medplum.com>
2024-04-16 16:34:48 -0700

    Conform patient profile edits to US Core Patient Profile

--- examples/medplum-provider/src/pages/patient/EditTab.tsx
+++ examples/medplum-provider/src/pages/patient/EditTab.tsx
@@ -1,9 +1,20 @@
 import { showNotification } from '@mantine/notifications';
-import { deepClone, normalizeErrorString, normalizeOperationOutcome } from '@medplum/core';
+import {
+  InternalTypeSchema,
+  deepClone,
+  normalizeErrorString,
+  normalizeOperationOutcome,
+  tryGetProfile,
+} from '@medplum/core';
 import { OperationOutcome, Resource } from '@medplum/fhirtypes';
-import { Document, ResourceForm, useMedplum } from '@medplum/react';
+import { Document, Loading, ResourceForm, useMedplum } from '@medplum/react';
 import { useCallback, useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
+import { addProfileToResource, removeProfileFromResource } from '../../utils';
+import { Alert, Anchor } from '@mantine/core';
+import { IconAlertCircle } from '@tabler/icons-react';
+
+const PATIENT_PROFILE_URL = 'http://hl7.org/fhir/us/core/StructureDefinition/us-core-patientX';
 
 export function EditTab(): JSX.Element | null {
   const medplum = useMedplum();
@@ -11,6 +22,9 @@ export function EditTab(): JSX.Element | null {
   const [value, setValue] = useState<Resource | undefined>();
   const navigate = useNavigate();
   const [outcome, setOutcome] = useState<OperationOutcome | undefined>();
+  const [loadingProfile, setLoadingProfile] = useState(true);
+  const [profileError, setProfileError] = useState<any>();
+  const [profile, setProfile] = useState<InternalTypeSchema>();
 
   useEffect(() => {
     medplum
@@ -22,9 +36,30 @@ export function EditTab(): JSX.Element | null {
       });
   }, [medplum, patientId]);
 
+  useEffect(() => {
+    medplum
+      .requestProfileSchema(PATIENT_PROFILE_URL, { expandProfile: true })
+      .finally(() => setLoadingProfile(false))
+      .then(() => {
+        const patientProfile = tryGetProfile(PATIENT_PROFILE_URL);
+        if (patientProfile) {
+          setProfile(patientProfile);
+        }
+      })
+      .catch((reason) => {
+        console.error(reason);
+        setProfileError(reason);
+      });
+  }, [medplum]);
+
   const handleSubmit = useCallback(
     (newResource: Resource): void => {
       setOutcome(undefined);
+      if (profile) {
+        addProfileToResource(newResource, PATIENT_PROFILE_URL);
+      } else {
+        removeProfileFromResource(newResource, PATIENT_PROFILE_URL);
+      }
       medplum
         .updateResource(newResource)
         .then(() => {
@@ -36,8 +71,33 @@ export function EditTab(): JSX.Element | null {
           showNotification({ color: 'red', message: normalizeErrorString(err), autoClose: false });
         });
     },
-    [medplum, patientId, navigate]
+    [profile, medplum, navigate, patientId]
   );
+
+  if (loadingProfile) {
+    return <Loading />;
+  }
+
+  if (!profile) {
+    let displayString: React.ReactNode;
+    if (profileError) {
+      displayString = normalizeErrorString(profileError);
+    } else {
+      displayString = (
+        <>
+          Could not find the{' '}
+          <Anchor href={PATIENT_PROFILE_URL} target="_blank">
+            US Core Patient Profile
+          </Anchor>
+        </>
+      );
+    }
+    return (
+      <Alert icon={<IconAlertCircle size={16} />} title="Not found" color="red">
+        {displayString}
+      </Alert>
+    );
+  }
 
   if (!value) {
     return null;
@@ -45,7 +105,7 @@ export function EditTab(): JSX.Element | null {
 
   return (
     <Document>
-      <ResourceForm defaultValue={value} onSubmit={handleSubmit} outcome={outcome} />
+      <ResourceForm defaultValue={value} onSubmit={handleSubmit} outcome={outcome} profileUrl={PATIENT_PROFILE_URL} />
     </Document>
   );
 }
