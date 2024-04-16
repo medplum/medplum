a88b2dd57b5c9d5c7a0efe0c46cea0284d174b00
ksmith94 <102421938+ksmith94@users.noreply.github.com>
2024-04-16 18:52:11 -0400

    Document how to revert changes to a resource (#4382)

--- packages/examples/src/fhir-datastore/resource-history.ts
+++ packages/examples/src/fhir-datastore/resource-history.ts
@@ -1,5 +1,6 @@
 // start-block imports
 import { MedplumClient } from '@medplum/core';
+import { Bundle } from '@medplum/fhirtypes';
 // end-block imports
 
 const medplum = new MedplumClient();
@@ -19,3 +20,23 @@ curl 'https://api.medplum.com/fhir/R4/Patient/homer-simpson/_history' \
   -H 'content-type: application/fhir+json' \
 // end-block accessHistoryCurl
 */
+
+// start-block revertChanges
+// Read the history, returning a bundle of history entries
+const history = await medplum.readHistory('Patient', 'homer-simpson');
+
+// Implement your own logic to get the historic version of the resource you want.
+// You will need the versionId to use the readVersion function.
+const versionId = getVersionId(history);
+
+// readVersion will return the historic Patient resource
+const version = await medplum.readVersion('Patient', 'homer-simpson', versionId);
+
+// Pass the historic version to updateResource to revert to that version
+await medplum.updateResource(version);
+// end-block revertChanges
+
+function getVersionId(history: Bundle): string {
+  console.log(history);
+  return 'versionId';
+}
