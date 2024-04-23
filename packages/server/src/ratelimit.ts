a300eefcf37a7eaef86173e0ee4bd48120dee1e3
Cody Ebberson <cody@ebberson.com>
2024-04-23 15:34:08 -0700

    Fixes #3240 - rate-limit-redis persistent rate limit storage (#4435)
    * fix-3240 rate-limit-redis persistent rate limit storage
    
    * prettier and package-lock
    
    * Revert pckage-lock.json
    
    * Fixed package-lock.json
    
    * Increase rate limit for tests
    
    * Update packages/server/src/ratelimit.ts
    
    Co-authored-by: Derrick Farris <derrick@medplum.com>
    
    * Updated defaults and comment
    
    ---------
    
    Co-authored-by: dillon streator <dillonstreator+git@gmail.com>
    Co-authored-by: Derrick Farris <derrick@medplum.com>

--- packages/server/src/ratelimit.ts
+++ packages/server/src/ratelimit.ts
@@ -1,21 +1,30 @@
 import { tooManyRequests } from '@medplum/core';
 import { Request } from 'express';
-import rateLimit, { MemoryStore, RateLimitRequestHandler } from 'express-rate-limit';
+import rateLimit, { RateLimitRequestHandler } from 'express-rate-limit';
+import RedisStore from 'rate-limit-redis';
 import { AuthenticatedRequestContext, getRequestContext } from './context';
+import { getRedis } from './redis';
 
-/*
- * MemoryStore must be shutdown to cleanly stop the server.
- */
-
+// TODO: These values should be server config settings
+// History:
+// Before, the default "auth rate limit" was 600 per 15 minutes, but used "MemoryStore" rather than "RedisStore"
+// That meant that the rate limit was per server instance, rather than per server cluster
+// The value was primarily tuned for one particular cluster with 6 server instances
+// Therefore, to maintain parity, the new default "auth rate limit" is 1200 per 15 minutes
 const DEFAULT_RATE_LIMIT_PER_15_MINUTES = 15 * 60 * 1000; // 1000 requests per second
-const DEFAULT_AUTH_RATE_LIMIT_PER_15_MINUTES = 600;
+const DEFAULT_AUTH_RATE_LIMIT_PER_15_MINUTES = 2400;
 
 let handler: RateLimitRequestHandler | undefined = undefined;
-let store: MemoryStore | undefined = undefined;
+let store: RedisStore | undefined = undefined;
 
 export function getRateLimiter(): RateLimitRequestHandler {
   if (!handler) {
-    store = new MemoryStore();
+    const client = getRedis();
+    store = new RedisStore({
+      // See: https://www.npmjs.com/package/rate-limit-redis#:~:text=//%20%40ts%2Dexpect%2Derror%20%2D%20Known%20issue%3A
+      // @ts-expect-error - The ioredis call function expects structured string arguments
+      sendCommand: (...args: string[]): unknown => client.call(...args),
+    });
     handler = rateLimit({
       windowMs: 15 * 60 * 1000, // 15 minutes
       limit: getRateLimitForRequest,
@@ -29,7 +38,6 @@ export function getRateLimiter(): RateLimitRequestHandler {
 
 export function closeRateLimiter(): void {
   if (handler) {
-    store?.shutdown();
     store = undefined;
     handler = undefined;
   }
