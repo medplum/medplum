10bc983ad8bd82b30c2ad6d04543c14b1301ba2f
Derrick Farris <derrick@medplum.com>
2024-04-23 00:36:48 -0700

    test(coverage): add back coverage of `src` to `seed` tests

--- packages/server/jest.seed.config.ts
+++ packages/server/jest.seed.config.ts
@@ -4,5 +4,5 @@ import defaultConfig from './jest.config';
 export default {
   ...defaultConfig,
   testMatch: ['<rootDir>/seed-tests/**/*.test.ts'],
-  collectCoverageFrom: ['<rootDir>/seed-tests/**/*'],
+  collectCoverageFrom: ['<rootDir>/seed-tests/**/*', '<rootDir>/src/**/*'],
 } satisfies Config;
