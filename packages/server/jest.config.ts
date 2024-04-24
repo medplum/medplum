8369583005126317f4422b76345326bca6cee06d
Derrick Farris <derrick@medplum.com>
2024-04-23 17:19:06 -0700

    docs(server/jest): add comment about weird glob behavior

--- packages/server/jest.config.ts
+++ packages/server/jest.config.ts
@@ -7,6 +7,9 @@ export default {
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },
+  // Oddly, the glob `<rootDir>/src/seed*.test.ts` correctly matches both seed tests in the positive case in `jest.seed.config.ts`
+  // But `!<rootDir>/src/seed*.test.ts` doesn't match both in the negative case, and only matches `seed-serial.test.ts`
+  // That's why we use `!**/src/seed*.test.ts` here
   testMatch: ['<rootDir>/src/**/*.test.ts', '!**/src/seed*.test.ts'],
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
   coverageDirectory: 'coverage',
