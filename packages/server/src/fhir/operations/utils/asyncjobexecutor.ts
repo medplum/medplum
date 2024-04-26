009bd17d35fb7d1d8ebd33387abbecfe22d818be
Derrick Farris <derrick@medplum.com>
2024-04-25 17:53:58 -0700

    feat(agent): support `Prefer: respond-async` for `Agent/$push`

--- packages/server/src/fhir/operations/utils/asyncjobexecutor.ts
+++ packages/server/src/fhir/operations/utils/asyncjobexecutor.ts
@@ -1,6 +1,10 @@
-import { AsyncJob } from '@medplum/fhirtypes';
-import { AsyncLocalStorage } from 'async_hooks';
-import { getRequestContext } from '../../../context';
+import { accepted } from '@medplum/core';
+import { AsyncJob, Parameters } from '@medplum/fhirtypes';
+import { Request, Response } from 'express';
+import { AsyncLocalStorage } from 'node:async_hooks';
+import { getConfig } from '../../../config';
+import { getAuthenticatedContext, getRequestContext } from '../../../context';
+import { sendOutcome } from '../../outcomes';
 import { Repository, getSystemRepo } from '../../repo';
 
 export class AsyncJobExecutor {
@@ -34,18 +38,19 @@ export class AsyncJobExecutor {
       );
   }
 
-  async run(callback: () => Promise<any>): Promise<void> {
+  async run(callback: () => Promise<Parameters | undefined>): Promise<void> {
     callback = AsyncLocalStorage.bind(callback);
     if (!this.resource) {
       throw new Error('AsyncJob missing');
     }
     const systemRepo = getSystemRepo();
     try {
-      await callback();
+      const output = await callback();
       await systemRepo.updateResource<AsyncJob>({
         ...this.resource,
         status: 'completed',
         transactionTime: new Date().toISOString(),
+        output,
       });
     } catch (err) {
       await systemRepo.updateResource<AsyncJob>({
@@ -66,4 +71,13 @@ export class AsyncJobExecutor {
 
     return `${baseUrl}fhir/R4/job/${this.resource.id}/status`;
   }
+}
+
+export async function sendAsyncResponse(req: Request, res: Response, callback: () => Promise<any>): Promise<void> {
+  const ctx = getAuthenticatedContext();
+  const { baseUrl } = getConfig();
+  const exec = new AsyncJobExecutor(ctx.repo);
+  await exec.init(req.protocol + '://' + req.get('host') + req.originalUrl);
+  exec.start(callback);
+  sendOutcome(res, accepted(exec.getContentLocation(baseUrl)));
 }
