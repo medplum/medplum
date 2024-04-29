a94ec1920d1e2bb6f85c48028ece195f876c1883
Matt Long <mattlong@users.noreply.github.com>
2024-04-29 16:34:28 -0700

    [Medplum Provider app] Integrate specialized Tasks tab from examples/medplum-task-demo (#4478)
    * First steps of task demo migration
    
    * Spruce up DeleteTask a bit
    
    * Consistent styling in ClaimTask
    
    * Consistent spacing when Complete Task hidden
    
    * TaskTab needs useState after all
    
    * Deleting a task goes back to the correct search page
    
    * Cleanup addSearchValues and rename it
    
    * Clean up some unfinished bits

--- examples/medplum-provider/src/pages/patient/PatientPage.utils.ts
+++ examples/medplum-provider/src/pages/patient/PatientPage.utils.ts
@@ -11,3 +11,62 @@ export function prependPatientPath(patient: Patient | undefined, path: string): 
 
   return path;
 }
+
+export function formatPatientPageTabUrl(patientId: string, tab: PatientPageTabInfo): string {
+  return `${patientPathPrefix(patientId)}/${tab.url.replace('%patient.id', patientId)}`;
+}
+
+export type PatientPageTabInfo = {
+  id: string;
+  url: string;
+  label: string;
+};
+
+export const TasksTab: PatientPageTabInfo = {
+  id: 'tasks',
+  url: 'Task?_fields=_lastUpdated,code,status,focus&_offset=0&_sort=-_lastUpdated&patient=%patient.id',
+  label: 'Tasks',
+};
+
+export const PatientPageTabs: PatientPageTabInfo[] = [
+  { id: 'timeline', url: '', label: 'Timeline' },
+  { id: 'edit', url: 'edit', label: 'Edit' },
+  { id: 'encounter', url: 'encounter', label: 'Encounter' },
+  TasksTab,
+  {
+    id: 'meds',
+    url: 'MedicationRequest?_fields=medication[x],intent,status&_offset=0&_sort=-_lastUpdated&patient=%patient.id',
+    label: 'Meds',
+  },
+  {
+    id: 'labs',
+    url: 'ServiceRequest?_fields=_lastUpdated,code,status,orderDetail,category&_offset=0&_sort=-_lastUpdated&category=108252007&patient=%patient.id',
+    label: 'Labs',
+  },
+  {
+    id: 'devices',
+    url: 'Device?_fields=manufacturer,deviceName,status,distinctIdentifier,serialNumber&_offset=0&_sort=-_lastUpdated&patient=%patient.id',
+    label: 'Devices',
+  },
+  {
+    id: 'diagnosticreports',
+    url: 'DiagnosticReport?_fields=_lastUpdated,category,code,status&_offset=0&_sort=-_lastUpdated&patient=%patient.id',
+    label: 'Reports',
+  },
+  {
+    id: 'documentreference',
+    url: 'DocumentReference?_fields=_lastUpdated,category,type,status,author&_offset=0&_sort=-_lastUpdated&patient=%patient.id',
+    label: 'Documents',
+  },
+  {
+    id: 'appointments',
+    url: 'Appointment?_fields=_lastUpdated,category,type,status,author&_offset=0&_sort=-_lastUpdated&patient=%patient.id',
+    label: 'Appointments',
+  },
+  {
+    id: 'careplan',
+    url: 'CarePlan?_fields=_lastUpdated,status,intent,category,period&_sort=-_lastUpdated&patient=%patient.id',
+    label: 'Care Plans',
+  },
+  { id: 'communication', url: 'communication', label: 'Communications' },
+];
