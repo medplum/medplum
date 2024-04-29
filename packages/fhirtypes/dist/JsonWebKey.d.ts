5cc2803807c6f595dcd50dce71d175ef7c4b7573
Cody Ebberson <cody@ebberson.com>
2024-04-29 13:28:30 -0700

    Update all Medplum types to extend DomainResource

--- packages/fhirtypes/dist/JsonWebKey.d.ts
+++ packages/fhirtypes/dist/JsonWebKey.d.ts
@@ -3,7 +3,10 @@
  * Do not edit manually.
  */
 
+import { Extension } from './Extension';
 import { Meta } from './Meta';
+import { Narrative } from './Narrative';
+import { Resource } from './Resource';
 
 /**
  * A JSON object that represents a cryptographic key. The members of the
@@ -41,6 +44,51 @@ export interface JsonWebKey {
    * The base language in which the resource is written.
    */
   language?: string;
+
+  /**
+   * A human-readable narrative that contains a summary of the resource and
+   * can be used to represent the content of the resource to a human. The
+   * narrative need not encode all the structured data, but is required to
+   * contain sufficient detail to make it &quot;clinically safe&quot; for a human to
+   * just read the narrative. Resource definitions may define what content
+   * should be represented in the narrative to ensure clinical safety.
+   */
+  text?: Narrative;
+
+  /**
+   * These resources do not have an independent existence apart from the
+   * resource that contains them - they cannot be identified independently,
+   * and nor can they have their own independent transaction scope.
+   */
+  contained?: Resource[];
+
+  /**
+   * May be used to represent additional information that is not part of
+   * the basic definition of the resource. To make the use of extensions
+   * safe and manageable, there is a strict set of governance  applied to
+   * the definition and use of extensions. Though any implementer can
+   * define an extension, there is a set of requirements that SHALL be met
+   * as part of the definition of the extension.
+   */
+  extension?: Extension[];
+
+  /**
+   * May be used to represent additional information that is not part of
+   * the basic definition of the resource and that modifies the
+   * understanding of the element that contains it and/or the understanding
+   * of the containing element's descendants. Usually modifier elements
+   * provide negation or qualification. To make the use of extensions safe
+   * and manageable, there is a strict set of governance applied to the
+   * definition and use of extensions. Though any implementer is allowed to
+   * define an extension, there is a set of requirements that SHALL be met
+   * as part of the definition of the extension. Applications processing a
+   * resource are required to check for modifier extensions.
+   *
+   * Modifier extensions SHALL NOT change the meaning of any elements on
+   * Resource or DomainResource (including cannot change the meaning of
+   * modifierExtension itself).
+   */
+  modifierExtension?: Extension[];
 
   /**
    * Whether this key is in active use.
