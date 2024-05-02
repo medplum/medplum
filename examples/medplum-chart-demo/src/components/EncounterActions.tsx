a493bac07755f90964e384ee80801372c6225736
ksmith94 <102421938+ksmith94@users.noreply.github.com>
2024-05-02 07:13:10 -0400

    Remove finalize note button

--- examples/medplum-chart-demo/src/components/EncounterActions.tsx
+++ examples/medplum-chart-demo/src/components/EncounterActions.tsx
@@ -1,7 +1,6 @@
 import { Stack, Title } from '@mantine/core';
 import { Encounter } from '@medplum/fhirtypes';
 import { EditType } from './actions/EditType';
-import { FinalizeNote } from './actions/FinalizeNote';
 
 interface EncounterActionsProps {
   encounter: Encounter;
@@ -12,7 +11,6 @@ export function EncounterActions(props: EncounterActionsProps): JSX.Element {
   return (
     <Stack p="xs" m="xs">
       <Title>Encounter Actions</Title>
-      <FinalizeNote />
       <EditType encounter={props.encounter} onChange={props.onChange} />
     </Stack>
   );
