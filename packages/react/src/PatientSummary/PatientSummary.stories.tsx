12ed9d93919c69ae1212631a8bd7599cd34a32ec
Matt Long <ml@medplum.com>
2024-05-06 16:31:12 -0700

    Add story for PatientSummary

--- packages/react/src/PatientSummary/PatientSummary.stories.tsx
+++ packages/react/src/PatientSummary/PatientSummary.stories.tsx
@@ -1,6 +1,8 @@
 import { HomerSimpson } from '@medplum/mock';
 import { Meta } from '@storybook/react';
 import { PatientSummary } from './PatientSummary';
+import { Anchor } from '@mantine/core';
+import { Link } from 'react-router-dom';
 
 export default {
   title: 'Medplum/PatientSummary',
@@ -22,5 +24,26 @@ export const BackgroundImage = (): JSX.Element => (
     my="xl"
     shadow="xs"
     background="url(https://images.unsplash.com/photo-1535961652354-923cb08225a7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bmF0dXJlJTIwc21hbGx8ZW58MHwwfDB8fHww&auto=format&fit=crop&w=800&q=60)"
+  />
+);
+
+export const CustomLinkStyle = (): JSX.Element => (
+  <PatientSummary
+    patient={HomerSimpson}
+    w={350}
+    withBorder
+    padding="lg"
+    radius="md"
+    mx="md"
+    my="xl"
+    shadow="xs"
+    linkRenderers={{
+      appointments: (msg) => (
+        <Anchor component={Link} to="/some-custom-appointments-page" fw="bold" c="salmon">
+          {msg}
+        </Anchor>
+      ),
+      encounters: () => undefined,
+    }}
   />
 );
