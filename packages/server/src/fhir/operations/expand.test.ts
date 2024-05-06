8926589b2b1be9b4a3c964589f75e3b39cc4761b
Matthew Willer <matt@medplum.com>
2024-05-06 11:12:52 -0700

    Full linked Project ordering in CodeSystem lookup

--- packages/server/src/fhir/operations/expand.test.ts
+++ packages/server/src/fhir/operations/expand.test.ts
@@ -1,4 +1,4 @@
-import { ContentType, HTTP_HL7_ORG, HTTP_TERMINOLOGY_HL7_ORG, LOINC, SNOMED } from '@medplum/core';
+import { ContentType, HTTP_HL7_ORG, HTTP_TERMINOLOGY_HL7_ORG, LOINC, SNOMED, createReference } from '@medplum/core';
 import {
   CodeSystem,
   OperationOutcome,
@@ -546,6 +546,60 @@ describe('Updated implementation', () => {
     expect(coding.system).toBe(SNOMED);
     expect(coding.code).toBe('314159265');
     expect(coding.display).toEqual('Test SNOMED override');
+  });
+
+  test('Prefers CodeSystem from linked Projects in link order', async () => {
+    // Set up linked Projects and CodeSystem resources
+    const url = 'http://example.com/cs' + randomUUID();
+    const codeSystem: CodeSystem = {
+      resourceType: 'CodeSystem',
+      status: 'active',
+      content: 'complete',
+      url,
+    };
+    const { project: p1, accessToken: a1 } = await createTestProject({ withAccessToken: true });
+    const cs1 = await request(app)
+      .post(`/fhir/R4/CodeSystem`)
+      .set('Authorization', 'Bearer ' + a1)
+      .set('Content-Type', ContentType.FHIR_JSON)
+      .send({ ...codeSystem, concept: [{ code: '1', display: 'Correct coding' }] });
+    expect(cs1.status).toEqual(201);
+
+    const { project: p2, accessToken: a2 } = await createTestProject({ withAccessToken: true });
+    const cs2 = await request(app)
+      .post(`/fhir/R4/CodeSystem`)
+      .set('Authorization', 'Bearer ' + a2)
+      .set('Content-Type', ContentType.FHIR_JSON)
+      .send({ ...codeSystem, concept: [{ code: '1', display: 'Incorrect coding' }] });
+    expect(cs2.status).toEqual(201);
+
+    accessToken = await initTestAuth({
+      project: {
+        features: ['terminology'],
+        link: [{ project: createReference(p1) }, { project: createReference(p2) }],
+      },
+    });
+
+    const res2 = await request(app)
+      .post(`/fhir/R4/ValueSet`)
+      .set('Authorization', 'Bearer ' + accessToken)
+      .set('Content-Type', ContentType.FHIR_JSON)
+      .send({
+        resourceType: 'ValueSet',
+        status: 'active',
+        url: 'https://example.com/snomed-all' + randomUUID(),
+        compose: { include: [{ system: url }] },
+      });
+    expect(res2.status).toBe(201);
+
+    const res3 = await request(app)
+      .get(`/fhir/R4/ValueSet/$expand?url=${encodeURIComponent(res2.body.url)}`)
+      .set('Authorization', 'Bearer ' + accessToken);
+    expect(res3.status).toBe(200);
+    const coding = res3.body.expansion.contains[0];
+    expect(coding.system).toBe(url);
+    expect(coding.code).toBe('1');
+    expect(coding.display).toEqual('Correct coding');
   });
 
   test('Returns error when property filter is invalid for CodeSystem', async () => {
