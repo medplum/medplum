3dcf06be503783d254b5f80f435f71fb64e5151e
Matt Long <ml@medplum.com>
2024-05-09 13:57:12 -0700

    Fix tests

--- packages/core/src/typeschema/types.ts
+++ packages/core/src/typeschema/types.ts
@@ -68,7 +68,7 @@ export interface SlicingRules {
 
 export interface SliceDefinition extends InternalSchemaElement {
   // narrowed InternalSchemaElement fields
-  slicing: undefined;
+  slicing?: undefined;
 
   // additional fields for SliceDefinition
   name: string;
