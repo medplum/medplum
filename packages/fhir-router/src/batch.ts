d30a0ae61ec031aec1ac9c41b739ad287f84b7f1
Cody Ebberson <cody@ebberson.com>
2024-05-30 10:29:24 -0700

    Better PATCH error messages (#4607)
    * Better PATCH error messages
    
    * [autofix.ci] apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- packages/fhir-router/src/batch.ts
+++ packages/fhir-router/src/batch.ts
@@ -196,7 +196,16 @@ class BatchProcessor {
       throw new OperationOutcomeError(badRequest('Missing entry.resource.data'));
     }
 
-    return this.rewriteIdsInArray(JSON.parse(Buffer.from(patchResource.data, 'base64').toString('utf8')));
+    const body = JSON.parse(Buffer.from(patchResource.data, 'base64').toString('utf8'));
+    if (!body) {
+      throw new OperationOutcomeError(badRequest('Empty patch body'));
+    }
+
+    if (!Array.isArray(body)) {
+      throw new OperationOutcomeError(badRequest('Patch body must be an array'));
+    }
+
+    return this.rewriteIdsInArray(body);
   }
 
   private addReplacementId(fullUrl: string, resource: Resource): void {
