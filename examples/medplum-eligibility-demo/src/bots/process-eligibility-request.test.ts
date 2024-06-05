9fa5d07a2d703c0a0cc039d0f4ec01a3f4e5f526
Rahul Agarwal <rahul@medplum.com>
2024-06-04 22:20:45 -0700

    Fix demo app tests

--- examples/medplum-eligibility-demo/src/bots/process-eligibility-request.test.ts
+++ examples/medplum-eligibility-demo/src/bots/process-eligibility-request.test.ts
@@ -1,5 +1,5 @@
 import { ContentType, indexSearchParameterBundle, indexStructureDefinitionBundle } from '@medplum/core';
-import { readJson } from '@medplum/definitions';
+import { SEARCH_PARAMETER_BUNDLE_FILES, readJson } from '@medplum/definitions';
 import { Bot, Bundle, CoverageEligibilityRequest, Reference, SearchParameter } from '@medplum/fhirtypes';
 import { MockClient } from '@medplum/mock';
 import { beforeAll, describe, expect, test, vi } from 'vitest';
@@ -19,8 +19,10 @@ describe('Process Eligibility Request', async () => {
   beforeAll(() => {
     indexStructureDefinitionBundle(readJson('fhir/r4/profiles-types.json') as Bundle);
     indexStructureDefinitionBundle(readJson('fhir/r4/profiles-resources.json') as Bundle);
-    indexSearchParameterBundle(readJson('fhir/r4/search-parameters.json') as Bundle<SearchParameter>);
-    indexSearchParameterBundle(readJson('fhir/r4/search-parameters-medplum.json') as Bundle<SearchParameter>);
+    indexStructureDefinitionBundle(readJson('fhir/r4/profiles-medplum.json') as Bundle);
+    for (const filename of SEARCH_PARAMETER_BUNDLE_FILES) {
+      indexSearchParameterBundle(readJson(filename) as Bundle<SearchParameter>);
+    }
   });
 
   test('Success', async () => {
--- examples/medplum-task-demo/src/bots/example/create-respond-to-message-task.test.ts
+++ examples/medplum-task-demo/src/bots/example/create-respond-to-message-task.test.ts
@@ -16,6 +16,7 @@ describe('Create Respond to Message Task', async () => {
   beforeAll(() => {
     indexStructureDefinitionBundle(readJson('fhir/r4/profiles-types.json') as Bundle);
     indexStructureDefinitionBundle(readJson('fhir/r4/profiles-resources.json') as Bundle);
+    indexStructureDefinitionBundle(readJson('fhir/r4/profiles-medplum.json') as Bundle);
     for (const filename of SEARCH_PARAMETER_BUNDLE_FILES) {
       indexSearchParameterBundle(readJson(filename) as Bundle<SearchParameter>);
     }
