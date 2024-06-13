94e15d07f7ef634814df1bb673722d3d42de6f31
Clay Coleman <jclaytoncoleman@gmail.com>
2024-06-13 17:04:37 -0400

    Export QuestionnaireFormContext and QuestionnairePageSequence

--- packages/react/src/QuestionnaireForm/QuestionnaireForm.tsx
+++ packages/react/src/QuestionnaireForm/QuestionnaireForm.tsx
@@ -15,6 +15,9 @@ import { buildInitialResponse, getNumberOfPages, isQuestionEnabled } from '../ut
 import { QuestionnaireFormContext } from './QuestionnaireForm.context';
 import { QuestionnairePageSequence } from './QuestionnaireFormComponents/QuestionnaireFormPageSequence';
 
+export { QuestionnaireFormContext } from './QuestionnaireForm.context';
+export { QuestionnairePageSequence } from './QuestionnaireFormComponents/QuestionnaireFormPageSequence';
+
 export interface QuestionnaireFormProps {
   readonly questionnaire: Questionnaire | Reference<Questionnaire>;
   readonly subject?: Reference;
