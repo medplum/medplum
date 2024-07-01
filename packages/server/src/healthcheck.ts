a8bb00f316486abe58bdc492cb15dcdbd98a8970
Derrick Farris <derrick@medplum.com>
2024-07-01 12:34:13 -0500

    feat(otel): add basic metrics for pg (#4737)
    * feat(otel): add basic metrics for pg
    
    * fix(healthcheck): rm duplicate line
    
    * tweak(otel): add tests for `Gauge`, change metric names
    
    * feat(otel): add `instanceId` attribute to `healthcheck` metrics
    
    * fix(server): remove unused var
    
    * fix(aws/metadata): fix `fetchInstanceId` when not in fargate
    
    * wip(otel): test if hostname works as expected
    
    * cleanup(otel): use `hostname` instead of `instanceId`

--- packages/server/src/healthcheck.ts
+++ packages/server/src/healthcheck.ts
@@ -1,16 +1,33 @@
 import { MEDPLUM_VERSION } from '@medplum/core';
 import { Request, Response } from 'express';
+import os from 'node:os';
 import { getDatabasePool } from './database';
+import { setGauge } from './otel/otel';
 import { getRedis } from './redis';
 
+const hostname = os.hostname();
+
 export async function healthcheckHandler(_req: Request, res: Response): Promise<void> {
+  setGauge('medplum.db.idleConnections', getDatabasePool().idleCount, { hostname });
+  setGauge('medplum.db.queriesAwaitingClient', getDatabasePool().waitingCount, { hostname });
+
+  let startTime = Date.now();
+  const postgresOk = await testPostgres();
+  const dbRoundtripMs = Date.now() - startTime;
+  setGauge('medplum.db.healthcheckRTT', dbRoundtripMs / 1000, { hostname });
+
+  startTime = Date.now();
+  const redisOk = await testRedis();
+  const redisRoundtripMs = Date.now() - startTime;
+  setGauge('medplum.redis.healthcheckRTT', redisRoundtripMs / 1000, { hostname });
+
   res.json({
     ok: true,
     version: MEDPLUM_VERSION,
     platform: process.platform,
     runtime: process.version,
-    postgres: await testPostgres(),
-    redis: await testRedis(),
+    postgres: postgresOk,
+    redis: redisOk,
   });
 }
 
