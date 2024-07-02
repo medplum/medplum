26eb78de82ba0c667c520e52dad680f1ab6f2d7e
Filipe Ximenes <filipeximenes@gmail.com>
2024-07-02 16:49:16 -0300

    uses useResource to fetch practitioner in the Foomedical MessagesPage

--- examples/foomedical/src/pages/MessagesPage.tsx
+++ examples/foomedical/src/pages/MessagesPage.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useState, useCallback, useMemo } from 'react';
+import { useState, useCallback, useMemo } from 'react';
 import { useParams } from 'react-router-dom';
 import { Alert } from '@mantine/core';
 import { Communication, HumanName, Patient, Practitioner } from '@medplum/fhirtypes';
 import { createReference, formatGivenName, getReferenceString, normalizeErrorString } from '@medplum/core';
-import { BaseChat, Document, useMedplum, useMedplumProfile } from '@medplum/react';
+import { BaseChat, Document, useMedplum, useMedplumProfile, useResource } from '@medplum/react';
 import { Loading } from '../components/Loading';
 import { showNotification } from '@mantine/notifications';
 import { IconCircleOff } from '@tabler/icons-react';
@@ -12,36 +12,11 @@ export function Messages(): JSX.Element {
   const medplum = useMedplum();
   const profile = useMedplumProfile() as Patient;
   const profileRef = useMemo(() => (profile ? createReference(profile) : undefined), [profile]);
-  const [practitioner, setPractitioner] = useState<Practitioner>();
   const [communications, setCommunications] = useState<Communication[]>([]);
   const { practitionerId } = useParams();
-
-  useEffect(() => {
-    medplum
-      .graphql(
-        `
-        {
-          Practitioner(id: "${practitionerId}") {
-            resourceType
-            id
-            name {
-              given
-              family
-            }
-          }
-        }
-      `
-      )
-      .then((value) => setPractitioner(value.data.Practitioner as Practitioner))
-      .catch((err) => {
-        showNotification({
-          color: 'red',
-          icon: <IconCircleOff />,
-          title: 'Error',
-          message: normalizeErrorString(err),
-        });
-      });
-  }, [medplum, profile, practitionerId]);
+  const practitioner = useResource<Practitioner>({
+    reference: `Practitioner/${practitionerId}`,
+  });
 
   const sendMessage = useCallback(
     (content: string): void => {
