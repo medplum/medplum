17b227b2444f4954d1d81c8cb58dd253881e465b
Derrick Farris <derrick@medplum.com>
2024-07-08 12:22:57 -0700

    tweak(otel): add units to some metrics

--- packages/server/src/healthcheck.ts
+++ packages/server/src/healthcheck.ts
@@ -4,40 +4,42 @@ import os from 'node:os';
 import v8 from 'node:v8';
 import { Pool } from 'pg';
 import { DatabaseMode, getDatabasePool } from './database';
-import { setGauge } from './otel/otel';
+import { RecordMetricOptions, setGauge } from './otel/otel';
 import { getRedis } from './redis';
 
 const hostname = os.hostname();
+const BASE_METRIC_OPTIONS = { attributes: { hostname } } satisfies RecordMetricOptions;
+const METRIC_IN_SECS_OPTIONS = { ...BASE_METRIC_OPTIONS, options: { unit: 's' } } satisfies RecordMetricOptions;
 
 export async function healthcheckHandler(_req: Request, res: Response): Promise<void> {
   const pool = getDatabasePool(DatabaseMode.WRITER);
 
-  setGauge('medplum.db.idleConnections', pool.idleCount, { hostname });
-  setGauge('medplum.db.queriesAwaitingClient', pool.waitingCount, { hostname });
+  setGauge('medplum.db.idleConnections', pool.idleCount, BASE_METRIC_OPTIONS);
+  setGauge('medplum.db.queriesAwaitingClient', pool.waitingCount, BASE_METRIC_OPTIONS);
 
   let startTime = Date.now();
   const postgresOk = await testPostgres(pool);
   const dbRoundtripMs = Date.now() - startTime;
-  setGauge('medplum.db.healthcheckRTT', dbRoundtripMs / 1000, { hostname });
+  setGauge('medplum.db.healthcheckRTT', dbRoundtripMs / 1000, METRIC_IN_SECS_OPTIONS);
 
   startTime = Date.now();
   const redisOk = await testRedis();
   const redisRoundtripMs = Date.now() - startTime;
-  setGauge('medplum.redis.healthcheckRTT', redisRoundtripMs / 1000, { hostname });
+  setGauge('medplum.redis.healthcheckRTT', redisRoundtripMs / 1000, METRIC_IN_SECS_OPTIONS);
 
   const heapStats = v8.getHeapStatistics();
-  setGauge('medplum.node.usedHeapSize', heapStats.used_heap_size, { hostname });
+  setGauge('medplum.node.usedHeapSize', heapStats.used_heap_size, BASE_METRIC_OPTIONS);
 
   const heapSpaceStats = v8.getHeapSpaceStatistics();
   setGauge(
     'medplum.node.oldSpaceUsedSize',
     heapSpaceStats.find((entry) => entry.space_name === 'old_space')?.space_used_size ?? -1,
-    { hostname }
+    BASE_METRIC_OPTIONS
   );
   setGauge(
     'medplum.node.newSpaceUsedSize',
     heapSpaceStats.find((entry) => entry.space_name === 'new_space')?.space_used_size ?? -1,
-    { hostname }
+    BASE_METRIC_OPTIONS
   );
 
   res.json({
--- packages/server/src/otel/otel.test.ts
+++ packages/server/src/otel/otel.test.ts
@@ -1,4 +1,4 @@
-import { incrementCounter, recordHistogramValue, setGauge } from '../otel/otel';
+import { getGauge, incrementCounter, recordHistogramValue, setGauge } from '../otel/otel';
 
 describe('OpenTelemetry', () => {
   const OLD_ENV = process.env;
@@ -14,21 +14,41 @@ describe('OpenTelemetry', () => {
   });
 
   test('Increment counter, disabled', async () => {
-    expect(incrementCounter('test', {})).toBe(false);
+    expect(incrementCounter('test')).toBe(false);
   });
 
   test('Increment counter, enabled', async () => {
     process.env.OTLP_METRICS_ENDPOINT = 'http://localhost:4318/v1/metrics';
-    expect(incrementCounter('test', {})).toBe(true);
+    expect(incrementCounter('test')).toBe(true);
+  });
+
+  test('Increment counter, enabled, attributes specified', async () => {
+    process.env.OTLP_METRICS_ENDPOINT = 'http://localhost:4318/v1/metrics';
+    expect(incrementCounter('test', { attributes: { hostname: 'https://example.com' } })).toBe(true);
+  });
+
+  test('Increment counter, enabled, options specified', async () => {
+    process.env.OTLP_METRICS_ENDPOINT = 'http://localhost:4318/v1/metrics';
+    expect(incrementCounter('test', { options: { unit: 's' } })).toBe(true);
   });
 
   test('Record histogram value, disabled', async () => {
-    expect(recordHistogramValue('test', 1, {})).toBe(false);
+    expect(recordHistogramValue('test', 1)).toBe(false);
   });
 
   test('Record histogram value, enabled', async () => {
     process.env.OTLP_METRICS_ENDPOINT = 'http://localhost:4318/v1/metrics';
-    expect(recordHistogramValue('test', 1, {})).toBe(true);
+    expect(recordHistogramValue('test', 1)).toBe(true);
+  });
+
+  test('Record histogram value, enabled, attributes specified', async () => {
+    process.env.OTLP_METRICS_ENDPOINT = 'http://localhost:4318/v1/metrics';
+    expect(recordHistogramValue('test', 1, { attributes: { hostname: 'https://example.com' } })).toBe(true);
+  });
+
+  test('Record histogram value, enabled, options specified', async () => {
+    process.env.OTLP_METRICS_ENDPOINT = 'http://localhost:4318/v1/metrics';
+    expect(recordHistogramValue('test', 1, { options: { unit: 's' } })).toBe(true);
   });
 
   test('Set gauge, disabled', async () => {
@@ -38,5 +58,16 @@ describe('OpenTelemetry', () => {
   test('Set gauge, enabled', async () => {
     process.env.OTLP_METRICS_ENDPOINT = 'http://localhost:4318/v1/metrics';
     expect(setGauge('test', 1)).toBe(true);
+  });
+
+  test('Set gauge, enabled, attributes specified', async () => {
+    process.env.OTLP_METRICS_ENDPOINT = 'http://localhost:4318/v1/metrics';
+    expect(setGauge('test', 1, { attributes: { hostname: 'https://example.com' } })).toBe(true);
+  });
+
+  test('Set gauge, enabled, options specified', async () => {
+    process.env.OTLP_METRICS_ENDPOINT = 'http://localhost:4318/v1/metrics';
+    expect(setGauge('test', 1, { options: { unit: 's' } })).toBe(true);
+    getGauge('test');
   });
 });
