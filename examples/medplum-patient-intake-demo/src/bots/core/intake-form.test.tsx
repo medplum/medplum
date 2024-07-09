92f479bf1f885e1688421ece8980b75a7f9ca647
Filipe Ximenes <filipeximenes@gmail.com>
2024-07-09 16:45:12 -0300

    [Patient Intake Sample] improves organization of tests

--- examples/medplum-patient-intake-demo/src/bots/core/intake-form.test.tsx
+++ examples/medplum-patient-intake-demo/src/bots/core/intake-form.test.tsx
@@ -30,91 +30,109 @@ describe('Intake form', async () => {
     patient = await medplum.createResource(intakePatient);
   });
 
-  test('update patient demographic information', async () => {
-    await handler({ bot, input: response, contentType, secrets: {} }, medplum);
+  describe('Update Patient demographic information', async () => {
+    test('Patient attributes', async () => {
+      await handler({ bot, input: response, contentType, secrets: {} }, medplum);
 
-    patient = await medplum.readResource('Patient', patient.id as string);
+      patient = await medplum.readResource('Patient', patient.id as string);
 
-    const sexualOrientation = await medplum.searchOne('Observation', {
-      code: '76690-7',
-      subject: getReferenceString(patient),
+      expect(patient.name?.[0].given).toEqual(['FirstName', 'MiddleName']);
+      expect(patient.name?.[0].family).toEqual('LastName');
+      expect(patient.gender).toEqual('other');
+      expect(patient.birthDate).toEqual('2000-01-01');
     });
 
-    expect(patient.name?.[0].given).toEqual(['FirstName', 'MiddleName']);
-    expect(patient.name?.[0].family).toEqual('LastName');
-    expect(patient.gender).toEqual('other');
-    expect(patient.birthDate).toEqual('2000-01-01');
-    expect(getExtensionValue(patient, 'http://terminology.hl7.org/CodeSystem/v3-Race')).toEqual({
-      code: '1068-6',
-      display: 'Canadian and Latin American Indian',
-      system: 'http://terminology.hl7.org/CodeSystem/v3-Race',
+    test('Race and etinicity', async () => {
+      await handler({ bot, input: response, contentType, secrets: {} }, medplum);
+
+      patient = await medplum.readResource('Patient', patient.id as string);
+
+      expect(getExtensionValue(patient, 'http://terminology.hl7.org/CodeSystem/v3-Race')).toEqual({
+        code: '1068-6',
+        display: 'Canadian and Latin American Indian',
+        system: 'http://terminology.hl7.org/CodeSystem/v3-Race',
+      });
+      expect(getExtensionValue(patient, 'http://terminology.hl7.org/CodeSystem/v3-Ethnicity')).toEqual({
+        code: '2135-2',
+        display: 'Hispanic or Latino',
+        system: 'http://terminology.hl7.org/CodeSystem/v3-Ethnicity',
+      });
     });
-    expect(getExtensionValue(patient, 'http://terminology.hl7.org/CodeSystem/v3-Ethnicity')).toEqual({
-      code: '2135-2',
-      display: 'Hispanic or Latino',
-      system: 'http://terminology.hl7.org/CodeSystem/v3-Ethnicity',
+
+    test('Sexual orientation', async () => {
+      await handler({ bot, input: response, contentType, secrets: {} }, medplum);
+
+      patient = await medplum.readResource('Patient', patient.id as string);
+
+      const sexualOrientation = await medplum.searchOne('Observation', {
+        code: '76690-7',
+        subject: getReferenceString(patient),
+      });
+
+      expect(sexualOrientation?.valueCodeableConcept?.coding?.[0].code).toEqual('42035005');
     });
-    expect(sexualOrientation?.valueCodeableConcept?.coding?.[0].code).toEqual('42035005');
   });
 
-  test('adds languages', async () => {
-    await handler({ bot, input: response, contentType, secrets: {} }, medplum);
+  describe('Language information', async () => {
+    test('add languages', async () => {
+      await handler({ bot, input: response, contentType, secrets: {} }, medplum);
 
-    patient = await medplum.readResource('Patient', patient.id as string);
+      patient = await medplum.readResource('Patient', patient.id as string);
 
-    expect(patient.communication?.length).toEqual(2);
-    expect(patient.communication?.[0].language.coding?.[0].code).toEqual('pt-BR');
-    expect(patient.communication?.[1].language.coding?.[0].code).toEqual('en');
-    expect(patient.communication?.[1].preferred).toBeTruthy();
-  });
+      expect(patient.communication?.length).toEqual(2);
+      expect(patient.communication?.[0].language.coding?.[0].code).toEqual('pt-BR');
+      expect(patient.communication?.[1].language.coding?.[0].code).toEqual('en');
+      expect(patient.communication?.[1].preferred).toBeTruthy();
+    });
 
-  test('does not duplicate existing language and sets as preferred', async () => {
-    await medplum.updateResource({
-      ...patient,
-      communication: [
-        {
-          language: {
-            coding: [
-              {
-                system: 'urn:ietf:bcp:47',
-                code: 'en',
-                display: 'English',
-              },
-            ],
+    test('does not duplicate existing language', async () => {
+      await medplum.updateResource({
+        ...patient,
+        communication: [
+          {
+            language: {
+              coding: [
+                {
+                  system: 'urn:ietf:bcp:47',
+                  code: 'en',
+                  display: 'English',
+                },
+              ],
+            },
           },
-        },
-      ],
+        ],
+      });
+
+      await handler({ bot, input: response, contentType, secrets: {} }, medplum);
+
+      patient = await medplum.readResource('Patient', patient.id as string);
+
+      expect(patient.communication?.length).toEqual(2);
     });
 
-    await handler({ bot, input: response, contentType, secrets: {} }, medplum);
-
-    patient = await medplum.readResource('Patient', patient.id as string);
-
-    expect(patient.communication?.length).toEqual(2);
-  });
-
-  test('sets existing language as preferred', async () => {
-    await medplum.updateResource({
-      ...patient,
-      communication: [
-        {
-          language: {
-            coding: [
-              {
-                system: 'urn:ietf:bcp:47',
-                code: 'en',
-                display: 'English',
-              },
-            ],
+    test('sets existing language as preferred', async () => {
+      await medplum.updateResource({
+        ...patient,
+        communication: [
+          {
+            language: {
+              coding: [
+                {
+                  system: 'urn:ietf:bcp:47',
+                  code: 'en',
+                  display: 'English',
+                },
+              ],
+            },
           },
-        },
-      ],
+        ],
+      });
+
+      await handler({ bot, input: response, contentType, secrets: {} }, medplum);
+
+      patient = await medplum.readResource('Patient', patient.id as string);
+
+      expect(patient.communication?.[0].preferred).toBeTruthy();
     });
-
-    await handler({ bot, input: response, contentType, secrets: {} }, medplum);
-
-    patient = await medplum.readResource('Patient', patient.id as string);
-
-    expect(patient.communication?.[0].preferred).toBeTruthy();
   });
 });
