2d1d1592b59fec41184ed777d71576108052d286
ksmith94 <102421938+ksmith94@users.noreply.github.com>
2024-07-10 07:26:58 -0400

    Create repo and update readme

---
+++ examples/medplum-photon-integration/src/App.tsx
@@ -0,0 +1,51 @@
+import { AppShell, ErrorBoundary, Loading, Logo, useMedplum, useMedplumProfile } from '@medplum/react';
+import { IconUser } from '@tabler/icons-react';
+import { Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { PatientHistory } from './components/PatientHistory';
+import { PatientOverview } from './components/PatientOverview';
+import { PatientPrescription } from './components/PatientPrescription';
+import { Timeline } from './components/Timeline';
+import { HomePage } from './pages/HomePage';
+import { LandingPage } from './pages/LandingPage';
+import { PatientPage } from './pages/PatientPage';
+import { ResourcePage } from './pages/ResourcePage';
+import { SignInPage } from './pages/SignInPage';
+
+export function App(): JSX.Element | null {
+  const medplum = useMedplum();
+  const profile = useMedplumProfile();
+
+  if (medplum.isLoading()) {
+    return null;
+  }
+
+  return (
+    <AppShell
+      logo={<Logo size={24} />}
+      menus={[
+        {
+          title: 'My Links',
+          links: [{ icon: <IconUser />, label: 'Patients', href: '/' }],
+        },
+      ]}
+    >
+      <ErrorBoundary>
+        <Suspense fallback={<Loading />}>
+          <Routes>
+            <Route path="/" element={profile ? <HomePage /> : <LandingPage />} />
+            <Route path="/signin" element={<SignInPage />} />
+            <Route path="/Patient/:id" element={<PatientPage />}>
+              <Route index element={<PatientOverview />} />
+              <Route path="overview" element={<PatientOverview />} />
+              <Route path="timeline" element={<Timeline />} />
+              <Route path="history" element={<PatientHistory />} />
+            </Route>
+            <Route path="/:resourceType/:id" element={<ResourcePage />} />
+            <Route path="/:resourceType/:id/_history/:versionId" element={<ResourcePage />} />
+          </Routes>
+        </Suspense>
+      </ErrorBoundary>
+    </AppShell>
+  );
+}
