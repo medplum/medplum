1e18a3966cd13f99a19b6c9b8071f6c40011bb42
ksmith94 <102421938+ksmith94@users.noreply.github.com>
2024-07-15 07:31:01 -0400

    Add button and bot to test connection to photon health api

--- examples/medplum-photon-integration/src/components/PatientPrescription.tsx
+++ examples/medplum-photon-integration/src/components/PatientPrescription.tsx
@@ -1,11 +1,30 @@
 import { Button, Group, Title } from '@mantine/core';
-import { Document } from '@medplum/react';
+import { Document, useMedplum } from '@medplum/react';
 
 export function PatientPrescription(): JSX.Element {
+  const medplum = useMedplum();
+  async function testConnection(): Promise<void> {
+    try {
+      const result = await medplum.executeBot(
+        {
+          system: 'https://neutron.health/bots',
+          value: 'test-auth-bot',
+        },
+        {},
+        'application/json'
+      );
+
+      console.log(result);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   return (
     <Document>
       <Group justify="space-between" mb="md">
         <Title order={3}>Prescription Management</Title>
+        <Button onClick={testConnection}>Test Connection</Button>
         <Button>Sync Patient to Photon Health</Button>
       </Group>
       <photon-prescribe-workflow />
