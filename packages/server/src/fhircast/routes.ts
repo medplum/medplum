571ad9046c7c00764e1d8eb4e7e043606555f7a6
Derrick Farris <derrick@medplum.com>
2024-07-19 21:55:25 -0700

    fix(fhircast): associate topic with separate random connection endpoint

--- packages/server/src/fhircast/routes.ts
+++ packages/server/src/fhircast/routes.ts
@@ -5,9 +5,11 @@ import {
   generateId,
   getWebSocketUrl,
   normalizeErrorString,
+  serverError,
 } from '@medplum/core';
 import { Request, Response, Router } from 'express';
 import { body, oneOf, validationResult } from 'express-validator';
+import assert from 'node:assert';
 import { asyncWrap } from '../async';
 import { getConfig } from '../config';
 import { getLogger } from '../context';
@@ -146,9 +148,35 @@ async function handleSubscriptionRequest(req: Request, res: Response): Promise<v
   }
 
   const topic = req.body['hub.topic'];
-  const subscriptionEndpoint = topic; // TODO: Create separate subscription endpoint for topic
-  const config = getConfig();
+  let subscriptionEndpoint: string;
+  try {
+    const results = await getRedis()
+      .multi()
+      .setnx(`medplum:fhircast:topic:${topic}:endpoint`, generateId())
+      .get(`medplum:fhircast:topic:${topic}:endpoint`)
+      .exec();
 
+    if (!results) {
+      throw new Error('Redis returned no results while retrieving endpoint for this topic');
+    }
+
+    assert(results.length === 2, 'Redis did not return 2 command results for FHIRcast endpoint retrieval');
+
+    const [err, result] = results[1];
+    if (err) {
+      throw err;
+    }
+    subscriptionEndpoint = result as string;
+  } catch (err) {
+    sendOutcome(res, serverError(new Error('Failed to get endpoint for topic')));
+    getLogger().error(`[FHIRcast]: Received error while retrieving endpoint for topic`, {
+      topic,
+      error: normalizeErrorString(err),
+    });
+    return;
+  }
+
+  const config = getConfig();
   switch (mode) {
     case 'subscribe':
       res.status(202).json({
@@ -170,11 +198,12 @@ async function handleSubscriptionRequest(req: Request, res: Response): Promise<v
           })
         )
         .catch((err: Error) => {
-          getLogger().error(`Error when publishing to Redis channel for FHIRcast topic: ${normalizeErrorString(err)}`, {
-            topic,
-          });
+          getLogger().error(
+            `[FHIRcast]: Error when publishing to Redis channel for FHIRcast topic: ${normalizeErrorString(err)}`,
+            { topic }
+          );
         });
-    // break;
+      break;
   }
 }
 
