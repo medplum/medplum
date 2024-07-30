399d4a6f607af3f42447f089ce52b4901a32da93
Matthew Willer <matt@medplum.com>
2024-07-30 14:32:57 -0700

    Remove terminology feature flag

--- packages/server/src/config.ts
+++ packages/server/src/config.ts
@@ -56,15 +56,7 @@ export interface MedplumServerConfig {
   accurateCountThreshold: number;
   defaultBotRuntimeVersion: 'awslambda' | 'vmcontext';
   defaultProjectFeatures?:
-    | (
-        | 'email'
-        | 'bots'
-        | 'cron'
-        | 'google-auth-required'
-        | 'graphql-introspection'
-        | 'terminology'
-        | 'websocket-subscriptions'
-      )[]
+    | ('email' | 'bots' | 'cron' | 'google-auth-required' | 'graphql-introspection' | 'websocket-subscriptions')[]
     | undefined;
   defaultRateLimit?: number;
   defaultAuthRateLimit?: number;
