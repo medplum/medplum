5c74356c5fbac490607a8120884d590ee92de682
Filipe Ximenes <filipeximenes@gmail.com>
2024-07-30 10:14:25 -0300

    [Scheduling Sample] passes Schedule as QuestionnaireForm subject and moves status selector to be a questionnaire item

--- examples/medplum-scheduling-demo/src/components/CreateUpdateSlot.tsx
+++ examples/medplum-scheduling-demo/src/components/CreateUpdateSlot.tsx
@@ -1,8 +1,8 @@
 import { Button, Modal, Group, Radio } from '@mantine/core';
 import { createReference, getQuestionnaireAnswers, normalizeErrorString } from '@medplum/core';
-import { Questionnaire, QuestionnaireResponse, Schedule, Slot } from '@medplum/fhirtypes';
+import { Questionnaire, QuestionnaireItem, QuestionnaireResponse, Reference, Schedule, Slot } from '@medplum/fhirtypes';
 import { Event } from 'react-big-calendar';
-import { QuestionnaireForm, useMedplum } from '@medplum/react';
+import { Loading, QuestionnaireForm, useMedplum } from '@medplum/react';
 import { ScheduleContext } from '../Schedule.context';
 import { useContext } from 'react';
 import { showNotification } from '@mantine/notifications';
@@ -23,16 +23,15 @@ export function CreateUpdateSlot(props: CreateUpdateSlotProps): JSX.Element {
   const { event, opened, handlers } = props;
   const medplum = useMedplum();
   const { schedule } = useContext(ScheduleContext);
-  const [availableToggle, toggleAvailable] = useToggle([true, false]);
 
   const editingSlot: Slot = event?.resource;
 
+  if (!schedule) {
+    return <Loading />;
+  }
+
   // If an editing slot was passed, update it otherwise create a new slot
   async function handleQuestionnaireSubmit(formData: QuestionnaireResponse): Promise<void> {
-    if (!schedule) {
-      return;
-    }
-
     const answers = getQuestionnaireAnswers(formData);
 
     try {
@@ -40,10 +39,10 @@ export function CreateUpdateSlot(props: CreateUpdateSlotProps): JSX.Element {
         // Create new slot
         await medplum.createResource({
           resourceType: 'Slot',
-          status: availableToggle ? 'free' : 'busy-unavailable',
+          status: answers['status']?.valueCoding?.code as 'free' | 'busy-unavailable',
           start: answers['start-date'].valueDateTime as string,
           end: answers['end-date'].valueDateTime as string,
-          schedule: createReference(schedule as Schedule),
+          schedule: formData.subject as Reference<Schedule>,
         });
       } else {
         // Edit the existing slot
@@ -73,10 +72,6 @@ export function CreateUpdateSlot(props: CreateUpdateSlotProps): JSX.Element {
 
   // Handles deleting the slot
   async function handleDeleteSlot(): Promise<void> {
-    if (!editingSlot) {
-      return;
-    }
-
     try {
       await medplum.deleteResource('Slot', editingSlot.id as string);
       showNotification({
@@ -96,12 +91,9 @@ export function CreateUpdateSlot(props: CreateUpdateSlotProps): JSX.Element {
     handlers.close();
   }
 
-  const formTitle = editingSlot ? 'Update Slot' : 'Create a Slot';
-
   const appointmentQuestionnaire: Questionnaire = {
     resourceType: 'Questionnaire',
     status: 'active',
-    title: formTitle,
     id: 'new-appointment',
     item: [
       {
@@ -121,20 +113,33 @@ export function CreateUpdateSlot(props: CreateUpdateSlotProps): JSX.Element {
     ],
   };
 
+  // If creating a Slot add a field to select the status
+  if (!editingSlot) {
+    (appointmentQuestionnaire.item as QuestionnaireItem[]).unshift({
+      linkId: 'status',
+      type: 'choice',
+      answerOption: [
+        { valueCoding: { code: 'free', display: 'Available' } },
+        { valueCoding: { code: 'busy-unavailable', display: 'Block' } },
+      ],
+      required: true,
+      initial: [{ valueCoding: { code: 'free', display: 'Available' } }],
+    });
+  }
+
   return (
     <Modal opened={opened} onClose={handlers.close}>
       {editingSlot ? (
         <Button onClick={handleDeleteSlot} fullWidth color="red">
-          Delete Schedule
+          Delete Slot
         </Button>
-      ) : (
-        <Group>
-          <Radio checked={availableToggle} onChange={() => toggleAvailable()} label="Available" />
-          <Radio checked={!availableToggle} onChange={() => toggleAvailable()} label="Block" />
-        </Group>
-      )}
-      <p>{formTitle}</p>
-      <QuestionnaireForm questionnaire={appointmentQuestionnaire} onSubmit={handleQuestionnaireSubmit} />
+      ) : null}
+      <h2>{editingSlot ? 'Update Slot' : 'Create a Slot'}</h2>
+      <QuestionnaireForm
+        questionnaire={appointmentQuestionnaire}
+        subject={createReference(schedule)}
+        onSubmit={handleQuestionnaireSubmit}
+      />
     </Modal>
   );
 }
