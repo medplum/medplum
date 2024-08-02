cab43ceb4ac2a3cefb0c5e8841f23bd3e2589aa1
Matt Willer <matt@medplum.com>
2024-08-02 09:57:50 -0700

    Handle timeout errors in database migrations (#5012)
    * Handle migration timeouts gracefully
    
    * Remove statement timeout for migrations
    
    * Fix test
    
    * Skip flaky test

--- packages/server/src/database.ts
+++ packages/server/src/database.ts
@@ -91,11 +91,20 @@ async function runMigrations(pool: Pool): Promise<void> {
   try {
     client = await pool.connect();
     await client.query('SELECT pg_advisory_lock($1)', [locks.migration]);
+    await client.query(`SET statement_timeout TO 0`); // Disable timeout for migrations AFTER getting lock
     await migrate(client);
+  } catch (err: any) {
+    globalLogger.error('Database schema migration error', err);
+    if (client) {
+      await client.query('SELECT pg_advisory_unlock($1)', [locks.migration]);
+      client.release(err);
+      client = undefined;
+    }
   } finally {
     if (client) {
       await client.query('SELECT pg_advisory_unlock($1)', [locks.migration]);
       client.release();
+      client = undefined;
     }
   }
 }
