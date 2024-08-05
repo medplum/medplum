6e12eb14b40b22ae473ca71cbf02331dee343dbd
ksmith94 <102421938+ksmith94@users.noreply.github.com>
2024-08-05 07:05:40 -0400

    Remove medplum photon id

--- examples/medplum-photon-integration/src/config.ts
+++ examples/medplum-photon-integration/src/config.ts
@@ -1,3 +1,3 @@
 // Default values to enable Photon Elements. Replace with your own photon credentials for use. https://docs.photon.health/docs/elements
-export const PHOTON_CLIENT_ID = 'NzATQk3GC5qoCZHWtLH1WHNV3pcZesPJ';
-export const PHOTON_ORG_ID = 'org_q5l4IPPdSR95k8Lc';
+export const PHOTON_CLIENT_ID = 'EXAMPLE_CLIENT_ID';
+export const PHOTON_ORG_ID = 'EXAMPLE_ORG_ID';
