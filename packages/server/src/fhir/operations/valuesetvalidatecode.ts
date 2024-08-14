4a8d9d55db4275a0b7fc5285a2204b25644ce19f
Matt Willer <matt@medplum.com>
2024-08-13 17:38:28 -0700

    Support `in` ValueSet property filter (#5092)
    * Support `in` ValueSet property filter
    
    * Add tests

--- packages/server/src/fhir/operations/valuesetvalidatecode.ts
+++ packages/server/src/fhir/operations/valuesetvalidatecode.ts
@@ -128,9 +128,17 @@ async function satisfies(
 
   switch (filter.op) {
     case '=':
-      query = addPropertyFilter(query, filter.property, filter.value, true);
+      query = addPropertyFilter(query, filter.property, '=', filter.value);
+      break;
+    case 'in':
+      query = addPropertyFilter(query, filter.property, 'IN', filter.value.split(','));
       break;
     case 'is-a':
+    case 'descendent-of':
+      if (filter.op !== 'is-a') {
+        query.where('code', '!=', filter.value);
+      }
+
       // Recursively find parents until one matches
       query = findAncestor(query, codeSystem, filter.value);
       break;
