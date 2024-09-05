dbbe202d482d3fb652bdd4f504388970cc894de9
Matt Long <ml@medplum.com>
2024-09-05 13:45:59 -0700

    Delete most of old packages/health-gorilla

--- packages/health-gorilla/src/constants.ts
+++
@@ -1,7 +0,0 @@
-/**
- * The standard Health Gorilla system URL.
- *
- * When synchronizing data between Medplum and Health Gorilla, the Health Gorilla system URL is used
- * as the "system" value in FHIR resource identifiers.
- */
-export const HEALTH_GORILLA_SYSTEM = 'https://www.healthgorilla.com';
