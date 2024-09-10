b2be6138277af5432e818f10ea7958bf6ddf73ba
Matt Long <matt@mattlong.org>
2024-09-10 14:34:11 -0700

    http --> https

--- packages/health-gorilla-core/src/constants.ts
+++ packages/health-gorilla-core/src/constants.ts
@@ -1,11 +1,13 @@
+import { HTTP_HL7_ORG } from '@medplum/core';
+
 export const MEDPLUM_HEALTH_GORILLA_TENANT_PROFILE =
-  'http://medplum.com/profiles/integrations/health-gorilla/StructureDefinition/MedplumHealthGorillaTenant';
+  'https://medplum.com/profiles/integrations/health-gorilla/StructureDefinition/MedplumHealthGorillaTenant';
 export const MEDPLUM_HEALTH_GORILLA_LAB_ORDER_PROFILE =
-  'http://medplum.com/profiles/integrations/health-gorilla/StructureDefinition/MedplumHealthGorillaOrder';
+  'https://medplum.com/profiles/integrations/health-gorilla/StructureDefinition/MedplumHealthGorillaOrder';
 
 export const MEDPLUM_HEALTH_GORILLA_LAB_ORDER_EXTENSION_URL_BILL_TO = 'billTo';
 export const MEDPLUM_HEALTH_GORILLA_LAB_ORDER_EXTENSION_URL_PERFORMING_LAB_AN = 'performingLabAccountNumber';
 
 export const HEALTH_GORILLA_SYSTEM = 'https://www.healthgorilla.com';
 
-export const NPI_SYSTEM = 'http://hl7.org/fhir/sid/us-npi';
+export const NPI_SYSTEM = HTTP_HL7_ORG + '/fhir/sid/us-npi';
