30c77882279b550ebcf5f9c04c36aadc0c3b63d3
Matt Long <ml@medplum.com>
2024-09-12 13:25:09 -0700

    Health Gorilla demo app uses local HG packages

--- examples/medplum-health-gorilla-demo/vite.config.ts
+++ examples/medplum-health-gorilla-demo/vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,4 +8,10 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-})
+  resolve: {
+    alias: {
+      '@medplum/health-gorilla-core': path.resolve('../../packages/health-gorilla-core/src'),
+      '@medplum/health-gorilla-react': path.resolve('../../packages/health-gorilla-react/src'),
+    },
+  },
+});
