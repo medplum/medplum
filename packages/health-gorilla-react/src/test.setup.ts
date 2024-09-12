849c77040893f87ca11c24b87eae310b2757502e
Matt Long <ml@medplum.com>
2024-09-12 09:12:54 -0700

    PR feedback

--- packages/health-gorilla-react/src/test.setup.ts
+++ packages/health-gorilla-react/src/test.setup.ts
@@ -1,9 +1,5 @@
 import { afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
-// import * as matchers from '@testing-library/jest-dom/matchers';
-import '@testing-library/jest-dom';
-
-// expect.extend(matchers);
 
 afterEach(() => {
   cleanup();
