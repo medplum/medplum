58b2f9c8ebe4717049c5af8d31857bb6373deb60
Matt Long <ml@medplum.com>
2024-09-23 09:33:53 -0700

    temp

--- packages/cdk/src/backend.ts
+++ packages/cdk/src/backend.ts
@@ -58,6 +58,10 @@ export class BackEnd extends Construct {
   redisSecretsParameter: ssm.StringParameter;
   botLambdaRoleParameter: ssm.StringParameter;
 
+  pg16ClusterParameterGroup: rds.ParameterGroup;
+  pg16WriterParameterGroup: rds.ParameterGroup;
+  pg16ReaderParameterGroup: rds.ParameterGroup;
+
   constructor(scope: Construct, config: MedplumInfraConfig) {
     super(scope, 'BackEnd');
 
@@ -94,6 +98,22 @@ export class BackEnd extends Construct {
     });
 
     // RDS
+    const pg16Engine = rds.DatabaseClusterEngine.auroraPostgres({
+      version: rds.AuroraPostgresEngineVersion.VER_16_3,
+    });
+    this.pg16ClusterParameterGroup = new ParameterGroup(this, 'MedlumPostgres16ClusterParams', {
+      engine: pg16Engine,
+    });
+    this.pg16ClusterParameterGroup.bindToCluster({});
+    this.pg16WriterParameterGroup = new ParameterGroup(this, 'MedlumPostgres16WriterParams', {
+      engine: pg16Engine,
+    });
+    this.pg16WriterParameterGroup.bindToInstance({});
+    this.pg16ReaderParameterGroup = new ParameterGroup(this, 'MedlumPostgres16ReaderParams', {
+      engine: pg16Engine,
+    });
+    this.pg16ReaderParameterGroup.bindToInstance({});
+
     this.rdsSecretsArn = config.rdsSecretsArn;
     if (!this.rdsSecretsArn) {
       // See: https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_rds-readme.html#migrating-from-instanceprops
@@ -156,6 +176,7 @@ export class BackEnd extends Construct {
         },
         writer: ClusterInstance.provisioned('Instance1', writerInstanceProps),
         readers,
+        deletionProtection: false,
         backup: {
           retention: Duration.days(7),
         },
