0d7900f71fd5d50632a7d4b8ac6855f8b5cc1730
Derrick Farris <derrick@medplum.com>
2024-09-28 19:38:32 -0700

    cleanup(tests/cli): rm unused test

--- packages/cli/src/index.test.ts
+++ packages/cli/src/index.test.ts
@@ -53,9 +53,4 @@ describe('CLI', () => {
     await main(['node', 'index.js', 'xyz']);
     expect(processError).toHaveBeenCalledWith(expect.stringContaining(`error: unknown command 'xyz'`));
   });
-
-  test('Verbose mode', async () => {
-    await main(['node', 'index.js', '--help', '--verbose']);
-    expect(processError).toHaveBeenCalledWith(expect.stringContaining(`error: unknown command 'xyz'`));
-  });
 });
