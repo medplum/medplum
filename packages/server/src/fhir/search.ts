1e237f82f6ba6929839b3e3906d7037c6223a326
Matthew Willer <matt@medplum.com>
2024-10-01 13:37:04 -0700

    Add comment

--- packages/server/src/fhir/search.ts
+++ packages/server/src/fhir/search.ts
@@ -1293,13 +1293,15 @@ function buildChainedSearch(
   }
 
   // Special case: single-link chain of the form param._id=<id> can be rewritten as param=ResourceType/<id>
+  // Note that this does slightly change the behavior of the search query: true chained search would require the
+  // reference to point to an existing resource, while the rewritten query just matches the reference string
   if (param.chain.length === 1 && param.chain[0].filter?.code === '_id') {
-    const searchParam = param.chain[0];
-    const targetId = searchParam.filter?.value;
+    const { resourceType: targetType, code, filter } = param.chain[0];
+    const targetId = filter.value;
     return buildSearchFilterExpression(repo, selectQuery, resourceType as ResourceType, resourceType, {
-      code: searchParam.code,
+      code,
       operator: Operator.EQUALS,
-      value: `${searchParam.resourceType}/${targetId}`,
+      value: `${targetType}/${targetId}`,
     });
   }
 
