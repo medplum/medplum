18a86ae616a5dd0f24f2de2759bee546df602e84
Derrick Farris <derrick@medplum.com>
2024-10-17 17:10:32 -0700

    wip(db): add data migration to startup

--- packages/server/src/database.ts
+++ packages/server/src/database.ts
@@ -1,7 +1,11 @@
+import { readFileSync } from 'node:fs';
+import { resolve } from 'node:path';
 import { Pool, PoolClient } from 'pg';
 import { MedplumDatabaseConfig, MedplumServerConfig } from './config';
+import { getSystemRepo } from './fhir/repo';
 import { globalLogger } from './logger';
-import * as migrations from './migrations/schema';
+import * as dataMigrations from './migrations/data';
+import * as schemaMigrations from './migrations/schema';
 
 export enum DatabaseMode {
   READER = 'reader',
@@ -10,6 +14,7 @@ export enum DatabaseMode {
 
 let pool: Pool | undefined;
 let readonlyPool: Pool | undefined;
+let serverVersion: string | undefined;
 
 export function getDatabasePool(mode: DatabaseMode): Pool {
   if (!pool) {
@@ -97,6 +102,7 @@ async function runMigrations(pool: Pool): Promise<void> {
     await client.query(`SET statement_timeout TO 0`); // Disable timeout for migrations AFTER getting lock
     await migrate(client);
   } catch (err: any) {
+    // TODO: we need to abort the process in most cases here
     globalLogger.error('Database schema migration error', err);
     if (client) {
       await client.query('SELECT pg_advisory_unlock($1)', [locks.migration]);
@@ -119,16 +125,51 @@ async function migrate(client: PoolClient): Promise<void> {
     "dataVersion" INTEGER NOT NULL
   )`);
 
-  const result = await client.query('SELECT "version" FROM "DatabaseMigration"');
-  const version = result.rows[0]?.version ?? -1;
+  const result = await client.query('SELECT "version", "dataVersion" FROM "DatabaseMigration"');
+  let version = result.rows[0]?.version ?? -1;
+  let dataVersion = result.rows[0]?.dataVersion ?? -1;
+  const allDataVersions = getMigrationVersions(dataMigrations);
+  let pendingDataMigration = 0;
 
+  // If this is the first time the server has been started up (version < 0)
+  // We need to initialize our migrations table
+  // This also opts us into the fast path for data migrations, so we can skip all checks for server version and go straight to the latest data version
   if (version < 0) {
-    await client.query('INSERT INTO "DatabaseMigration" ("id", "version", "dataVersion") VALUES (1, 0, 0)');
+    const latestDataVersion = allDataVersions[allDataVersions.length - 1];
+    await client.query(
+      `INSERT INTO "DatabaseMigration" ("id", "version", "dataVersion") VALUES (1, 0, ${latestDataVersion})`
+    );
+    version = 0;
+    dataVersion = latestDataVersion;
+  } else if (allDataVersions.includes(dataVersion + 1)) {
+    // Before migrating, check if we have pending data migrations to apply
+    // We have to check these first since they depend on particular versions of the server code to be present in order
+    // To ensure that the migration is applied before at a particular point in time before the version that requires it
+    pendingDataMigration = dataVersion + 1;
+
+    // Since an outstanding data migration exists, we need to apply it. To apply it we need to check the corresponding server version
+    // There is a manifest that contains the required server version for a given data migration
+    // That we can check
+    const manifest = JSON.parse(
+      readFileSync(resolve(__dirname, 'migrations/data/data-version-manifest.json'), { encoding: 'utf-8' })
+    ) as Record<string, { serverVersion: string }>;
+    const requiredServerVersion = manifest['v' + pendingDataMigration].serverVersion;
+
+    // If the current server version is not the one we require for this data migration
+    // Then we should throw and abort the migration process
+    const serverVersion = getServerVersion();
+    if (serverVersion !== requiredServerVersion) {
+      throw new Error(
+        `Unable to run data migration against the current server version. Migration requires server at version ${requiredServerVersion}, but current server version is ${serverVersion}`
+      );
+    }
+
+    // If we make it here, we have a pending migration, but we don't want to apply it until we make sure we apply all the schema migrations first
   }
 
-  const migrationKeys = Object.keys(migrations);
-  for (let i = version + 1; i <= migrationKeys.length; i++) {
-    const migration = (migrations as Record<string, migrations.Migration>)['v' + i];
+  const schemaKeys = Object.keys(schemaMigrations);
+  for (let i = version + 1; i <= schemaKeys.length; i++) {
+    const migration = (schemaMigrations as Record<string, schemaMigrations.Migration>)['v' + i];
     if (migration) {
       const start = Date.now();
       await migration.run(client);
@@ -136,4 +177,31 @@ async function migrate(client: PoolClient): Promise<void> {
       await client.query('UPDATE "DatabaseMigration" SET "version"=$1', [i]);
     }
   }
+
+  // TODO: Instead of running the migration here, queue it up in an async job
+  if (pendingDataMigration) {
+    const migration = (dataMigrations as Record<string, dataMigrations.Migration>)['v' + pendingDataMigration];
+    const start = Date.now();
+    await migration.run(getSystemRepo());
+    globalLogger.info('Database data migration', {
+      dataVersion: `v${pendingDataMigration}`,
+      duration: `${Date.now() - start} ms`,
+    });
+    await client.query('UPDATE "DatabaseMigration" SET "dataVersion"=$1', [pendingDataMigration]);
+  }
+}
+
+function getMigrationVersions(migrationModule: Record<string, any>): number[] {
+  const prefixedVersions = Object.keys(migrationModule).filter((key) => key.startsWith('v'));
+  const migrationVersions = prefixedVersions.map((key) => Number.parseInt(key.slice(1), 10)).sort((a, b) => a - b);
+  return migrationVersions;
+}
+
+function getServerVersion(): string {
+  if (!serverVersion) {
+    serverVersion = (
+      JSON.parse(readFileSync(resolve(__dirname, '../package.json'), { encoding: 'utf-8' })) as Record<string, any>
+    ).version as string;
+  }
+  return serverVersion;
 }
