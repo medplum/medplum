98faacfbab7377d9d4fdbb1403712ee519407211
pablorussomc <prusso@zencore.dev>
2024-10-21 19:50:15 -0300

    Update secrets and environment variables configuration

--- terraform/gcp/outputs.tf
+++ terraform/gcp/outputs.tf
@@ -8,3 +8,7 @@ output "medplum_repo_url" {
   description = "The URL of the Medplum Docker repository"
   value       = google_artifact_registry_repository.medplum_repo.id
 }
+
+output "redis_ip_address" {
+  value = module.redis_cluster.redis_cluster.discovery_endpoints[0].address
+}
--- terraform/gcp/secrets.tf
+++ terraform/gcp/secrets.tf
@@ -40,3 +40,48 @@ resource "google_secret_manager_secret_version" "instance_connection_string" {
 
   depends_on = [google_project_service.project]
 }
+
+
+# postgresql instance IP address
+resource "google_secret_manager_secret" "postgresql-ip-address" {
+  project   = var.project_id
+  secret_id = "postgresql-ip-address"
+
+  labels = var.labels
+
+  replication {
+    auto {}
+  }
+
+  depends_on = [google_project_service.project]
+}
+
+resource "google_secret_manager_secret_version" "postgresql-ip-address" {
+  secret      = google_secret_manager_secret.postgresql-ip-address.id
+  secret_data = tostring(module.sql-db.instance_ip_address[0].ip_address)
+
+  depends_on = [google_project_service.project]
+}
+
+# Redis instance IP address
+resource "google_secret_manager_secret" "redis-ip-address" {
+  project   = var.project_id
+  secret_id = "redis-ip-address"
+
+  labels = var.labels
+
+  replication {
+    auto {}
+  }
+
+  depends_on = [google_project_service.project]
+}
+
+resource "google_secret_manager_secret_version" "redis-ip-address" {
+  secret      = google_secret_manager_secret.redis-ip-address.id
+  secret_data = tostring(module.redis_cluster.redis_cluster.discovery_endpoints[0].address)
+
+
+
+  depends_on = [google_project_service.project]
+}
