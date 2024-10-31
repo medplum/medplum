831825bb0c60aa777c96d4dc03d95103c5f0c567
pablorussomc <prusso@zencore.dev>
2024-10-31 17:04:50 -0300

    Refactor Helm values.yaml and Terraform configuration

--- terraform/gcp/redis.tf
+++ terraform/gcp/redis.tf
@@ -8,8 +8,9 @@ module "memorystore" {
   memory_size_gb     = "1"
   enable_apis        = "true"
   authorized_network = module.vpc.network_self_link
-  connect_mode       = "PRIVATE_SERVICE_ACCESS"
-  reserved_ip_range  = google_compute_global_address.psa_reserved_ip.name
+  transit_encryption_mode = "DISABLED"
+  connect_mode            = "PRIVATE_SERVICE_ACCESS"
+  reserved_ip_range       = google_compute_global_address.psa_reserved_ip.name
   persistence_config = {
     persistence_mode    = "RDB"
     rdb_snapshot_period = "ONE_HOUR"
