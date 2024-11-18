055ab46d6f63450176b2d5ac441321ef2ca412ad
Daveed <daveed@santocabron.com>
2024-11-18 02:21:03 -0500

    Added missing types for vite/client

--- packages/core/esbuild.mjs
+++ packages/core/esbuild.mjs
@@ -5,7 +5,7 @@
 import { execSync } from 'child_process';
 import esbuild from 'esbuild';
 import { writeFileSync } from 'fs';
-import packageJson from './package.json' with { type: 'json' };
+import * as packageJson from './package.json' assert { type: 'json' };
 
 let gitHash;
 try {
