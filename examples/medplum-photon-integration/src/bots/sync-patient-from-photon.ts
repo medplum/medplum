13bc6dad7e30752deb97720099cc46feaccb7ea2
ksmith94 <102421938+ksmith94@users.noreply.github.com>
2024-11-22 07:34:38 -0800

    Build bot and tests for syncing from photon

--- examples/medplum-photon-integration/src/bots/sync-patient-from-photon.ts
+++ examples/medplum-photon-integration/src/bots/sync-patient-from-photon.ts
@@ -1,8 +1,35 @@
-import { BotEvent, MedplumClient } from '@medplum/core';
-import { PhotonPatient } from '../photon-types';
-import { handlePhotonAuth, photonGraphqlFetch } from './utils';
+import {
+  BotEvent,
+  createReference,
+  getDisplayString,
+  MedplumClient,
+  normalizeErrorString,
+  RXNORM,
+} from '@medplum/core';
+import {
+  Address,
+  AllergyIntolerance,
+  Bundle,
+  BundleEntry,
+  ContactPoint,
+  HumanName,
+  MedicationRequest,
+  Patient,
+  Practitioner,
+  Reference,
+} from '@medplum/fhirtypes';
+import { randomUUID } from 'crypto';
+import {
+  PhotonAddress,
+  PhotonPatient,
+  PhotonPatientAllergy,
+  PhotonPrescription,
+  PhotonProvider,
+} from '../photon-types';
+import { NEUTRON_HEALTH, NEUTRON_HEALTH_PATIENTS } from './constants';
+import { getMedicationElement, handlePhotonAuth, photonGraphqlFetch } from './utils';
 
-export async function handler(medplum: MedplumClient, event: BotEvent) {
+export async function handler(medplum: MedplumClient, event: BotEvent): Promise<void> {
   const photonClientId = event.secrets['PHOTON_CLIENT_ID']?.valueString;
   const photonClientSecret = event.secrets['PHOTON_CLIENT_SECRET']?.valueString;
   const photonAuthToken = await handlePhotonAuth(photonClientId, photonClientSecret);
@@ -39,35 +66,63 @@ export async function handler(medplum: MedplumClient, event: BotEvent) {
           comment
           onset
         }
-        medicationHistory{
-          prescription {
+        prescription {
+          id
+          externalId
+          prescriber {
             id
             externalId
-            dispenseAsWritten
-            dispenseQuantity
-            dispenseUnit
-            refillsAllowed
-            refillsRemaining
-            fillsAllowed
-            fillsRemaining
-            daysSupply
-            instructions
-            notes
-            effectiveDate
-            expirationDAte
-            writtenAt
-          }
-          medication {
-            id
-            name
-            codes {
-              rxcui
-              productNDC
-              packageNDC
+            name {
+              title
+              first
+              last
+            }
+            email
+            phone
+            address {
+              street1
+              street2
+              postalCode
+              country
+              state
+              city
+            }
+            organizations {
+              id
+              name
             }
           }
-          comment
-          active
+          state
+          treatment {
+            id
+            codes {
+              rxcui
+            }
+          }
+          dispenseAsWritten
+          dispenseQuantity
+          refillsAllowed
+          refillsRemaining
+          fillsAllowed
+          fillsRemaining
+          daysSupply
+          instructions
+          notes
+          effectiveDate
+          expirationDate
+          writtenAt
+          fills {
+            id
+            treatment {
+              id
+              codes {
+                rxcui
+              }
+            }
+            state
+            requestedAt
+            filledAt
+          }
         }
         address {
           street1
@@ -85,26 +140,260 @@ export async function handler(medplum: MedplumClient, event: BotEvent) {
 
   const result = await photonGraphqlFetch(body, photonAuthToken);
   const photonPatients = result.data.patients as PhotonPatient[];
+  const batch: Bundle = {
+    resourceType: 'Bundle',
+    type: 'batch',
+    entry: [],
+  };
 
   for (const photonPatient of photonPatients) {
     // Make sure the patient does not already exist
-    if (await checkForExistingPatient(photonPatient, medplum)) {
-      continue;
+    if (!(await checkForExistingPatient(photonPatient, medplum))) {
+      // Create the patient resource
+      const medplumPatient = createPatientResource(photonPatient);
+      const patientUrl = 'urn:uuid:' + randomUUID();
+      const patientReference: Reference<Patient> = { reference: patientUrl, display: getDisplayString(medplumPatient) };
+
+      // Create any allergies the patient has
+      const allergies = createAllergies(patientReference, photonPatient.allergies);
+
+      // Create any prescriptions
+      const prescriptions = await createPrescriptions(patientReference, medplum, photonPatient.prescriptions);
+
+      // Add the patient resource to a bundle
+      const patientEntry: BundleEntry = {
+        fullUrl: patientUrl,
+        request: { method: 'POST', url: 'Patient' },
+        resource: medplumPatient,
+      };
+      batch.entry?.push(patientEntry);
+
+      // If there are allergies, create entries and add them to the bundle
+      if (allergies) {
+        const allergyEntries: BundleEntry[] = allergies.map((allergy) => {
+          return {
+            fullUrl: 'urn:uuid:' + randomUUID(),
+            request: { method: 'POST', url: 'AllergyIntolerance' },
+            resource: allergy,
+          };
+        });
+        batch.entry?.push(...allergyEntries);
+      }
+
+      // If there are prescriptions, create entries and add them to the bundle
+      if (prescriptions) {
+        const prescriptionEntries: BundleEntry[] = prescriptions.map((prescription) => {
+          return {
+            fullUrl: 'urn:uuid:' + randomUUID(),
+            request: { method: 'POST', url: 'MedicationRequest' },
+            resource: prescription,
+          };
+        });
+        batch.entry?.push(...prescriptionEntries);
+      }
     }
-
-    // Create the patient resource
-
-    // Add the patient resource to a bundle
   }
 
   // execute the bundle
+  await medplum.executeBatch(batch);
 }
 
-async function checkForExistingPatient(photonPatient: PhotonPatient, medplum: MedplumClient): Promise<boolean> {
+export async function checkForExistingPatient(photonPatient: PhotonPatient, medplum: MedplumClient): Promise<boolean> {
   if (!photonPatient.externalId) {
     return false;
   }
 
-  const patient = await medplum.readResource('Patient', photonPatient.externalId);
-  return !!patient;
+  try {
+    const patient = await medplum.readResource('Patient', photonPatient.externalId);
+    return !!patient;
+  } catch {
+    return false;
+  }
+}
+
+export function createPatientResource(photonPatient: PhotonPatient): Patient {
+  const telecom: ContactPoint[] = [{ system: 'phone', value: photonPatient.phone }];
+  if (photonPatient.email) {
+    telecom.push({ system: 'email', value: photonPatient.email });
+  }
+
+  const patient: Patient = {
+    resourceType: 'Patient',
+    identifier: [{ system: NEUTRON_HEALTH_PATIENTS, value: photonPatient.id }],
+    name: [{ family: photonPatient.name.last, given: [photonPatient.name.first] }],
+    gender: photonPatient.sex.toLowerCase() as Patient['gender'],
+    birthDate: photonPatient.dateOfBirth,
+    telecom,
+  };
+
+  const address = photonPatient.address && formatAddress(photonPatient.address);
+
+  if (address) {
+    patient.address = address;
+  }
+
+  return patient;
+}
+
+export function createAllergies(
+  patientReference: Reference<Patient>,
+  photonAllergies?: PhotonPatientAllergy[]
+): AllergyIntolerance[] | undefined {
+  if (!photonAllergies || photonAllergies.length === 0) {
+    return undefined;
+  }
+  const allergies: AllergyIntolerance[] = [];
+  for (const photonAllergy of photonAllergies) {
+    const { allergen, comment, onset } = photonAllergy;
+    if (!allergen.rxcui) {
+      continue;
+    }
+
+    const allergy: AllergyIntolerance = {
+      resourceType: 'AllergyIntolerance',
+      patient: patientReference,
+      code: { coding: [{ system: RXNORM, code: allergen.rxcui, display: allergen.name }] },
+      onsetDateTime: onset,
+    };
+
+    if (comment) {
+      allergy.note = [{ text: comment }];
+    }
+    allergies.push(allergy);
+  }
+
+  return allergies;
+}
+
+export async function createPrescriptions(
+  patientReference: Reference<Patient>,
+  medplum: MedplumClient,
+  photonPrescriptions?: PhotonPrescription[]
+): Promise<MedicationRequest[] | undefined> {
+  if (!photonPrescriptions || photonPrescriptions.length === 0) {
+    return undefined;
+  }
+
+  const prescriptions: MedicationRequest[] = [];
+  for (const photonPrescription of photonPrescriptions) {
+    const { codes, name } = photonPrescription.treatment;
+    const status = getStatusFromPhotonState(photonPrescription.state);
+    const medicationElement = await getMedicationElement(medplum, codes.rxcui, name);
+    const prescriber = await getPrescriber(medplum, photonPrescription.prescriber);
+
+    const prescription: MedicationRequest = {
+      resourceType: 'MedicationRequest',
+      status,
+      intent: 'order',
+      subject: patientReference,
+      identifier: [{ system: NEUTRON_HEALTH, value: photonPrescription.id }],
+      dispenseRequest: {
+        quantity: {
+          value: photonPrescription.dispenseQuantity,
+          unit: photonPrescription.dispenseUnit,
+        },
+        numberOfRepeatsAllowed: photonPrescription.refillsAllowed,
+        expectedSupplyDuration: { value: photonPrescription.daysSupply, unit: 'days' },
+        validityPeriod: {
+          start: photonPrescription.effectiveDate,
+          end: photonPrescription.expirationDate,
+        },
+      },
+      substitution: { allowedBoolean: !photonPrescription.dispenseAsWritten },
+      dosageInstruction: [{ patientInstruction: photonPrescription.instructions }],
+      authoredOn: photonPrescription.writtenAt,
+      medicationCodeableConcept: medicationElement,
+      requester: createReference(prescriber),
+    };
+
+    if (photonPrescription.notes) {
+      prescription.note = [{ text: photonPrescription.notes }];
+    }
+
+    prescriptions.push(prescription);
+  }
+  return prescriptions;
+}
+
+export async function getPrescriber(medplum: MedplumClient, photonProvider: PhotonProvider): Promise<Practitioner> {
+  const prescriberPhotonId = photonProvider.id;
+  const prescriberMedplumId = photonProvider.externalId;
+
+  if (prescriberMedplumId) {
+    return medplum.readResource('Practitioner', prescriberMedplumId);
+  }
+
+  const trackedPractitioner = await medplum.searchOne('Practitioner', {
+    identifier: NEUTRON_HEALTH + `|${prescriberPhotonId}`,
+  });
+
+  if (trackedPractitioner) {
+    return trackedPractitioner;
+  } else {
+    const newPractitioner = await createPractitioner(medplum, photonProvider);
+    return newPractitioner;
+  }
+}
+
+export async function createPractitioner(
+  medplum: MedplumClient,
+  photonProvider: PhotonProvider
+): Promise<Practitioner> {
+  const providerName: HumanName = { given: [photonProvider.name.first], family: photonProvider.name.last };
+  if (photonProvider.name.title) {
+    providerName.prefix = [photonProvider.name.title];
+  }
+
+  const practitionerData: Practitioner = {
+    resourceType: 'Practitioner',
+    identifier: [{ system: NEUTRON_HEALTH, value: photonProvider.id }],
+    name: [providerName],
+    telecom: [
+      { system: 'email', value: photonProvider.email },
+      { system: 'phone', value: photonProvider.phone },
+    ],
+    address: formatAddress(photonProvider.address),
+  };
+
+  try {
+    const practitioner = await medplum.createResource(practitionerData);
+    return practitioner;
+  } catch (err) {
+    throw new Error(normalizeErrorString(err));
+  }
+}
+
+export function getStatusFromPhotonState(
+  photonPrescriptionState: PhotonPrescription['state']
+): MedicationRequest['status'] {
+  switch (photonPrescriptionState) {
+    case 'ACTIVE':
+      return 'active';
+    case 'CANCELED':
+      return 'cancelled';
+    case 'DEPLETED':
+      return 'completed';
+    case 'EXPIRED':
+      return 'stopped';
+    default:
+      throw new Error('Invalid state provided');
+  }
+}
+
+function formatAddress(photonAddress: PhotonAddress): Address[] {
+  const { street1, street2, city, state, postalCode, country } = photonAddress;
+  const line: Address['line'] = [street1];
+  if (street2) {
+    line.push(street2);
+  }
+
+  const address: Address = {
+    line,
+    city,
+    state,
+    postalCode,
+    country,
+  };
+
+  return [address];
 }
