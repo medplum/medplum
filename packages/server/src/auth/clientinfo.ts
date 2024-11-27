3b3750d4a1e72b16616dcb2df97cc845d5a87520
David Yanez <david@Davids-MacBook-Pro.local>
2024-11-27 15:29:51 -0800

    OAuth page custom UI

---
+++ packages/server/src/auth/clientinfo.ts
@@ -0,0 +1,18 @@
+import { asyncWrap } from '../async';
+import { badRequest } from '@medplum/core';
+import { ClientApplication } from '@medplum/fhirtypes';
+import { getSystemRepo } from '../fhir/repo';
+import { Request, RequestHandler, Response } from 'express';
+import { sendOutcome } from '../fhir/outcomes';
+
+export const clientInfoHandler: RequestHandler = asyncWrap(async (req: Request, res: Response) => {
+  const { clientId } = req.params
+  const systemRepo = getSystemRepo();
+  
+  try {
+    const client = await systemRepo.readResource<ClientApplication>('ClientApplication', clientId);
+    res.status(200).json({ name: client.signInForm?.welcomeString, logoUrl: client.signInForm?.logo });
+  } catch (_err) {
+    sendOutcome(res, badRequest('Client not found'));
+  }
+});
