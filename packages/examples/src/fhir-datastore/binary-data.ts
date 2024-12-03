61718a2d253b7dda9535b6ec618dbf7876b00af4
Rahul Agarwal <rahul@medplum.com>
2024-12-03 14:33:14 -0800

    Fix markdown formatting on binary-data page (#5521)
    * Fix markdown formatting on binary-data page
    
    * Fixed linting error

--- packages/examples/src/fhir-datastore/binary-data.ts
+++ packages/examples/src/fhir-datastore/binary-data.ts
@@ -4,6 +4,22 @@ import { Patient } from '@medplum/fhirtypes';
 // end-block imports
 
 // start-block downloadBinary
+/**
+ * Parses the pre-signed storage url to extract the binary resource id
+ * @param url - Pre-signed storage url
+ * @returns id of `Binary` resource stored in Medplum
+ */
+function getBinaryId(url?: string): string {
+  if (!url) {
+    throw new Error('Invalid url');
+  }
+
+  const parts: string[] = url.split('/');
+  const id = parts[parts.length - 1];
+  return id;
+}
+
+// Example resource
 const patient: Patient = {
   resourceType: 'Patient',
   photo: [
@@ -21,13 +37,3 @@ const binaryUrl = getBinaryId(patient.photo?.[0].url);
 // Download the binary
 await medplum.download(`Binary/${binaryUrl}`);
 // end-block downloadBinary
-
-function getBinaryId(url?: string): string {
-  if (!url) {
-    throw new Error('Invalid url');
-  }
-
-  const parts: string[] = url.split('/');
-  const id = parts[parts.length - 1];
-  return id;
-}
