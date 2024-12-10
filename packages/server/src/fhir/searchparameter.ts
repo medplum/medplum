824c9af68016199c9fb144ef9f767e408f29b051
Matt Long <matt@mattlong.org>
2024-12-10 14:06:50 -0800

    Add implementation to SearchParameterImplementation

--- packages/server/src/fhir/searchparameter.ts
+++ packages/server/src/fhir/searchparameter.ts
@@ -1,12 +1,14 @@
-import { capitalize, getSearchParameterDetails, SearchParameterDetails } from '@medplum/core';
-import { SearchParameter } from '@medplum/fhirtypes';
+import { capitalize, getSearchParameterDetails, PropertyType, SearchParameterDetails } from '@medplum/core';
+import { ResourceType, SearchParameter } from '@medplum/fhirtypes';
 
 interface ImplementationBuilder extends SearchParameterDetails {
   columnName: string;
+  implementation: 'column' | 'lookup-table';
 }
 
 export interface SearchParameterImplementation extends SearchParameterDetails {
   readonly columnName: string;
+  readonly implementation: 'column' | 'lookup-table';
 }
 
 interface ResourceTypeSearchParameterInfo {
@@ -49,11 +51,22 @@ function buildSearchParameterImplementation(
   searchParam: SearchParameter
 ): SearchParameterImplementation {
   const code = searchParam.code;
+  const builder = getSearchParameterDetails(resourceType, searchParam) as ImplementationBuilder;
+
   const columnName = convertCodeToColumnName(code);
-  const impl = getSearchParameterDetails(resourceType, searchParam) as ImplementationBuilder;
-  impl.columnName = columnName;
-  setSearchParameterImplementation(resourceType, code, impl);
-  return impl;
+  builder.columnName = columnName;
+
+  let implementation: ImplementationBuilder['implementation'] = 'column';
+  if (!searchParam.base?.includes(resourceType as ResourceType)) {
+    // TODO is ignoring this really the right behavior? When does this happen in practice?
+    // If the search parameter is not defined on the resource type itself, skip special implementations
+  } else if (isLookupTableParam(searchParam, builder)) {
+    implementation = 'lookup-table';
+  }
+  builder.implementation = implementation;
+
+  setSearchParameterImplementation(resourceType, code, builder);
+  return builder;
 }
 
 /**
@@ -63,4 +76,75 @@ function buildSearchParameterImplementation(
  */
 function convertCodeToColumnName(code: string): string {
   return code.split('-').reduce((result, word, index) => result + (index ? capitalize(word) : word), '');
+}
+
+function isLookupTableParam(searchParam: SearchParameter, builder: ImplementationBuilder): boolean {
+  // HumanName
+  const nameParams = [
+    'individual-given',
+    'individual-family',
+    'Patient-name',
+    'Person-name',
+    'Practitioner-name',
+    'RelatedPerson-name',
+  ];
+  if (nameParams.includes(searchParam.id as string)) {
+    return true;
+  }
+
+  // Telecom
+  const telecomParams = [
+    'individual-telecom',
+    'individual-email',
+    'individual-phone',
+    'OrganizationAffiliation-telecom',
+    'OrganizationAffiliation-email',
+    'OrganizationAffiliation-phone',
+  ];
+  if (telecomParams.includes(searchParam.id as string)) {
+    // return true;
+  }
+
+  // Address
+  const addressParams = ['individual-address', 'InsurancePlan-address', 'Location-address', 'Organization-address'];
+  if (addressParams.includes(searchParam.id as string)) {
+    return true;
+  }
+
+  // "address-"
+  if (searchParam.code?.startsWith('address-')) {
+    return true;
+  }
+
+  // Token
+  if (isTokenParam(searchParam, builder)) {
+    return true;
+  }
+
+  return false;
+}
+
+function isTokenParam(searchParam: SearchParameter, builder: ImplementationBuilder): boolean {
+  if (searchParam.type === 'token') {
+    if (searchParam.code?.endsWith(':identifier')) {
+      return true;
+    }
+    for (const elementDefinition of builder.elementDefinitions ?? []) {
+      // Check for any "Identifier", "CodeableConcept", or "Coding"
+      // Any of those value types require the "Token" table for full system|value search semantics.
+      // The common case is that the "type" property only has one value,
+      // but we need to support arrays of types for the choice-of-type properties such as "value[x]".
+      for (const type of elementDefinition.type ?? []) {
+        if (
+          type.code === PropertyType.Identifier ||
+          type.code === PropertyType.CodeableConcept ||
+          type.code === PropertyType.Coding ||
+          type.code === PropertyType.ContactPoint
+        ) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
 }
