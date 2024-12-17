9788e4a7fcd59e7ea1790e2822944866945735f4
Cody Ebberson <cody@ebberson.com>
2024-12-17 10:28:05 -0800

    Tests

--- packages/server/src/scim/utils.test.ts
+++ packages/server/src/scim/utils.test.ts
@@ -1,24 +1,111 @@
 import { convertScimToJsonPatch } from './utils';
 
-test('convertScimToJsonPatch', () => {
-  // See https://developer.okta.com/docs/api/openapi/okta-scim/guides/scim-20/#update-a-specific-user-patch
-  expect(
-    convertScimToJsonPatch({
-      schemas: ['urn:ietf:params:scim:api:messages:2.0:PatchOp'],
-      Operations: [
-        {
-          op: 'replace',
-          value: {
-            active: false,
+describe('convertScimToJsonPatch', () => {
+  test('Okta example', () => {
+    // See https://developer.okta.com/docs/api/openapi/okta-scim/guides/scim-20/#update-a-specific-user-patch
+    expect(
+      convertScimToJsonPatch({
+        schemas: ['urn:ietf:params:scim:api:messages:2.0:PatchOp'],
+        Operations: [
+          {
+            op: 'replace',
+            value: {
+              active: false,
+            },
           },
-        },
-      ],
-    })
-  ).toEqual([
-    {
-      op: 'replace',
-      path: '/active',
-      value: false,
-    },
-  ]);
+        ],
+      })
+    ).toEqual([
+      {
+        op: 'replace',
+        path: '/active',
+        value: false,
+      },
+    ]);
+  });
+
+  test('Valid operation passthrough', () => {
+    expect(
+      convertScimToJsonPatch({
+        schemas: ['urn:ietf:params:scim:api:messages:2.0:PatchOp'],
+        Operations: [
+          {
+            op: 'add',
+            path: '/active',
+            value: true,
+          },
+        ],
+      })
+    ).toMatchObject([
+      {
+        op: 'add',
+        path: '/active',
+        value: true,
+      },
+    ]);
+  });
+
+  test('Adds path prefix', () => {
+    expect(
+      convertScimToJsonPatch({
+        schemas: ['urn:ietf:params:scim:api:messages:2.0:PatchOp'],
+        Operations: [
+          {
+            op: 'add',
+            path: 'active',
+            value: true,
+          },
+        ],
+      })
+    ).toMatchObject([
+      {
+        op: 'add',
+        path: '/active',
+        value: true,
+      },
+    ]);
+  });
+
+  test('Invalid operation', () => {
+    expect(() =>
+      convertScimToJsonPatch({
+        schemas: ['urn:ietf:params:scim:api:messages:2.0:PatchOp'],
+        Operations: [
+          {
+            op: 'invalid',
+            path: '/x',
+            value: 'x',
+          },
+        ],
+      })
+    ).toThrow('Invalid SCIM patch: unsupported operation');
+  });
+
+  test('Test op requires path', () => {
+    expect(() =>
+      convertScimToJsonPatch({
+        schemas: ['urn:ietf:params:scim:api:messages:2.0:PatchOp'],
+        Operations: [
+          {
+            op: 'test',
+            value: 'x',
+          },
+        ],
+      })
+    ).toThrow('Invalid SCIM patch: missing required path');
+  });
+
+  test('Add op without path must be object', () => {
+    expect(() =>
+      convertScimToJsonPatch({
+        schemas: ['urn:ietf:params:scim:api:messages:2.0:PatchOp'],
+        Operations: [
+          {
+            op: 'add',
+            value: 'x',
+          },
+        ],
+      })
+    ).toThrow('Invalid SCIM patch: value must be an object if path is missing');
+  });
 });
