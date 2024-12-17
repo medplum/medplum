e37638504a264d5b0c51238edf380926afbb27ff
Ian Plunkett <1857868+ianplunkett@users.noreply.github.com>
2024-12-17 14:52:49 -0800

    ValueSet selector example

---
+++ examples/medplum-valueset-selector/src/App.tsx
@@ -0,0 +1,40 @@
+import { AppShell, ErrorBoundary, Loading, Logo, useMedplum, useMedplumProfile } from '@medplum/react';
+import { IconUser } from '@tabler/icons-react';
+import { Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { HomePage } from './pages/HomePage';
+import { LandingPage } from './pages/LandingPage';
+import { ResourcePage } from './pages/ResourcePage';
+import { SignInPage } from './pages/SignInPage';
+
+export function App(): JSX.Element | null {
+  const medplum = useMedplum();
+  const profile = useMedplumProfile();
+
+  if (medplum.isLoading()) {
+    return null;
+  }
+
+  return (
+    <AppShell
+      logo={<Logo size={24} />}
+      menus={[
+        {
+          title: 'My Links',
+          links: [{ icon: <IconUser />, label: 'Patients', href: '/' }, { icon: <IconUser />, label: 'Patients', href: '/' }],
+        },
+      ]}
+    >
+      <ErrorBoundary>
+        <Suspense fallback={<Loading />}>
+          <Routes>
+            <Route path="/" element={profile ? <HomePage /> : <LandingPage />} />
+            <Route path="/signin" element={<SignInPage />} />
+           <Route path="/:resourceType/:id" element={<ResourcePage />} />
+            <Route path="/:resourceType/:id/_history/:versionId" element={<ResourcePage />} />
+          </Routes>
+        </Suspense>
+      </ErrorBoundary>
+    </AppShell>
+  );
+}
