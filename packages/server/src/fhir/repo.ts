9f764deee79f6f09b9404c8ef0e92cd14ab94121
Matt Long <matt@mattlong.org>
2024-12-18 14:39:35 -0800

    Be a little more permissive

--- packages/server/src/fhir/repo.ts
+++ packages/server/src/fhir/repo.ts
@@ -2571,14 +2571,20 @@ function patchObject(obj: any, patch: Operation[]): void {
   }
 }
 
+const textEncoder = new TextEncoder();
+
 /**
  * Apply a maximum string length to ensure the value can accommodate the maximum
- * size for a btree index entry: 2704 bytes. Be as conservative as possible to avoid
- * write errors; so limit to 675 characters to accommodate the entire string being
- * random 4-byte UTF-8 characters.
+ * size for a btree index entry: 2704 bytes. If the string is too large,
+ * be as conservative as possible to avoid write errors by truncating to 675 characters
+ * to accommodate the entire string being 4-byte UTF-8 code points.
  * @param value - The column value to truncate.
  * @returns The possibly truncated column value.
  */
 function truncateTextColumn(value: string): string {
-  return value.substring(0, 675);
+  if (textEncoder.encode(value).length <= 2704) {
+    return value;
+  }
+
+  return Array.from(value).slice(0, 675).join('');
 }
