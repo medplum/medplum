3754df9897dc359b4d90e06d02b5f3756dc1c678
David <david@Davids-MacBook-Pro.local>
2025-01-10 17:24:05 -0800

    new encounter popup

--- examples/medplum-provider/src/App.tsx
+++ examples/medplum-provider/src/App.tsx
@@ -38,6 +38,8 @@ import { ResourceDetailPage } from './pages/resource/ResourceDetailPage';
 import { ResourceEditPage } from './pages/resource/ResourceEditPage';
 import { ResourceHistoryPage } from './pages/resource/ResourceHistoryPage';
 import { ResourcePage } from './pages/resource/ResourcePage';
+import { EncounterModal } from './pages/encounter/EncounterModal';
+import { EncounterChart } from './pages/encounter/EcounterChart';
 
 export function App(): JSX.Element | null {
   const medplum = useMedplum();
@@ -119,8 +121,10 @@ export function App(): JSX.Element | null {
             <>
               <Route path="/" element={<HomePage />} />
               <Route path="/Patient/:patientId" element={<PatientPage />}>
+                <Route path="Encounter/new" element={<EncounterModal />} />
+                <Route path="Encounter/chart" element={<EncounterChart/>} />
                 <Route path="edit" element={<EditTab />} />
-                <Route path="encounter" element={<EncounterTab />} />
+                {/* <Route path="encounter" element={<EncounterTab />} /> */}
                 <Route path="communication" element={<CommunicationTab />} />
                 <Route path="communication/:id" element={<CommunicationTab />} />
                 {hasDoseSpot && <Route path="dosespot" element={<DoseSpotTab />} />}
