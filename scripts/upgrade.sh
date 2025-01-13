8bac4ca0b5d9896745a2363d372fcdd61f80ce8f
Derrick Farris <derrick@medplum.com>
2025-01-13 12:43:57 -0600

    actions(upgrade-deps): make sure pr exists before step 2 (#5775)
    * actions(upgrade-deps): make sure pr exists b4 step 2
    
    * fix(upgrade.sh): use exit code after running view
    
    * fix(upgrade.sh): make sure script doesn't exit on err

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -84,10 +84,24 @@ if [ "$LAST_STEP" -lt 1 ]; then
     git add -u .
     git commit -m "Dependency upgrades - step 1"
     git push origin "$BRANCH_NAME"
-    
-    if [ "$LAST_STEP" -eq 0 ]; then
-        gh pr create --title "Dependency upgrades $DATE" --body "Dependency upgrades" --draft
-    fi
+fi
+
+# Temporarily unset -e flag so that script won't exit early on next error
+set +e
+
+# Check if there is a PR for this branch already
+# Skip creation if there is, otherwise create the PR for this branch
+gh pr view
+PR_VIEW_EXIT_CODE=$?
+
+# Set the -e flag back 
+set -e
+
+if [ "$PR_VIEW_EXIT_CODE" -ne 0 ]; then
+    echo "No existing PR found, creating PR..."
+    gh pr create --title "Dependency upgrades $DATE" --body "Dependency upgrades" --draft
+else
+    echo "Existing PR found, skipping create"
 fi
 
 if [ "$LAST_STEP" -lt 2 ]; then
