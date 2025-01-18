8f268d2a91e80644671f1b95f3d6503613110034
David <david@Davids-MacBook-Pro.local>
2025-01-18 13:27:07 -0800

    Add David to about.tsx

---
+++ packages/definitions/dist/index.d 2.ts
@@ -0,0 +1,7 @@
+export declare function readJson(filename: string): any;
+/**
+ * The list of all known search parameter definition bundle file paths relative to the
+ * `@medplum/definitions` package. Typically used in conjunction with `readJson`.
+ */
+export declare const SEARCH_PARAMETER_BUNDLE_FILES: string[];
+//# sourceMappingURL=index.d.ts.map
