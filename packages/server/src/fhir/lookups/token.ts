a02c6e746621c199e398d3fba311dad21c09f629
Matt Long <ml@medplum.com>
2025-01-20 14:48:28 -0800

    Handle "|<value>" token searches correctly

--- packages/server/src/fhir/lookups/token.ts
+++ packages/server/src/fhir/lookups/token.ts
@@ -516,7 +516,7 @@ function buildWhereCondition(
   // Handle the case where the query value is a system|value pair (e.g. token or identifier search)
   if (parts.length === 2) {
     const [system, value] = parts;
-    const systemCondition = new Condition(new Column(tableName, 'system'), '=', system);
+    const systemCondition = new Condition(new Column(tableName, 'system'), '=', system || null);
     return value
       ? new Conjunction([systemCondition, buildValueCondition(tableName, operator, caseSensitive, value)])
       : systemCondition;
