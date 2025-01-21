b27b638af28083263239437f972c6e1f19832cf6
Ian Plunkett <1857868+ianplunkett@users.noreply.github.com>
2025-01-21 14:37:57 -0800

    Add back in the ResourcePage

--- examples/medplum-valueset-selector/src/App.tsx
+++ examples/medplum-valueset-selector/src/App.tsx
@@ -3,6 +3,7 @@ import { IconVariablePlus } from '@tabler/icons-react';
 import { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { HomePage } from './pages/HomePage';
+import { ResourcePage } from './pages/ResourcePage';
 import { LandingPage } from './pages/LandingPage';
 import { SignInPage } from './pages/SignInPage';
 
@@ -28,7 +29,9 @@ export function App(): JSX.Element | null {
         <Suspense fallback={<Loading />}>
           <Routes>
             <Route path="/" element={profile ? <HomePage /> : <LandingPage />} />
+            <Route path="/" element={profile ? <HomePage /> : <LandingPage />} />
             <Route path="/signin" element={<SignInPage />} />
+          <Route path="/:resourceType/:id" element={<ResourcePage />} />
           </Routes>
         </Suspense>
       </ErrorBoundary>
