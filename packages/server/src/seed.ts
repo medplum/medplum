bdb2bbb884638152f1efb42a46245d064608de2c
Matt Long <ml@medplum.com>
2025-01-30 10:45:47 -0800

    TEMP: perf testing seeds

--- packages/server/src/seed.ts
+++ packages/server/src/seed.ts
@@ -1,5 +1,5 @@
-import { createReference } from '@medplum/core';
-import { Practitioner, Project, ProjectMembership, User } from '@medplum/fhirtypes';
+import { createReference, getReferenceString, Operator } from '@medplum/core';
+import { ClientApplication, Practitioner, Project, ProjectMembership, User } from '@medplum/fhirtypes';
 import { bcryptHashPassword } from './auth/utils';
 import { r4ProjectId } from './constants';
 import { getSystemRepo, Repository } from './fhir/repo';
@@ -7,14 +7,20 @@ import { globalLogger } from './logger';
 import { rebuildR4SearchParameters } from './seeds/searchparameters';
 import { rebuildR4StructureDefinitions } from './seeds/structuredefinitions';
 import { rebuildR4ValueSets } from './seeds/valuesets';
+import { createClient } from './admin/client';
+
+const DO_PERF_TESTING = false;
 
 export async function seedDatabase(): Promise<void> {
+  const systemRepo = getSystemRepo();
+  if (DO_PERF_TESTING) {
+    await ensurePerfTestingResources(systemRepo);
+  }
+
   if (await isSeeded()) {
     globalLogger.info('Already seeded');
     return;
   }
-
-  const systemRepo = getSystemRepo();
 
   await systemRepo.withTransaction(async () => {
     await createSuperAdmin(systemRepo);
@@ -34,6 +40,100 @@ export async function seedDatabase(): Promise<void> {
     await rebuildR4SearchParameters();
     globalLogger.info('Finished building search parameters', { durationMs: Date.now() - startTime });
   });
+}
+
+async function ensurePerfTestingResources(systemRepo: Repository): Promise<void> {
+  const projects = await systemRepo.searchResources<Project>({
+    resourceType: 'Project',
+    filters: [{ code: 'name', operator: Operator.EQUALS, value: 'Super Admin' }],
+  });
+  const superAdminProject = projects.find((p) => p.superAdmin);
+  if (!superAdminProject) {
+    throw new Error('Super Admin project not found, aborting.');
+  }
+
+  const clientAppName = 'Perf Testing Client Application';
+  let clientApp = await systemRepo.searchOne<ClientApplication>({
+    resourceType: 'ClientApplication',
+    filters: [{ code: 'name', operator: Operator.EQUALS, value: clientAppName }],
+  });
+
+  if (clientApp) {
+    console.log(`Found existing Perf Testing Client App: ${clientApp.id}`);
+  } else {
+    console.log('Creating new Perf Testing Client Application...');
+    clientApp = await createClient(systemRepo, {
+      project: superAdminProject,
+      name: clientAppName,
+    });
+  }
+
+  console.log(`Perf Testing Client App: ${clientApp.id} ${clientApp.secret}`);
+
+  const email = 'ml@medplum.com';
+  const superUsers = await systemRepo.searchResources<User>({
+    resourceType: 'User',
+    filters: [
+      { code: 'email', operator: Operator.EQUALS, value: email },
+      { code: 'project', operator: Operator.MISSING, value: 'true' },
+    ],
+  });
+
+  if (superUsers.length !== 1) {
+    console.log(superUsers.map((u) => JSON.stringify(u, null, 2)));
+    throw new Error(`Expected one super user but found ${superUsers.length}`);
+  }
+  const superUser = superUsers[0];
+
+  const pms = await systemRepo.searchResources<ProjectMembership & { id: string }>({
+    resourceType: 'ProjectMembership',
+    filters: [
+      { code: 'project', operator: Operator.EQUALS, value: getReferenceString(superAdminProject) },
+      { code: 'user', operator: Operator.EQUALS, value: getReferenceString(superUser) },
+    ],
+  });
+
+  if (pms.length > 0) {
+    console.log('Found existing', getReferenceString(pms[0]));
+    if (pms.length > 1) {
+      for (const pm of pms.slice(1)) {
+        console.log('Deleting', pm.resourceType, pm.id);
+        await systemRepo.deleteResource(pm.resourceType, pm.id);
+      }
+    }
+    return;
+  }
+
+  const practitioners = await systemRepo.searchResources<Practitioner>({
+    resourceType: 'Practitioner',
+    filters: [
+      { code: '_project', operator: Operator.EQUALS, value: getReferenceString(superAdminProject) },
+      { code: 'email', operator: Operator.EQUALS, value: email },
+    ],
+  });
+  let practitioner: Practitioner;
+  if (practitioners.length > 0) {
+    practitioner = practitioners[0];
+  } else {
+    practitioner = await systemRepo.createResource<Practitioner>({
+      resourceType: 'Practitioner',
+      meta: {
+        project: superAdminProject.id,
+      },
+      name: [{ given: [superUser.firstName], family: superUser.lastName }],
+      telecom: [{ system: 'email', use: 'work', value: superUser.email }],
+    });
+    console.log('Created', getReferenceString(practitioner));
+  }
+
+  const pm = await systemRepo.createResource<ProjectMembership>({
+    resourceType: 'ProjectMembership',
+    project: createReference(superAdminProject),
+    user: createReference(superUser),
+    profile: createReference(practitioner),
+    admin: true,
+  });
+  console.log('Created', getReferenceString(pm));
 }
 
 async function createSuperAdmin(systemRepo: Repository): Promise<void> {
