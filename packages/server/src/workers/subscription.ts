651344d6401aa23b5ca7220f54f4e1eca09503fd
Matthew Willer <matt@medplum.com>
2025-02-03 15:33:32 -0800

    Reuse variable

--- packages/server/src/workers/subscription.ts
+++ packages/server/src/workers/subscription.ts
@@ -353,7 +353,7 @@ async function getSubscriptions(resource: Resource, project: Project): Promise<S
       if (hitRate < 0.1 && redisOnlySubRefStrs.length >= 100) {
         getLogger().warn('Excessive subscription cache miss', { keys: redisOnlySubRefStrs, hitRate });
       }
-      const subArrStr = '[' + redisOnlySubStrs.filter(Boolean).join(',') + ']';
+      const subArrStr = '[' + activeSubStrs.join(',') + ']';
       const inMemorySubs = JSON.parse(subArrStr) as { resource: Subscription; projectId: string }[];
       for (const { resource } of inMemorySubs) {
         subscriptions.push(resource);
