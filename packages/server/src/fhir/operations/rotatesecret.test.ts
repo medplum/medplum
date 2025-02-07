2af5e255c6e645e0b02515ef40a5672fc0e7d4f8
Matthew Willer <matt@medplum.com>
2025-02-07 11:19:41 -0800

    Safety checks

--- packages/server/src/fhir/operations/rotatesecret.test.ts
+++ packages/server/src/fhir/operations/rotatesecret.test.ts
@@ -79,6 +79,43 @@ describe('ClientApplication $rotate-secret', () => {
     expect(res.body.issue?.[0]?.code).toStrictEqual('invalid');
   });
 
+  test('Remove retired secret', async () => {
+    const { client, accessToken } = await createTestProject({
+      withAccessToken: true,
+      withClient: true,
+      membership: { admin: true },
+    });
+
+    const res = await request(app)
+      .post(`/fhir/R4/ClientApplication/${client.id}/$rotate-secret`)
+      .set('Authorization', 'Bearer ' + accessToken)
+      .set('Content-Type', ContentType.FHIR_JSON)
+      .set('X-Medplum', 'extended')
+      .send({
+        resourceType: 'Parameters',
+        parameter: [{ name: 'secret', valueString: client.secret }],
+      } satisfies Parameters);
+    expect(res.status).toBe(200);
+    const updated = res.body as ClientApplication;
+    expect(updated.secret).toBeDefined();
+    expect(updated.secret).not.toStrictEqual(client.secret);
+    expect(updated.retiringSecret).toStrictEqual(client.secret);
+
+    const res2 = await request(app)
+      .post(`/fhir/R4/ClientApplication/${client.id}/$rotate-secret`)
+      .set('Authorization', 'Bearer ' + accessToken)
+      .set('Content-Type', ContentType.FHIR_JSON)
+      .set('X-Medplum', 'extended')
+      .send({
+        resourceType: 'Parameters',
+        parameter: [{ name: 'retiringSecret', valueString: updated.retiringSecret }],
+      } satisfies Parameters);
+    expect(res2.status).toBe(200);
+    const retired = res2.body as ClientApplication;
+    expect(retired.secret).toStrictEqual(updated.secret);
+    expect(retired.retiringSecret).toBeUndefined();
+  });
+
   test('Access denied for non-admin user', async () => {
     const { client, accessToken } = await createTestProject({
       withAccessToken: true,
@@ -90,8 +127,54 @@ describe('ClientApplication $rotate-secret', () => {
       .set('Authorization', 'Bearer ' + accessToken)
       .set('Content-Type', ContentType.FHIR_JSON)
       .set('X-Medplum', 'extended')
-      .send({});
+      .send({
+        resourceType: 'Parameters',
+        parameter: [{ name: 'secret', valueString: client.secret }],
+      } satisfies Parameters);
     expect(res.status).toBe(403);
     expect(res.body.issue?.[0]?.code).toStrictEqual('forbidden');
+  });
+
+  test('Secret must be provided', async () => {
+    const { client, accessToken } = await createTestProject({
+      withAccessToken: true,
+      withClient: true,
+      membership: { admin: true },
+    });
+
+    const res = await request(app)
+      .post(`/fhir/R4/ClientApplication/${client.id}/$rotate-secret`)
+      .set('Authorization', 'Bearer ' + accessToken)
+      .set('Content-Type', ContentType.FHIR_JSON)
+      .set('X-Medplum', 'extended')
+      .send({
+        resourceType: 'Parameters',
+        parameter: [],
+      } satisfies Parameters);
+    expect(res.status).toBe(400);
+    expect(res.body.issue?.[0]?.code).toStrictEqual('invalid');
+  });
+
+  test('Only one secret can be provided', async () => {
+    const { client, accessToken } = await createTestProject({
+      withAccessToken: true,
+      withClient: true,
+      membership: { admin: true },
+    });
+
+    const res = await request(app)
+      .post(`/fhir/R4/ClientApplication/${client.id}/$rotate-secret`)
+      .set('Authorization', 'Bearer ' + accessToken)
+      .set('Content-Type', ContentType.FHIR_JSON)
+      .set('X-Medplum', 'extended')
+      .send({
+        resourceType: 'Parameters',
+        parameter: [
+          { name: 'secret', valueString: client.secret },
+          { name: 'retiringSecret', valueString: client.secret },
+        ],
+      } satisfies Parameters);
+    expect(res.status).toBe(400);
+    expect(res.body.issue?.[0]?.code).toStrictEqual('invalid');
   });
 });
--- packages/server/src/fhir/operations/rotatesecret.ts
+++ packages/server/src/fhir/operations/rotatesecret.ts
@@ -1,8 +1,8 @@
 import { FhirRequest, FhirResponse } from '@medplum/fhir-router';
-import { OperationDefinition } from '@medplum/fhirtypes';
+import { ClientApplication, OperationDefinition } from '@medplum/fhirtypes';
 import { getAuthenticatedContext } from '../../context';
 import { generateSecret } from '../../oauth/keys';
-import { allOk, forbidden } from '@medplum/core';
+import { allOk, badRequest, forbidden, OperationOutcomeError } from '@medplum/core';
 import { buildOutputParameters, parseInputParameters } from './utils/parameters';
 import { getSystemRepo } from '../repo';
 
@@ -18,13 +18,15 @@ const operation: OperationDefinition = {
   type: false,
   instance: true,
   parameter: [
-    { use: 'in', name: 'secret', type: 'string', min: 1, max: '1' },
+    { use: 'in', name: 'secret', type: 'string', min: 0, max: '1' },
+    { use: 'in', name: 'retiringSecret', type: 'string', min: 0, max: '1' },
     { use: 'out', name: 'return', type: 'ClientApplication', min: 1, max: '1' },
   ],
 };
 
 type RotateSecretParameters = {
-  secret: string;
+  secret?: string;
+  retiringSecret?: string;
 };
 
 /**
@@ -43,24 +45,34 @@ export async function rotateSecretHandler(req: FhirRequest): Promise<FhirRespons
   }
 
   const params = parseInputParameters<RotateSecretParameters>(operation, req);
+  if (!params.secret && !params.retiringSecret) {
+    return [badRequest('Secret to rotate must be provided')];
+  }
+  if (params.secret && params.retiringSecret) {
+    return [badRequest('Only one secret can be rotated at a time')];
+  }
 
   // Patch using system repo since the secret fields should not generally be user-writeable
-  const clientApp = await getSystemRepo().patchResource('ClientApplication', req.params.id, [
-    {
-      op: 'test',
-      path: '/secret',
-      value: params.secret,
-    },
-    {
-      op: 'copy',
-      from: '/secret',
-      path: '/retiringSecret',
-    },
-    {
-      op: 'replace',
-      path: '/secret',
-      value: generateSecret(32),
-    },
-  ]);
+  const systemRepo = getSystemRepo();
+  const clientApp = await systemRepo.withTransaction(async () => {
+    let clientApp = await systemRepo.readResource<ClientApplication>('ClientApplication', req.params.id);
+    if (params.secret && params.secret === clientApp.secret) {
+      clientApp = await systemRepo.updateResource({
+        ...clientApp,
+        secret: generateSecret(32), // Generate new secret
+        retiringSecret: clientApp.secret, // Rotate existing secret to "retiring" slot
+      });
+    } else if (params.retiringSecret && params.retiringSecret === clientApp.retiringSecret) {
+      clientApp = await systemRepo.updateResource({
+        ...clientApp,
+        retiringSecret: undefined, // Remove rotated secret after it's been retired
+      });
+    } else {
+      throw new OperationOutcomeError(badRequest('Provided secret does not match client'));
+    }
+
+    return clientApp;
+  });
+
   return [allOk, buildOutputParameters(operation, clientApp)];
 }
