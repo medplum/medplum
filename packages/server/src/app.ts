0375cd88251306c2568ef6a2573f584a0396707d
Matthew Willer <matt@medplum.com>
2025-02-13 11:44:09 -0800

    Log requests made on behalf of another user

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -256,7 +256,11 @@ const loggingMiddleware = (req: Request, res: Response, next: NextFunction): voi
     let userProfile: string | undefined;
     let projectId: string | undefined;
     if (ctx instanceof AuthenticatedRequestContext) {
-      userProfile = ctx.profile.reference;
+      if (ctx.profile.reference === ctx.realProfile.reference) {
+        userProfile = ctx.profile.reference;
+      } else {
+        userProfile = `${ctx.realProfile.reference} (as ${ctx.profile.reference})`;
+      }
       projectId = ctx.project.id;
     }
 
