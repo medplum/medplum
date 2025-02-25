892807bd9908b8f47885909df843c17115ca5bfc
Diego Sucaria <diegosucaria@gmail.com>
2025-02-25 13:57:24 +0000

    added azure blob storage support

--- packages/server/src/fhir/storage.ts
+++ packages/server/src/fhir/storage.ts
@@ -5,6 +5,7 @@ import { copyFileSync, createReadStream, createWriteStream, existsSync, mkdirSyn
 import { resolve, sep } from 'path';
 import { Readable, pipeline } from 'stream';
 import { S3Storage } from '../cloud/aws/storage';
+import { AzureBlobStorage } from '../cloud/azure/storage';
 import { getConfig } from '../config/loader';
 
 /**
@@ -28,6 +29,8 @@ let binaryStorage: BinaryStorage | undefined = undefined;
 export function initBinaryStorage(type?: string): void {
   if (type?.startsWith('s3:')) {
     binaryStorage = new S3Storage(type.replace('s3:', ''));
+  } else if (type?.startsWith('azure:')) {
+    binaryStorage = new AzureBlobStorage(type.replace('azure:', ''));
   } else if (type?.startsWith('file:')) {
     binaryStorage = new FileSystemStorage(type.replace('file:', ''));
   } else {
