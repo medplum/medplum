3fc61754b4eb0a9b4b058bc13945c4f3fa6ca83f
David Yanez <me@davidyanez.com>
2025-02-28 15:20:38 -0800

    clean uo

--- examples/medplum-provider/src/pages/components/Encounter/EncounterHeader.tsx
+++ examples/medplum-provider/src/pages/components/Encounter/EncounterHeader.tsx
@@ -20,12 +20,12 @@ export const EncounterHeader = (props: EncounterHeaderProps): JSX.Element => {
       <Box className={classes.row}>
         <Group gap="xl">
           <Box>
-            <Text className={classes.label}>Patient</Text>
+            <Text className={classes.label} c="dimmed">Patient</Text>
             <Text className={classes.value}>{formatHumanName(patient.name?.[0] as HumanName)}</Text>
           </Box>
 
           <Box>
-            <Text className={classes.label}>Practitioner</Text>
+            <Text className={classes.label} c="dimmed">Practitioner</Text>
             <Text className={classes.value}>
               {practitioner ? `${formatHumanName(practitioner.name?.[0] as HumanName)}` : 'N/A'}
             </Text>
@@ -34,20 +34,20 @@ export const EncounterHeader = (props: EncounterHeaderProps): JSX.Element => {
 
         <Group gap="xl">
           <Box>
-            <Text className={classes.label}>Status</Text>
+            <Text className={classes.label} c="dimmed">Status</Text>
             <Badge className={classes.badge} radius="xl">
               {encounter.status}
             </Badge>
           </Box>
 
           <Box>
-            <Text className={classes.label}>Checked in</Text>
+            <Text className={classes.label} c="dimmed">Checked in</Text>
             <Text className={classes.dash}>—</Text>
           </Box>
 
           <Box>
-            <Text className={classes.label}>Checked out</Text>
-            <Text className={classes.dash}>—</Text>
+            <Text className={classes.label} c="dimmed">Checked out</Text>
+            <Text>—</Text>
           </Box>
         </Group>
       </Box>
