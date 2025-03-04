f788f382918ac6d611fc89ab091e62e51e40fdec
David Yanez <me@davidyanez.com>
2025-03-04 10:17:50 -0800

    pr feedback

--- examples/medplum-provider/src/hooks/useEncounter.ts
+++ examples/medplum-provider/src/hooks/useEncounter.ts
@@ -1,16 +1,9 @@
-import { OperationOutcome, Encounter } from '@medplum/fhirtypes';
+import { Encounter } from '@medplum/fhirtypes';
 import { useResource } from '@medplum/react';
 import { useParams } from 'react-router';
 
-type Options = {
-  ignoreMissingEncounterId?: boolean;
-  setOutcome?: (outcome: OperationOutcome) => void;
-};
-
-export function useEncounter(options?: Options): Encounter | undefined {
+export function useEncounter(): Encounter | undefined {
   const { encounterId } = useParams();
-  if (!encounterId && !options?.ignoreMissingEncounterId) {
-    throw new Error('Encounter ID not found');
-  }
-  return useResource<Encounter>({ reference: `Encounter/${encounterId}` }, options?.setOutcome);
+
+  return useResource<Encounter>({ reference: `Encounter/${encounterId}` });
 }
