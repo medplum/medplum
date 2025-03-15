56c9ffaded60fe1592076c17b3ce50d14a75c653
Derrick Farris <derrick@medplum.com>
2025-03-15 14:09:08 -0700

    tweak(agent): rework pid file locs, get tests working

--- packages/agent/src/pid.ts
+++ packages/agent/src/pid.ts
@@ -1,8 +1,10 @@
-import { normalizeErrorString } from '@medplum/core';
+import { Logger, normalizeErrorString } from '@medplum/core';
 import fs from 'node:fs';
-import { platform } from 'node:os';
+import { platform, tmpdir } from 'node:os';
 import path from 'node:path';
 import process from 'node:process';
+
+const pidLogger = new Logger((msg) => `[PID]: ${msg}`);
 
 /**
  * Get the appropriate PID file location based on OS
@@ -13,7 +15,9 @@ export function getPidFilePath(appName: string): string {
   switch (platform()) {
     case 'linux':
     case 'darwin':
-      return `/var/run/${appName}.pid`;
+      // We use tmpdir for linux and Mac because /var/run requires root access
+      // The benefit of using the convention is outweighed by the required permission level to create files/directories inside
+      return path.join(tmpdir(), 'medplum-agent', `${appName}.pid`);
     case 'win32':
       return path.join('C:', 'ProgramData', 'MedplumAgent', 'pids', `${appName}.pid`);
     default:
@@ -29,9 +33,9 @@ export function removePidFile(pidFilePath: string): void {
   if (fs.existsSync(pidFilePath)) {
     try {
       fs.unlinkSync(pidFilePath);
-      console.log(`PID file removed: ${pidFilePath}`);
+      pidLogger.info(`PID file removed: ${pidFilePath}`);
     } catch (err) {
-      console.error(`Failed to remove PID file: ${normalizeErrorString(err)}`);
+      pidLogger.error(`Failed to remove PID file: ${normalizeErrorString(err)}`);
     }
   }
 }
@@ -54,19 +58,25 @@ export function createPidFile(appName: string): string {
     const existingPid = fs.readFileSync(pidFilePath, 'utf8').trim();
 
     // Check if the process with this PID is still running
+    let processRunning = false;
     try {
+      pidLogger.info('Checking if process is running');
       // Sending signal 0 doesn't actually send a signal but checks if process exists
       process.kill(Number.parseInt(existingPid, 10), 0);
-      console.error(`Process already running with PID ${existingPid}`);
-      throw new Error(`${appName} already running`);
+      // If we didn't throw an error above, the process is still running, and PID is not stale
+      processRunning = true;
     } catch (_err) {
       // Process not running, we can overwrite the PID file
-      console.log('Stale PID file found. Overwriting...');
+      pidLogger.info('Stale PID file found. Overwriting...');
+    }
+
+    if (processRunning) {
+      throw new Error(`${appName} already running`);
     }
   }
 
-  // On Windows we need to make sure the directory exists first since we aren't just using a system-created dir like /var/run
-  if (!dirExists && platform() === 'win32') {
+  // We need to make sure the directory exists first since we aren't just using a system-created dir like /var/run
+  if (!dirExists) {
     ensureDirectoryExists(path.dirname(pidFilePath));
   }
 
@@ -75,7 +85,7 @@ export function createPidFile(appName: string): string {
   fs.writeFileSync(tempFile, pid.toString(), { flag: 'w+' });
   fs.renameSync(tempFile, pidFilePath);
 
-  console.log(`PID file created at: ${pidFilePath}`);
+  pidLogger.info(`PID file created at: ${pidFilePath}`);
 
   return pidFilePath;
 }
@@ -84,8 +94,32 @@ export function createPidFile(appName: string): string {
 export function ensureDirectoryExists(directoryPath: string): void {
   if (!fs.existsSync(directoryPath)) {
     fs.mkdirSync(directoryPath, { recursive: true });
-    console.log(`Directory created: ${directoryPath}`);
+    pidLogger.info(`Directory created: ${directoryPath}`);
   } else {
-    console.log(`Directory already exists: ${directoryPath}`);
+    pidLogger.info(`Directory already exists: ${directoryPath}`);
   }
+}
+
+/**
+ * Cleans up the PID file in the event of any process exit scenario.
+ * @param pidFilePath - The PID file for this application.
+ */
+export function registerAgentCleanup(pidFilePath: string): void {
+  // Handle normal exit
+  process.on('exit', () => removePidFile(pidFilePath));
+
+  // Handle various signals
+  for (const signal of ['SIGINT', 'SIGTERM', 'SIGHUP']) {
+    process.on(signal, () => {
+      removePidFile(pidFilePath);
+      process.exit(0);
+    });
+  }
+
+  // Handle uncaught exceptions
+  process.on('uncaughtException', (err) => {
+    pidLogger.error('Uncaught exception:', err);
+    removePidFile(pidFilePath);
+    process.exit(1);
+  });
 }
