163be3b5ac43ebe87a8eb0a35fc1d84fe6383625
Matt Long <matt@mattlong.org>
2025-03-18 14:00:08 -0700

    ReindexJob no longer uses LongJob

---
+++ packages/server/src/migrations/data/types.ts
@@ -0,0 +1,30 @@
+import { WithId } from '@medplum/core';
+import { AsyncJob } from '@medplum/fhirtypes';
+import { Repository } from '../../fhir/repo';
+
+export interface PostDeployJobData {
+  readonly type: 'reindex' | 'custom';
+  readonly asyncJobId: string;
+  readonly requestId?: string;
+  readonly traceId?: string;
+}
+
+export type PostDeployJobRunResult = 'finished' | 'interrupted' | 'ineligible';
+export interface PostDeployMigration<T extends PostDeployJobData = PostDeployJobData> {
+  readonly type: T['type'];
+  /** Prepares the job data needed to run the migration */
+  prepareJobData(asyncJob: WithId<AsyncJob>): T;
+  /**
+   * Runs the migration. Is responsible for updating AsyncJob.status and AsyncJob.output,
+   * generally through usage `AsyncJobExecutor`
+   *
+   * @param repo - A repository instance
+   * @param data - The job data to use while running the migration logic
+   * @returns - Returns one of:
+   * 'finished' if the job either succeeded or failed,
+   * 'interrupted' if the job detected that the AsyncJob was cancelled, paused, etc. out of band,
+   * 'ineligible' if the processor decided it was not capable of running the job, typically
+   *            due to being an outdated version of Medplum.
+   */
+  run(repo: Repository, data: T): Promise<PostDeployJobRunResult>;
+}
