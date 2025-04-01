68bec3da8584766d86d4415470d6c0d6f1b51868
Matthew Willer <matt@medplum.com>
2025-04-01 11:49:35 -0700

    Add timing metrics for expensive server code paths

--- packages/server/src/fhir/repo.ts
+++ packages/server/src/fhir/repo.ts
@@ -1358,9 +1358,18 @@ export class Repository extends FhirRepository<PoolClient> implements Disposable
 
     const searchParams = getSearchParameters(resourceType);
     if (searchParams) {
+      const startTime = process.hrtime.bigint();
       for (const searchParam of Object.values(searchParams)) {
         this.buildColumn(resource, row, searchParam);
       }
+      recordHistogramValue(
+        'medplum.server.indexingDurationMs',
+        Number((process.hrtime.bigint() - startTime) / 1_000_000n), // High resolution time, converted from ns to ms
+        {
+          attributes: { resourceType },
+          options: { unit: 'ms' },
+        }
+      );
     }
     return row;
   }
