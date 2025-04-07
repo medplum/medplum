99a10524f53e3c1451aa262d77701184bcf968e1
Matthew Willer <matt@medplum.com>
2025-04-07 15:53:22 -0700

    Add migration file

---
+++ packages/server/src/migrations/schema/v88.ts
@@ -0,0 +1,12 @@
+/*
+ * This is a generated file
+ * Do not edit manually.
+ */
+
+import { PoolClient } from 'pg';
+
+// prettier-ignore
+export async function run(client: PoolClient): Promise<void> {
+  await client.query('ALTER TABLE IF EXISTS "Group" ADD COLUMN IF NOT EXISTS "characteristicReference" TEXT[]');
+  await client.query('CREATE INDEX CONCURRENTLY IF NOT EXISTS "Group_characteristicReference_idx" ON "Group" USING gin ("characteristicReference")');
+}
