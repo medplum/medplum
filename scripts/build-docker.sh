c2c9041d83d50159986b1883354ce843b0b4acc1
Derrick Farris <derrick@medplum.com>
2025-04-14 11:31:46 -0700

    feat(docker): add `@medplum/app` Dockerfile (#6338)
    * wip(docker): add Dockerfile for app
    
    * feat(docker): add `@medplum/app` Dockerfile
    
    * fix(app/docker): run container as `nginx` user
    
    * fix(app/docker): add defaults for app env vars
    
    * fix(docker): use the unprivileged img 4 non-root user
    
    * tweak(docker): revert `secrets.SERVER_..` -> `secrets.DOCKERHUB_REPOSITORY`
    
    * tweak(docker): one-liner for placeholder replace

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-if [[ -z "${DOCKERHUB_REPOSITORY}" ]]; then
-  echo "DOCKERHUB_REPOSITORY is missing"
+if [[ -z "${SERVER_DOCKERHUB_REPOSITORY}" ]]; then
+  echo "SERVER_DOCKERHUB_REPOSITORY is missing"
   exit 1
 fi
 
@@ -35,6 +35,13 @@ tar \
   packages/server/package.json \
   packages/server/dist
 
+# Build app tarball
+# The -C flag rewrites the base path from packages/app/dist/ to ./
+tar \
+  --no-xattrs \
+  -czf ./packages/app/medplum-app.tar.gz \
+  -C packages/app/dist .
+
 # Supply chain attestations
 # See: https://docs.docker.com/scout/policy/#supply-chain-attestations
 ATTESTATIONS="--provenance=true --sbom=true"
@@ -42,19 +49,34 @@ ATTESTATIONS="--provenance=true --sbom=true"
 # Target platforms
 PLATFORMS="--platform linux/amd64,linux/arm64"
 
-# Build tags
-TAGS="--tag $DOCKERHUB_REPOSITORY:latest --tag $DOCKERHUB_REPOSITORY:$GITHUB_SHA"
-
-# If this is a release, tag with version
+# If this is a release, get version information
 # Release is specified with a "--release" argument
+IS_RELEASE=false
 for arg in "$@"; do
   if [[ "$arg" == "--release" ]]; then
+    IS_RELEASE=true
     FULL_VERSION=$(node -p "require('./package.json').version")
     MAJOR_DOT_MINOR=$(node -p "require('./package.json').version.split('.').slice(0, 2).join('.')")
-    TAGS="$TAGS --tag $DOCKERHUB_REPOSITORY:$FULL_VERSION --tag $DOCKERHUB_REPOSITORY:$MAJOR_DOT_MINOR"
     break
   fi
 done
 
-# Build and push Docker images
-docker buildx build $ATTESTATIONS $PLATFORMS $TAGS --push .
+# Build and push server Docker images
+SERVER_TAGS="--tag $SERVER_DOCKERHUB_REPOSITORY:latest --tag $SERVER_DOCKERHUB_REPOSITORY:$GITHUB_SHA"
+if [[ "$IS_RELEASE" == "true" ]]; then
+  SERVER_TAGS="$SERVER_TAGS --tag $SERVER_DOCKERHUB_REPOSITORY:$FULL_VERSION --tag $SERVER_DOCKERHUB_REPOSITORY:$MAJOR_DOT_MINOR"
+fi
+docker buildx build $ATTESTATIONS $PLATFORMS $SERVER_TAGS --push .
+
+# Only build app if APP_DOCKERHUB_REPOSITORY was passed
+# This is so we can build the staging server Dockerfile without having to build app
+if [[ -n "${APP_DOCKERHUB_REPOSITORY}" ]]; then
+  # Build and push app Docker images
+  APP_TAGS="--tag $APP_DOCKERHUB_REPOSITORY:latest --tag $APP_DOCKERHUB_REPOSITORY:$GITHUB_SHA"
+  if [[ "$IS_RELEASE" == "true" ]]; then
+    APP_TAGS="$APP_TAGS --tag $APP_DOCKERHUB_REPOSITORY:$FULL_VERSION --tag $APP_DOCKERHUB_REPOSITORY:$MAJOR_DOT_MINOR"
+  fi
+  pushd packages/app
+  docker buildx build $ATTESTATIONS $PLATFORMS $APP_TAGS --push .
+  popd
+fi
