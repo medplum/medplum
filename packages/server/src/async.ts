ab64a918a7ff2575cdb4a00efde4bf6d3ffadcf4
Matthew Willer <matt@medplum.com>
2025-04-22 13:32:55 -0700

    Preserve wrapped function names

--- packages/server/src/async.ts
+++ packages/server/src/async.ts
@@ -10,7 +10,9 @@ import { NextFunction, Request, RequestHandler, Response } from 'express';
  * @returns Async wrapped handler.
  */
 export function asyncWrap(callback: (req: Request, res: Response, next: NextFunction) => Promise<any>): RequestHandler {
-  return (req: Request, res: Response, next: NextFunction): void => {
+  const fn = function (req: Request, res: Response, next: NextFunction): void {
     callback(req, res, next).catch(next);
   };
+  Object.defineProperty(fn, 'name', { writable: false, value: callback.name });
+  return fn;
 }
