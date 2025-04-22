4e7955ffc9daba354ee2fe502b8f8c919916b0c7
Matt Long <mattlong@users.noreply.github.com>
2025-04-22 13:04:18 -0700

    Let `/admin/super/vacuum` run `ANALYZE` queries (#6425)
    * Allow /vacuum endpoint to only analyze
    
    * Include last_analyze and last_vacuum
    
    * Include query in output
    
    * Add --prefer-async to medplum post

--- packages/server/src/admin/super.ts
+++ packages/server/src/admin/super.ts
@@ -384,6 +384,7 @@ superAdminRouter.post(
       .withMessage('Table name(s) must be a snake_cased_string')
       .optional(),
     body('analyze').isBoolean().optional().default(false),
+    body('vacuum').isBoolean().optional().default(true),
     checkExact(),
   ],
   asyncWrap(async (req: Request, res: Response) => {
@@ -396,20 +397,33 @@ superAdminRouter.post(
       return;
     }
 
-    const query = `VACUUM${req.body.analyze ? ' ANALYZE' : ''}${req.body.tableNames?.length ? ` ${req.body.tableNames.map((name: string) => `"${name}"`).join(', ')}` : ''};`;
+    const vacuum = req.body.vacuum ?? true;
+
+    let action = vacuum ? 'VACUUM' : '';
+    action += req.body.analyze ? ' ANALYZE' : '';
+    if (!action) {
+      throw new OperationOutcomeError(badRequest('At least one of vacuum or analyze must be true'));
+    }
+
+    const query =
+      `${action}${req.body.tableNames?.length ? ` ${req.body.tableNames.map((name: string) => `"${name}"`).join(', ')}` : ''};`.trim();
 
     await sendAsyncResponse(req, res, async () => {
       const startTime = Date.now();
       await getSystemRepo().getDatabaseClient(DatabaseMode.WRITER).query(query);
       globalLogger.info('[Super Admin]: Vacuum completed', {
         tableNames: req.body.tableNames,
+        vacuum,
         analyze: req.body.analyze,
         query,
         durationMs: Date.now() - startTime,
       });
       return {
         resourceType: 'Parameters',
-        parameter: [{ name: 'outcome', resource: allOk }],
+        parameter: [
+          { name: 'outcome', resource: allOk },
+          { name: 'query', valueString: query },
+        ],
       };
     });
   })
