50d310521660c3818e5fb6a9a4638a24ad2352ac
David Yanez <me@davidyanez.com>
2025-04-24 11:02:37 -0700

    clean up

--- examples/medplum-provider/src/Questionnaire.context.tsx
+++
@@ -1,4 +0,0 @@
-import { Questionnaire } from '@medplum/fhirtypes';
-import { createContext } from 'react';
-
-export const IntakeQuestionnaireContext = createContext<{ questionnaire?: Questionnaire }>({});
