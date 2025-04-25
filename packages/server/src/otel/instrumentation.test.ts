bd342ff5c75d06f8adffd48a17bc8513a8776e0c
Matthew Willer <matt@medplum.com>
2025-04-25 11:42:19 -0700

    Add test coverage

--- packages/server/src/otel/instrumentation.test.ts
+++ packages/server/src/otel/instrumentation.test.ts
@@ -1,5 +1,9 @@
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { initOpenTelemetry, shutdownOpenTelemetry } from './instrumentation';
+import { initApp } from '../app';
+import { loadTestConfig } from '../config/loader';
+import express from 'express';
+import request from 'supertest';
 
 describe('Instrumentation', () => {
   const OLD_ENV = process.env;
@@ -39,6 +43,21 @@ describe('Instrumentation', () => {
   test('Only traces', async () => {
     process.env.OTLP_TRACE_ENDPOINT = 'http://localhost:4318/v1/traces';
     initOpenTelemetry();
+    await shutdownOpenTelemetry();
+    expect(sdkSpy).toHaveBeenCalled();
+  });
+
+  test('Both metrics and traces', async () => {
+    process.env.OTLP_METRICS_ENDPOINT = 'http://localhost:4318/v1/metrics';
+    process.env.OTLP_TRACE_ENDPOINT = 'http://localhost:4318/v1/traces';
+    initOpenTelemetry();
+
+    const app = express();
+    const config = await loadTestConfig();
+    await initApp(app, config);
+
+    await request(app).get('/healthcheck').send();
+
     await shutdownOpenTelemetry();
     expect(sdkSpy).toHaveBeenCalled();
   });
