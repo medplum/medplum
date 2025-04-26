5453b2257cc5a64a8829f8b73609679cffd92e8e
Matt Long <ml@medplum.com>
2025-04-26 09:55:21 -0700

    Combine system, system+value, value cols

--- packages/server/src/fhir/repo.ts
+++ packages/server/src/fhir/repo.ts
@@ -1366,8 +1366,10 @@ export class Repository extends FhirRepository<PoolClient> implements Disposable
    * 3. 04/09/25 - Added columns for `token-column` search strategy (https://github.com/medplum/medplum/pull/6291)
    * 4. 04/25/25 - Consider `resource.id` in lookup table batch reindex (https://github.com/medplum/medplum/pull/6479)
    * 5. 04/29/25 - Added `status` param for `Flag` resources (https://github.com/medplum/medplum/pull/6500)
+   * 6. 04/25/25 - Added columns per token search parameter (TODO)
+   *
    */
-  static readonly VERSION: number = 5;
+  static readonly VERSION: number = 6;
 
   private buildResourceRow(resource: Resource): Record<string, any> {
     const resourceType = resource.resourceType;
