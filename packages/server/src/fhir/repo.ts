8e494a4d0b7738802ac4685112b8092298fa584a
Matt Long <mattlong@users.noreply.github.com>
2025-05-02 12:42:12 -0700

    Project.systemSetting to disable token table writes (#6532)
    * Project.systemSetting to disable writing to token tables
    
    * Tests for reindex by super admin as well
    
    * Test  coverage

--- packages/server/src/fhir/repo.ts
+++ packages/server/src/fhir/repo.ts
@@ -115,6 +115,7 @@ import {
 import { tryGetRequestContext } from '../context';
 import { buildTokenColumns } from './token-column';
 import { isLegacyTokenColumnSearchParameter, TokenColumnsFeature } from './tokens';
+import { TokenTable } from './lookups/token';
 
 const transactionAttempts = 2;
 const retryableTransactionErrorCodes = ['40001'];
@@ -981,6 +982,10 @@ export class Repository extends FhirRepository<PoolClient> implements Disposable
    * @param resources - The resource(s) to reindex.
    */
   async reindexResources<T extends Resource>(conn: PoolClient, resources: WithId<T>[]): Promise<void> {
+    if (!this.isSuperAdmin()) {
+      throw new OperationOutcomeError(forbidden);
+    }
+
     // Since the page size could be relatively large (1k+), preferring a simple for loop with re-used variables
     // eslint-disable-next-line @typescript-eslint/prefer-for-of
     for (let i = 0; i < resources.length; i++) {
@@ -1779,6 +1784,17 @@ export class Repository extends FhirRepository<PoolClient> implements Disposable
     return undefined;
   }
 
+  private disableTokenTableWrites: boolean | undefined;
+  private skipTokenTableWrite(): boolean {
+    if (this.disableTokenTableWrites === undefined) {
+      const project = this.currentProject();
+      const maybeWriteBoolean = project?.systemSetting?.find((s) => s.name === 'disableTokenTableWrites')?.valueBoolean;
+      // If the Project.systemSetting exists, use its value. Otherwise, default to false
+      this.disableTokenTableWrites = maybeWriteBoolean ?? false;
+    }
+    return this.disableTokenTableWrites;
+  }
+
   /**
    * Writes resources values to the lookup tables.
    * @param client - The database client inside the transaction.
@@ -1787,6 +1803,9 @@ export class Repository extends FhirRepository<PoolClient> implements Disposable
    */
   private async writeLookupTables(client: PoolClient, resource: WithId<Resource>, create: boolean): Promise<void> {
     for (const lookupTable of lookupTables) {
+      if (lookupTable instanceof TokenTable && this.skipTokenTableWrite()) {
+        continue;
+      }
       await lookupTable.indexResource(client, resource, create);
     }
   }
@@ -1797,6 +1816,9 @@ export class Repository extends FhirRepository<PoolClient> implements Disposable
     create: boolean
   ): Promise<void> {
     for (const lookupTable of lookupTables) {
+      if (lookupTable instanceof TokenTable && this.skipTokenTableWrite()) {
+        continue;
+      }
       await lookupTable.batchIndexResources(client, resources, create);
     }
   }
