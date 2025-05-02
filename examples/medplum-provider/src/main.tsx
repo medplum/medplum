717d8e070514be424c1790bc709ce27141c49fd5
Reshma Khilnani <reshma.khilnani@gmail.com>
2025-05-02 16:41:27 -0700

    Uncomment styles (#6526)
    * Uncomment styles
    
    * Fixed react imports
    
    ---------
    
    Co-authored-by: Cody Ebberson <cody@ebberson.com>

--- examples/medplum-provider/src/main.tsx
+++ examples/medplum-provider/src/main.tsx
@@ -2,7 +2,7 @@ import { MantineProvider, createTheme } from '@mantine/core';
 import '@mantine/core/styles.css';
 import { MedplumClient } from '@medplum/core';
 import { MedplumProvider } from '@medplum/react';
-// import '@medplum/react/styles.css';
+import '@medplum/react/styles.css';
 import { Notifications } from '@mantine/notifications';
 import '@mantine/notifications/styles.css';
 import { StrictMode } from 'react';
