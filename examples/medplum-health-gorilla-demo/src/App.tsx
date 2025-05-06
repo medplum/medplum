9d3d92f64bf59e7961d9b55ebf61995952b9f2e7
finnbergquist <finnyb38@gmail.com>
2025-05-06 12:03:43 -0700

    Health Gorilla env restructure (#6549)
    * .env file for environment variables
    
    Also a general restructure of pages to follow patterns of the other example apps
    
    * Update README.md

--- examples/medplum-health-gorilla-demo/src/App.tsx
+++ examples/medplum-health-gorilla-demo/src/App.tsx
@@ -1,12 +1,12 @@
-import { AppShell, Container, Logo, Panel, useMedplum } from '@medplum/react';
+import { AppShell, Container, Logo, Panel, useMedplum, useMedplumProfile } from '@medplum/react';
 import { Link, Route, Routes } from 'react-router';
-import { SignInPage } from './SignInPage';
-import { HomePage } from './HomePage';
+import { SignInPage } from './pages/SignInPage';
+import { HomePage } from './pages/HomePage';
 import { Title } from '@mantine/core';
 
 export function App(): JSX.Element {
   const medplum = useMedplum();
-
+  const profile = useMedplumProfile();
   if (medplum.isLoading()) {
     return <div>Loading...</div>;
   }
@@ -14,7 +14,7 @@ export function App(): JSX.Element {
   return (
     <AppShell logo={<Logo size={24} />}>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={profile ? <HomePage /> : <SignInPage />} />
         <Route path="/signin" element={<SignInPage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
--- examples/medplum-health-gorilla-demo/src/main.tsx
+++ examples/medplum-health-gorilla-demo/src/main.tsx
@@ -8,10 +8,10 @@ import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import { createTheme, MantineProvider } from '@mantine/core';
 import { createBrowserRouter, RouterProvider } from 'react-router';
+import { getConfig } from './config';
 
 const medplum = new MedplumClient({
-  // Uncomment this line to use a local Medplum server
-  // baseUrl: 'http://localhost:8103/',
+  baseUrl: getConfig().baseUrl,
   cacheTime: 60000,
   autoBatchTime: 100,
   onUnauthenticated: () => {
