f022226f5d6ef6ca6ca3ad3fa361f30c8907c32b
Derrick Farris <derrick@medplum.com>
2025-05-06 17:05:44 -0700

    fix(agent): update all ref to files w/o full version

--- scripts/build-agent-sea-win64.sh
+++ scripts/build-agent-sea-win64.sh
@@ -5,6 +5,8 @@ set -e
 
 # Get the current version number
 export MEDPLUM_VERSION=$(node -p "require('./package.json').version")
+# Construct full version, including the git shorthash
+export MEDPLUM_FULL_VERSION="$MEDPLUM_VERSION-$MEDPLUM_GIT_SHORTHASH"
 
 # Move into packages/agent
 pushd packages/agent
@@ -23,7 +25,7 @@ npm run build
 node --experimental-sea-config sea-config.json
 
 # Copy the local node binary
-node -e "require('fs').copyFileSync(process.execPath, 'dist/medplum-agent-$MEDPLUM_VERSION-win64.exe')" 
+node -e "require('fs').copyFileSync(process.execPath, 'dist/medplum-agent-$MEDPLUM_FULL_VERSION-win64.exe')" 
 
 # Remove signature from binary
 
@@ -31,11 +33,11 @@ echo "Removing signature from Node binary..."
 # If no defined signtool path, try to find it in PATH
 if [[ -z "$SIGNTOOL_PATH" ]]; then
   echo "Using signtool from PATH"
-  signtool remove -s dist/medplum-agent-$MEDPLUM_VERSION-win64.exe
+  signtool remove -s dist/medplum-agent-$MEDPLUM_FULL_VERSION-win64.exe
 else
   echo "Using signtool located at $SIGNTOOL_PATH"
   # Otherwise use the defined path from the env var
-  "$SIGNTOOL_PATH" remove -s dist/medplum-agent-$MEDPLUM_VERSION-win64.exe
+  "$SIGNTOOL_PATH" remove -s dist/medplum-agent-$MEDPLUM_FULL_VERSION-win64.exe
 fi
 echo "Signature successfully removed."
 
@@ -46,6 +48,6 @@ echo "Signature successfully removed."
 # https://stackoverflow.com/a/52963704
 
 # Inject blob into binary
-npx postject dist/medplum-agent-$MEDPLUM_VERSION-win64.exe NODE_SEA_BLOB sea-prep.blob --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2 
+npx postject dist/medplum-agent-$MEDPLUM_FULL_VERSION-win64.exe NODE_SEA_BLOB sea-prep.blob --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2 
 
 popd
