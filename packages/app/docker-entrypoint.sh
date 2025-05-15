16ff23fe04e7e6ac94383c84f7f94877dbf75aad
Derrick Farris <derrick@medplum.com>
2025-05-15 08:37:38 -0700

    fix(app/docker): unescape $ in entryfile replacement (#6628)

--- packages/app/docker-entrypoint.sh
+++ packages/app/docker-entrypoint.sh
@@ -15,12 +15,12 @@ set -e
 # Update the app config
 # Recursively apply to all text files in the app dist directory
 find "/usr/share/nginx/html/assets" -type f -exec sed -i \
-  -e "s|__MEDPLUM_BASE_URL__|\${MEDPLUM_BASE_URL}|g" \
-  -e "s|__MEDPLUM_CLIENT_ID__|\${MEDPLUM_CLIENT_ID}|g" \
-  -e "s|__GOOGLE_CLIENT_ID__|\${GOOGLE_CLIENT_ID}|g" \
-  -e "s|__RECAPTCHA_SITE_KEY__|\${RECAPTCHA_SITE_KEY}|g" \
-  -e "s|__MEDPLUM_REGISTER_ENABLED__|\${MEDPLUM_REGISTER_ENABLED}|g" \
-  -e "s|__MEDPLUM_AWS_TEXTRACT_ENABLED__|\${MEDPLUM_AWS_TEXTRACT_ENABLED}|g" \
+  -e "s|__MEDPLUM_BASE_URL__|${MEDPLUM_BASE_URL}|g" \
+  -e "s|__MEDPLUM_CLIENT_ID__|${MEDPLUM_CLIENT_ID}|g" \
+  -e "s|__GOOGLE_CLIENT_ID__|${GOOGLE_CLIENT_ID}|g" \
+  -e "s|__RECAPTCHA_SITE_KEY__|${RECAPTCHA_SITE_KEY}|g" \
+  -e "s|__MEDPLUM_REGISTER_ENABLED__|${MEDPLUM_REGISTER_ENABLED}|g" \
+  -e "s|__MEDPLUM_AWS_TEXTRACT_ENABLED__|${MEDPLUM_AWS_TEXTRACT_ENABLED}|g" \
   {} \;
 
 echo "Environment variable replacement complete."
