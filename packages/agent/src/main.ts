b314072a3d3c7c501ca7e38d18d1e1bebb24665b
Derrick Farris <derrick@medplum.com>
2025-05-16 10:26:36 -0700

    debug: output logs for service stopping step

--- packages/agent/src/main.ts
+++ packages/agent/src/main.ts
@@ -1,10 +1,13 @@
 import { MEDPLUM_VERSION, normalizeErrorString } from '@medplum/core';
 import { execSync } from 'node:child_process';
-import { existsSync } from 'node:fs';
+import { appendFileSync, existsSync, writeFileSync } from 'node:fs';
+import path, { dirname } from 'node:path';
 import { agentMain } from './agent-main';
 import { createPidFile, registerAgentCleanup } from './pid';
 import { upgraderMain } from './upgrader';
 import { UPGRADE_MANIFEST_PATH } from './upgrader-utils';
+
+const TEMP_LOG_FILE = path.join(dirname(UPGRADE_MANIFEST_PATH), `stop-service-logs-${new Date().toISOString()}.txt`);
 
 export async function main(argv: string[]): Promise<void> {
   registerAgentCleanup();
@@ -12,9 +15,9 @@ export async function main(argv: string[]): Promise<void> {
     createPidFile('medplum-agent-upgrader');
     await upgraderMain(argv);
   } else if (argv[2] === '--stop-old-services') {
-    const output = execSync('cmd.exe /c sc query type= service state= all | findstr /i "SERVICE_NAME.*MedplumAgent"', {
-      windowsHide: true,
-    });
+    writeFileSync(TEMP_LOG_FILE, '', { flag: 'w+' });
+    const output = execSync('cmd.exe /c sc query type= service state= all | findstr /i "SERVICE_NAME.*MedplumAgent"');
+    appendFileSync(TEMP_LOG_FILE, `${output}\r\n`, { encoding: 'utf-8' });
     const allAgentServices = output.toString().trim().split('\r\n');
     const servicesToStop =
       argv[3] === '--all'
@@ -24,16 +27,22 @@ export async function main(argv: string[]): Promise<void> {
       // We try to stop the service and continue even if it fails
       try {
         execSync(`net stop ${serviceName}`);
+        appendFileSync(TEMP_LOG_FILE, `${serviceName} stopped\r\n`, { encoding: 'utf-8' });
         console.log(`${serviceName} stopped`);
       } catch (err) {
+        appendFileSync(TEMP_LOG_FILE, `Failed to stop service: ${serviceName}\r\n`, { encoding: 'utf-8' });
+        appendFileSync(TEMP_LOG_FILE, `${normalizeErrorString(err)}\r\n`, { encoding: 'utf-8' });
         console.error(`Failed to stop service: ${serviceName}`);
         console.error(normalizeErrorString(err));
       }
       // We try to delete the service even if stopping it failed
       try {
         execSync(`sc.exe delete ${serviceName}`);
-        console.log(`${serviceName} stopped`);
+        appendFileSync(TEMP_LOG_FILE, `${serviceName} deleted\r\n`, { encoding: 'utf-8' });
+        console.log(`${serviceName} deleted`);
       } catch (err) {
+        appendFileSync(TEMP_LOG_FILE, `Failed to delete service: ${serviceName}\r\n`, { encoding: 'utf-8' });
+        appendFileSync(TEMP_LOG_FILE, `${normalizeErrorString(err)}\r\n`, { encoding: 'utf-8' });
         console.error(`Failed to delete service: ${serviceName}`);
         console.error(normalizeErrorString(err));
       }
