b3628cdbb5177df2dfe6233d59d5c0f7cf0d7e97
Matt Long <ml@medplum.com>
2025-05-22 10:56:57 -0700

    TEMP: env var to control token strategy

--- packages/server/src/fhir/search.ts
+++ packages/server/src/fhir/search.ts
@@ -1810,6 +1810,10 @@ function getCanonicalUrl(resource: Resource): string | undefined {
 }
 
 export function readFromTokenColumns(repo: Repository): typeof TokenColumnsFeature.read {
+  if (process.env['READ_FROM_TOKEN_COLUMNS']) {
+    return TokenColumnsFeature.read;
+  }
+
   const project = repo.currentProject();
   const maybeSystemSetting = project?.systemSetting?.find((s) => s.name === 'searchTokenColumns');
 
