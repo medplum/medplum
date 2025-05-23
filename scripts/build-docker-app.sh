8aab9947191493563929928944ce071b98ff1929
Derrick Farris <derrick@medplum.com>
2025-05-22 19:56:48 -0700

    fix: fix comment

--- scripts/build-docker-app.sh
+++ scripts/build-docker-app.sh
@@ -8,7 +8,7 @@ fi
 # Only build app if APP_DOCKERHUB_REPOSITORY was passed
 if [[ -z "${APP_DOCKERHUB_REPOSITORY}" ]]; then
   echo "APP_DOCKERHUB_REPOSITORY env var not configured. Skipping..."
-  # Don't fail if no APP_DOCKER_REPOSITORY present
+  # Don't fail if no APP_DOCKERHUB_REPOSITORY present
   exit 0
 fi
 
