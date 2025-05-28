0c9e411fa40e9e01624cd2b40bb6d4ae9ee27d88
finnbergquist <finnyb38@gmail.com>
2025-05-28 16:11:05 -0700

    Webhook handler processing of Binary Bot resp

--- packages/server/src/fhir/response.ts
+++ packages/server/src/fhir/response.ts
@@ -1,6 +1,6 @@
 import { ContentType, concatUrls, getStatus, isCreated } from '@medplum/core';
 import { FhirResponseOptions } from '@medplum/fhir-router';
-import { OperationOutcome, Resource } from '@medplum/fhirtypes';
+import { Binary, OperationOutcome, Resource } from '@medplum/fhirtypes';
 import { Request, Response } from 'express';
 import { getConfig } from '../config/loader';
 import { getAuthenticatedContext } from '../context';
@@ -29,6 +29,16 @@ export function sendResponseHeaders(_req: Request, res: Response, outcome: Opera
   res.status(getStatus(outcome));
 }
 
+export async function sendBinaryResponse(res: Response, binaryResource: Binary): Promise<void> {
+    res.contentType(binaryResource.contentType as string);
+    if (binaryResource.data) {
+      res.send(Buffer.from(binaryResource.data, 'base64'));
+    } else {
+      const stream = await getBinaryStorage().readBinary(binaryResource);
+    stream.pipe(res);
+  }
+}
+
 export async function sendFhirResponse(
   req: Request,
   res: Response,
@@ -45,13 +55,7 @@ export async function sendFhirResponse(
     // When the read request has some other type in the Accept header,
     // then the content should be returned with the content type stated in the resource in the Content-Type header.
     // E.g. if the content type in the resource is "application/pdf", then the content should be returned as a PDF directly.
-    res.contentType(body.contentType as string);
-    if (body.data) {
-      res.send(Buffer.from(body.data, 'base64'));
-    } else {
-      const stream = await getBinaryStorage().readBinary(body);
-      stream.pipe(res);
-    }
+    await sendBinaryResponse(res, body as Binary);
     return;
   }
 
