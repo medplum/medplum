5d218fca465be7f11d398d1fcf11b7959dbbc560
finnbergquist <finnyb38@gmail.com>
2025-05-30 09:30:57 -0700

    Webhook handler processing of Binary Bot response (#6685)
    * Webhook handler processing of Binary Bot resp
    
    * Using same alias of .type() & .contentType() methods for consistency
    
    * [autofix.ci] apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- packages/server/src/webhook/routes.ts
+++ packages/server/src/webhook/routes.ts
@@ -1,10 +1,11 @@
 import { allOk, badRequest, getStatus, isOperationOutcome } from '@medplum/core';
-import { Bot, ProjectMembership, Reference } from '@medplum/fhirtypes';
+import { Binary, Bot, ProjectMembership, Reference } from '@medplum/fhirtypes';
 import { Request, Response, Router } from 'express';
 import { asyncWrap } from '../async';
 import { executeBot, getResponseBodyFromResult, getResponseContentType } from '../fhir/operations/execute';
 import { sendOutcome } from '../fhir/outcomes';
 import { getSystemRepo } from '../fhir/repo';
+import { sendBinaryResponse } from '../fhir/response';
 
 /**
  * Allowed signature headers are:
@@ -70,7 +71,11 @@ export const webhookHandler = asyncWrap(async (req: Request, res: Response) => {
   const responseBody = getResponseBodyFromResult(result);
   const outcome = result.success ? allOk : badRequest(result.logResult);
 
-  res.status(getStatus(outcome)).type(getResponseContentType(req)).send(responseBody);
+  if (result.returnValue?.resourceType === 'Binary') {
+    await sendBinaryResponse(res, result.returnValue as Binary);
+  } else {
+    res.status(getStatus(outcome)).contentType(getResponseContentType(req)).send(responseBody);
+  }
 });
 
 export const webhookRouter = Router();
