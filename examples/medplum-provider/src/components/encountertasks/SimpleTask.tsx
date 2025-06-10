e416e00f94968cd03006ad3e55b3ac9c472d4cdd
David Yanez <58953476+techdavidy@users.noreply.github.com>
2025-06-10 15:18:46 -0700

    Update Task management UI (#6717)
    * Update UI and autosave in Encounter
    
    * clean up
    
    * clean up
    
    * clean up
    
    * [autofix.ci] apply automated fixes
    
    * clean up
    
    * update navigation
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- examples/medplum-provider/src/components/encountertasks/SimpleTask.tsx
+++ examples/medplum-provider/src/components/encountertasks/SimpleTask.tsx
@@ -17,7 +17,14 @@ export const SimpleTask = ({ task }: SimpleTaskProps): JSX.Element => {
         )}
         <Text>{task.description}</Text>
         {task.focus?.reference?.startsWith('ServiceRequest/') && (
-          <Button component="a" href={`/${task.focus.reference}`} target="_blank">
+          <Button
+            mt="xs"
+            component="a"
+            href={`/${task.focus.reference}`}
+            target="_blank"
+            w="fit-content"
+            variant="outline"
+          >
             View Service Request
           </Button>
         )}
--- examples/medplum-provider/src/components/encountertasks/TaskPanel.tsx
+++ examples/medplum-provider/src/components/encountertasks/TaskPanel.tsx
@@ -1,15 +1,15 @@
 import { Card, Stack } from '@mantine/core';
-import { showNotification } from '@mantine/notifications';
-import { createReference, getReferenceString, normalizeErrorString } from '@medplum/core';
-import { Annotation, QuestionnaireResponse, Task } from '@medplum/fhirtypes';
-import { useMedplum, useMedplumProfile } from '@medplum/react';
-import { IconCircleOff } from '@tabler/icons-react';
-import { JSX, useRef } from 'react';
+import { getReferenceString } from '@medplum/core';
+import { QuestionnaireResponse, Task } from '@medplum/fhirtypes';
+import { useMedplum } from '@medplum/react';
+import { JSX } from 'react';
 import { useNavigate } from 'react-router';
 import { SAVE_TIMEOUT_MS } from '../../config/constants';
 import { SimpleTask } from './SimpleTask';
 import { TaskQuestionnaireForm } from './TaskQuestionnaireForm';
 import { TaskStatusPanel } from './TaskStatusPanel';
+import { useDebouncedCallback } from '@mantine/hooks';
+import { showErrorNotification } from '../../utils/notifications';
 
 interface TaskPanelProps {
   task: Task;
@@ -20,30 +20,14 @@ export const TaskPanel = (props: TaskPanelProps): JSX.Element => {
   const { task, onUpdateTask } = props;
   const navigate = useNavigate();
   const medplum = useMedplum();
-  const author = useMedplumProfile();
-  const saveTimeoutRef = useRef<ReturnType<typeof setTimeout>>(undefined);
 
   const onActionButtonClicked = async (): Promise<void> => {
-    if (task.status === 'ready' || task.status === 'requested') {
-      // Task status is Ready or Requested. Action will mark as complete.
-      await updateTaskStatus({ ...task, status: 'completed' }, medplum, onUpdateTask);
-    } else {
-      // Fallback navigation to Task details.
-      navigate(`Task/${task.id}`)?.catch(console.error);
-    }
+    navigate(`Task/${task.id}`)?.catch(console.error);
   };
 
-  const onChangeResponse = (response: QuestionnaireResponse): void => {
-    saveQuestionnaireResponse(task, response);
-  };
-
-  const saveQuestionnaireResponse = (task: Task, response: QuestionnaireResponse): void => {
-    try {
-      if (saveTimeoutRef.current) {
-        clearTimeout(saveTimeoutRef.current);
-      }
-
-      saveTimeoutRef.current = setTimeout(async () => {
+  const saveQuestionnaireResponse = useDebouncedCallback(
+    async (task: Task, response: QuestionnaireResponse): Promise<void> => {
+      try {
         if (response.id) {
           await medplum.updateResource<QuestionnaireResponse>(response);
         } else {
@@ -59,59 +43,36 @@ export const TaskPanel = (props: TaskPanelProps): JSX.Element => {
           });
           onUpdateTask(updatedTask);
         }
-      }, SAVE_TIMEOUT_MS);
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  const onAddNote = async (note: string): Promise<void> => {
-    const newNote: Annotation = {
-      text: note,
-      authorReference: author && createReference(author),
-      time: new Date().toISOString(),
-    };
-
-    const taskNotes = task?.note || [];
-    taskNotes.push(newNote);
-    const updatedTask: Task = { ...task, note: taskNotes };
-    await updateTaskStatus(updatedTask, medplum, onUpdateTask);
-  };
+      } catch (err) {
+        showErrorNotification(err);
+      }
+    },
+    SAVE_TIMEOUT_MS
+  );
 
   const onChangeStatus = async (status: Task[`status`]): Promise<void> => {
-    const updatedTask: Task = { ...task, status: status };
-    await updateTaskStatus(updatedTask, medplum, onUpdateTask);
+    try {
+      const response = await medplum.updateResource({ ...task, status: status });
+      onUpdateTask(response);
+    } catch (err) {
+      showErrorNotification(err);
+    }
   };
 
   return (
     <Card withBorder shadow="sm" p={0}>
       <Stack gap="xs">
         {task.input && task.input[0]?.type?.text === 'Questionnaire' && task.input[0]?.valueReference ? (
-          <TaskQuestionnaireForm key={task.id} task={task} onChangeResponse={onChangeResponse} />
+          <TaskQuestionnaireForm
+            key={task.id}
+            task={task}
+            onChangeResponse={(response) => saveQuestionnaireResponse(task, response)}
+          />
         ) : (
           <SimpleTask key={task.id} task={task} />
         )}
-        <TaskStatusPanel
-          task={task}
-          onActionButtonClicked={onActionButtonClicked}
-          onAddNote={onAddNote}
-          onChangeStatus={onChangeStatus}
-        />
+        <TaskStatusPanel task={task} onActionButtonClicked={onActionButtonClicked} onChangeStatus={onChangeStatus} />
       </Stack>
     </Card>
   );
-};
-
-const updateTaskStatus = async (task: Task, medplum: any, onUpdateTask: (task: Task) => void): Promise<void> => {
-  try {
-    const response = await medplum.updateResource(task);
-    onUpdateTask(response);
-  } catch (err) {
-    showNotification({
-      color: 'red',
-      icon: <IconCircleOff />,
-      title: 'Error',
-      message: normalizeErrorString(err),
-    });
-  }
 };
