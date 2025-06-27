69c8d7c646718feaf1a9848f5c31bebd34ca0320
David Yanez <58953476+techdavidy@users.noreply.github.com>
2025-06-27 11:12:07 -0700

    Provider - use threads/topics to fetch messages (#6883)
    * use threads/topics to fetch messages
    
    * Graphql query
    
    * [autofix.ci] apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- examples/medplum-provider/src/components/messages/ChatList.tsx
+++ examples/medplum-provider/src/components/messages/ChatList.tsx
@@ -1,30 +1,75 @@
 import { Stack } from '@mantine/core';
-import { Communication, Patient, Reference } from '@medplum/fhirtypes';
-import { JSX } from 'react';
+import { Communication } from '@medplum/fhirtypes';
+import { JSX, useEffect, useState } from 'react';
 import { ChatListItem } from './ChatListItem';
+import { createReference, getReferenceString } from '@medplum/core';
+import { useMedplum } from '@medplum/react';
+import { showErrorNotification } from '../../utils/notifications';
 
 interface ChatListProps {
   communications: Communication[];
-  selectedPatient: Reference<Patient>;
-  onClick: (patient: Reference<Patient>) => void;
+  selectedCommunication: Communication | undefined;
+  onClick: (communication: Communication) => void;
 }
 
 export const ChatList = (props: ChatListProps): JSX.Element => {
-  const { communications, selectedPatient, onClick } = props;
+  const { communications, selectedCommunication, onClick } = props;
+  const medplum = useMedplum();
+  const [lastCommunications, setLastCommunications] = useState<
+    { id: string; communication: Communication }[] | undefined
+  >(undefined);
+
+  useEffect(() => {
+    const fetchLastCommunications = async (): Promise<void> => {
+      const communicationReferences = communications.map((communication) => createReference(communication));
+      const allCommunications = await medplum.graphql(`
+        query {
+          CommunicationList(
+            _sort: "-sent"
+            _filter: "part-of eq ${communicationReferences.map((ref) => getReferenceString(ref)).join(' or part-of eq ')}"
+          ) {
+            id
+            partOf {
+              reference
+            }
+            payload {
+              contentString
+            }
+            status
+          }
+        }
+      `);
+      const lastCommunications = allCommunications.data.CommunicationList.map((communication: Communication) => {
+        return {
+          id: communication.partOf?.[0]?.reference?.split('/')[1],
+          communication: communication,
+        };
+      });
+
+      setLastCommunications(lastCommunications);
+    };
+
+    fetchLastCommunications().catch((error) => {
+      showErrorNotification(error);
+    });
+  }, [communications, medplum]);
 
   return (
     <Stack gap={0} p="xs">
       {communications.map((communication: Communication) => {
-        const patient = communication.subject as Reference<Patient>;
-        const _isSelected = selectedPatient.reference === patient.reference;
+        const lastCommunication = lastCommunications?.find((lc) => lc.id === communication.id)?.communication;
+        if (!lastCommunication) {
+          return null;
+        }
+        const _isSelected = selectedCommunication?.id === communication.id;
         return (
           <ChatListItem
             key={communication.id}
-            patient={patient}
-            communication={communication}
+            topic={communication}
+            lastCommunication={lastCommunication}
             isSelected={_isSelected}
             onClick={() => {
-              onClick(patient);
+              onClick(communication);
             }}
           />
         );
