4aa04d86e5faf71e9727585aa213dbbb8c97b0b4
David Yanez <me@davidyanez.com>
2025-07-02 16:12:17 -0700

    Fix css issues

--- examples/medplum-provider/src/pages/messages/MessagesPage.tsx
+++ examples/medplum-provider/src/pages/messages/MessagesPage.tsx
@@ -1,10 +1,12 @@
-import { ScrollArea, Text, Loader, Grid, Paper, Group } from '@mantine/core';
+import { ScrollArea, Text, Loader, Paper, Group, Stack, Divider, Flex, Button } from '@mantine/core';
 import { Communication, Patient, Reference } from '@medplum/fhirtypes';
 import { useMedplum, PatientSummary, ThreadChat } from '@medplum/react';
 import { JSX, useEffect, useState } from 'react';
 import { getReferenceString } from '@medplum/core';
 import { ChatList } from '../../components/messages/ChatList';
 import { showErrorNotification } from '../../utils/notifications';
+import { IconTrash } from '@tabler/icons-react';
+import classes from './MessagesPage.module.css';
 
 /**
  * Messages page that matches the Home page layout but without the patient list.
@@ -40,50 +42,71 @@ export function MessagesPage(): JSX.Element {
   }, [medplum, selectedThread]);
 
   return (
-    <Grid style={{ display: 'flex', height: 'calc(100vh - 60px)' }}>
-      {/* Left sidebar - Messages list */}
-      <Grid.Col span={3} h="100%">
-        <Paper h="100%">
-          <ScrollArea h="100%" scrollbarSize={10} type="hover" scrollHideDelay={250}>
-            <Group p="md">
-              <Text fz="h4" fw={800} truncate>
-                Messages
-              </Text>
-            </Group>
-            {loading ? (
-              <Group h="100%" align="center" justify="center">
-                <Loader />
+    <div className={classes.container}>
+      <Flex h="100%">
+        {/* Left sidebar - Messages list */}
+        <Flex direction="column" w="25%" h="100%">
+          <Paper h="100%">
+            <ScrollArea h="100%" scrollbarSize={10} type="hover" scrollHideDelay={250}>
+              <Group p="md">
+                <Text fz="h4" fw={800} truncate>
+                  Messages
+                </Text>
               </Group>
-            ) : (
-              threadMessages.length > 0 && (
-                <ChatList
-                  communications={threadMessages}
-                  selectedCommunication={selectedThread}
-                  onClick={(thread) => {
-                    setSelectedThread(thread);
-                  }}
-                />
-              )
-            )}
-          </ScrollArea>
-        </Paper>
-      </Grid.Col>
+              {loading ? (
+                <Group h="100%" align="center" justify="center">
+                  <Loader />
+                </Group>
+              ) : (
+                threadMessages.length > 0 && (
+                  <ChatList
+                    communications={threadMessages}
+                    selectedCommunication={selectedThread}
+                    onClick={(thread) => {
+                      setSelectedThread(thread);
+                    }}
+                  />
+                )
+              )}
+            </ScrollArea>
+          </Paper>
+        </Flex>
 
-      {/* Main chat area */}
-      <Grid.Col span={6} h="100%">
-        {selectedThread && (
-          <ThreadChat key={`${getReferenceString(selectedThread)}`} title={'Messages'} thread={selectedThread} />
-        )}
-      </Grid.Col>
+        {/* Main chat area */}
+        <Flex direction="column" w="50%" h="100%">
+          {selectedThread && (
+            <Paper h="100%">
+              <Stack h="100%" gap={0}>
+                <Flex h={64} align="center" justify="space-between" p="md">
+                  <Text fz="h4" fw={800} truncate>
+                    {selectedThread.topic?.text ?? 'Messages'}
+                  </Text>
+                  <Button variant="outline" size="xs" leftSection={<IconTrash />}>
+                    Delete
+                  </Button>
+                </Flex>
+                <Divider />
+                <Flex direction="column" h="100%">
+                  <ThreadChat
+                    key={`${getReferenceString(selectedThread)}`}
+                    title={'Messages'}
+                    thread={selectedThread}
+                  />
+                </Flex>
+              </Stack>
+            </Paper>
+          )}
+        </Flex>
 
-      {/* Right sidebar - Patient summary */}
-      <Grid.Col span={3} h="100%">
-        {selectedThread && (
-          <ScrollArea p={0} h="100%" scrollbarSize={10} type="hover" scrollHideDelay={250}>
-            <PatientSummary key={selectedThread.id} patient={selectedThread.subject as Reference<Patient>} />
-          </ScrollArea>
-        )}
-      </Grid.Col>
-    </Grid>
+        {/* Right sidebar - Patient summary */}
+        <Flex direction="column" w="25%" h="100%">
+          {selectedThread && (
+            <ScrollArea p={0} h="100%" scrollbarSize={10} type="hover" scrollHideDelay={250}>
+              <PatientSummary key={selectedThread.id} patient={selectedThread.subject as Reference<Patient>} />
+            </ScrollArea>
+          )}
+        </Flex>
+      </Flex>
+    </div>
   );
 }
