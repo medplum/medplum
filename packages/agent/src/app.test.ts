a5b6eb179a374123854864ed6674772be6098802
Derrick Farris <derrick@medplum.com>
2025-07-08 16:26:32 -0700

    test(agent): mock `spawnSync`

--- packages/agent/src/app.test.ts
+++ packages/agent/src/app.test.ts
@@ -2149,6 +2149,7 @@ describe('App', () => {
 
       const platformSpy = jest.spyOn(os, 'platform').mockImplementation(jest.fn(() => 'win32'));
       const fetchSpy = mockFetchForUpgrader();
+      const spawnSyncSpy = jest.spyOn(child_process, 'spawnSync').mockImplementation(jest.fn());
 
       function mockConnectionHandler(socket: Client): void {
         state.mySocket = socket;
@@ -2231,6 +2232,7 @@ describe('App', () => {
 
       platformSpy.mockRestore();
       fetchSpy.mockRestore();
+      spawnSyncSpy.mockRestore();
       console.log = originalConsoleLog;
     });
 
