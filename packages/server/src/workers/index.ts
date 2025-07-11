e232dc6d375da989ea640a7d58c439a5827baa0c
Matt Long <ml@medplum.com>
2025-07-11 13:09:00 -0700

    clean up

--- packages/server/src/workers/index.ts
+++ packages/server/src/workers/index.ts
@@ -1,7 +1,6 @@
 import { BackgroundJobContext, WithId } from '@medplum/core';
 import { Resource } from '@medplum/fhirtypes';
 import { MedplumServerConfig } from '../config/types';
-import { Repository } from '../fhir/repo';
 import { getLogger, globalLogger } from '../logger';
 import { initBatchWorker } from './batch';
 import { addCronJobs, initCronWorker } from './cron';
@@ -42,13 +41,11 @@ export async function closeWorkers(): Promise<void> {
 
 /**
  * Adds all background jobs for a given resource.
- * @param repo - The repository to use for adding background jobs.
  * @param resource - The resource that was created or updated.
  * @param previousVersion - The previous version of the resource, if available.
  * @param context - The background job context.
  */
 export async function addBackgroundJobs(
-  repo: Repository,
   resource: WithId<Resource>,
   previousVersion: Resource | undefined,
   context: BackgroundJobContext
