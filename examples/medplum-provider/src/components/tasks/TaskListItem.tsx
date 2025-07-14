b03f4c1ea9f843d7677ba27d269fe93f6ddbf429
David Yanez <me@davidyanez.com>
2025-07-14 14:17:59 -0700

    clean up

---
+++ examples/medplum-provider/src/components/tasks/TaskListItem.tsx
@@ -0,0 +1,54 @@
+import { Group, Stack, Text } from '@mantine/core';
+import { formatDate, formatHumanName, getDisplayString } from '@medplum/core';
+import { HumanName, Patient, Practitioner, Reference, Task } from '@medplum/fhirtypes';
+import { JSX } from 'react';
+import classes from './TaskListItem.module.css';
+import cx from 'clsx';
+import { useResource } from '@medplum/react';
+
+interface TaskListItemProps {
+  task: Task;
+  selectedTask: Task | undefined;
+  onClick: () => void;
+}
+
+export function TaskListItem(props: TaskListItemProps): JSX.Element {
+  const { task, selectedTask, onClick } = props;
+  const isSelected = selectedTask?.id === task.id;
+  const patient = useResource(task.for as Reference<Patient>);
+  const owner = useResource(task.owner as Reference<Practitioner>);
+
+  return (
+    <Group
+      p="xs"
+      align="center"
+      wrap="nowrap"
+      className={cx(classes.contentContainer, {
+        [classes.selected]: isSelected,
+      })}
+      onClick={onClick}
+      
+    >
+      <Stack gap={0}>
+        <Text fw={700} className={classes.content}>
+          {getDisplayString(task)}
+        </Text>
+        {task.restriction?.period && (
+          <Text key={task.id} fw={500}>
+            Due {formatDate(task.restriction?.period?.end)}
+          </Text>
+        )}
+        {patient && (
+          <Text >
+            For: {formatHumanName(patient.name?.[0] as HumanName)}
+          </Text>
+        )}
+        {owner && (
+          <Text size="sm" c="dimmed">
+            Assigned to {formatHumanName(owner.name?.[0] as HumanName)}
+          </Text>
+        )}
+      </Stack>
+    </Group>
+  );
+}
--- examples/medplum-provider/src/pages/tasks/TasksPage.tsx
+++ examples/medplum-provider/src/pages/tasks/TasksPage.tsx
@@ -1,20 +1,23 @@
-import { Flex, Group, Paper, Stack, Text } from '@mantine/core';
+import { Box, Divider, Flex, Paper, SegmentedControl } from '@mantine/core';
 import { JSX, useEffect, useMemo, useState } from 'react';
 import styles from './TasksPage.module.css';
-import { Task } from '@medplum/fhirtypes';
-import { useMedplum, useMedplumProfile } from '@medplum/react';
+import { Patient, Task } from '@medplum/fhirtypes';
+import { PatientSummary, PatientTimeline, useMedplum, useMedplumProfile } from '@medplum/react';
 import { showErrorNotification } from '../../utils/notifications';
-import { createReference, formatDate, getReferenceString, ProfileResource } from '@medplum/core';
+import { createReference, getReferenceString, ProfileResource } from '@medplum/core';
+import { TaskListItem } from '../../components/tasks/TaskListItem';
+import { TaskInfo } from '../../components/tasks/TaskInfo';
 
 export function TasksPage(): JSX.Element {
   const medplum = useMedplum();
   const profile = useMedplumProfile();
   const [tasks, setTasks] = useState<Task[]>([]);
+  const [selectedTask, setSelectedTask] = useState<Task | undefined>(undefined);
+  const [activeTab, setActiveTab] = useState<string>('patient-summary');
   const profileRef = useMemo(() => (profile ? createReference(profile as ProfileResource) : undefined), [profile]);
-  
+
   useEffect(() => {
     const fetchTasks = async (): Promise<void> => {
-
       const searchParams = new URLSearchParams();
       searchParams.append('status:not', 'completed');
       searchParams.append('status:not', 'cancelled');
@@ -24,35 +27,82 @@ export function TasksPage(): JSX.Element {
       }
       const tasks = await medplum.searchResources('Task', searchParams, { cache: 'no-cache' });
       setTasks(tasks);
+      if (tasks.length > 0) {
+        setSelectedTask(tasks[0]);
+      }
     };
 
     fetchTasks().catch(showErrorNotification);
   }, [medplum, profileRef]);
 
+  const handleTaskChange = (task: Task): void => {
+    setSelectedTask(task);
+    setTasks(tasks.map((t) => (t.id === task.id ? task : t)));
+  };
+
+  const handleTabChange = (value: string): void => {
+    setActiveTab(value);
+  };
+
   return (
     <div className={styles.container}>
       <Flex h="100%" w="100%">
         <Flex direction="column" w="25%" h="100%" style={{ borderRight: '1px solid var(--mantine-color-gray-3)' }}>
-          <Paper h="100%">
-
+          <Paper h="100%" p="xs">
             {tasks.map((task) => (
-              <Stack key={task.id} p="md" gap={0}>
-                <Text key={task.id} fw={500}>{task.description ?? task.id}</Text>
-                <Group>
-                  {task.restriction?.period && (
-                    <Text key={task.id} fw={500}>Due {formatDate(task.restriction?.period?.end)}</Text>
-                  )}
-                  
-                </Group>
-                
-              </Stack>
+              <>
+                <TaskListItem
+                  key={task.id}
+                  task={task}
+                  selectedTask={selectedTask}
+                  onClick={() => setSelectedTask(task)}
+                />
+                <Divider />
+              </>
             ))}
-
           </Paper>
         </Flex>
 
-        <Flex direction="column" w="75%" h="100%">
-          <Text fw={500}>Placeholder</Text>
+        <Flex direction="column" w="40%" h="100%">
+          <Paper h="100%" p="md" style={{ overflow: 'auto' }}>
+            {selectedTask && <TaskInfo key={selectedTask.id} task={selectedTask} onTaskChange={handleTaskChange} />}
+          </Paper>
+        </Flex>
+
+        <Flex direction="column" w="35%" h="100%" style={{ borderLeft: '1px solid var(--mantine-color-gray-3)' }}>
+          <Paper h="100%" p="xs">
+            <Box px="md" pb="md">
+              <SegmentedControl
+                value={activeTab}
+                onChange={(value: string) => handleTabChange(value)}
+                data={[
+                  { label: 'Patient Summary', value: 'patient-summary' },
+                  { label: 'Activity Log', value: 'activity-log' },
+                ]}
+                fullWidth
+                radius="md"
+                color="gray"
+                size="md"
+                styles={(theme) => ({
+                  root: {
+                    backgroundColor: theme.colors.gray[1],
+                    borderRadius: theme.radius.md,
+                  },
+                  indicator: {
+                    backgroundColor: theme.white,
+                  },
+                  label: {
+                    fontWeight: 500,
+                    color: theme.colors.dark[9],
+                    padding: `${theme.spacing.xs} ${theme.spacing.md}`,
+                  },
+                })}
+              />
+            </Box>
+
+            {activeTab === 'patient-summary' && <PatientSummary patient={selectedTask?.for as Patient} />}
+            {activeTab === 'activity-log' && <PatientTimeline patient={selectedTask?.for as Patient} />}
+          </Paper>
         </Flex>
       </Flex>
     </div>
