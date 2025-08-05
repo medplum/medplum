6da7ff5583ef0e15b52170a45dca22b876f92e44
Matthew Willer <matt@medplum.com>
2025-08-05 11:50:59 -0700

    Add test cases
    Signed-off-by: Matthew Willer <matt@medplum.com>

--- packages/server/src/fhir/operations/update-user-email.test.ts
+++ packages/server/src/fhir/operations/update-user-email.test.ts
@@ -1,4 +1,4 @@
-import { ContentType, ProfileResource, WithId } from '@medplum/core';
+import { ContentType, createReference, WithId } from '@medplum/core';
 import { ContactPoint, Parameters, Practitioner, Project, User } from '@medplum/fhirtypes';
 import express from 'express';
 import { randomUUID } from 'node:crypto';
@@ -6,6 +6,7 @@ import request from 'supertest';
 import { inviteUser } from '../../admin/invite';
 import { initApp, shutdownApp } from '../../app';
 import { loadTestConfig } from '../../config/loader';
+import { getAuthTokens, tryLogin } from '../../oauth/utils';
 import { createTestProject } from '../../test.setup';
 import { Repository } from '../repo';
 
@@ -14,9 +15,6 @@ describe('User/$update-email', () => {
   let repo: Repository;
   let project: WithId<Project>;
   let accessToken: string;
-  let email: string;
-  let user: WithId<User>;
-  let profile: WithId<ProfileResource>;
 
   beforeAll(async () => {
     const config = await loadTestConfig();
@@ -28,17 +26,6 @@ describe('User/$update-email', () => {
       membership: { admin: true },
       withRepo: true,
     }));
-
-    email = `user+${randomUUID()}@example.com`;
-    ({ user, profile } = await inviteUser({
-      project,
-      email,
-      sendEmail: false,
-      resourceType: 'Practitioner',
-      firstName: 'Test',
-      lastName: 'User',
-      scope: 'project',
-    }));
   });
 
   afterAll(async () => {
@@ -46,10 +33,205 @@ describe('User/$update-email', () => {
   });
 
   test('Updates user email and profile email', async () => {
+    const email = `user+${randomUUID()}@example.com`;
+    const password = randomUUID();
+    const { user } = await inviteUser({
+      project,
+      email,
+      password,
+      sendEmail: false,
+      resourceType: 'Practitioner',
+      firstName: 'Test',
+      lastName: 'User',
+      scope: 'project',
+    });
+
     const newEmail = `user+${randomUUID()}@example.com`;
     const res = await request(app)
       .post(`/fhir/R4/User/${user.id}/$update-email`)
       .set('Authorization', 'Bearer ' + accessToken)
+      .set('Content-Type', ContentType.FHIR_JSON)
+      .set('X-Medplum', 'extended')
+      .send({
+        resourceType: 'Parameters',
+        parameter: [
+          { name: 'email', valueString: newEmail },
+          { name: 'skipEmailVerification', valueBoolean: true },
+        ],
+      } satisfies Parameters);
+    expect(res.status).toBe(200);
+    const updated = res.body as User;
+    expect(updated.email).toStrictEqual(newEmail);
+  });
+
+  test('Updates profile email if flag set', async () => {
+    const email = `user+${randomUUID()}@example.com`;
+    const password = randomUUID();
+    const { user, profile } = await inviteUser({
+      project,
+      email,
+      password,
+      sendEmail: false,
+      resourceType: 'Practitioner',
+      firstName: 'Test',
+      lastName: 'User',
+      scope: 'project',
+    });
+
+    const newEmail = `user+${randomUUID()}@example.com`;
+    const res = await request(app)
+      .post(`/fhir/R4/User/${user.id}/$update-email`)
+      .set('Authorization', 'Bearer ' + accessToken)
+      .set('Content-Type', ContentType.FHIR_JSON)
+      .set('X-Medplum', 'extended')
+      .send({
+        resourceType: 'Parameters',
+        parameter: [
+          { name: 'email', valueString: newEmail },
+          { name: 'updateProfileTelecom', valueBoolean: true },
+          { name: 'skipEmailVerification', valueBoolean: true },
+        ],
+      } satisfies Parameters);
+    expect(res.status).toBe(200);
+    const updated = res.body as User;
+
+    expect(updated.email).toStrictEqual(newEmail);
+
+    const practitioner = await repo.readResource<Practitioner>('Practitioner', profile.id);
+    expect(practitioner.telecom).toStrictEqual(
+      expect.arrayContaining<ContactPoint>([
+        { use: 'old', system: 'email', value: email },
+        { use: 'work', system: 'email', value: newEmail },
+      ])
+    );
+  });
+
+  test('Requires admin privileges', async () => {
+    const email = `user+${randomUUID()}@example.com`;
+    const password = randomUUID();
+    const { user, profile } = await inviteUser({
+      project,
+      email,
+      password,
+      sendEmail: false,
+      resourceType: 'Practitioner',
+      firstName: 'Test',
+      lastName: 'User',
+      scope: 'project',
+    });
+
+    // Log in as non-admin user
+    const login = await tryLogin({
+      email,
+      password,
+      projectId: project.id,
+      authMethod: 'password',
+      scope: 'openid *.*',
+      nonce: randomUUID(),
+    });
+    const { accessToken } = await getAuthTokens(user, login, createReference(profile));
+
+    const res = await request(app)
+      .post(`/fhir/R4/User/${user.id}/$update-email`)
+      .set('Authorization', 'Bearer ' + accessToken)
+      .set('Content-Type', ContentType.FHIR_JSON)
+      .set('X-Medplum', 'extended')
+      .send({
+        resourceType: 'Parameters',
+        parameter: [
+          { name: 'email', valueString: `user+${randomUUID()}@example.com` },
+          { name: 'updateProfileTelecom', valueBoolean: true },
+          { name: 'skipEmailVerification', valueBoolean: true },
+        ],
+      } satisfies Parameters);
+    expect(res.status).toBe(403);
+  });
+
+  test('Requires Project-scoped user', async () => {
+    const email = `user+${randomUUID()}@example.com`;
+    const password = randomUUID();
+    const { user } = await inviteUser({
+      project,
+      email,
+      password,
+      sendEmail: false,
+      resourceType: 'Practitioner',
+      firstName: 'Test',
+      lastName: 'User',
+      scope: 'server',
+    });
+
+    const res = await request(app)
+      .post(`/fhir/R4/User/${user.id}/$update-email`)
+      .set('Authorization', 'Bearer ' + accessToken)
+      .set('Content-Type', ContentType.FHIR_JSON)
+      .set('X-Medplum', 'extended')
+      .send({
+        resourceType: 'Parameters',
+        parameter: [
+          { name: 'email', valueString: `user+${randomUUID()}@example.com` },
+          { name: 'updateProfileTelecom', valueBoolean: true },
+          { name: 'skipEmailVerification', valueBoolean: true },
+        ],
+      } satisfies Parameters);
+    expect(res.status).toBe(403);
+  });
+
+  test('Cannot alter user from other Project', async () => {
+    const { project: otherProject } = await createTestProject();
+    const email = `user+${randomUUID()}@example.com`;
+    const password = randomUUID();
+    const { user } = await inviteUser({
+      project: otherProject,
+      email,
+      password,
+      sendEmail: false,
+      resourceType: 'Practitioner',
+      firstName: 'Other',
+      lastName: 'User',
+      scope: 'project',
+    });
+
+    const res = await request(app)
+      .post(`/fhir/R4/User/${user.id}/$update-email`)
+      .set('Authorization', 'Bearer ' + accessToken)
+      .set('Content-Type', ContentType.FHIR_JSON)
+      .set('X-Medplum', 'extended')
+      .send({
+        resourceType: 'Parameters',
+        parameter: [
+          { name: 'email', valueString: `user+${randomUUID()}@example.com` },
+          { name: 'updateProfileTelecom', valueBoolean: true },
+          { name: 'skipEmailVerification', valueBoolean: true },
+        ],
+      } satisfies Parameters);
+    expect(res.status).toBe(403);
+  });
+
+  test('Permitted for Super Admin', async () => {
+    const email = `user+${randomUUID()}@example.com`;
+    const password = randomUUID();
+    const { user, profile } = await inviteUser({
+      project,
+      email,
+      password,
+      sendEmail: false,
+      resourceType: 'Practitioner',
+      firstName: 'Test',
+      lastName: 'User',
+      scope: 'project',
+    });
+
+    // User Super Admin to call operation
+    const { accessToken: superAdminToken } = await createTestProject({
+      withAccessToken: true,
+      project: { superAdmin: true },
+    });
+
+    const newEmail = `user+${randomUUID()}@example.com`;
+    const res = await request(app)
+      .post(`/fhir/R4/User/${user.id}/$update-email`)
+      .set('Authorization', 'Bearer ' + superAdminToken)
       .set('Content-Type', ContentType.FHIR_JSON)
       .set('X-Medplum', 'extended')
       .send({
