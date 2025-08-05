76a7a663b4d0ac3c145119af90ffdaad0b51bb7e
Cody Ebberson <cody@ebberson.com>
2025-08-05 08:25:25 -0700

    Fixed cli-wrapper double execution
    Signed-off-by: Cody Ebberson <cody@ebberson.com>

--- packages/cli-wrapper/src/index.ts
+++ packages/cli-wrapper/src/index.ts
@@ -1,9 +1,1 @@
-import { run } from '@medplum/cli';
-import { normalizeErrorString } from '@medplum/core';
-
-if (require.main === module) {
-  run().catch((err) => {
-    console.error('Unhandled error:', normalizeErrorString(err));
-    process.exit(1);
-  });
-}
+import '@medplum/cli';
