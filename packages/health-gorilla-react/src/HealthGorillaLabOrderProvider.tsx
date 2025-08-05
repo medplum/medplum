e8c37cfa9ef989f3ba90f224277428d6e3f0da82
Matt Long <ml@medplum.com>
2025-08-05 16:35:58 -0700

    Avoid circular deps
    Signed-off-by: Matt Long <ml@medplum.com>

--- packages/health-gorilla-react/src/HealthGorillaLabOrderProvider.tsx
+++ packages/health-gorilla-react/src/HealthGorillaLabOrderProvider.tsx
@@ -1,8 +1,7 @@
 // SPDX-FileCopyrightText: Copyright Orangebot, Inc. and Medplum contributors
 // SPDX-License-Identifier: Apache-2.0
 import { JSX, PropsWithChildren } from 'react';
-import { HealthGorillaLabOrderContext } from './HealthGorillaLabOrderContext';
-import { UseHealthGorillaLabOrderReturn } from './useHealthGorillaLabOrder';
+import { HealthGorillaLabOrderContext, UseHealthGorillaLabOrderReturn } from './HealthGorillaLabOrderContext';
 
 /**
  * A provider component that propagates the `useHealthGorillaLabOrder` return value to all children components via [React Context](https://reactjs.org/docs/context.html) API. To be used with {@link useHealthGorillaLabOrderContext}.
