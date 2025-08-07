e6a3816432b505bf3fac81bcd1788b47b994c162
David Yanez <me@davidyanez.com>
2025-08-07 14:24:43 -0700

    clean up
    Signed-off-by: David Yanez <me@davidyanez.com>

--- examples/medplum-provider/src/pages/tasks/TaskDetails.tsx
+++ examples/medplum-provider/src/pages/tasks/TaskDetails.tsx
@@ -22,6 +22,7 @@ export function TaskDetails(): JSX.Element {
   const [task, setTask] = useState<Task | undefined>(contextTask);
   const selectedPatient = useResource(task?.for);
   const [activeTab, setActiveTab] = useState<string>('properties');
+  const showRight = Boolean(task?.for && selectedPatient?.resourceType === 'Patient');
 
   useEffect(() => {
     setTask(contextTask);
@@ -37,47 +38,62 @@ export function TaskDetails(): JSX.Element {
 
   return (
     <Flex direction="row" w="100%" h="100%">
-      <Flex direction="column" w="60%" h="100%" className={classes.borderRight}>
+      <Flex
+        direction="column"
+        w={showRight ? '60%' : '100%'}
+        h="100%"
+        className={showRight ? classes.borderRight : undefined}
+      >
         <TasksInputNote task={task} onTaskChange={onTaskChange} onDeleteTask={onDeleteTask} />
       </Flex>
 
-      <Flex direction="column" w="40%" h="100%">
-        <Paper h="100%">
-          <Box px="md" pb="md" pt="md">
-            <SegmentedControl
-              value={activeTab}
-              onChange={(value: string) => handleTabChange(value)}
-              data={[
-                { label: 'Properties', value: 'properties' },
-                { label: 'Activity Log', value: 'activity-log' },
-                { label: 'Patient Summary', value: 'patient-summary' },
-              ]}
-              fullWidth
-              radius="md"
-              color="gray"
-              size="md"
-              className={classes.segmentedControl}
-            />
-          </Box>
+      {showRight && (
+        <Flex direction="column" w="40%" h="100%">
+          <Paper h="100%">
+            <Box px="md" pb="md" pt="md">
+              <SegmentedControl
+                value={activeTab}
+                onChange={(value: string) => handleTabChange(value)}
+                data={[
+                  { label: 'Properties', value: 'properties' },
+                  { label: 'Activity Log', value: 'activity-log' },
+                  { label: 'Patient Summary', value: 'patient-summary' },
+                ]}
+                fullWidth
+                radius="md"
+                color="gray"
+                size="md"
+                className={classes.segmentedControl}
+              />
+            </Box>
 
-          {selectedPatient?.resourceType === 'Patient' && task && (
-            <>
-              {activeTab === 'properties' && <TaskProperties p="md" task={task} onTaskChange={onTaskChange} />}
-              {activeTab === 'activity-log' && (
-                <ScrollArea h="calc(100% - 50px)">
-                  <ResourceTimeline
-                    value={task}
-                    loadTimelineResources={async (medplum: MedplumClient, _resourceType: ResourceType, id: string) => {
-                      return Promise.allSettled([medplum.readHistory('Task', id)]);
-                    }}
-                  />
-                </ScrollArea>
-              )}
-              {activeTab === 'patient-summary' && <PatientSummary patient={selectedPatient} />}
-            </>
-          )}
-        </Paper>
-      </Flex>
+            {task && selectedPatient?.resourceType === 'Patient' && (
+              <>
+                {activeTab === 'properties' && (
+                  <TaskProperties key={task.id} p="md" task={task} onTaskChange={onTaskChange} />
+                )}
+                {activeTab === 'activity-log' && (
+                  <ScrollArea h="calc(100% - 50px)">
+                    <ResourceTimeline
+                      value={task}
+                      loadTimelineResources={async (
+                        medplum: MedplumClient,
+                        _resourceType: ResourceType,
+                        id: string
+                      ) => {
+                        return Promise.allSettled([medplum.readHistory('Task', id)]);
+                      }}
+                    />
+                  </ScrollArea>
+                )}
+                {activeTab === 'patient-summary' && selectedPatient && (
+                  <PatientSummary patient={selectedPatient} />
+                )}
+              </>
+            )}
+          </Paper>
+        </Flex>
+      )}
     </Flex>
   );
 }
