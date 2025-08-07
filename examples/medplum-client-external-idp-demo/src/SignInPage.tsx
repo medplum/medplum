8688db2a2ad83ba636dff308332bb509c90570d8
David Yanez <me@davidyanez.com>
2025-08-07 14:32:07 -0700

    clean up
    Signed-off-by: David Yanez <me@davidyanez.com>

--- examples/medplum-client-external-idp-demo/src/SignInPage.tsx
+++ examples/medplum-client-external-idp-demo/src/SignInPage.tsx
@@ -3,7 +3,14 @@
 import { Button, Stack, Title } from '@mantine/core';
 import { Document, useMedplum, useMedplumNavigate } from '@medplum/react';
 import { JSX, useCallback, useEffect } from 'react';
-import { MEDPLUM_BASE_URL } from './contants';
+
+/**
+ * The Medplum API server URL.
+ * The default value for Medplum's hosted API server is "https://api.medplum.com/".
+ * If you are using your own Medplum server, then you can set this value to your server URL.
+ */
+export const MEDPLUM_BASE_URL = 'https://api.medplum.com/';
+// export const MEDPLUM_BASE_URL = 'http://localhost:8103/';
 
 /**
  * Your Medplum project ID.
