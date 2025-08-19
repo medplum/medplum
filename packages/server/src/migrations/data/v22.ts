ee90c9738b66beae8cd985b42577378b07fe4f6b
Matthew Willer <matt@medplum.com>
2025-08-19 14:50:04 -0700

    Add generated migrations
    Signed-off-by: Matthew Willer <matt@medplum.com>

---
+++ packages/server/src/migrations/data/v22.ts
@@ -0,0 +1,28 @@
+// SPDX-FileCopyrightText: Copyright Orangebot, Inc. and Medplum contributors
+// SPDX-License-Identifier: Apache-2.0
+import { PoolClient } from 'pg';
+import { prepareCustomMigrationJobData, runCustomMigration } from '../../workers/post-deploy-migration';
+import * as fns from '../migrate-functions';
+import { withLongRunningDatabaseClient } from '../migration-utils';
+import { MigrationActionResult } from '../types';
+import { CustomPostDeployMigration } from './types';
+
+export const migration: CustomPostDeployMigration = {
+  type: 'custom',
+  prepareJobData: (asyncJob) => prepareCustomMigrationJobData(asyncJob),
+  run: async (repo, job, jobData) => {
+    return runCustomMigration(repo, job, jobData, async () => {
+      return withLongRunningDatabaseClient(async (client) => {
+        const results: MigrationActionResult[] = [];
+        await run(client, results);
+        return results;
+      });
+    });
+  },
+};
+
+// prettier-ignore
+async function run(client: PoolClient, results: MigrationActionResult[]): Promise<void> {
+  await fns.idempotentCreateIndex(client, results, 'ServiceRequest___reason_idx', `CREATE INDEX CONCURRENTLY IF NOT EXISTS "ServiceRequest___reason_idx" ON "ServiceRequest" USING gin ("__reason")`);
+  await fns.idempotentCreateIndex(client, results, 'ServiceRequest___reasonTextTrgm_idx', `CREATE INDEX CONCURRENTLY IF NOT EXISTS "ServiceRequest___reasonTextTrgm_idx" ON "ServiceRequest" USING gin (token_array_to_text("__reasonText") gin_trgm_ops)`);
+}
