0084fe6e139ee4e5fc523bf378b2f3df5bc5662f
Afreen Sikandara <afreen.sikandara@data4life-asia.care>
2025-09-02 22:17:05 +0800

    Update package

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,31 +1,17 @@
 #!/usr/bin/env bash
 
 PACKAGES=(
-  "agent"
-  "app"
-  "bot-layer"
   "ccda"
-  "cdk"
-  "cli"
-  "cli-wrapper"
   "core"
-  "create-medplum"
   "definitions"
-  "dosespot-react"
-  "eslint-config"
   "fhir-router"
   "fhirtypes"
-  "health-gorilla-core"
-  "health-gorilla-react"
-  "hl7"
-  "mock"
-  "react"
-  "react-hooks"
+  "server"
 )
 
 for package in ${PACKAGES[@]}; do
   echo "Publish $package"
   pushd packages/$package
-  npm publish --access public
+  npm publish
   popd
 done
