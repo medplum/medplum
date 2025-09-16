fe9cceba768e60d71a38f18d58df760741b1c0e5
Karl Pietrzak <karl@medplum.com>
2025-09-16 16:46:01 -0400

    adding 'smtp' as supported key in config
    allows reading SMTP config from AWS Parameter Store, GCP, Azure, etc.
    
    Signed-off-by: Karl Pietrzak <karl@medplum.com>

---
+++ packages/server/src/config/utils.test.ts
@@ -0,0 +1,19 @@
+// SPDX-FileCopyrightText: Copyright Orangebot, Inc. and Medplum contributors
+// SPDX-License-Identifier: Apache-2.0
+import { isBooleanConfig, isIntegerConfig, isObjectConfig } from './utils';
+
+describe('utils', () => {
+  test('isObjectConfig', () => {
+    expect(isObjectConfig('smtp')).toBe(true);
+  });
+
+  test('isBooleanConfig', () => {
+    expect(isBooleanConfig('baseUrl')).toBe(false);
+    expect(isBooleanConfig('logRequests')).toBe(true);
+  });
+
+  test('isIntegerConfig', () => {
+    expect(isIntegerConfig('baseUrl')).toBe(false);
+    expect(isIntegerConfig('port')).toBe(true);
+  });
+});
