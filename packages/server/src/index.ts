85147e1f5c386159f060dde32131acc791266c1d
Afreen Sikandara <afreen.sikandara@data4life-asia.care>
2025-09-19 10:10:42 +0800

    Remove graceful shutdown

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
-import gracefulShutdown from 'http-graceful-shutdown';
-import { initApp, shutdownApp } from './app';
+// import gracefulShutdown from 'http-graceful-shutdown';
+import { initApp } from './app';
 import { loadConfig } from './config/loader';
 import { globalLogger } from './logger';
 import { getServerVersion } from './util/version';
@@ -38,20 +38,20 @@ export async function main(configName: string): Promise<void> {
   const server = app.listen(config.port);
   server.keepAliveTimeout = config.keepAliveTimeout ?? 90000;
   globalLogger.info('Server started', { port: config.port });
-  gracefulShutdown(server, {
-    timeout: config.shutdownTimeoutMilliseconds,
-    development: process.env.NODE_ENV !== 'production',
-    preShutdown: async (signal) => {
-      globalLogger.info(
-        `Shutdown signal received... allowing graceful shutdown for up to ${config.shutdownTimeoutMilliseconds} milliseconds`,
-        { signal }
-      );
-    },
-    onShutdown: () => shutdownApp(),
-    finally: () => {
-      globalLogger.info('Shutdown complete');
-    },
-  });
+  // gracefulShutdown(server, {
+  //   timeout: config.shutdownTimeoutMilliseconds,
+  //   development: process.env.NODE_ENV !== 'production',
+  //   preShutdown: async (signal) => {
+  //     globalLogger.info(
+  //       `Shutdown signal received... allowing graceful shutdown for up to ${config.shutdownTimeoutMilliseconds} milliseconds`,
+  //       { signal }
+  //     );
+  //   },
+  //   onShutdown: () => shutdownApp(),
+  //   finally: () => {
+  //     globalLogger.info('Shutdown complete');
+  //   },
+  // });
 }
 
 main('file:medplum.config.json').catch((reason) => {
