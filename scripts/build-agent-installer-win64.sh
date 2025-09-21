91b89eac8785e6ffc82d40ed9b6448077499d5a9
Cody Ebberson <cody@ebberson.com>
2025-09-21 16:55:40 -0700

    Sign agent release artifacts (#7433)
    * Sign agent release artifacts
    
    Signed-off-by: Cody Ebberson <cody@ebberson.com>
    
    * Added key_id and passphrase
    
    Signed-off-by: Cody Ebberson <cody@ebberson.com>
    
    * PR feedback
    
    Signed-off-by: Cody Ebberson <cody@ebberson.com>
    
    ---------
    
    Signed-off-by: Cody Ebberson <cody@ebberson.com>

--- scripts/build-agent-installer-win64.sh
+++ scripts/build-agent-installer-win64.sh
@@ -25,7 +25,7 @@ if ! command -v wget >/dev/null 2>&1; then
 fi
 
 # If we are skipping signing, none of the pre-reqs for signing are required
-if [ ! SKIP_SIGNING ]; then
+if [ -z "$SKIP_SIGNING" ]; then
   if ! command -v java >/dev/null 2>&1; then
       echo "Java required"
       exit 1
@@ -75,7 +75,7 @@ pushd ../..
 
 popd
 
-if [ ! SKIP_SIGNING ]; then
+if [ -z "$SKIP_SIGNING" ]; then
   # Download JSign
   if [ ! -f "jsign-5.0.jar" ]; then
       wget https://github.com/ebourg/jsign/releases/download/5.0/jsign-5.0.jar
@@ -105,7 +105,7 @@ wget https://github.com/mtkennerly/shawl/releases/download/v1.5.0/shawl-v1.5.0-l
 unzip shawl-v1.5.0-legal.zip
 mv shawl-v1.5.0-legal.txt dist
 
-if [ ! SKIP_SIGNING ]; then
+if [ -z "$SKIP_SIGNING" ]; then
   # Sign the Shawl executable
   java -jar jsign-5.0.jar \
     --storetype DIGICERTONE \
@@ -115,7 +115,7 @@ if [ ! SKIP_SIGNING ]; then
 fi
 
 # Build the installer
-if [ SKIP_SIGNING ]; then
+if [ -z "$SKIP_SIGNING" ]; then
   makensis -DSKIP_SIGNING installer.nsi # globally defines the SKIP_SIGNING symbol
 else
   makensis installer.nsi
@@ -126,6 +126,27 @@ sha256sum "medplum-agent-installer-$MEDPLUM_FULL_VERSION.exe" > "medplum-agent-i
 
 # Check the installer checksum
 sha256sum --check "medplum-agent-installer-$MEDPLUM_FULL_VERSION.exe.sha256"
+
+if [ -z "$SKIP_SIGNING" ]; then
+  # Generate a GPG signature for the installer
+  # --batch = Use batch mode. Never ask, do not allow interactive commands.
+  # --yes = Assume "yes" on most questions. Should not be used in an option file.
+  # --pinentry-mode loopback = Allows the passphrase to be set via command line or fd.
+  # --passphrase-fd 0 = Read the passphrase from file descriptor 0 (stdin).
+  # --local-user = Specify the key to use for signing.
+  # --detach-sign --armor = Create a detached ASCII armored signature.
+  echo "$GPG_PASSPHRASE" | gpg \
+    --batch \
+    --yes \
+    --pinentry-mode loopback \
+    --passphrase-fd 0 \
+    --local-user "$GPG_KEY_ID" \
+    --detach-sign --armor \
+    "medplum-agent-installer-$MEDPLUM_FULL_VERSION.exe"
+
+  # Check the signature
+  gpg --verify "medplum-agent-installer-$MEDPLUM_FULL_VERSION.exe.asc"
+fi
 
 # Check the build output
 ls -la
