2dfb1da190df05408411ba452bab8427dfbd25ef
Karl Pietrzak <karl@medplum.com>
2025-10-02 11:17:57 -0400

    partition ConceptMapping table by conceptMap
    Signed-off-by: Karl Pietrzak <karl@medplum.com>

--- packages/server/src/migrations/schema/v98.ts
+++ packages/server/src/migrations/schema/v98.ts
@@ -12,7 +12,7 @@ import * as fns from '../migrate-functions';
 export async function run(client: PoolClient): Promise<void> {
   const results: { name: string; durationMs: number }[] = []
   await fns.query(client, results, `CREATE TABLE IF NOT EXISTS "ConceptMapping" (
-    "id" BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "id" BIGINT GENERATED ALWAYS AS IDENTITY NOT NULL,
     "conceptMap" UUID NOT NULL,
     "sourceSystem" BIGINT NOT NULL,
     "sourceCode" TEXT NOT NULL,
@@ -21,8 +21,31 @@ export async function run(client: PoolClient): Promise<void> {
     "relationship" TEXT,
     "sourceDisplay" TEXT,
     "targetDisplay" TEXT,
-    "comment" TEXT
-  )`);
+    "comment" TEXT,
+    PRIMARY KEY("id", "conceptMap") -- partition key must be part of PK
+  ) PARTITION BY HASH ("conceptMap")`);
+  /*
+   * We partition the ConceptMapping table by hashing the conceptMap column.
+   * This is a good choice because it is a good hash function for UUIDv4s.
+   */
+  await fns.query(client, results, `
+    CREATE TABLE IF NOT EXISTS "ConceptMapping_00" PARTITION OF "ConceptMapping" FOR VALUES WITH (MODULUS 16, REMAINDER 0);
+    CREATE TABLE IF NOT EXISTS "ConceptMapping_01" PARTITION OF "ConceptMapping" FOR VALUES WITH (MODULUS 16, REMAINDER 1);
+    CREATE TABLE IF NOT EXISTS "ConceptMapping_02" PARTITION OF "ConceptMapping" FOR VALUES WITH (MODULUS 16, REMAINDER 2);
+    CREATE TABLE IF NOT EXISTS "ConceptMapping_03" PARTITION OF "ConceptMapping" FOR VALUES WITH (MODULUS 16, REMAINDER 3);
+    CREATE TABLE IF NOT EXISTS "ConceptMapping_04" PARTITION OF "ConceptMapping" FOR VALUES WITH (MODULUS 16, REMAINDER 4);
+    CREATE TABLE IF NOT EXISTS "ConceptMapping_05" PARTITION OF "ConceptMapping" FOR VALUES WITH (MODULUS 16, REMAINDER 5);
+    CREATE TABLE IF NOT EXISTS "ConceptMapping_06" PARTITION OF "ConceptMapping" FOR VALUES WITH (MODULUS 16, REMAINDER 6);
+    CREATE TABLE IF NOT EXISTS "ConceptMapping_07" PARTITION OF "ConceptMapping" FOR VALUES WITH (MODULUS 16, REMAINDER 7);
+    CREATE TABLE IF NOT EXISTS "ConceptMapping_08" PARTITION OF "ConceptMapping" FOR VALUES WITH (MODULUS 16, REMAINDER 8);
+    CREATE TABLE IF NOT EXISTS "ConceptMapping_09" PARTITION OF "ConceptMapping" FOR VALUES WITH (MODULUS 16, REMAINDER 9);
+    CREATE TABLE IF NOT EXISTS "ConceptMapping_10" PARTITION OF "ConceptMapping" FOR VALUES WITH (MODULUS 16, REMAINDER 10);
+    CREATE TABLE IF NOT EXISTS "ConceptMapping_11" PARTITION OF "ConceptMapping" FOR VALUES WITH (MODULUS 16, REMAINDER 11);
+    CREATE TABLE IF NOT EXISTS "ConceptMapping_12" PARTITION OF "ConceptMapping" FOR VALUES WITH (MODULUS 16, REMAINDER 12);
+    CREATE TABLE IF NOT EXISTS "ConceptMapping_13" PARTITION OF "ConceptMapping" FOR VALUES WITH (MODULUS 16, REMAINDER 13);
+    CREATE TABLE IF NOT EXISTS "ConceptMapping_14" PARTITION OF "ConceptMapping" FOR VALUES WITH (MODULUS 16, REMAINDER 14);
+    CREATE TABLE IF NOT EXISTS "ConceptMapping_15" PARTITION OF "ConceptMapping" FOR VALUES WITH (MODULUS 16, REMAINDER 15);
+    `);
   await fns.query(client, results, `CREATE UNIQUE INDEX IF NOT EXISTS "ConceptMapping_map_source_target_idx" ON "ConceptMapping" ("conceptMap", "sourceSystem", "sourceCode", "targetSystem", "targetCode")`);
   await fns.query(client, results, `CREATE INDEX IF NOT EXISTS "ConceptMapping_map_reverse_idx" ON "ConceptMapping" ("conceptMap", "targetSystem", "targetCode", "sourceSystem")`);
   await fns.query(client, results, `CREATE TABLE IF NOT EXISTS "CodingSystem" (
