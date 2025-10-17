654c7e92ba71f34c2697b900bf58893ade071031
David Yanez <me@davidyanez.com>
2025-10-17 13:28:47 -0700

    Sign as current Practitionaire and Addendum
    Signed-off-by: David Yanez <me@davidyanez.com>

---
+++ examples/medplum-provider/src/types/encounter.ts
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: Copyright Orangebot, Inc. and Medplum contributors
+// SPDX-License-Identifier: Apache-2.0
+
+export enum ChartNoteStatus {
+  Unsigned = 'unsigned',
+  Signed = 'signed',
+  SignedAndLocked = 'signedAndLocked',
+}
+
+
