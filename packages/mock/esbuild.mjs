c5be66b7f5011e32610c36be48c2a7c2b6499096
Cody Ebberson <cody@ebberson.com>
2025-10-17 17:25:32 -0700

    Fixed license header in build scripts (#7632)
    Signed-off-by: Cody Ebberson <cody@ebberson.com>

--- packages/mock/esbuild.mjs
+++ packages/mock/esbuild.mjs
@@ -1,6 +1,8 @@
-/* global console */
+// SPDX-FileCopyrightText: Copyright Orangebot, Inc. and Medplum contributors
+// SPDX-License-Identifier: Apache-2.0
+
 /* global process */
-/* eslint no-console: "off" */
+/* global console */
 
 import esbuild from 'esbuild';
 import { writeFileSync } from 'fs';
