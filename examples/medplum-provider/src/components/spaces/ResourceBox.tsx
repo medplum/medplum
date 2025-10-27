7b0ac81ed129327d213d8a97c863a5238cf1e6ab
David Yanez <me@davidyanez.com>
2025-10-27 14:29:12 -0600

    Spaces display resource
    Signed-off-by: David Yanez <me@davidyanez.com>

---
+++ examples/medplum-provider/src/components/spaces/ResourceBox.tsx
@@ -0,0 +1,49 @@
+// SPDX-FileCopyrightText: Copyright Orangebot, Inc. and Medplum contributors
+// SPDX-License-Identifier: Apache-2.0
+import { Box, Group, Paper, Text, ThemeIcon } from '@mantine/core';
+import { getDisplayString } from '@medplum/core';
+import { useResource } from '@medplum/react';
+import { IconFileText } from '@tabler/icons-react';
+import type { JSX } from 'react';
+import classes from './ResourceBox.module.css';
+
+interface ResourceBoxProps {
+  resourceReference: string;
+  onClick: (resourceReference: string) => void;
+}
+
+export function ResourceBox({ resourceReference, onClick }: ResourceBoxProps): JSX.Element {
+  const resource = useResource({ reference: resourceReference });
+
+  if (!resource) {
+    return (
+      <Paper withBorder p="sm" className={classes.resourceBox}>
+        <Text size="sm" c="dimmed">
+          Unable to find resource
+        </Text>
+      </Paper>
+    );
+  }
+
+  const displayName = getDisplayString(resource) ?? '';
+  const resourceType = resource.resourceType;
+
+  return (
+    <Paper withBorder p="sm" className={classes.resourceBox} onClick={() => onClick(resourceReference)}>
+      <Group gap="sm" wrap="nowrap">
+        <ThemeIcon size="lg" variant="light" color="violet">
+          <IconFileText size={20} />
+        </ThemeIcon>
+        <Box className={classes.content}>
+          <Text size="sm" fw={600} c="violet.7">
+            {resourceType}
+          </Text>
+          <Text size="xs" c="dimmed" truncate>
+            {displayName}
+          </Text>
+        </Box>
+      </Group>
+    </Paper>
+  );
+}
+
