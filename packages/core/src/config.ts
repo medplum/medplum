a899facb15beb6fddc5a07000ab656befdf95f12
Matt Long <mattlong@users.noreply.github.com>
2025-10-27 11:09:27 -0700

    Allow specifying autoMinorVersionUpgrade on RDS cluster (#7670)
    * Allow specifying autoMinorVersionUpgrade on RDS cluster
    
    Signed-off-by: Matt Long <ml@medplum.com>
    
    * Support external secrets
    
    Signed-off-by: Matt Long <ml@medplum.com>
    
    ---------
    
    Signed-off-by: Matt Long <ml@medplum.com>
    Signed-off-by: David Yanez <me@davidyanez.com>

--- packages/core/src/config.ts
+++ packages/core/src/config.ts
@@ -52,6 +52,7 @@ export interface MedplumSourceInfraConfig {
   rdsReaderInstanceType?: ValueOrExternalSecret<string>;
   rdsProxyEnabled?: ValueOrExternalSecret<boolean>;
   rdsClusterParameters?: StringMap;
+  rdsAutoMinorVersionUpgrade?: ValueOrExternalSecret<boolean>;
   cacheNodeType?: ValueOrExternalSecret<string>;
   cacheSecurityGroupId?: ValueOrExternalSecret<string>;
   desiredServerCount: ValueOrExternalSecret<number>;
@@ -96,8 +97,8 @@ export interface MedplumSourceInfraConfig {
   };
   environment?: StringMap;
 
-  rdsIdsMajorVersionSuffix?: boolean;
-  rdsPersistentParameterGroups?: boolean;
+  rdsIdsMajorVersionSuffix?: ValueOrExternalSecret<boolean>;
+  rdsPersistentParameterGroups?: ValueOrExternalSecret<boolean>;
 
   fireLens?: {
     enabled: true;
@@ -151,6 +152,7 @@ export interface MedplumInfraConfig {
   rdsInstanceType: string;
   rdsInstanceVersion?: string;
   rdsClusterParameters?: StringMap;
+  rdsAutoMinorVersionUpgrade?: boolean;
   rdsSecretsArn?: string;
   rdsReaderInstanceType?: string;
   rdsProxyEnabled?: boolean;
