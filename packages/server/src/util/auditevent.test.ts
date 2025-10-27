d938e71601fcf00096f48c7f350784027cc912b0
Cody Ebberson <cody@ebberson.com>
2025-10-27 08:12:11 -0700

    [v5] Remove select deprecated features (#7611)
    Signed-off-by: Cody Ebberson <cody@ebberson.com>
    Signed-off-by: David Yanez <me@davidyanez.com>

--- packages/server/src/util/auditevent.test.ts
+++ packages/server/src/util/auditevent.test.ts
@@ -1,37 +1,11 @@
 // SPDX-FileCopyrightText: Copyright Orangebot, Inc. and Medplum contributors
 // SPDX-License-Identifier: Apache-2.0
-import {
-  CloudWatchLogsClient,
-  CreateLogGroupCommand,
-  CreateLogStreamCommand,
-  PutLogEventsCommand,
-} from '@aws-sdk/client-cloudwatch-logs';
 import type { AuditEvent } from '@medplum/fhirtypes';
-import type { AwsClientStub } from 'aws-sdk-client-mock';
-import { mockClient } from 'aws-sdk-client-mock';
 import 'aws-sdk-client-mock-jest';
 import { loadTestConfig } from '../config/loader';
-import { waitFor } from '../test.setup';
 import { logAuditEvent } from './auditevent';
 
 describe('AuditEvent utils', () => {
-  let mockCloudWatchLogsClient: AwsClientStub<CloudWatchLogsClient>;
-
-  beforeEach(() => {
-    mockCloudWatchLogsClient = mockClient(CloudWatchLogsClient);
-
-    mockCloudWatchLogsClient.on(CreateLogGroupCommand).resolves({});
-    mockCloudWatchLogsClient.on(CreateLogStreamCommand).resolves({});
-    mockCloudWatchLogsClient.on(PutLogEventsCommand).resolves({
-      nextSequenceToken: '',
-      rejectedLogEventsInfo: {},
-    });
-  });
-
-  afterEach(() => {
-    mockCloudWatchLogsClient.restore();
-  });
-
   test('AuditEvents disabled', async () => {
     console.info = jest.fn();
     console.log = jest.fn();
@@ -56,30 +30,5 @@ describe('AuditEvent utils', () => {
 
     // It should have been logged
     expect(console.log).toHaveBeenCalledWith('{"resourceType":"AuditEvent"}');
-  });
-
-  test('AuditEvent to CloudWatch Logs', async () => {
-    console.info = jest.fn();
-    console.log = jest.fn();
-
-    const config = await loadTestConfig();
-    config.logAuditEvents = true;
-    config.auditEventLogGroup = 'test-log-group';
-    config.auditEventLogStream = 'test-log-stream';
-
-    // Log an AuditEvent
-    logAuditEvent({ resourceType: 'AuditEvent' } as AuditEvent);
-    logAuditEvent({ resourceType: 'AuditEvent' } as AuditEvent);
-
-    await waitFor(async () => expect(mockCloudWatchLogsClient).toHaveReceivedCommand(PutLogEventsCommand));
-
-    // CloudWatch logs should have been created
-    expect(mockCloudWatchLogsClient.send.callCount).toBe(3);
-    expect(mockCloudWatchLogsClient).toHaveReceivedCommandTimes(CreateLogGroupCommand, 1);
-    expect(mockCloudWatchLogsClient).toHaveReceivedCommandTimes(CreateLogStreamCommand, 1);
-    expect(mockCloudWatchLogsClient).toHaveReceivedCommandTimes(PutLogEventsCommand, 1);
-
-    expect(console.info).toHaveBeenCalled();
-    expect(console.log).not.toHaveBeenCalled();
   });
 });
