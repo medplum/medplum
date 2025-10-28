1a84ff373a896ffd25990daa85514247783c084c
Cody Ebberson <cody@ebberson.com>
2025-10-28 15:23:55 -0700

    Revert "Embed the CMS 1500 base png as a data url (#7699)" (#7703)
    This reverts commit 1a66ff88f14fbc18669e693e99496f76b6d15f55.
    
    Signed-off-by: David Yanez <me@davidyanez.com>

--- packages/server/src/fhir/operations/utils/cms1500pdf.ts
+++ packages/server/src/fhir/operations/utils/cms1500pdf.ts
@@ -9,9 +9,10 @@ import {
   HTTP_HL7_ORG,
 } from '@medplum/core';
 import type { Address, Claim, HumanName, Practitioner, RelatedPerson } from '@medplum/fhirtypes';
+import { dirname, relative, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
 import type { Content, TDocumentDefinitions } from 'pdfmake/interfaces';
 import { getAuthenticatedContext } from '../../../context';
-import imageData from './cms1500.png';
 
 const PAGE_WIDTH = 612;
 const PAGE_HEIGHT = 792;
@@ -105,7 +106,7 @@ export async function getClaimPDFDocDefinition(claim: Claim): Promise<TDocumentD
     pageMargins: 0,
     content: [
       {
-        image: imageData,
+        image: relative(process.cwd(), resolve(getDirName(), '../../../../static/cms1500.png')),
         absolutePosition: { x: 0, y: 0 },
         width: PAGE_WIDTH,
         height: PAGE_HEIGHT,
@@ -392,4 +393,13 @@ export function formatHumanName(name: HumanName | undefined): string {
   }
 
   return parts.join(', ');
+}
+
+/**
+ * Returns the directory name of the current module.
+ * Works with both CommonJS and ES modules.
+ * @returns The directory name of the current module.
+ */
+function getDirName(): string {
+  return typeof __dirname !== 'undefined' ? __dirname : dirname(fileURLToPath(import.meta.url));
 }
--- packages/server/src/images.d.ts
+++
@@ -1,6 +0,0 @@
-// SPDX-FileCopyrightText: Copyright Orangebot, Inc. and Medplum contributors
-// SPDX-License-Identifier: Apache-2.0
-declare module '*.png' {
-  const dataUrl: string;
-  export default dataUrl;
-}
