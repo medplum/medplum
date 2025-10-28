1a66ff88f14fbc18669e693e99496f76b6d15f55
Cody Ebberson <cody@ebberson.com>
2025-10-28 09:24:32 -0700

    Embed the CMS 1500 base png as a data url (#7699)
    * Embed the CMS 1500 base png as a data url
    
    Signed-off-by: Cody Ebberson <cody@ebberson.com>
    
    * Fixed tests
    
    Signed-off-by: Cody Ebberson <cody@ebberson.com>
    
    * Fixed tests
    
    Signed-off-by: Cody Ebberson <cody@ebberson.com>
    
    ---------
    
    Signed-off-by: Cody Ebberson <cody@ebberson.com>

---
+++ packages/server/src/images.d.ts
@@ -0,0 +1,6 @@
+// SPDX-FileCopyrightText: Copyright Orangebot, Inc. and Medplum contributors
+// SPDX-License-Identifier: Apache-2.0
+declare module '*.png' {
+  const dataUrl: string;
+  export default dataUrl;
+}
