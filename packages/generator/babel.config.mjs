fd477d4beef768b433f573bb65c4c2610ed9e411
Matthew Willer <matt@medplum.com>
2025-10-29 13:45:00 -0700

    WIP: Fix
    Signed-off-by: Matthew Willer <matt@medplum.com>

--- packages/generator/babel.config.mjs
+++ packages/generator/babel.config.mjs
@@ -1,14 +1,24 @@
 // SPDX-FileCopyrightText: Copyright Orangebot, Inc. and Medplum contributors
 // SPDX-License-Identifier: Apache-2.0
+
+import { types } from '@babel/core';
+
+const rewrites = {
+  env: 'process.env',
+  dirname: '__dirname',
+};
+
 export default {
   presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-typescript'],
   plugins: [
     () => ({
       visitor: {
-        MetaProperty(path) {
-          if (path.node.meta.name === 'import' && path.node.property.name === 'meta') {
-            // Replace "import.meta" with "process"
-            path.replaceWithSourceString('process');
+        MemberExpression(path) {
+          if (types.isMetaProperty(path.node.object)) {
+            const rewrite = rewrites[path.node.property.name];
+            if (rewrite) {
+              path.replaceWithSourceString(rewrite);
+            }
           }
         },
       },
