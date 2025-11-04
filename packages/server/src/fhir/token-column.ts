71bf48164e74c081b25a0568f4cd252a71ae6d28
David Yanez <me@davidyanez.com>
2025-11-04 11:45:41 -0800

    merge
    Signed-off-by: David Yanez <me@davidyanez.com>

--- packages/server/src/fhir/token-column.ts
+++ packages/server/src/fhir/token-column.ts
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 import type { Filter, SortRule } from '@medplum/core';
 import {
+  badRequest,
   Operator as FhirOperator,
   invalidSearchOperator,
   OperationOutcomeError,
