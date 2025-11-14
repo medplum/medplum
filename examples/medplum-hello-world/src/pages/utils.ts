9420bec38ead3e571de4993b213f8728fd746e6e
Darren Eam <darreneam65@gmail.com>
2025-11-14 10:58:57 -0800

    remove unused imports
    Signed-off-by: Darren Eam <darreneam65@gmail.com>

--- examples/medplum-hello-world/src/pages/utils.ts
+++ examples/medplum-hello-world/src/pages/utils.ts
@@ -1,8 +1,6 @@
 // SPDX-FileCopyrightText: Copyright Orangebot, Inc. and Medplum contributors
 // SPDX-License-Identifier: Apache-2.0
-import type { Filter, MedplumClient, SearchRequest, SortRule } from '@medplum/core';
-import { convertToTransactionBundle, DEFAULT_SEARCH_COUNT, formatSearchQuery } from '@medplum/core';
-import type { Bundle, ResourceType, UserConfiguration } from '@medplum/fhirtypes';
+import type { ResourceType } from '@medplum/fhirtypes';
 
 export const RESOURCE_PROFILE_URLS: Partial<Record<ResourceType, string>> = {
   ServiceRequest: 'http://medplum.com/StructureDefinition/medplum-provider-lab-procedure-servicerequest',
