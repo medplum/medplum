398a756ae2545d130062404bbc0129d760d6154f
Derrick Farris <derrick@medplum.com>
2025-11-17 10:47:13 -0800

    ci(test): lean into turbo, set turbo concurrency to 1
    Signed-off-by: Derrick Farris <derrick@medplum.com>

--- scripts/test.sh
+++ scripts/test.sh
@@ -14,27 +14,27 @@ rm -rf coverage
 mkdir -p coverage/packages
 mkdir -p coverage/combined
 
-# Seed the database
+# Test
+# Run them separately because code coverage is resource intensive
+# Previously we iterated over the directories in packages/ here and called test on each one, one-by-one
+# This didn't actually do what we wanted it to do, since if a package (say @medplum/agent), depends on other packages
+# It will try to work through dependencies first
+# We can instead reduce overhead of context switching by setting concurrency to 1 and allowing jest to handle concurrency within one test suite
+# We leave out the server tests from this batch since (for now) we need to manually run the seed test separately
+npx turbo run test --filter=@medplum/app --filter=@medplum/agent --concurrency=1
+
+# Seed the database for server tests
 # This is a special "test" which runs all of the seed logic, such as setting up structure definitions
 # On a normal developer machine, this is run only rarely when setting up a new database
 # This test must be run first, and cannot be run concurrently with other tests
-SHOULD_RUN_SEED_TEST=$(date) time npx turbo run test:seed --filter=./packages/server -- --coverage
+SHOULD_RUN_SEED_TEST=$(date) time npx turbo run test:seed --filter=@medplum/server -- --coverage
 cp "packages/server/coverage/coverage-final.json" "coverage/packages/coverage-server-seed.json"
 
-# Test
-# Run them separately because code coverage is resource intensive
+# Finally run the rest of the server tests
+npx turbo run test --filter=@medplum/server --concurrency=1
 
-for dir in `ls packages`; do
-  if test -f "packages/$dir/package.json" && grep -q "\"test\":" "packages/$dir/package.json"; then
-    npx turbo run test --filter=./packages/$dir -- --coverage
-  fi
-done
-
-for dir in `ls examples`; do
-  if test -f "examples/$dir/package.json" && grep -q "\"test\":" "examples/$dir/package.json"; then
-    npx turbo run test --filter=./examples/$dir
-  fi
-done
+# Now run tests for all examples
+npx turbo run test --filter="./examples/*" --concurrency=1
 
 # Find all coverage-final.json files in packages subdirectories
 for coverage_file in packages/*/coverage/coverage-final.json; do
