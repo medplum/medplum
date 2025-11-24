493437082ce88bb4775867c0943b9d9b7433f3a0
Cody Ebberson <cody@ebberson.com>
2025-11-24 15:22:29 -0800

    Fixes #7896 - remove Next.js examples from monorepo (#7901)
    Signed-off-by: Cody Ebberson <cody@ebberson.com>

--- examples/medplum-nextjs-demo/app/theme.ts
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: Copyright Orangebot, Inc. and Medplum contributors
-// SPDX-License-Identifier: Apache-2.0
-'use client';
-
-import { createTheme } from '@mantine/core';
-
-export const theme = createTheme({
-  /* Put your mantine theme override here */
-});
