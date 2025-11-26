88b215f4ced293da29cdec376585cba3580e2ffd
Noah Silas <noah@medplum.com>
2025-11-26 10:58:52 -0800

    Add findAlignedSlots $find helper function
    Given an interval of availability, find the resulting slots that should
    be presented to a user. This will be a building block for the upcoming
    `$find` operation.
    
    Part of this implementation around jumping to the aligned offset was
    inspired by Claude AI.
    
    Signed-off-by: Noah Silas <noah@medplum.com>

--- packages/server/src/fhir/operations/utils/find.test.ts
+++ packages/server/src/fhir/operations/utils/find.test.ts
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 import { tz, TZDate } from '@date-fns/tz';
 import type { Interval } from 'date-fns';
-import { normalizeIntervals, removeAvailability, resolveAvailability } from './find';
+import { findAlignedSlots, normalizeIntervals, removeAvailability, resolveAvailability } from './find';
 import type { SchedulingParameters } from './scheduling-parameters';
 
 const tzny = tz('America/New_York');
@@ -232,6 +232,132 @@ describe('removeAvailability', () => {
     expect(removeAvailability(availability, blocks)).toEqual([
       { start: new Date('2025-12-01'), end: new Date('2025-12-02') },
       { start: new Date('2025-12-05'), end: new Date('2025-12-06') },
+    ]);
+  });
+});
+
+describe('findAlignedSlots', () => {
+  test('can find a slot that exactly coincides with the interval', () => {
+    const slots = findAlignedSlots(
+      { start: new Date('2025-12-01T00:00:00Z'), end: new Date('2025-12-01T01:00:00Z') },
+      { alignment: 60, offsetMinutes: 0, durationMinutes: 60 }
+    );
+    expect(slots).toEqual([{ start: new Date('2025-12-01T00:00:00Z'), end: new Date('2025-12-01T01:00:00Z') }]);
+  });
+
+  test('returns empty when the interval is less than the duration', () => {
+    const slots = findAlignedSlots(
+      { start: new Date('2025-12-01T00:00:00Z'), end: new Date('2025-12-01T01:00:00Z') },
+      { alignment: 60, offsetMinutes: 0, durationMinutes: 90 }
+    );
+    expect(slots).toEqual([]);
+  });
+
+  test('it finds slots aligned to hours', () => {
+    const slots = findAlignedSlots(
+      { start: new Date('2025-12-01T00:00:00Z'), end: new Date('2025-12-01T02:00:00Z') },
+      { alignment: 60, offsetMinutes: 0, durationMinutes: 10 }
+    );
+    expect(slots).toEqual([
+      { start: new Date('2025-12-01T00:00:00Z'), end: new Date('2025-12-01T00:10:00Z') },
+      { start: new Date('2025-12-01T01:00:00Z'), end: new Date('2025-12-01T01:10:00Z') },
+    ]);
+  });
+
+  test('it finds slots aligned to half hours', () => {
+    const slots = findAlignedSlots(
+      { start: new Date('2025-12-01T00:00:00Z'), end: new Date('2025-12-01T02:00:00Z') },
+      { alignment: 30, offsetMinutes: 0, durationMinutes: 10 }
+    );
+    expect(slots).toEqual([
+      { start: new Date('2025-12-01T00:00:00Z'), end: new Date('2025-12-01T00:10:00Z') },
+      { start: new Date('2025-12-01T00:30:00Z'), end: new Date('2025-12-01T00:40:00Z') },
+      { start: new Date('2025-12-01T01:00:00Z'), end: new Date('2025-12-01T01:10:00Z') },
+      { start: new Date('2025-12-01T01:30:00Z'), end: new Date('2025-12-01T01:40:00Z') },
+    ]);
+  });
+
+  test('it finds slots aligned to quarter hours', () => {
+    const slots = findAlignedSlots(
+      { start: new Date('2025-12-01T00:00:00Z'), end: new Date('2025-12-01T02:00:00Z') },
+      { alignment: 15, offsetMinutes: 0, durationMinutes: 10 }
+    );
+    expect(slots).toEqual([
+      { start: new Date('2025-12-01T00:00:00Z'), end: new Date('2025-12-01T00:10:00Z') },
+      { start: new Date('2025-12-01T00:15:00Z'), end: new Date('2025-12-01T00:25:00Z') },
+      { start: new Date('2025-12-01T00:30:00Z'), end: new Date('2025-12-01T00:40:00Z') },
+      { start: new Date('2025-12-01T00:45:00Z'), end: new Date('2025-12-01T00:55:00Z') },
+      { start: new Date('2025-12-01T01:00:00Z'), end: new Date('2025-12-01T01:10:00Z') },
+      { start: new Date('2025-12-01T01:15:00Z'), end: new Date('2025-12-01T01:25:00Z') },
+      { start: new Date('2025-12-01T01:30:00Z'), end: new Date('2025-12-01T01:40:00Z') },
+      { start: new Date('2025-12-01T01:45:00Z'), end: new Date('2025-12-01T01:55:00Z') },
+    ]);
+  });
+
+  test('it finds slots aligned to ten minute marks', () => {
+    const slots = findAlignedSlots(
+      { start: new Date('2025-12-01T00:00:00Z'), end: new Date('2025-12-01T02:00:00Z') },
+      { alignment: 10, offsetMinutes: 0, durationMinutes: 10 }
+    );
+    expect(slots).toEqual([
+      { start: new Date('2025-12-01T00:00:00Z'), end: new Date('2025-12-01T00:10:00Z') },
+      { start: new Date('2025-12-01T00:10:00Z'), end: new Date('2025-12-01T00:20:00Z') },
+      { start: new Date('2025-12-01T00:20:00Z'), end: new Date('2025-12-01T00:30:00Z') },
+      { start: new Date('2025-12-01T00:30:00Z'), end: new Date('2025-12-01T00:40:00Z') },
+      { start: new Date('2025-12-01T00:40:00Z'), end: new Date('2025-12-01T00:50:00Z') },
+      { start: new Date('2025-12-01T00:50:00Z'), end: new Date('2025-12-01T01:00:00Z') },
+      { start: new Date('2025-12-01T01:00:00Z'), end: new Date('2025-12-01T01:10:00Z') },
+      { start: new Date('2025-12-01T01:10:00Z'), end: new Date('2025-12-01T01:20:00Z') },
+      { start: new Date('2025-12-01T01:20:00Z'), end: new Date('2025-12-01T01:30:00Z') },
+      { start: new Date('2025-12-01T01:30:00Z'), end: new Date('2025-12-01T01:40:00Z') },
+      { start: new Date('2025-12-01T01:40:00Z'), end: new Date('2025-12-01T01:50:00Z') },
+      { start: new Date('2025-12-01T01:50:00Z'), end: new Date('2025-12-01T02:00:00Z') },
+    ]);
+  });
+
+  test('offsetting alignment by five minutes', () => {
+    const slots = findAlignedSlots(
+      { start: new Date('2025-12-01T00:00:00Z'), end: new Date('2025-12-01T02:00:00Z') },
+      { alignment: 15, offsetMinutes: 5, durationMinutes: 10 }
+    );
+    expect(slots).toEqual([
+      { start: new Date('2025-12-01T00:05:00Z'), end: new Date('2025-12-01T00:15:00Z') },
+      { start: new Date('2025-12-01T00:20:00Z'), end: new Date('2025-12-01T00:30:00Z') },
+      { start: new Date('2025-12-01T00:35:00Z'), end: new Date('2025-12-01T00:45:00Z') },
+      { start: new Date('2025-12-01T00:50:00Z'), end: new Date('2025-12-01T01:00:00Z') },
+      { start: new Date('2025-12-01T01:05:00Z'), end: new Date('2025-12-01T01:15:00Z') },
+      { start: new Date('2025-12-01T01:20:00Z'), end: new Date('2025-12-01T01:30:00Z') },
+      { start: new Date('2025-12-01T01:35:00Z'), end: new Date('2025-12-01T01:45:00Z') },
+      { start: new Date('2025-12-01T01:50:00Z'), end: new Date('2025-12-01T02:00:00Z') },
+    ]);
+  });
+
+  test('offsetting alignment by 20 minutes', () => {
+    const slots = findAlignedSlots(
+      { start: new Date('2025-12-01T00:00:00Z'), end: new Date('2025-12-01T02:00:00Z') },
+      { alignment: 30, offsetMinutes: 20, durationMinutes: 10 }
+    );
+    expect(slots).toEqual([
+      { start: new Date('2025-12-01T00:20:00Z'), end: new Date('2025-12-01T00:30:00Z') },
+      { start: new Date('2025-12-01T00:50:00Z'), end: new Date('2025-12-01T01:00:00Z') },
+      { start: new Date('2025-12-01T01:20:00Z'), end: new Date('2025-12-01T01:30:00Z') },
+      { start: new Date('2025-12-01T01:50:00Z'), end: new Date('2025-12-01T02:00:00Z') },
+    ]);
+  });
+
+  test('offsetting alignment by a negative number', () => {
+    // While we don't expect users to offset by a negative number, doing so can be helpful
+    // for situations like "bufferBefore", where we want to check that availability starts
+    // some time before the resulting slot start time.
+    const slots = findAlignedSlots(
+      { start: new Date('2025-12-01T00:00:00Z'), end: new Date('2025-12-01T02:00:00Z') },
+      { alignment: 30, offsetMinutes: -20, durationMinutes: 10 }
+    );
+    expect(slots).toEqual([
+      { start: new Date('2025-12-01T00:10:00Z'), end: new Date('2025-12-01T00:20:00Z') },
+      { start: new Date('2025-12-01T00:40:00Z'), end: new Date('2025-12-01T00:50:00Z') },
+      { start: new Date('2025-12-01T01:10:00Z'), end: new Date('2025-12-01T01:20:00Z') },
+      { start: new Date('2025-12-01T01:40:00Z'), end: new Date('2025-12-01T01:50:00Z') },
     ]);
   });
 });
