4935001e8c94026d3a9328b687482c4f485b60a3
Matt Long <ml@medplum.com>
2025-12-09 15:48:50 -0800

    Support publishing non-latest versions
    Signed-off-by: Matt Long <ml@medplum.com>

--- scripts/build-docker-server.sh
+++ scripts/build-docker-server.sh
@@ -48,17 +48,25 @@ PLATFORMS="--platform linux/amd64,linux/arm64"
 # If this is a release, get version information
 # Release is specified with a "--release" argument
 IS_RELEASE=false
+IS_LATEST=false
 for arg in "$@"; do
   if [[ "$arg" == "--release" ]]; then
     IS_RELEASE=true
     FULL_VERSION=$(node -p "require('./package.json').version")
     MAJOR_DOT_MINOR=$(node -p "require('./package.json').version.split('.').slice(0, 2).join('.')")
-    break
+    continue
+  fi
+  if [[ "$arg" == "--latest" ]]; then
+    IS_LATEST=true
+    continue
   fi
 done
 
 # Build and push server Docker images
-SERVER_TAGS="--tag $SERVER_DOCKERHUB_REPOSITORY:latest --tag $SERVER_DOCKERHUB_REPOSITORY:$GITHUB_SHA"
+SERVER_TAGS="--tag $SERVER_DOCKERHUB_REPOSITORY:$GITHUB_SHA"
+if [[ "$IS_LATEST" == "true" ]]; then
+  SERVER_TAGS="$SERVER_TAGS --tag $SERVER_DOCKERHUB_REPOSITORY:latest"
+fi
 if [[ "$IS_RELEASE" == "true" ]]; then
   SERVER_TAGS="$SERVER_TAGS --tag $SERVER_DOCKERHUB_REPOSITORY:$FULL_VERSION --tag $SERVER_DOCKERHUB_REPOSITORY:$MAJOR_DOT_MINOR"
 fi
