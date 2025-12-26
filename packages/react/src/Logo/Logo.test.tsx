cb8593a4c32c792b3f3d13f371c816182fa13a10
David Yanez <me@davidyanez.com>
2025-12-26 13:44:07 -0800

    Improve testing coverage in React package
    Signed-off-by: David Yanez <me@davidyanez.com>

--- packages/react/src/Logo/Logo.test.tsx
+++ packages/react/src/Logo/Logo.test.tsx
@@ -8,4 +8,17 @@ describe('Logo', () => {
     render(<Logo size={100} />);
     expect(screen.getByTitle('Medplum Logo')).toBeDefined();
   });
+
+  test('Renders with overrideUrl', async () => {
+    (import.meta.env as any).MEDPLUM_LOGO_URL = 'https://example.com/custom-logo.png';
+    
+    jest.resetModules();
+    const { Logo: LogoWithOverride } = await import('./Logo');
+    
+    render(<LogoWithOverride size={100} />);
+    const img = screen.getByAltText('Logo');
+    expect(img).toBeDefined();
+    expect(img).toHaveAttribute('src', 'https://example.com/custom-logo.png');
+    expect(img).toHaveStyle({ maxHeight: '100px' });
+  });
 });
