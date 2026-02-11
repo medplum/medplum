12057cf471b56e5de6ed2250f020d97f41e19c7b
Matthew Willer <matt@medplum.com>
2026-02-11 11:27:10 -0800

    Fix lint error
    Signed-off-by: Matthew Willer <matt@medplum.com>

--- packages/server/src/fhir/onbehalfof.test.ts
+++ packages/server/src/fhir/onbehalfof.test.ts
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: Copyright Orangebot, Inc. and Medplum contributors
 // SPDX-License-Identifier: Apache-2.0
 import { ContentType, getReferenceString } from '@medplum/core';
-import { OperationOutcome, OperationOutcomeIssue, type Patient } from '@medplum/fhirtypes';
+import type { OperationOutcome, OperationOutcomeIssue, Patient } from '@medplum/fhirtypes';
 import { randomUUID } from 'crypto';
 import express from 'express';
 import request from 'supertest';
