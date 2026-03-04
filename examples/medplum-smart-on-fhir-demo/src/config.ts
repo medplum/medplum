d00ad4a1415a5e463f29a4e2d856ab8493dce2dc
Alex Lin <alexanderlin999@gmail.com>
2026-03-03 16:50:43 -0800

    Update config.ts
    Signed-off-by: Alex Lin <alexanderlin999@gmail.com>

--- examples/medplum-smart-on-fhir-demo/src/config.ts
+++ examples/medplum-smart-on-fhir-demo/src/config.ts
@@ -1,5 +1,5 @@
 export const MEDPLUM_CLIENT_ID = 'e7459dd9-06ba-446f-bb4c-e6ef05c21979';
-export const SMART_HEALTH_IT_CLIENT_ID = '0194accc-2db7-72db-b3b3-a551811ac2cb';
+export const SMART_HEALTH_IT_CLIENT_ID = 'your-client-id';
 export const MEDPLUM_FHIR_URL = 'https://api.medplum.com/fhir/R4';
 export const SMART_HEALTH_IT_FHIR_URL = 'https://launch.smarthealthit.org/v/r4/sim/e30=/fhir';
 export const STANDALONE_SCOPE = 'launch/patient patient/*.read';
