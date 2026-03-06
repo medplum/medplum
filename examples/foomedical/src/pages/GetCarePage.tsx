66aa2f7be6417e8cbbe4a07ce9aa85e34e3ee9ad
Noah Silas <noah@medplum.com>
2026-03-06 11:11:34 -0800

    FooMedical: add scheduling loading/error states
    Signed-off-by: Noah Silas <noah@medplum.com>

--- examples/foomedical/src/pages/GetCarePage.tsx
+++ examples/foomedical/src/pages/GetCarePage.tsx
@@ -1,16 +1,35 @@
 // SPDX-FileCopyrightText: Copyright Orangebot, Inc. and Medplum contributors
 // SPDX-License-Identifier: Apache-2.0
-import type { Schedule } from '@medplum/fhirtypes';
-import { Document, Scheduler, useMedplum } from '@medplum/react';
+import { Alert, Loader } from '@mantine/core';
+import { Document, Scheduler } from '@medplum/react';
+import { useSearchOne } from '@medplum/react-hooks';
+import { IconInfoCircle } from '@tabler/icons-react';
 import type { JSX } from 'react';
 
 export function GetCare(): JSX.Element {
-  const medplum = useMedplum();
-  const schedule = medplum.searchOne('Schedule').read();
+  const [schedule, loading] = useSearchOne('Schedule');
+
+  if (loading) {
+    return (
+      <Document width={800}>
+        <Loader />
+      </Document>
+    );
+  }
+
+  if (!schedule) {
+    return (
+      <Document width={800}>
+        <Alert variant="outline" color="red" title="Schedule unavailable" icon={<IconInfoCircle />}>
+          Loading the schedule failed.
+        </Alert>
+      </Document>
+    );
+  }
 
   return (
     <Document width={800}>
-      <Scheduler schedule={schedule as Schedule} />
+      <Scheduler schedule={schedule} />
     </Document>
   );
 }
