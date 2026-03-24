88079e0f9599eb8271296e824a5b70bac9a90b06
Dmytro Smyk <porovozls@gmail.com>
2026-03-25 02:42:03 +0700

    Populate Appointment.slot references in $book operation (#8765)
    Fixes #8763
    
    Signed-off-by: parikls <porovozls@gmail.com>

--- packages/server/src/fhir/operations/book.ts
+++ packages/server/src/fhir/operations/book.ts
@@ -279,13 +279,6 @@ export async function appointmentBookHandler(req: FhirRequest): Promise<FhirResp
         });
       }
 
-      const appointment = await ctx.repo.createResource<Appointment>({
-        resourceType: 'Appointment',
-        status: 'booked',
-        participant,
-        start,
-        end,
-      });
       const createdSlots = await Promise.all(
         proposedSlots.map((slot) =>
           ctx.repo.createResource({
@@ -295,6 +288,15 @@ export async function appointmentBookHandler(req: FhirRequest): Promise<FhirResp
         )
       );
       const createdBufferSlots = await Promise.all(bufferSlots.map((slot) => ctx.repo.createResource(slot)));
+
+      const appointment = await ctx.repo.createResource<Appointment>({
+        resourceType: 'Appointment',
+        status: 'booked',
+        slot: createdSlots.map((slot) => ({ reference: getReferenceString(slot) })),
+        participant,
+        start,
+        end,
+      });
       return [appointment, ...createdSlots, ...createdBufferSlots];
     },
     { serializable: true }
