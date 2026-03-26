9cd7be3b688ec3d61028bb0c3ceafd74b3071ff2
Matt Willer <matt@medplum.com>
2026-03-26 16:40:22 -0700

    Parse mixed alphanumeric tokens with leading digit as string (#8792)
    * Parse mixed alphanumeric tokens with leading digit as string
    
    Signed-off-by: Matthew Willer <matt@medplum.com>
    
    * Use more specific regex
    
    Signed-off-by: Matthew Willer <matt@medplum.com>
    
    ---------
    
    Signed-off-by: Matthew Willer <matt@medplum.com>

--- packages/core/src/filter/parse.test.ts
+++ packages/core/src/filter/parse.test.ts
@@ -231,4 +231,12 @@ describe('_filter Parameter parser', () => {
   test('Unsupported search operator', () => {
     expect(() => parseFilterParameter('name ew ali')).toThrow('Invalid operator: ew');
   });
+
+  test('parse raw token with leading digits', () => {
+    const result = parseFilterParameter('identifier eq 123_abc');
+    expect(result).toBeInstanceOf(FhirFilterComparison);
+
+    const comp = result as FhirFilterComparison;
+    expect(comp.value).toEqual('123_abc');
+  });
 });
