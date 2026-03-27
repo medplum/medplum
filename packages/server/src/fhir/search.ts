83fd0733d21153ceabc7117400f24f7b999ff344
Matt Willer <matt@medplum.com>
2026-03-27 12:55:17 -0700

    Fix use of modifiers in _filter search (#8783)
    * Fix use of modifiers in _filter search
    
    Signed-off-by: Matthew Willer <matt@medplum.com>
    
    * Construct terminal filter directly
    
    Signed-off-by: Matthew Willer <matt@medplum.com>
    
    * [autofix.ci] apply automated fixes
    
    ---------
    
    Signed-off-by: Matthew Willer <matt@medplum.com>
    Signed-off-by: Matt Willer <matt@medplum.com>
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- packages/server/src/fhir/search.ts
+++ packages/server/src/fhir/search.ts
@@ -25,7 +25,6 @@ import {
   Operator,
   parseFhirPath,
   parseFilterParameter,
-  parseParameter,
   PropertyType,
   SearchParameterType,
   serverError,
@@ -1191,8 +1190,11 @@ function trySpecialSearchParameter(
         filter
       );
     }
-    case '_filter':
-      return buildFilterParameterExpression(repo, selectQuery, resourceType, table, parseFilterParameter(filter.value));
+    case '_filter': {
+      const filterExpr = parseFilterParameter(filter.value);
+      return buildFilterParameterExpression(repo, selectQuery, resourceType, table, filterExpr);
+    }
+
     default:
       return undefined;
   }
@@ -1640,7 +1642,7 @@ function buildChainedSearch(
     const targetId = param.filter.value;
     return buildSearchFilterExpression(repo, selectQuery, resourceType as ResourceType, resourceType, {
       code,
-      operator: Operator.EQUALS,
+      operator: param.filter.operator,
       value: `${targetType}/${targetId}`,
     });
   }
@@ -1819,7 +1821,7 @@ function parseChainedParameter(resourceType: string, searchFilter: Filter): Chai
         if (!searchParam) {
           throw new Error(`Invalid search parameter at end of chain: ${currentResourceType}?${code}`);
         }
-        filter = parseParameter(searchParam, modifier ?? searchFilter.operator, searchFilter.value);
+        filter = { code, operator: (modifier as Operator) ?? searchFilter.operator, value: searchFilter.value };
       }
     } else {
       const link = parseChainLink(part, currentResourceType);
