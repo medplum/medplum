df6672a0d3d730dbecc03da83df1221957f43a40
Cody Ebberson <cody@ebberson.com>
2026-04-06 19:34:46 -0700

    PR feedback
    Signed-off-by: Cody Ebberson <cody@ebberson.com>

--- packages/server/src/fhir/operations/patientmatch.ts
+++ packages/server/src/fhir/operations/patientmatch.ts
@@ -6,6 +6,7 @@ import {
   badRequest,
   DEFAULT_SEARCH_COUNT,
   EMPTY,
+  HTTP_HL7_ORG,
   normalizeErrorString,
   OperationOutcomeError,
   Operator,
@@ -20,7 +21,7 @@ import { parseInputParameters } from './utils/parameters';
 const operation = getOperationDefinition('Patient', 'match');
 
 // Extension URL for match-grade, as defined by the FHIR spec
-const MATCH_GRADE_EXTENSION_URL = 'http://hl7.org/fhir/StructureDefinition/match-grade';
+const MATCH_GRADE_EXTENSION_URL = `${HTTP_HL7_ORG}/StructureDefinition/match-grade`;
 
 // Thresholds for match-grade classification
 const CERTAIN_THRESHOLD = 0.9;
