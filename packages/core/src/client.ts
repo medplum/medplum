5a252052b65efc35a6ccc1766ffd6ded46fe7b0e
Devi Manoharan <reachdevi.manoharan@gmail.com>
2026-04-28 08:49:48 -0400

    chore: fix stray character on line 1 of client.ts
    Signed-off-by: Devi Manoharan <reachdevi.manoharan@gmail.com>

--- packages/core/src/client.ts
+++ packages/core/src/client.ts
@@ -1,4 +1,4 @@
-§// SPDX-FileCopyrightText: Copyright Orangebot, Inc. and Medplum contributors
+// SPDX-FileCopyrightText: Copyright Orangebot, Inc. and Medplum contributors
 // SPDX-License-Identifier: Apache-2.0
 import type {
   AccessPolicy,
