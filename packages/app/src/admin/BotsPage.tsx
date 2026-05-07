6d8c5f64458ff33d32a4936b6634d0d77f2644cf
maddyli <56186844+maddyli@users.noreply.github.com>
2026-05-07 13:18:16 -0700

    Update packages/app/src/admin/BotsPage.tsx
    Co-authored-by: Matt Willer <matt@medplum.com>
    Signed-off-by: maddyli <56186844+maddyli@users.noreply.github.com>

--- packages/app/src/admin/BotsPage.tsx
+++ packages/app/src/admin/BotsPage.tsx
@@ -13,24 +13,22 @@ export function BotsPage(): JSX.Element {
         profileTypeOptions={[{ label: 'Bot', value: 'Bot' }]}
         fields={['user', 'profile', 'accessPolicy', 'userConfiguration', 'active', 'admin']}
         toolbarLeft={
-          <>
-            <Paper withBorder p="sm" radius="sm" maw={520}>
-              <Text size="sm" c="dimmed">
-                Please note that this list is limited to Bots that have a ProjectMembership. Bots that are run as user
-                commonly do not have a ProjectMembership, and are not included, such as most Bots in Medplum-managed
-                integrations. For a complete list of Bots, see <MedplumLink to="/Bot">Bots</MedplumLink>. For more
-                information, see{' '}
-                <Anchor
-                  href="https://www.medplum.com/docs/bots/bot-run-as-user"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  running Bots as user
-                </Anchor>
-                .
-              </Text>
-            </Paper>
-          </>
+          <Paper withBorder p="sm" radius="sm" maw={520}>
+            <Text size="sm" c="dimmed">
+              Please note that this list is limited to Bots that have a ProjectMembership. Bots that are run as user
+              commonly do not have a ProjectMembership, and are not included, such as most Bots in Medplum-managed
+              integrations. For a complete list of Bots, see <MedplumLink to="/Bot">Bots</MedplumLink>. For more
+              information, see{' '}
+              <Anchor
+                href="https://www.medplum.com/docs/bots/bot-run-as-user"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                running Bots as user
+              </Anchor>
+              .
+            </Text>
+          </Paper>
         }
         toolbarRight={
           <Anchor href="https://www.medplum.com/docs/bots" target="_blank" rel="noopener noreferrer" size="sm">
