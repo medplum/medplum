4fe7ab3207006844ad83c6bfda8e4c79fac0a6db
maddyli <maddy@medplum.com>
2026-05-08 10:18:33 -0700

    remove getfhirquota from context.ts
    Signed-off-by: maddyli <maddy@medplum.com>

--- packages/server/src/context.ts
+++ packages/server/src/context.ts
@@ -15,7 +15,7 @@ import type { NextFunction, Request, Response } from 'express';
 import { randomUUID } from 'node:crypto';
 import { getConfig } from './config/loader';
 import { getRepoForLogin } from './fhir/accesspolicy';
-import { FhirRateLimiter, getFhirQuotaConfig } from './fhir/fhirquota';
+import { FhirRateLimiter } from './fhir/fhirquota';
 import type { Repository, SystemRepository } from './fhir/repo';
 import { ResourceCap } from './fhir/resource-cap';
 import { getLogger, globalLogger } from './logger';
@@ -263,9 +263,12 @@ function write(msg: string): void {
 }
 
 function getFhirRateLimiter(authState: AuthState, logger?: Logger, async?: boolean): FhirRateLimiter | undefined {
-  const { userLimit: defaultUserLimit, projectLimit } = getFhirQuotaConfig(authState.project);
+  const defaultUserLimit = authState.project?.systemSetting?.find((s) => s.name === 'userFhirQuota')?.valueInteger;
   const userSpecificLimit = authState.userConfig.option?.find((o) => o.id === 'fhirQuota')?.valueInteger;
-  const userLimit = userSpecificLimit ?? defaultUserLimit;
+  const userLimit = userSpecificLimit ?? defaultUserLimit ?? getConfig().defaultFhirQuota;
+
+  const perProjectLimit = authState.project?.systemSetting?.find((s) => s.name === 'totalFhirQuota')?.valueInteger;
+  const projectLimit = perProjectLimit ?? userLimit * 10;
 
   return authState.membership
     ? new FhirRateLimiter(getRateLimitRedis(), authState, userLimit, projectLimit, logger ?? globalLogger, async)
