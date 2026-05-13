ab149b1030293bb58ffc01d8939de8da933755e5
Matt Long <ml@medplum.com>
2026-05-13 16:47:44 -0700

    Fix type for import
    Signed-off-by: Matt Long <ml@medplum.com>

--- packages/server/src/fhir/operations/patientsummary.ts
+++ packages/server/src/fhir/operations/patientsummary.ts
@@ -56,6 +56,7 @@ import type {
   Immunization,
   MedicationRequest,
   Observation,
+  OperationDefinition,
   OperationDefinitionParameter,
   Organization,
   Patient,
@@ -83,23 +84,27 @@ export const OBSERVATION_CATEGORY_SYSTEM = `${HTTP_TERMINOLOGY_HL7_ORG}/CodeSyst
 // Patient summary operation
 // https://build.fhir.org/ig/HL7/fhir-ips/OperationDefinition-summary.html
 
-export const operation = makeOperationDefinition({ scope: 'type-and-instance', resource: 'Patient' }, {
-  id: 'summary',
-  name: 'IpsSummary',
-  title: 'IPS Summary',
-  affectsState: false,
-  code: 'summary',
-  parameter: [
-    ['author', 'in', 0, 1, 'Reference'],
-    ['authoredOn', 'in', 0, 1, 'instant'],
-    ['start', 'in', 0, 1, 'date'],
-    ['end', 'in', 0, 1, 'date'],
-    ['_since', 'in', 0, 1, 'instant'],
-    ['identifier', 'in', 0, 1, 'string'],
-    ['profile', 'in', 0, 1, 'canonical'],
-    ['return', 'out', 0, 1, 'Bundle'],
-  ].map(([name, use, min, max, type]) => ({ name, use, min, max, type }) as OperationDefinitionParameter),
-});
+export const patientSummaryOperation = makeOperationDefinition(
+  { scope: 'type-and-instance', resource: 'Patient' },
+  {
+    id: 'summary',
+    name: 'IpsSummary',
+    title: 'IPS Summary',
+    affectsState: false,
+    code: 'summary',
+    parameter: [
+      ['author', 'in', 0, 1, 'Reference'],
+      ['authoredOn', 'in', 0, 1, 'instant'],
+      ['start', 'in', 0, 1, 'date'],
+      ['end', 'in', 0, 1, 'date'],
+      ['_since', 'in', 0, 1, 'instant'],
+      ['identifier', 'in', 0, 1, 'string'],
+      ['profile', 'in', 0, 1, 'canonical'],
+      ['return', 'out', 0, 1, 'Bundle'],
+    ].map(([name, use, min, max, type]) => ({ name, use, min, max, type }) as OperationDefinitionParameter),
+  }
+  // cast since this op gets imported elsewhere where its useful to know it definitely has parameters
+) as OperationDefinition & { parameter: OperationDefinitionParameter[] };
 
 const resourceTypes: ResourceType[] = [
   'Account',
@@ -136,7 +141,7 @@ export interface PatientSummaryParameters extends PatientEverythingParameters {
 export async function patientSummaryHandler(req: FhirRequest): Promise<FhirResponse> {
   const ctx = getAuthenticatedContext();
   const { id } = req.params;
-  const params = parseInputParameters<PatientSummaryParameters>(operation, req);
+  const params = parseInputParameters<PatientSummaryParameters>(patientSummaryOperation, req);
   const bundle = await getPatientSummary(ctx, { reference: `Patient/${id}` }, params);
   return [allOk, bundle];
 }
