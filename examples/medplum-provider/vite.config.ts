058ac0fa681152e1625c3fe239e0e1cb0ae9ef64
Karl Pietrzak <karl@medplum.com>
2026-06-03 17:20:07 -0400

    restore original config
    we don't need to change it
    
    Signed-off-by: Karl Pietrzak <karl@medplum.com>

--- examples/medplum-provider/vite.config.ts
+++ examples/medplum-provider/vite.config.ts
@@ -2,9 +2,26 @@
 // SPDX-License-Identifier: Apache-2.0
 import react from '@vitejs/plugin-react';
 import dns from 'dns';
+import { existsSync } from 'fs';
+import path from 'path';
+import type { UserConfig } from 'vite';
 import { defineConfig } from 'vitest/config';
 
 dns.setDefaultResultOrder('verbatim');
+
+// Resolve aliases to local packages when working within the monorepo
+const alias: NonNullable<UserConfig['resolve']>['alias'] = Object.fromEntries(
+  Object.entries({
+    '@medplum/core': path.resolve(__dirname, '../../packages/core/src'),
+    '@medplum/dosespot-react': path.resolve(__dirname, '../../packages/dosespot-react/src'),
+    '@medplum/scriptsure-react': path.resolve(__dirname, '../../packages/scriptsure-react/src'),
+    '@medplum/react$': path.resolve(__dirname, '../../packages/react/src'),
+    '@medplum/react/styles.css': path.resolve(__dirname, '../../packages/react/dist/esm/index.css'),
+    '@medplum/react-hooks': path.resolve(__dirname, '../../packages/react-hooks/src'),
+    '@medplum/health-gorilla-core': path.resolve(__dirname, '../../packages/health-gorilla-core/src'),
+    '@medplum/health-gorilla-react': path.resolve(__dirname, '../../packages/health-gorilla-react/src'),
+  }).filter(([, relPath]) => existsSync(relPath))
+);
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,6 +33,9 @@ export default defineConfig({
   preview: {
     host: 'localhost',
     port: 3001,
+  },
+  resolve: {
+    alias,
   },
   test: {
     globals: true,
