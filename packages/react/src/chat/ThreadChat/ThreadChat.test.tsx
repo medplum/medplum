95a94ea8b99b818059c017916a75f677b664420a
Karl Pietrzak <karl@medplum.com>
2026-06-10 15:42:16 -0400

    linting fixes
    Signed-off-by: Karl Pietrzak <karl@medplum.com>

--- packages/react/src/chat/ThreadChat/ThreadChat.test.tsx
+++ packages/react/src/chat/ThreadChat/ThreadChat.test.tsx
@@ -19,7 +19,7 @@ type SubscriptionControllerEvents = {
 
 vi.mock(import('@medplum/react-hooks'), async (importOriginal) => {
   const { TypedEventTarget } = await import('@medplum/core');
-  const _subscriptionController = new TypedEventTarget() as TypedEventTarget<SubscriptionControllerEvents>;
+  const _subscriptionController = new TypedEventTarget();
   const original = await importOriginal();
   return {
     ...original,
