97cbf186ab7bef2bbc630643ff677f1af28b15cf
Noah Silas <noah@medplum.com>
2026-06-18 17:28:55 -0700

    Multi-scheduling demo
    - Ability to select a HealthcareService. Dims actors in the ActorChooser
      who are not eligible.
    
    - When a HealthcareService is chosen, and all selected actors schedules
      are linked to that HealthcareService, calls `Appointment/$find` to
      find schedulable slots that are available on all the calendars. These
      are revealed in a right-hand pane.
    
    - Choosing an appointment from that pane opens a Drawer that lets you
      create the appointment with `Appointment/$book`.
    
    - Not handled yet: encounter creation. This is going to need some
      additional wiring for handling multiple practitioners.
    
    - Not handled yet: clicking into a detail view of any appointment
    
    Signed-off-by: Noah Silas <noah@medplum.com>

--- examples/medplum-provider/src/utils/scheduling.ts
+++ examples/medplum-provider/src/utils/scheduling.ts
@@ -1,7 +1,9 @@
 // SPDX-FileCopyrightText: Copyright Orangebot, Inc. and Medplum contributors
 // SPDX-License-Identifier: Apache-2.0
+import type { WithId } from '@medplum/core';
 import { generateId, getExtension, getIdentifier, setIdentifier } from '@medplum/core';
-import type { HealthcareService, Identifier, Resource, Schedule } from '@medplum/fhirtypes';
+import type { HealthcareService, Identifier, Reference, Resource, Schedule } from '@medplum/fhirtypes';
+import { isCodeableReferenceLikeTo } from './servicetype';
 
 const SchedulingParametersURI = 'https://medplum.com/fhir/StructureDefinition/SchedulingParameters';
 const MedplumSchedulingTransientIdentifierURI = 'https://medplum.com/fhir/scheduling-transient-id';
@@ -26,4 +28,11 @@ export const SchedulingTransientIdentifier = {
 
 export function hasSchedulingParameters(resource: Schedule | HealthcareService): boolean {
   return !!getExtension(resource, SchedulingParametersURI);
+}
+
+export function isSchedulableFor(
+  schedule: Schedule,
+  healthcareService: WithId<HealthcareService> | (Reference<HealthcareService> & { reference: string })
+): boolean {
+  return isCodeableReferenceLikeTo(schedule.serviceType, healthcareService);
 }
