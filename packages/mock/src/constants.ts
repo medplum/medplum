fa79a2543b8fcbd1841036aef763f0b8ba5df7ac
Karl Pietrzak <karl@medplum.com>
2026-06-23 17:12:11 -0400

    use 125 for the mock organization ID to minimize confusion
    Signed-off-by: Karl Pietrzak <karl@medplum.com>

--- packages/mock/src/constants.ts
+++ packages/mock/src/constants.ts
@@ -8,4 +8,4 @@ export const MOCK_HOMER_PATIENT_ID = '123';
 export const MOCK_ALICE_PRACTITIONER_ID = '124';
 
 /** Well-known MockClient Organization id for Test Organization. */
-export const MOCK_TEST_ORGANIZATION_ID = '123';
+export const MOCK_TEST_ORGANIZATION_ID = '125';
