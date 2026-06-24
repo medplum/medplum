2b159ca755b5d5daeb223a5d817f38a2409a3c70
Matt Long <mattlong@users.noreply.github.com>
2026-06-24 15:31:55 -0700

    First pass at normalizing Bot OperationOutcome response (#9522)
    * First pass at normalizing Bot OperationOutcome response
    
    Signed-off-by: Matt Long <ml@medplum.com>
    
    * Improved streaming handling
    
    Signed-off-by: Matt Long <ml@medplum.com>
    
    * [autofix.ci] apply automated fixes
    
    * Clean up
    
    Signed-off-by: Matt Long <ml@medplum.com>
    
    * Check instanceof
    
    Signed-off-by: Matt Long <ml@medplum.com>
    
    * Simplify fission changes
    
    Signed-off-by: Matt Long <ml@medplum.com>
    
    * Sonar feedback
    
    Signed-off-by: Matt Long <ml@medplum.com>
    
    * Test coverage
    
    Signed-off-by: Matt Long <ml@medplum.com>
    
    ---------
    
    Signed-off-by: Matt Long <ml@medplum.com>
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- packages/server/src/cloud/fission/utils.ts
+++ packages/server/src/cloud/fission/utils.ts
@@ -156,7 +156,13 @@ export async function deployFissionFunction(id: string, zipFile: Uint8Array): Pr
  * @param body - The request body to be sent to the function.
  * @returns A promise that resolves to the response body from the Fission function.
  */
-export async function executeFissionFunction(id: string, body: string): Promise<string> {
+export interface FissionFunctionResponse {
+  readonly ok: boolean;
+  readonly status: number;
+  readonly body: string;
+}
+
+export async function executeFissionFunction(id: string, body: string): Promise<FissionFunctionResponse> {
   const config = getFissionConfig();
 
   const relativeUrl = getRelativeUrl(id);
@@ -168,11 +174,9 @@ export async function executeFissionFunction(id: string, body: string): Promise<
     body,
   });
 
-  if (!response.ok) {
-    const errorText = await response.text();
-    throw new Error(`HTTP error! Status: ${response.status}, Message: ${errorText}`);
-  }
-
-  const data = await response.text();
-  return data;
+  return {
+    ok: response.ok,
+    status: response.status,
+    body: await response.text(),
+  };
 }
