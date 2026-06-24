5bc80594d5724c17353edf777efdb462bec93cec
Matt Willer <matt@medplum.com>
2026-06-24 10:33:04 -0700

    Duplicate failure logic for async batch
    Signed-off-by: Matt Willer <matt@medplum.com>

--- packages/server/src/workers/batch.ts
+++ packages/server/src/workers/batch.ts
@@ -67,6 +67,17 @@ export const initBatchWorker: WorkerInitializer = (config, options?: WorkerIniti
         ...workerBullmq,
       }
     );
+
+    worker.on('failed', async (job) => {
+      if (!job) {
+        return;
+      }
+
+      // Mark AsyncJob as failed
+      const systemRepo = getShardSystemRepo(PLACEHOLDER_SHARD_ID); // shardId will be available in job.data.authState in the future
+      const exec = new AsyncJobExecutor(systemRepo, job.data.asyncJob);
+      await exec.failJob();
+    });
   }
 
   return { queue, worker, name: queueName };
