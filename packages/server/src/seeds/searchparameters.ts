abb00276dae4e6641b48dfe88c48533fac18e14e
Matt Willer <matt@medplum.com>
2026-06-26 13:13:12 -0700

    Rebuild Operation Definitions (#9460)
    * WIP
    
    Signed-off-by: Matt Willer <matt@medplum.com>
    
    * Replace existing implementations
    
    Signed-off-by: Matt Willer <matt@medplum.com>
    
    * Fix lockfile
    
    Signed-off-by: Matt Willer <matt@medplum.com>
    
    * WIP: Add test
    
    Signed-off-by: Matt Willer <matt@medplum.com>
    
    * Get tests working
    
    Signed-off-by: Matt Willer <matt@medplum.com>
    
    * PR feedback
    
    Signed-off-by: Matt Willer <matt@medplum.com>
    
    * Fix tests
    
    Signed-off-by: Matt Willer <matt@medplum.com>
    
    * PR feedback
    
    Signed-off-by: Matt Willer <matt@medplum.com>
    
    * Fix package-lock
    
    Signed-off-by: Matt Willer <matt@medplum.com>
    
    * Correct transaction repo usage
    
    Signed-off-by: Matt Willer <matt@medplum.com>
    
    * Rebuild package-lock
    
    Signed-off-by: Matt Willer <matt@medplum.com>
    
    * Fix tests
    
    Signed-off-by: Matt Willer <matt@medplum.com>
    
    ---------
    
    Signed-off-by: Matt Willer <matt@medplum.com>
    Co-authored-by: Karl Pietrzak <karl@medplum.com>

--- packages/server/src/seeds/searchparameters.ts
+++ packages/server/src/seeds/searchparameters.ts
@@ -1,37 +1,12 @@
 // SPDX-FileCopyrightText: Copyright Orangebot, Inc. and Medplum contributors
 // SPDX-License-Identifier: Apache-2.0
-import { SEARCH_PARAMETER_BUNDLE_FILES, readJson } from '@medplum/definitions';
-import type { BundleEntry, SearchParameter } from '@medplum/fhirtypes';
-import { r4ProjectId } from '../constants';
-import { DatabaseMode } from '../database';
+import { rebuildBaseDefinitions } from '../fhir/operations/rebuild-base-definitions';
 import type { Repository } from '../fhir/repo';
-import { globalLogger } from '../logger';
 
 /**
  * Creates all SearchParameter resources.
  * @param systemRepo - The system repository to use
  */
 export async function rebuildR4SearchParameters(systemRepo: Repository): Promise<void> {
-  const client = systemRepo.getDatabaseClient(DatabaseMode.WRITER);
-  await client.query('DELETE FROM "SearchParameter" WHERE "projectId" = $1', [r4ProjectId]);
-
-  for (const filename of SEARCH_PARAMETER_BUNDLE_FILES) {
-    for (const entry of readJson(filename).entry as BundleEntry[]) {
-      await createParameter(systemRepo, entry.resource as SearchParameter);
-    }
-  }
-}
-
-async function createParameter(systemRepo: Repository, param: SearchParameter): Promise<void> {
-  globalLogger.debug('SearchParameter: ' + param.name);
-  await systemRepo.createResource<SearchParameter>({
-    ...param,
-    meta: {
-      ...param.meta,
-      project: r4ProjectId,
-      lastUpdated: undefined,
-      versionId: undefined,
-    },
-    text: undefined,
-  });
+  await rebuildBaseDefinitions(systemRepo, ['SearchParameter']);
 }
