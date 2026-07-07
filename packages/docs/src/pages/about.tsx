a269d68c16f86f1f69ee16f5dad97c7aee136091
Tim Ray <timray@Tims-MacBook-Pro.local>
2026-07-07 14:26:19 -0700

    Added myself to about page
    Signed-off-by: Tim Ray <timray@Tims-MacBook-Pro.local>

--- packages/docs/src/pages/about.tsx
+++ packages/docs/src/pages/about.tsx
@@ -207,6 +207,15 @@ export default function AboutPage(): JSX.Element {
               githubUrl="https://github.com/AndyStoneman"
             />
           </CardContainer>
+          <CardContainer>
+            <ProfileCard
+              name="Tim Ray"
+              title="Business Operations Associate"
+              imgUrl="/img/people/tim.webp"
+              linkedInUrl="https://www.linkedin.com/in/tim-ray98/"
+              githubUrl="https://github.com/timray721"
+            />
+          </CardContainer>
         </Section>
       </Container>
     </Layout>
