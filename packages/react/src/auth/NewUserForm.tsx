28f4ac13adf3b83554a7de0e7830ba5e84e18d17
Philip Knott <filupnot@proton.me>
2026-07-08 16:58:47 -0700

    Move login link outside form grouper
    Signed-off-by: Philip Knott <filupnot@proton.me>

--- packages/react/src/auth/NewUserForm.tsx
+++ packages/react/src/auth/NewUserForm.tsx
@@ -39,132 +39,128 @@ export function NewUserForm(props: NewUserFormProps): JSX.Element {
   }, [recaptchaSiteKey]);
 
   return (
-    <Form
-      onSubmit={async (formData: Record<string, string>) => {
-        setOutcome(undefined);
-        try {
-          let recaptchaToken = '';
-          if (recaptchaSiteKey) {
-            recaptchaToken = await getRecaptcha(recaptchaSiteKey);
+    <>
+      <Form
+        onSubmit={async (formData: Record<string, string>) => {
+          setOutcome(undefined);
+          try {
+            let recaptchaToken = '';
+            if (recaptchaSiteKey) {
+              recaptchaToken = await getRecaptcha(recaptchaSiteKey);
+            }
+            props.handleAuthResponse(
+              await medplum.startNewUser({
+                projectId: props.projectId,
+                clientId: props.clientId,
+                firstName: formData.firstName,
+                lastName: formData.lastName,
+                email: formData.email,
+                password: formData.password,
+                remember: formData.remember === 'true',
+                recaptchaSiteKey,
+                recaptchaToken,
+              })
+            );
+          } catch (err) {
+            setOutcome(normalizeOperationOutcome(err));
           }
-          props.handleAuthResponse(
-            await medplum.startNewUser({
-              projectId: props.projectId,
-              clientId: props.clientId,
-              firstName: formData.firstName,
-              lastName: formData.lastName,
-              email: formData.email,
-              password: formData.password,
-              remember: formData.remember === 'true',
-              recaptchaSiteKey,
-              recaptchaToken,
-            })
-          );
-        } catch (err) {
-          setOutcome(normalizeOperationOutcome(err));
-        }
-      }}
-    >
-      <Flex direction="column" align="center" justify="center">
-        {props.children}
-      </Flex>
-      <OperationOutcomeAlert issues={issues} mb="lg" />
-      {googleClientId && (
+        }}
+      >
+        <Flex direction="column" align="center" justify="center">
+          {props.children}
+        </Flex>
+        <OperationOutcomeAlert issues={issues} mb="lg" />
+        {googleClientId && (
+          <>
+            <Box style={{ minHeight: 40 }}>
+              <GoogleButton
+                googleClientId={googleClientId}
+                handleGoogleCredential={async (response: GoogleCredentialResponse) => {
+                  try {
+                    props.handleAuthResponse(
+                      await medplum.startGoogleLogin({
+                        googleClientId: response.clientId,
+                        googleCredential: response.credential,
+                        projectId: props.projectId,
+                        createUser: true,
+                      })
+                    );
+                  } catch (err) {
+                    setOutcome(normalizeOperationOutcome(err));
+                  }
+                }}
+              />
+            </Box>
+            <Divider label="or" labelPosition="center" my="lg" />
+          </>
+        )}
+        <Stack gap="sm">
+          <TextInput
+            name="firstName"
+            type="text"
+            label="First name"
+            placeholder="First name"
+            required={true}
+            autoFocus={true}
+            error={getErrorsForInput(outcome, 'firstName')}
+          />
+          <TextInput
+            name="lastName"
+            type="text"
+            label="Last name"
+            placeholder="Last name"
+            required={true}
+            error={getErrorsForInput(outcome, 'lastName')}
+          />
+          <TextInput
+            name="email"
+            type="email"
+            label="Email"
+            placeholder="name@domain.com"
+            required={true}
+            error={getErrorsForInput(outcome, 'email')}
+          />
+          <PasswordInput
+            name="password"
+            label="Password"
+            autoComplete="off"
+            required={true}
+            error={getErrorsForInput(outcome, 'password')}
+          />
+        </Stack>
+        <Stack gap="xs">
+          <Checkbox
+            id="remember"
+            name="remember"
+            label="Remember me"
+            size="xs"
+            style={{ lineHeight: 1 }}
+            pt="md"
+            pb="xs"
+          />
+          <SubmitButton fullWidth>Register Account</SubmitButton>
+          <Text c="dimmed" size="xs" pt="lg" ta="center">
+            By clicking "Register Account" you agree to the Medplum{' '}
+            <Anchor href="https://www.medplum.com/privacy">Privacy&nbsp;Policy</Anchor>
+            {' and '}
+            <Anchor href="https://www.medplum.com/terms">Terms&nbsp;of&nbsp;Service</Anchor>.
+          </Text>
+          <Text c="dimmed" size="xs" ta="center">
+            This site is protected by reCAPTCHA and the Google{' '}
+            <Anchor href="https://policies.google.com/privacy">Privacy&nbsp;Policy</Anchor>
+            {' and '}
+            <Anchor href="https://policies.google.com/terms">Terms&nbsp;of&nbsp;Service</Anchor> apply.
+          </Text>
+        </Stack>
+      </Form>
+      {props.onSignIn && (
         <>
-          <Box style={{ minHeight: 40 }}>
-            <GoogleButton
-              googleClientId={googleClientId}
-              handleGoogleCredential={async (response: GoogleCredentialResponse) => {
-                try {
-                  props.handleAuthResponse(
-                    await medplum.startGoogleLogin({
-                      googleClientId: response.clientId,
-                      googleCredential: response.credential,
-                      projectId: props.projectId,
-                      createUser: true,
-                    })
-                  );
-                } catch (err) {
-                  setOutcome(normalizeOperationOutcome(err));
-                }
-              }}
-            />
-          </Box>
-          <Divider label="or" labelPosition="center" my="lg" />
-        </>
-      )}
-      <Stack gap="sm">
-        <TextInput
-          name="firstName"
-          type="text"
-          label="First name"
-          placeholder="First name"
-          required={true}
-          autoFocus={true}
-          error={getErrorsForInput(outcome, 'firstName')}
-        />
-        <TextInput
-          name="lastName"
-          type="text"
-          label="Last name"
-          placeholder="Last name"
-          required={true}
-          error={getErrorsForInput(outcome, 'lastName')}
-        />
-        <TextInput
-          name="email"
-          type="email"
-          label="Email"
-          placeholder="name@domain.com"
-          required={true}
-          error={getErrorsForInput(outcome, 'email')}
-        />
-        <PasswordInput
-          name="password"
-          label="Password"
-          autoComplete="off"
-          required={true}
-          error={getErrorsForInput(outcome, 'password')}
-        />
-      </Stack>
-      <Stack gap="xs">
-        <Checkbox
-          id="remember"
-          name="remember"
-          label="Remember me"
-          size="xs"
-          style={{ lineHeight: 1 }}
-          pt="md"
-          pb="xs"
-        />
-        <SubmitButton fullWidth>Register Account</SubmitButton>
-        {props.onSignIn && (
-          <Text
-            size="sm"
-            mt="lg"
-            c="dimmed"
-            ta="center"
-            data-dashlane-ignore="true"
-            data-lp-ignore="true"
-            data-no-autofill="true"
-            data-form-type="navigation"
-          >
+          <Divider mt="lg" />
+          <Text size="sm" mt="lg" c="dimmed" ta="center">
             Already have an account? <Anchor onClick={props.onSignIn}>Sign In</Anchor>
           </Text>
-        )}
-        <Text c="dimmed" size="xs" pt="lg" ta="center">
-          By clicking "Register Account" you agree to the Medplum{' '}
-          <Anchor href="https://www.medplum.com/privacy">Privacy&nbsp;Policy</Anchor>
-          {' and '}
-          <Anchor href="https://www.medplum.com/terms">Terms&nbsp;of&nbsp;Service</Anchor>.
-        </Text>
-        <Text c="dimmed" size="xs" ta="center">
-          This site is protected by reCAPTCHA and the Google{' '}
-          <Anchor href="https://policies.google.com/privacy">Privacy&nbsp;Policy</Anchor>
-          {' and '}
-          <Anchor href="https://policies.google.com/terms">Terms&nbsp;of&nbsp;Service</Anchor> apply.
-        </Text>
-      </Stack>
-    </Form>
+        </>
+      )}
+    </>
   );
 }
