68a2f01e5da0edc33148c717e6a123328ec88a5e
Noah Silas <noah@medplum.com>
2026-08-03 16:43:12 -0700

    Pin Calendar Storybook time
    We perform visual regression tests, and so we want this story to be
    deterministic. For that reason, we pin the current time for the duration
    of the story.
    
    Implementation note: I evaluated some approaches like extracting a
    shared `@medplum/storybook-util` package, or exporting the
    MockDateWrapper from the `@medplum/react` package, but each of these
    came with a surprising amount of boilerplate/build complexity.
    
    For now the implementation is a very light-weight wrapper around a
    `sinon` dev-dependency, so copying the code felt like the most pragmatic
    path forward.
    
    Signed-off-by: Noah Silas <noah@medplum.com>

--- packages/react-scheduling/src/Calendar/Calendar.stories.tsx
+++ packages/react-scheduling/src/Calendar/Calendar.stories.tsx
@@ -2,11 +2,13 @@
 // SPDX-License-Identifier: Apache-2.0
 import type { Meta } from '@storybook/react';
 import type { JSX } from 'react';
+import { withMockedDate } from '../stories/decorators';
 import { Calendar } from './Calendar';
 
 export default {
   title: 'Medplum/Calendar',
   component: Calendar,
+  decorators: [withMockedDate],
 } as Meta;
 
 export const Basic = (): JSX.Element => (
