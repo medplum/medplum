4eeb15d3b080f5efadcf9d555f7932179d80d02a
adityaapraveen <adityapraveen18@gmail.com>
2026-08-11 23:46:34 +0530

    feat(definitions): add MedicationDispense context search parameter

--- packages/definitions/src/index.test.ts
+++ packages/definitions/src/index.test.ts
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: Copyright Orangebot, Inc. and Medplum contributors
 // SPDX-License-Identifier: Apache-2.0
-import { readJson } from '.';
+import type { SearchParameter } from '@medplum/fhirtypes';
+import { getDefinitionResource, readJson } from '.';
 
 describe('Definitions', () => {
   test('Read FHIR schema JSON', () => {
@@ -15,5 +16,21 @@ describe('Definitions', () => {
     // Call again to test caching
     const dataDir2 = readJson('fhir/r4/fhir.schema.json');
     expect(dataDir2).not.toBeNull();
+  });
+
+  test('MedicationDispense context search parameter', () => {
+    const searchParameter = getDefinitionResource<SearchParameter>(
+      'fhir/r4/search-parameters-medplum.json',
+      'SearchParameter',
+      'http://hl7.org/fhir/SearchParameter/MedicationDispense-context'
+    );
+
+    expect(searchParameter).toMatchObject({
+      code: 'context',
+      base: ['MedicationDispense'],
+      type: 'reference',
+      expression: 'MedicationDispense.context',
+      target: ['EpisodeOfCare', 'Encounter'],
+    });
   });
 });
