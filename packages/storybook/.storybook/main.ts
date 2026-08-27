40f2fcc7eee3afe9c46c50be95e484863cffc5b0
Matt Long <ml@medplum.com>
2026-08-27 16:27:05 -0700

    Storybook test mode and cleanups
    Signed-off-by: Matt Long <ml@medplum.com>

--- packages/storybook/.storybook/main.ts
+++ packages/storybook/.storybook/main.ts
@@ -8,12 +8,11 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const config: StorybookConfig = {
   stories: [
-    '../src/stories/Introduction.mdx',
     '../src/**/*.mdx',
-    '../src/**/*.stories.@(js|jsx|ts|tsx)',
-    '../../react/src/**/*.stories.@(js|jsx|ts|tsx)',
+    '../src/**/*.stories.@(ts|tsx)',
+    '../../react/src/**/*.stories.@(ts|tsx)',
     '../../react-scheduling/src/**/*.mdx',
-    '../../react-scheduling/src/**/*.stories.@(js|jsx|ts|tsx)',
+    '../../react-scheduling/src/**/*.stories.@(ts|tsx)',
   ],
   addons: ['@storybook/addon-links', '@storybook/addon-docs', '@vueless/storybook-dark-mode'],
   staticDirs: ['../public'],
