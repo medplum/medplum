6439063add0d6d16591a290dfa15dee87f0b2a2f
Karl Pietrzak <karl@medplum.com>
2026-09-17 13:07:21 -0400

    Address PR feedback on $expunge history tombstone.
    Signed-off-by: Karl Pietrzak <karl@medplum.com>

--- packages/server/src/fhir/repository/row-builder.ts
+++ packages/server/src/fhir/repository/row-builder.ts
@@ -41,7 +41,7 @@ export interface DeleteHistoryContentOptions {
 }
 
 export const ExpungedHistoryTag = {
-  system: 'http://terminology.hl7.org/CodeSystem/iso-21089-lifecycle',
+  system: 'https://terminology.hl7.org/CodeSystem/iso-21089-lifecycle',
   code: 'destroy',
   display: 'Destroy/Delete Record Lifecycle Event',
 } as const;
